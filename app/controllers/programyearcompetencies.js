01331f8438c28f118591e19b5960056e694f7eb2
Jonathan Johnson <jon.johnson@ucsf.edu>
2014-09-26 15:33:11 -0700

    Add breadcrumbs to program templates

--- app/controllers/programyearcompetencies.js
+++ app/controllers/programyearcompetencies.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  needs: ['programyear'],
+  programYear: Ember.computed.alias("controllers.programyear"),
   sortAscending: true,
   sortProperties: ['title'],
 });
