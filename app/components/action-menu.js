f1d7a342baf04128bba35dec052d5dd299b97e53
Jonathan Johnson <jon.johnson@ucsf.edu>
2014-09-29 23:16:11 -0700

    Move objective actions to a menu

---
+++ app/components/action-menu.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  didInsertElement: function(){
+    var element = this.get('element');
+    Ember.$(".button", element).click(function(){
+      Ember.$(".menu", element).toggleClass("show-menu");
+      Ember.$(".menu > li", element).click(function(){
+        Ember.$(".button", element).html(Ember.$(this).html());
+        Ember.$(".menu", element).removeClass("show-menu");
+      });
+    });
+  }
+});
