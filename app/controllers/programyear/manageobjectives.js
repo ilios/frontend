34060634ae7798e91a4403d2f3923860071217a9
Jonathan Johnson <jon.johnson@ucsf.edu>
2014-10-06 23:05:44 -0700

    When adding a new objective it should be at the bottom of the table

--- app/controllers/programyear/manageobjectives.js
+++ app/controllers/programyear/manageobjectives.js
@@ -6,7 +6,16 @@ export default Ember.ArrayController.extend(Ember.I18n.TranslateableProperties, 
   programYear: Ember.computed.alias("controllers.programyear"),
   sortAscending: true,
   sortProperties: ['title'],
-  newObjectives: Ember.A(),
+  //override the title sort to push new objectives to the bottom
+  sortFunction: function(a,b){
+    if(a == null){
+      return 1;
+    }
+    if(b == null){
+      return -1;
+    }
+    return Ember.compare(a,b);
+  },
   actions: {
     createNew: function(){
       var objective = this.store.createRecord('objective', {
