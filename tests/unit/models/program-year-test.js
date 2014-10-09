38b032b4f65eccef133cc0ca493564e5cf01e940
Jonathan Johnson <jon.johnson@ucsf.edu>
2014-10-09 01:01:04 -0700

    Added unit tests for model properties

--- tests/unit/models/program-year-test.js
+++ tests/unit/models/program-year-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import {
   moduleForModel,
   test
@@ -19,4 +20,30 @@ test('it exists', function() {
   var model = this.subject();
   // var store = this.store();
   ok(!!model);
+});
+
+test('academic year string', function() {
+  var model = this.subject();
+  Ember.run(function(){
+    model.set('startYear', 2000);
+    equal(model.get('academicYear'), '2000 - 2001');
+  });
+});
+
+test('classOf string', function() {
+  expect(3);
+  var model = this.subject();
+  var store = model.store;
+  Ember.run(function(){
+    store.createRecord('program', {id:99, duration:1});
+    store.find('program', 99).then(function(program){
+      model.set('program', program);
+      model.set('startYear', 2000);
+      equal(model.get('classOfYear'), '2001');
+      program.set('duration', 5);
+      equal(model.get('classOfYear'), '2005');
+      model.set('startYear', 2001);
+      equal(model.get('classOfYear'), '2006');
+    });
+  });
 });
