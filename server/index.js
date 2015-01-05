e8c5deede76646f432943856f7d354a5939e1f5c
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-01-05 15:19:59 -0800

    Dont log all proxy data requests by default

--- server/index.js
+++ server/index.js
@@ -20,9 +20,9 @@ module.exports = function(app) {
     extended: true
   }));
 
-  // Log proxy requests
-  var morgan  = require('morgan');
-  app.use(morgan('dev'));
+  // Uncomment to log proxy requests
+  // var morgan  = require('morgan');
+  // app.use(morgan('dev'));
 
   mocks.forEach(function(route) { route(app); });
   proxies.forEach(function(route) { route(app); });
