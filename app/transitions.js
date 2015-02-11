800b606e96f9a99f71b28b3a3d991561fe943fb8
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-02-11 10:56:01 -0800

    Added more top level routes to transtion.

--- app/transitions.js
+++ app/transitions.js
@@ -6,8 +6,11 @@ export default function(){
       var topRoutes = Ember.A();
       topRoutes.push('dashboard');
       topRoutes.push('courses');
+      topRoutes.push('course');
       topRoutes.push('learnergroups');
+      topRoutes.push('learnergroup');
       topRoutes.push('instructorgroups');
+      topRoutes.push('instructorgroup');
       topRoutes.push('programs');
       return topRoutes.contains(this);
     }),
