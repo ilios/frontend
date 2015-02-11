b1449a70b76a60f194370c10ee095470b03246d3
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-02-10 22:14:12 -0800

    Add a differntiating class to publication status [published, scheduled, notpublished]

--- app/components/publication-status.js
+++ app/components/publication-status.js
@@ -2,9 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'span',
-  classNames: ['status'],
+  classNameBindings: [
+    ':status',
+    'isPublished:published',
+    'isScheduled:scheduled',
+    'isNotPublished:notpublished'
+  ],
   item: null,
   isPublished: Ember.computed.alias('item.isPublished'),
   isScheduled: Ember.computed.alias('item.publishedAsTbd'),
+  isSomething: Ember.computed.or('isPublished', 'isScheduled'),
+  isNotPublished: Ember.computed.not('isSomething'),
   showIcon: true
 });
