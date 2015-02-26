6bc27256ccaad5fd47d982160f5aa55d941a9b24
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-02-26 14:54:20 -0800

    Add delete as dev api end point

--- server/helpers/fixtureStorage.js
+++ server/helpers/fixtureStorage.js
@@ -1,9 +1,11 @@
 module.exports = {
   changes: {},
+  deleted: {},
   get: function(name) {
     this.initializeChangeStore(name);
     var fixtures = require ('../fixtures/' + name + '.js');
     var changes = this.getChanges(name);
+    var deleted = this.getDeleted(name);
     changes.forEach(function(obj){
       if(!(obj.id in fixtures)){
         fixtures[obj.id] = obj;
@@ -13,6 +15,11 @@ module.exports = {
         }
       }
     });
+    deleted.forEach(function(id){
+      if(id in fixtures){
+        delete fixtures[id];
+      }
+    });
 
     return fixtures;
   },
@@ -20,13 +27,25 @@ module.exports = {
     this.initializeChangeStore(name);
     this.getChanges(name).push(obj);
   },
+  remove: function(name, obj){
+    this.getDeleted(name).push(obj.id);
+  },
   initializeChangeStore: function(name){
     if(! (name in this.changes)){
       this.changes[name] = [];
     }
   },
+  initializeDeleteStore: function(name){
+    if(! (name in this.deleted)){
+      this.deleted[name] = [];
+    }
+  },
   getChanges: function(name){
     this.initializeChangeStore(name);
     return this.changes[name];
+  },
+  getDeleted: function(name){
+    this.initializeDeleteStore(name);
+    return this.deleted[name];
   }
 };
