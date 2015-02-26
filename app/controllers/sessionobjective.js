e50a7eec7a8d1cb130da08cdae8c022b82af0e35
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-02-25 22:34:28 -0800

    Fix issue with removing parent and speed up the process

--- app/controllers/sessionobjective.js
+++ app/controllers/sessionobjective.js
@@ -7,32 +7,25 @@ export default Ember.ObjectController.extend(Ember.I18n.TranslateableProperties,
   actions: {
     addParent: function(parentProxy){
       var newParent = parentProxy.get('content');
-      var self = this;
       var sessionObjective = this.get('model');
       sessionObjective.get('parents').then(function(ourParents){
+        ourParents.addObject(newParent);
         newParent.get('children').then(function(newParentChildren){
           newParentChildren.addObject(sessionObjective);
-          newParent.save().then(function(newParent){
-            ourParents.addObject(newParent);
-            sessionObjective.save().then(function(sessionObjective){
-              self.set('model', sessionObjective);
-            });
-          });
+          newParent.save();
+          sessionObjective.save();
         });
       });
     },
     removeParent: function(parentProxy){
-      var self = this;
       var removingParent = parentProxy.get('content');
       var sessionObjective = this.get('model');
       sessionObjective.get('parents').then(function(ourParents){
         ourParents.removeObject(removingParent);
         removingParent.get('children').then(function(children){
-          children.addObject(sessionObjective);
-          sessionObjective.save().then(function(sessionObjective){
-            self.set('model', sessionObjective);
-            removingParent.save();
-          });
+          children.removeObject(sessionObjective);
+          removingParent.save();
+          sessionObjective.save();
         });
       });
     }
