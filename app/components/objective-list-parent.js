2327e17d1d504148c635df9e59ffe587bacad30f
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-03-07 01:07:21 -0800

    Show objective program details whenever there is more than one cohort in the course

--- app/components/objective-list-parent.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  actions: {
-    manageParents: function(objective){
-      this.sendAction('manageParents', objective);
-    }
-  }
-});
