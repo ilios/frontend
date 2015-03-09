880723afb6149097ac446a605206a5204b293a48
sascha benjamin cohen <sascha.cohen@ucsf.edu>
2015-03-09 09:46:33 -0700

    Revert "For ILM Sessions give the due date as the first offering date"

--- app/models/session.js
+++ app/models/session.js
@@ -4,68 +4,57 @@ import Ember from 'ember';
 
 
 var Session = DS.Model.extend({
-  title: DS.attr('string'),
-  attireRequired: DS.attr('boolean'),
-  equipmentRequired: DS.attr('boolean'),
-  supplemental: DS.attr('boolean'),
-  deleted: DS.attr('boolean'),
-  publishedAsTbd: DS.attr('boolean'),
+    title: DS.attr('string'),
+    attireRequired: DS.attr('boolean'),
+    equipmentRequired: DS.attr('boolean'),
+    supplemental: DS.attr('boolean'),
+    deleted: DS.attr('boolean'),
+    publishedAsTbd: DS.attr('boolean'),
 
-  updatedAt: DS.attr('date'),
-  course: DS.belongsTo('course', {async: true}),
-  sessionType: DS.belongsTo('session-type', {async: true}),
-  offerings: DS.hasMany('offering', {async: true}),
-  publishEvent: DS.belongsTo('publish-event', {async: true}),
-  disciplines: DS.hasMany('discipline', {async: true}),
-  objectives: DS.hasMany('objective', {async: true}),
-  meshDescriptors: DS.hasMany('mesh-descriptor', {async: true}),
-  sessionLearningMaterials: DS.hasMany('session-learning-material', {async: true}),
-  instructionHours: DS.hasMany('instruction-hour', {async: true}),
-  sessionDescription: DS.belongsTo('session-description', {async: true}),
-  ilmSessionFacet: DS.belongsTo('ilm-session', {async: true}),
-  offeringLearnerGroupsLength: Ember.computed.mapBy('offerings', 'learnerGroups.length'),
-  learnerGroupCount: Ember.computed.sum('offeringLearnerGroupsLength'),
-  offeringsWithStartDate: Ember.computed.filterBy('offerings', 'startDate'),
-  sortedOfferingsByDate: Ember.computed.sort('offeringsWithStartDate', function(a,b){
-    var aDate = moment(a.get('startDate'));
-    var bDate = moment(b.get('startDate'));
-    if(aDate === bDate){
-      return 0;
-    }
-    return aDate > bDate ? 1 : -1;
-  }),
-  firstOfferingDate: function(){
-    var self = this;
-    var deferred = Ember.RSVP.defer();
-    this.get('ilmSessionFacet').then(function(ilmSession){
-      if(ilmSession){
-        deferred.resolve(ilmSession.get('dueDate'));
-      } else {
-        var offerings = self.get('sortedOfferingsByDate');
-        if(offerings.length === 0){
-          deferred.resolve(null);
-        }
-        deferred.resolve(offerings.get('firstObject.startDate'));
+    updatedAt: DS.attr('date'),
+    course: DS.belongsTo('course', {async: true}),
+    sessionType: DS.belongsTo('session-type', {async: true}),
+    offerings: DS.hasMany('offering', {async: true}),
+    publishEvent: DS.belongsTo('publish-event', {async: true}),
+    disciplines: DS.hasMany('discipline', {async: true}),
+    objectives: DS.hasMany('objective', {async: true}),
+    meshDescriptors: DS.hasMany('mesh-descriptor', {async: true}),
+    sessionLearningMaterials: DS.hasMany('session-learning-material', {async: true}),
+    instructionHours: DS.hasMany('instruction-hour', {async: true}),
+    sessionDescription: DS.belongsTo('session-description', {async: true}),
+    ilmSessionFacet: DS.belongsTo('ilm-session', {async: true}),
+    offeringLearnerGroupsLength: Ember.computed.mapBy('offerings', 'learnerGroups.length'),
+    learnerGroupCount: Ember.computed.sum('offeringLearnerGroupsLength'),
+    offeringsWithStartDate: Ember.computed.filterBy('offerings', 'startDate'),
+    sortedOfferingsByDate: Ember.computed.sort('offeringsWithStartDate', function(a,b){
+      var aDate = moment(a.get('startDate'));
+      var bDate = moment(b.get('startDate'));
+      if(aDate === bDate){
+        return 0;
       }
-    });
-
-
-    return deferred.promise;
-  }.property('sortedOfferingsByDate.@each', 'ilmSessionFacet.dueDate'),
-  isPublished: Ember.computed.notEmpty('publishEvent'),
-  isNotPublished: Ember.computed.not('isPublished'),
-  status: function(){
-    if(this.get('publishedAsTbd')){
-      return Ember.I18n.t('general.scheduled');
-    } else if(this.get('isPublished')){
-      return Ember.I18n.t('general.published');
-    } else {
-      return Ember.I18n.t('general.notPublished');
-    }
-  }.property('publishEvent'),
-  searchString: function(){
-    return this.get('title') + this.get('sessionType.title') + this.get('status');
-  }.property('title', 'sessionType.title', 'status'),
+      return aDate > bDate ? 1 : -1;
+    }),
+    firstOfferingDate: function(){
+      var offerings = this.get('sortedOfferingsByDate');
+      if(offerings.length === 0){
+        return null;
+      }
+      return offerings.get('firstObject.startDate');
+    }.property('sortedOfferingsByDate.@each'),
+    isPublished: Ember.computed.notEmpty('publishEvent'),
+    isNotPublished: Ember.computed.not('isPublished'),
+    status: function(){
+      if(this.get('publishedAsTbd')){
+        return Ember.I18n.t('general.scheduled');
+      } else if(this.get('isPublished')){
+        return Ember.I18n.t('general.published');
+      } else {
+        return Ember.I18n.t('general.notPublished');
+      }
+    }.property('publishEvent'),
+    searchString: function(){
+      return this.get('title') + this.get('sessionType.title') + this.get('status');
+    }.property('title', 'sessionType.title', 'status'),
 });
 
 export default Session;
--- server/index.js
+++ server/index.js
@@ -19,9 +19,9 @@ module.exports = function(app) {
   app.use(bodyParser.urlencoded({
     extended: true
   }));
-  // app.use(function(req,res,next){
-  //   setTimeout(next, 1000);
-  // });
+  app.use(function(req,res,next){
+    setTimeout(next, 1000);
+  });
 
   // Uncomment to log proxy requests
   // var morgan  = require('morgan');
