ea02678510bd51c3d464dfee62a294469dad5ac0
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-04-21 09:46:07 -0700

    Make username an action menu in the header and remove the loggout button

--- tests/unit/components/ilios-header-test.js
+++ tests/unit/components/ilios-header-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleForComponent('ilios-header', 'IliosHeaderComponent', {
   // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['component:action-menu']
 });
 
 test('it renders', function(assert) {
