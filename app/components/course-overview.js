4d35d2e6d30194509fd3e1b68158ebc73b4386e5
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-04-20 21:54:14 -0700

    Activate editing for course header and overview all the time Fixes #423

--- app/components/course-overview.js
+++ app/components/course-overview.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
-  editable: false,
+  editable: true,
   course: null,
   directorsSort: ['lastName', 'firstName'],
   directorsWithFullName: Ember.computed.filterBy('course.directors', 'fullName'),
