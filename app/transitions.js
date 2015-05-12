3b2c191e4a5ffa46ef611fe27609de1739f5f7dd
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-05-11 21:26:19 -0700

    Better transitions for objective manager Fixed the broken horizontal slide in of objectives Used a scroll animation to move in and out of the manger in the right place

--- app/transitions.js
+++ app/transitions.js
@@ -1,20 +1,15 @@
 export default function(){
   this.transition(
     this.hasClass('horizontal'),
-    this.toModel(true),
+    this.toValue(true),
     this.use('toLeft', {duration: 1000}),
     this.reverse('toRight', {duration: 1000})
   );
   this.transition(
     this.hasClass('vertical'),
-    this.toModel(true),
+    this.toValue(true),
     this.use('toDown', {duration: 1000}),
     this.reverse('toUp', {duration: 1000})
-  );
-  this.transition(
-    this.hasClass('crossFade'),
-    this.toModel(true),
-    this.use('crossFade', {duration: 1000})
   );
   this.transition(
     this.toRoute(function(){
@@ -28,7 +23,7 @@ export default function(){
       topRoutes.push('programs');
       return topRoutes.contains(this);
     }),
-    this.use('crossFade')
+    this.use('crossFade', {duration: 500})
   );
   this.transition(
     this.fromRoute('course.index', 'session.index'),
