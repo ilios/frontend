5d202cbc80a10627d3778a59da80444cf3e03e5d
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-06-16 09:54:09 -0700

    Always scroll to 8am when a new day / week calendar is displayed

--- app/components/user-events-calendar.js
+++ app/components/user-events-calendar.js
@@ -6,30 +6,14 @@ export default Ember.Component.extend({
   isMonthView: Ember.computed.equal('view', 'month'),
   isWeekView: Ember.computed.equal('view', 'week'),
   isDayView: Ember.computed.equal('view', 'day'),
-  classNameBindings: ['calendarType'],
-  calendarType: Ember.computed('view', function(){
+  classNameBindings: ['calendarTypeClass'],
+  calendarTypeClass: Ember.computed('view', function(){
     return this.get('view') + '-calendar';
   }),
-  elementTops: [],
-  watchElements: function(){
-    var self = this;
-    Ember.run.next(function(){
-      let tops = [];
-      $('.event').each(function(){
-          tops.push($(this).offset().top);
-      });
-      self.set('elementTops', tops);
-    });
-  }.observes('view', 'userEvents.[]').on('didInsertElement'),
-  lowestTop: Ember.computed.min('elementTops'),
   scrollCalendar: function(){
-    let lowestTop = this.get('lowestTop');
-    let calendar = $('.el-calendar');
-    if(lowestTop && calendar.length){
-      let calendarTop = calendar.offset().top + 50;
-      let position = lowestTop - calendarTop;
-      $(".day-calendar .el-calendar .week").scrollTop(position);
-      $(".week-calendar .el-calendar .week").scrollTop(position);
-    }
-  }.observes('lowestTop').on('didInsertElement'),
+    Ember.run.next(function(){
+      $(".day-calendar .el-calendar .week").scrollTop(500);
+      $(".week-calendar .el-calendar .week").scrollTop(500);
+    });
+  }.observes('view').on('didInsertElement'),
 });
