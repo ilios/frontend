9ed64f1a62e00a113cd70632e4152c0075e41b94
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-06-17 14:28:44 -0700

    Use tooltips on events to show more information

--- app/components/ilios-calendar-event.js
+++ app/components/ilios-calendar-event.js
@@ -1,3 +1,4 @@
+/* global moment */
 import Ember from 'ember';
 import { default as CalendarEvent } from 'el-calendar/components/calendar-event';
 
@@ -5,7 +6,14 @@ export default CalendarEvent.extend({
   viewType: 'day',
   event: null,
   classNameBindings: [':event', ':ilios-calendar-event', 'event.eventClass', 'viewType'],
+  toolTipMessage: Ember.computed('event', function(){
+    let str = this.get('event.location') + '<br />' +
+      moment(this.get('event.startDate')).format('h:mma') + ' - ' +
+      moment(this.get('event.endDate')).format('h:mma') + '<br />' +
+      this.get('event.name'); 
 
+    return str;
+  }),
   style: Ember.computed('viewType', function() {
     let escape = Ember.Handlebars.Utils.escapeExpression;
     if(this.get('viewType') !== 'month-event'){
