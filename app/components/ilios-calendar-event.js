699147603d67d757d40caf1cd440b471f0c5c8d9
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-06-17 14:41:57 -0700

    Display simultaneous events side by side
    Updated elemental-calendar and fixed our ilios-calendar-event style
    property.

--- app/components/ilios-calendar-event.js
+++ app/components/ilios-calendar-event.js
@@ -5,23 +5,27 @@ import { default as CalendarEvent } from 'el-calendar/components/calendar-event'
 export default CalendarEvent.extend({
   viewType: 'day',
   event: null,
-  classNameBindings: [':event', ':ilios-calendar-event', 'event.eventClass', 'viewType'],
+  classNameBindings: [':event', ':event-pos', ':ilios-calendar-event', 'event.eventClass', 'viewType'],
   toolTipMessage: Ember.computed('event', function(){
     let str = this.get('event.location') + '<br />' +
       moment(this.get('event.startDate')).format('h:mma') + ' - ' +
       moment(this.get('event.endDate')).format('h:mma') + '<br />' +
-      this.get('event.name'); 
+      this.get('event.name');
 
     return str;
   }),
-  style: Ember.computed('viewType', function() {
-    let escape = Ember.Handlebars.Utils.escapeExpression;
-    if(this.get('viewType') !== 'month-event'){
-      return Ember.String.htmlSafe(
-        `top: ${escape(this.calculateTop())}%;
-         height: ${escape(this.calculateHeight())}%;`
-      );
+  style: Ember.computed(function() {
+    if(this.get('viewType') === 'month-event'){
+      return '';
     }
+    let escape = Ember.Handlebars.Utils.escapeExpression;
+
+    return Ember.String.htmlSafe(
+      `top: ${escape(this.calculateTop())}%;
+       height: ${escape(this.calculateHeight())}%;
+       left: ${escape(this.calculateLeft())}%;
+       width: ${escape(this.calculateWidth())}%;`
+    );
   }),
   click(){
     this.sendAction('action', this.get('event'));
