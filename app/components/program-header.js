e99d2b2bde4e9df7d11d329b562100c70a41a75a
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-06-18 09:58:05 -0700

    Move publish functions into a mixin
    This standardizes the behavior when taking publication actions on a
    course, session, program, or program year.

--- app/components/program-header.js
+++ app/components/program-header.js
@@ -1,100 +1,11 @@
 import Ember from 'ember';
+import Publishable from 'ilios/mixins/publishable';
 
-export default Ember.Component.extend({
-  currentUser: Ember.inject.service(),
-  store: Ember.inject.service(),
+export default Ember.Component.extend(Publishable, {
   program: null,
-  showCheckLink: true,
-  menuTitle: Ember.computed.oneWay('program.status'),
-  menuIcon: function(){
-    if(this.get('program.publishedAsTbd')){
-      return 'clock-o';
-    }
-    if(this.get('program.isPublished')){
-      return 'star';
-    }
-    return 'cloud';
-  }.property('program.isPublished', 'program.publishedAsTbd'),
-  showTbd: Ember.computed.not('program.isScheduled'),
-  showAsIs: function(){
-    return (
-      (!this.get('program.isPublished') || this.get('program.isScheduled')) &&
-      this.get('program.requiredPublicationIssues.length') === 0 &&
-      this.get('program.allPublicationIssuesLength') !== 0
-    );
-  }.property('program.isPublished','program.isScheduled', 'program.requiredPublicationIssues.length', 'program.allPublicationIssuesLength'),
-  showReview: function(){
-    return this.get('program.allPublicationIssuesLength') > 0 &&
-    this.get('showCheckLink');
-  }.property('program.allPublicationIssuesLength', 'showCheckLink'),
-  showPublish: function(){
-    return (
-      (!this.get('program.isPublished') || this.get('program.isScheduled')) &&
-      this.get('program.allPublicationIssuesLength') === 0
-    );
-  }.property('program.isPublished', 'program.allPublicationIssuesLength'),
-  showUnPublish: Ember.computed.or('program.isPublished', 'program.isScheduled'),
-  publicationStatus: function(){
-    if(this.get('program.isScheduled')){
-      return 'scheduled';
-    } else if (this.get('program.isPublished')){
-      return 'published';
-    }
-
-    return 'notpublished';
-  }.property('program.isPublished', 'program.isScheduled'),
+  publishTarget: Ember.computed.alias('program'),
+  publishEventCollectionName: 'programs',
   actions: {
-    unpublish: function(){
-      var program = this.get('program');
-      program.get('publishEvent').then(function(publishEvent){
-        program.set('publishedAsTbd', false);
-        program.set('publishEvent', null);
-        program.save();
-        if(publishEvent){
-          publishEvent.get('programs').removeObject(program);
-          if(publishEvent.get('totalRelated') === 0){
-            publishEvent.deleteRecord();
-          }
-          publishEvent.save();
-        }
-      });
-    },
-    publishAsTbd: function(){
-      var self = this;
-      var program = this.get('program');
-      program.set('publishedAsTbd', true);
-      program.get('publishEvent').then(function(publishEvent){
-        if(!publishEvent){
-          publishEvent = self.get('store').createRecord('publish-event', {
-            administrator: self.get('currentUser.model')
-          });
-          publishEvent.save().then(function(publishEvent){
-            program.set('publishEvent', publishEvent);
-            program.save();
-          });
-        } else {
-          program.save();
-        }
-      });
-    },
-    publish: function(){
-      var program = this.get('program');
-      var self = this;
-      program.set('publishedAsTbd', false);
-      program.get('publishEvent').then(function(publishEvent){
-        if(!publishEvent){
-          publishEvent = self.get('store').createRecord('publish-event', {
-              administrator: self.get('currentUser.model')
-          });
-          publishEvent.save().then(function(publishEvent){
-            program.set('publishEvent', publishEvent);
-            program.save();
-          });
-        } else {
-          program.save();
-        }
-      });
-    },
     changeTitle: function(newTitle){
       this.get('program').set('title', newTitle);
       this.get('program').save();
---
+++ app/mixins/publishable.js
@@ -0,0 +1,114 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  publishTarget: null,
+  publishEventCollectionName: null,
+  currentUser: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
+  store: Ember.inject.service(),
+  showCheckLink: true,
+  menuTitle: Ember.computed.oneWay('publishTarget.status'),
+  menuIcon: function(){
+    if(this.get('publishTarget.publishedAsTbd')){
+      return 'clock-o';
+    }
+    if(this.get('publishTarget.isPublished')){
+      return 'star';
+    }
+    return 'cloud';
+  }.property('publishTarget.isPublished', 'publishTarget.publishedAsTbd'),
+  showTbd: Ember.computed.not('publishTarget.isScheduled'),
+  showAsIs: function(){
+    return (
+      (!this.get('publishTarget.isPublished') || this.get('publishTarget.isScheduled')) &&
+      this.get('publishTarget.requiredPublicationIssues.length') === 0 &&
+      this.get('publishTarget.allPublicationIssuesLength') !== 0
+    );
+  }.property('publishTarget.isPublished', 'publishTarget.requiredPublicationIssues.length', 'publishTarget.allPublicationIssuesLength'),
+  showReview: function(){
+    return this.get('publishTarget.allPublicationIssuesLength') > 0 &&
+    this.get('showCheckLink');
+  }.property('publishTarget.allPublicationIssuesLength', 'showCheckLink'),
+  showPublish: function(){
+    return (
+      (!this.get('publishTarget.isPublished') || this.get('publishTarget.isScheduled')) &&
+      this.get('publishTarget.allPublicationIssuesLength') === 0
+    );
+  }.property('publishTarget.isPublished', 'publishTarget.allPublicationIssuesLength'),
+  showUnPublish: Ember.computed.or('publishTarget.isPublished', 'publishTarget.isScheduled'),
+  publicationStatus: function(){
+    if(this.get('publishTarget.isScheduled')){
+      return 'scheduled';
+    } else if (this.get('publishTarget.isPublished')){
+      return 'published';
+    }
+
+    return 'notpublished';
+  }.property('publishTarget.isPublished', 'publishTarget.isScheduled'),
+  actions: {
+    unpublish: function(){
+      var publishTarget = this.get('publishTarget');
+      publishTarget.get('publishEvent').then(publishEvent => {
+        publishTarget.set('publishedAsTbd', false);
+        publishTarget.set('publishEvent', null);
+        if(publishEvent){
+          publishEvent.get(this.get('publishEventCollectionName')).removeObject(publishTarget);
+          if(publishEvent.get('totalRelated') === 0){
+            publishEvent.deleteRecord();
+          }
+          publishTarget.save().then(()=>{
+            publishEvent.save().then(() => {
+              this.get('flashMessages').success('publish.message.unPublish');
+            });
+          });
+        } else {
+          publishTarget.save().then(()=>{
+            this.get('flashMessages').success('publish.message.unPublish');
+          });
+        }
+      });
+    },
+    publishAsTbd: function(){
+      var publishTarget = this.get('publishTarget');
+      publishTarget.set('publishedAsTbd', true);
+      publishTarget.get('publishEvent').then(publishEvent => {
+        if(!publishEvent){
+          publishEvent = this.get('store').createRecord('publish-event', {
+            administrator: this.get('currentUser.model')
+          });
+          publishEvent.save().then(publishEvent => {
+            publishTarget.set('publishEvent', publishEvent);
+            publishTarget.save().then(() => {
+              this.get('flashMessages').success('publish.message.schedule');
+            });
+          });
+        } else {
+          publishTarget.save().then(() => {
+            this.get('flashMessages').success('publish.message.schedule');
+          });
+        }
+      });
+    },
+    publish: function(){
+      var publishTarget = this.get('publishTarget');
+      publishTarget.set('publishedAsTbd', false);
+      publishTarget.get('publishEvent').then(publishEvent => {
+        if(!publishEvent){
+          publishEvent = this.get('store').createRecord('publish-event', {
+            administrator: this.get('currentUser.model')
+          });
+          publishEvent.save().then(publishEvent => {
+            publishTarget.set('publishEvent', publishEvent);
+            publishTarget.save().then(() => {
+              this.get('flashMessages').success('publish.message.publish');
+            });
+          });
+        } else {
+          publishTarget.save().then(() => {
+            this.get('flashMessages').success('publish.message.publish');
+          });
+        }
+      });
+    },
+  }
+});
