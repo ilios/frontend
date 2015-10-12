11435d7adacb85d8c2418f8655bdb680ee52a6ba
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-10-12 15:45:22 -0700

    Fill out the event details route
    Uses the ilios-calendar single-event component to display all of the
    details for a single event.

--- app/models/objective.js
+++ app/models/objective.js
@@ -1,21 +1,24 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
+let {computed, RSVP, isEmpty} =  Ember;
+let {alias, gt, gte} = computed;
+
 export default DS.Model.extend({
   title: DS.attr('string'),
   competency: DS.belongsTo('competency', {async: true}),
   courses: DS.hasMany('course', {async: true}),
   //While it is possible at some point that objectives will be allowed to
   //link to multiple courses, for now we just reflect a many to one relationship
-  course: Ember.computed.alias('courses.firstObject'),
+  course: alias('courses.firstObject'),
   programYears: DS.hasMany('program-year',  {async: true}),
   //While it is possible at some point that objectives will be allowed to
   //link to multiple program years, for now we just reflect a many to one relationship
-  programYear: Ember.computed.alias('programYears.firstObject'),
+  programYear: alias('programYears.firstObject'),
   sessions: DS.hasMany('session', {async: true}),
   //While it is possible at some point that objectives will be allowed to
   //link to multiple sessions, for now we just reflect a many to one relationship
-  session: Ember.computed.alias('sessions.firstObject'),
+  session: alias('sessions.firstObject'),
   parents: DS.hasMany('objective', {
     inverse: 'children',
     async: true
@@ -25,10 +28,10 @@ export default DS.Model.extend({
     async: true
   }),
   meshDescriptors: DS.hasMany('mesh-descriptor', {async: true}),
-  hasMultipleParents: Ember.computed.gt('parents.length', 1),
-  hasParents: Ember.computed.gte('parents.length', 1),
-  treeCompetencies: function(){
-    var defer = Ember.RSVP.defer();
+  hasMultipleParents: gt('parents.length', 1),
+  hasParents: gte('parents.length', 1),
+  treeCompetencies: computed('competency', 'parents.@each.treeCompetencies.[]', function(){
+    var defer = RSVP.defer();
     var self = this;
     this.get('competency').then(function(competency){
       self.get('parents').then(function(parents){
@@ -48,7 +51,29 @@ export default DS.Model.extend({
     return DS.PromiseArray.create({
       promise: defer.promise
     });
-  }.property('competency', 'parents.@each.treeCompetencies'),
+  }),
+  topParents: computed('parents.[]', function(){
+    var defer = RSVP.defer();
+    this.get('parents').then(parents => {
+      if(isEmpty(parents)){
+        defer.resolve([this]);
+      }
+      let allTopParents = [];
+      let promises = [];
+      parents.forEach( objective => {
+        promises.pushObject(objective.get('topParents').then(topParents => {
+          allTopParents.pushObjects(topParents.toArray());
+        }));
+      });
+      
+      RSVP.all(promises).then(()=>{
+        defer.resolve(allTopParents);
+      });
+    });
+    return DS.PromiseArray.create({
+      promise: defer.promise
+    });
+  }),
   shortTitle: function(){
     var title = this.get('title');
     if(title === undefined){
--- tests/unit/models/objective-test.js
+++ tests/unit/models/objective-test.js
@@ -3,6 +3,8 @@ import {
   test
 } from 'ember-qunit';
 import modelList from '../../helpers/model-list';
+import Ember from 'ember';
+let {run} = Ember;
 
 moduleForModel('objective', 'Objective', {
   needs: modelList
@@ -12,4 +14,61 @@ test('it exists', function(assert) {
   var model = this.subject();
   // var store = this.store();
   assert.ok(!!model);
+});
+
+test('top parent with no parents should be self', function(assert) {
+  assert.expect(2);
+  
+  var model = this.subject();
+  model.get('topParents').then(topParents => {
+    assert.ok(topParents.get('length') === 1);
+    assert.equal(topParents.get('firstObject'), model);
+  });
+});
+
+test('corrent top parents with single parent tree', function(assert) {
+  assert.expect(2);
+  let store = this.store();
+  let model = this.subject();
+  run(function(){
+    let parent1 = store.createRecord('objective', {
+      children: [model]
+    });
+    let parent2 = store.createRecord('objective', {
+      children: [parent1]
+    });
+    
+    model.get('topParents').then(topParents => {
+      assert.ok(topParents.get('length') === 1);
+      assert.equal(topParents.get('firstObject'), parent2);
+    });
+  });
+  
+});
+
+test('corrent top parents with multi parent tree', function(assert) {
+  assert.expect(3);
+  let store = this.store();
+  let model = this.subject();
+  run(function(){
+    let parent1 = store.createRecord('objective', {
+      children: [model]
+    });
+    let parent2 = store.createRecord('objective', {
+      children: [model]
+    });
+    let parent3 = store.createRecord('objective', {
+      children: [parent1]
+    });
+    let parent4 = store.createRecord('objective', {
+      children: [parent2]
+    });
+    
+    model.get('topParents').then(topParents => {
+      assert.ok(topParents.get('length') === 2);
+      assert.ok(topParents.contains(parent3));
+      assert.ok(topParents.contains(parent4));
+    });
+  });
+  
 });
