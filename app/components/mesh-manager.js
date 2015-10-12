8ed326cb77d667874cbefaa4fd2b1e4cd756beb7
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-10-12 16:51:38 -0700

    implemented pagination in mesh search.

--- app/components/mesh-manager.js
+++ app/components/mesh-manager.js
@@ -16,7 +16,11 @@ export default Ember.Component.extend({
   classNames: ['detail-block'],
   placeholder: t('courses.meshSearchPlaceholder'),
   terms: [],
+  query: '',
   searchResults: [],
+  searchPage: 0,
+  searchResultsPerPage: 50,
+  hasMoreSearchResults: false,
   targetItemTitle: '',
   searching: false,
   searchReturned: false,
@@ -35,8 +39,12 @@ export default Ember.Component.extend({
       var self = this;
       this.set('searchReturned', false);
       this.set('searching', true);
+      this.set('query', query);
       var terms = this.get('terms');
-      this.get('store').query('mesh-descriptor', {q: query}).then(function(descriptors){
+      this.get('store').query('mesh-descriptor', {
+        q: query,
+        limit: this.get('searchResultsPerPage') + 1,
+      }).then(function(descriptors){
         let results = descriptors.map(function(descriptor){
           return ProxiedDescriptors.create({
             content: descriptor,
@@ -45,13 +53,48 @@ export default Ember.Component.extend({
         });
         self.set('searchReturned', true);
         self.set('searching', false);
-        self.set('searchResults', results);
+        self.set('searchPage', 1);
+        self.set('hasMoreSearchResults', (results.length > self.get('searchResultsPerPage')));
+        if (self.get('hasMoreSearchResults')) {
+          results.pop();
+        }
+        self.set('searchResults', $.merge(self.get('searchResults'), results));
+      });
+    },
+
+    searchMore: function(query) {
+      var self = this;
+      this.set('searchReturned', false);
+      this.set('searching', true);
+      var terms = this.get('terms');
+      this.get('store').query('mesh-descriptor', {
+        q: query,
+        limit: this.get('searchResultsPerPage') + 1,
+        offset: this.get('searchPage') * this.get('searchResultsPerPage')
+      }).then(function(descriptors){
+        let results = descriptors.map(function(descriptor){
+          return ProxiedDescriptors.create({
+            content: descriptor,
+            terms: terms
+          });
+        });
+        self.set('searchReturned', true);
+        self.set('searching', false);
+        self.set('searchPage', self.get('searchPage') + 1);
+        self.set('hasMoreSearchResults', (results.length > self.get('searchResultsPerPage')));
+        if (self.get('hasMoreSearchResults')) {
+          results.pop();
+        }
+        self.set('searchResults', $.merge(self.get('searchResults'), results));
       });
     },
     clear: function(){
       this.set('searchResults', []);
       this.set('searchReturned', false);
       this.set('searching', false);
+      this.set('searchPage', 0);
+      this.set('hasMoreSearchResults', false);
+      this.set('query', '');
     },
     add: function(term){
       this.sendAction('add', term.get('content'));
