0a50aa10a5c30b6355d56412d1629ed8965af6d4
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-10-15 15:18:41 -0700

    Revert inplace-textarea to its original state

--- app/components/inplace-textarea.js
+++ app/components/inplace-textarea.js
@@ -3,37 +3,4 @@ import InPlace from 'ilios/mixins/inplace';
 
 export default Ember.Component.extend(InPlace, {
   classNames: ['editinplace', 'inplace-textarea'],
-  editorParams: Ember.computed('buffer', 'clickPrompt', function(){
-    let params = {
-      inlineMode: false,
-      placeholder: '',
-      allowHTML: true,
-      autosave: false,
-      plainPaste: true,
-      spellcheck: true,
-      buttons: [
-        'bold',
-        'italic',
-        'underline',
-        'subscript',
-        'superscript',
-        'insertOrderedList',
-        'insertUnorderedList',
-        'createLink',
-        'html'
-      ]
-    };
-    
-    return params;
-  }),
-  willDestroyElement(){
-    this.$('.control .froala-box').editable('destroy');
-  },
-  actions: {
-    grabChangedValue: function(event, editor){
-      if(editor){
-        this.send('changeValue', editor.getHTML());
-      }
-    }
-  }
 });
