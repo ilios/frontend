a4c396017cef21c755f0907942c85ad983cd926c
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-10-19 17:41:51 -0700

    Remove totalRelated from publish-event model
    We’re not using this anymore

--- app/models/publish-event.js
+++ app/models/publish-event.js
@@ -11,11 +11,4 @@ export default DS.Model.extend({
   programs: DS.hasMany('program', {async: true}),
   courses: DS.hasMany('course', {async: true}),
   programYears: DS.hasMany('program-year', {async: true}),
-  relatedCounts: Ember.computed.collect(
-    'sessions.length',
-    'courses.length',
-    'programs.length',
-    'programYears.length'
-  ),
-  totalRelated: Ember.computed.sum('relatedCounts'),
 });
