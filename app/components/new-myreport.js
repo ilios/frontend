f93a5df3cb974ec44fa8e886b889adba0bb49e31
Jason Hedrick <thecoolestguy@gmail.com>
2015-10-22 10:24:03 -0700

    cancel button working, list choices in place, styling

--- app/components/new-myreport.js
+++ app/components/new-myreport.js
@@ -1,4 +1,35 @@
 import Ember from 'ember';
+import { translationMacro as t } from "ember-i18n";
 
 export default Ember.Component.extend({
+  subjects: ['Courses',
+             'Sessions',
+             'Programs',
+             'Program Years',
+             'Instructors',
+             'Instructor Groups',
+             'Learning Materials',
+             'Competencies',
+             'Topics',
+             'MeSH Terms'
+             ],
+  prepositionalObjects: ['Session',
+                         'Program',
+                         'Program Year',
+                         'Instructor',
+                         'Instructor Group',
+                         'Learning Material',
+                         'Competency',
+                         'Topic',
+                         'MeSH Term'
+              ],
+  actions: {
+    toggleEditor() {
+      this.set('toggleFn', !this.get('toggleFn'));
+    },
+
+    closeEditor() {
+      this.set('toggleFn', false);
+    }
+  }
 });
