f9641314f9fb6b2fd3a18cecd1a537edc16ef96c
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-10-23 10:21:16 -0700

    Fix session serializer test

--- tests/unit/serializers/session-test.js
+++ tests/unit/serializers/session-test.js
@@ -1,16 +1,30 @@
 import {
-  moduleFor,
+  moduleForModel,
   test
 } from 'ember-qunit';
 import {a as testgroup} from 'ilios/tests/helpers/test-groups';
 
-moduleFor('serializer:session', 'Unit | Serializer | Session ' + testgroup, {
+moduleForModel('serializer:session', 'Unit | Serializer | Session ' + testgroup, {
   // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
+  needs: [
+    'serializer:session',
+    'model:publish-event',
+    'model:session-type',
+    'model:course',
+    'model:ilm-session',
+    'model:topic',
+    'model:objective',
+    'model:mesh-descriptor',
+    'model:session-description',
+    'model:session-learning-material',
+    'model:offering',
+  ]
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var serializer = this.subject();
-  assert.ok(serializer);
+test('it removes all non postable fields', function(assert) {
+  var record = this.subject();
+
+  var serializedRecord = record.serialize();
+  
+  assert.ok(!("updatedAt" in serializedRecord));
 });
