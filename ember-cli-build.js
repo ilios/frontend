0e685e98b310084ae8c9fae58f6fa34c585ccb1a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-10-28 13:49:47 -0700

    enable babel polyfills.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -32,6 +32,9 @@ module.exports = function(defaults) {
     emberCliFontAwesome: {
       useScss: true
     },
+    babel: {
+      includePolyfill: true
+    }
   });
   app.import('bower_components/FroalaWysiwygEditor/js/plugins/lists.min.js');
   
