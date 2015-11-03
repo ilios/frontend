caf919f0a089c7bfa9b4c8f2d7a4a5caa052f1d4
Jason Hedrick <thecoolestguy@gmail.com>
2015-11-03 13:46:16 -0800

    removed cssId anonymous function call

---
+++ tests/unit/utils/random-string-test.js
@@ -0,0 +1,10 @@
+import randomString from '../../../utils/random-string';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | random string');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = randomString();
+  assert.ok(result);
+});
