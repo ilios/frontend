f9eb40ad99e2809bd448e7673d12205ee132b040
Jason Hedrick <thecoolestguy@gmail.com>
2015-11-03 13:46:16 -0800

    removed cssId anonymous function call

--- app/components/toggle-wide.js
+++ app/components/toggle-wide.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
+import randomString from '../utils/random-string.js';
 
 export default Ember.Component.extend({
   tagName: 'label',
   classNames: ['switch', 'switch-wide', 'switch-green'],
   onLabel: null,
+  cssId: randomString(),
   offLabel: null,
   value: false,
   switchValue: Ember.computed.oneWay('value'),
