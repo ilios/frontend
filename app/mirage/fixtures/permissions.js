caa4f3a6db1e1223630711ed299eed5adb882b74
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-11-03 21:13:58 -0800

    Use permissions to determine which school a user is in

---
+++ app/mirage/fixtures/permissions.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    'id' : 1,
+    'user' : "4136",
+    'canRead' : true,
+    'canWrite' : true,
+    'tableRowId' : "1",
+    'tableName' : "school",
+  },
+  {
+    'id' : 2,
+    'user' : "4136",
+    'canRead' : true,
+    'canWrite' : true,
+    'tableRowId' : "3",
+    'tableName' : "school",
+  },
+];
