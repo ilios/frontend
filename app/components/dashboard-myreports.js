63af44abafec31e8be2596de3340a3300a07e78b
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-11-10 01:11:40 -0800

    Load and display report details

--- app/components/dashboard-myreports.js
+++ app/components/dashboard-myreports.js
@@ -1,21 +1,45 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
-export default Ember.Component.extend({
-  currentUser: Ember.inject.service(),
+const { computed, RSVP, inject, Component } = Ember;
+const { service } = inject;
+
+export default Component.extend({
+  currentUser: service(),
+  reportResults: service(),
   tagName: 'div',
   classNames: ['dashboard-block', 'dashboard-double-block'],
   myReportEditorOn: false,
   selectedReport: null,
   reportSorting: ['title'],
-  sortedReports: Ember.computed.sort('listOfReports', 'reportSorting'),
-  listOfReports: Ember.computed('currentUser.model.allRelatedCourses.[]', function(){
-    let defer = Ember.RSVP.defer();
+  sortedReports: computed.sort('listOfReports', 'reportSorting'),
+  listOfReports: computed('currentUser.model.allRelatedCourses.[]', function(){
+    let defer = RSVP.defer();
     this.get('currentUser').get('model').then( user => {
       user.get('reports').then( reports => {
         defer.resolve(reports);
       });
     });
+    return DS.PromiseArray.create({
+      promise: defer.promise
+    });
+  }),
+  reportResultsList: computed('selectedReport', function(){
+    let defer = RSVP.defer();
+    const reportResults = this.get('reportResults');
+    const report = this.get('selectedReport');
+    if(!report){
+      defer.resolve([]);
+      return;
+    }
+    const subject = report.get('subject');
+    const object = report.get('prepositionalObject');
+    const objectId = report.get('prepositionalObjectTableRowId');
+    
+    reportResults.getResults(subject, object, objectId).then(results => {
+      defer.resolve(results.sortBy('value'));
+    });
+    
     return DS.PromiseArray.create({
       promise: defer.promise
     });
