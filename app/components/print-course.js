a3180ce13b4b1a0de63510b916f485f8d6158fe5
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-11-11 15:28:45 -0800

    Prevent printing draft courses and draft sessions within a course

--- app/components/print-course.js
+++ app/components/print-course.js
@@ -1,28 +1,32 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
+const { computed, RSVP, ObjectProxy } = Ember;
+const { PromiseArray } = DS;
+
 export default Ember.Component.extend({
   course: null,
   tagName: 'section',
   classNames: ['printable course'],
   sortTitle: ['title'],
   sortDirectorsBy: ['lastName', 'firstName'],
-  sortedDirectors: Ember.computed.sort('course.directors', 'sortDirectorsBy'),
-  sortedTopics: Ember.computed.sort('course.topics', 'sortTitle'),
-  sortedMeshDescriptors: Ember.computed.sort('course.meshDescriptors', 'sortTitle'),
-  sortedSessionProxies: function(){
-    var course = this.get('course');
+  sortedDirectors: computed.sort('course.directors', 'sortDirectorsBy'),
+  sortedTopics: computed.sort('course.topics', 'sortTitle'),
+  sortedMeshDescriptors: computed.sort('course.meshDescriptors', 'sortTitle'),
+  sortedSessionProxies: computed('course.sessions.[]', function(){
+    const course = this.get('course');
     if(!course){
       return [];
     }
-    var deferred = Ember.RSVP.defer();
-    var SessionProxy = Ember.ObjectProxy.extend({
+    let deferred = RSVP.defer();
+    let SessionProxy = ObjectProxy.extend({
       sortTitle: ['title'],
-      sortedTopics: Ember.computed.sort('content.topics', 'sortTitle'),
-      sortedMeshDescriptors: Ember.computed.sort('content.meshDescriptors', 'sortTitle'),
+      sortedTopics: computed.sort('content.topics.[]', 'sortTitle'),
+      sortedMeshDescriptors: computed.sort('content.meshDescriptors.[]', 'sortTitle'),
     });
     course.get('sessions').then(function(sessions){
-      var proxiedSessions = sessions.map(function(session){
+      let noDraftSessions = sessions.filterBy('isPublishedOrScheduled');
+      let proxiedSessions = noDraftSessions.map(function(session){
         return SessionProxy.create({
           content: session
         });
@@ -31,9 +35,9 @@ export default Ember.Component.extend({
     });
 
 
-    return DS.PromiseArray.create({
+    return PromiseArray.create({
       promise: deferred.promise
     });
 
-  }.property('course.sessions.@each.{topics.@each,meshDescriptors.@each,title}'),
+  }),
 });
--- app/mixins/publishable-model.js
+++ app/mixins/publishable-model.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
+const { computed } = Ember;
+
 export default Ember.Mixin.create({
   i18n: Ember.inject.service(),
   publishedAsTbd: DS.attr('boolean'),
   publishEvent: DS.belongsTo('publish-event', {async: true}),
-  isPublished: Ember.computed.notEmpty('publishEvent.content'),
-  isNotPublished: Ember.computed.not('isPublished'),
-  isScheduled: Ember.computed.oneWay('publishedAsTbd'),
-  status: function(){
+  isPublished: computed.notEmpty('publishEvent.content'),
+  isNotPublished: computed.not('isPublished'),
+  isScheduled: computed.oneWay('publishedAsTbd'),
+  status: computed('i18n.locale', 'isPublished', 'publishedAsTbd', function(){
     if(this.get('publishedAsTbd')){
       return this.get('i18n').t('general.scheduled');
     }
@@ -16,9 +18,12 @@ export default Ember.Mixin.create({
       return this.get('i18n').t('general.published');
     }
     return this.get('i18n').t('general.notPublished');
-  }.property('i18n.locale', 'isPublished', 'publishedAsTbd'),
-  allPublicationIssuesCollection: Ember.computed.collect('requiredPublicationIssues.length', 'optionalPublicationIssues.length'),
-  allPublicationIssuesLength: Ember.computed.sum('allPublicationIssuesCollection'),
+  }),
+  isPublishedOrScheduled: computed('publishTarget.isPublished', 'publishTarget.isScheduled', function(){
+    return this.get('publishedAsTbd') || this.get('isPublished');
+  }),
+  allPublicationIssuesCollection: computed.collect('requiredPublicationIssues.length', 'optionalPublicationIssues.length'),
+  allPublicationIssuesLength: computed.sum('allPublicationIssuesCollection'),
   requiredPublicationSetFields: [],
   requiredPublicationLengthFields: [],
   optionalPublicationSetFields: [],
