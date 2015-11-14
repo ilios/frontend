b32dcacc2772f0566b23689dbdd76b6a358c9441
Thomas Lee <djvoa12@gmail.com>
2015-11-13 17:54:26 -0800

    updated true overall learner-group count

--- app/components/learnergroup-header.js
+++ app/components/learnergroup-header.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const { Component } = Ember;
+
+export default Component.extend({
   learnerGroup: null,
+
   actions: {
-    changeTitle: function(newTitle){
-      this.get('learnerGroup').set('title', newTitle);
-      this.get('learnerGroup').save();
-    },
+    changeTitle(newTitle) {
+      const learnerGroup = this.get('learnerGroup');
+
+      learnerGroup.set('title', newTitle);
+      learnerGroup.save();
+    }
   }
 });
