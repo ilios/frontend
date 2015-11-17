d35a375121ed88388b9aa49ee5bdfa2b03e4cb1e
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-11-17 11:15:04 -0800

    Fix dashboard route
    Remove beforeModel hook from dashboard route which was causing an
    inability to load the dashboard when not logged in.
    
    Replace the model hook hash with one that resolves the user first
    before creating the school and year promises.

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,24 +3,24 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 
 const { inject, Route, RSVP } = Ember;
 const { service } = inject;
-const { hash } = RSVP;
+const { hash, Promise } = RSVP;
 
 export default Route.extend(AuthenticatedRouteMixin, {
 	i18n: service(),
 	currentUser: service(),
+	store: service(),
 	today: new Date(),
 
-	beforeModel() {
-		return this.get('currentUser.model');
-	},
-
 	model() {
-		const user = this.get('currentUser.model');
-		const schools = user.get('schools');
-		const store = this.store;
-		const academicYears = store.findAll('academic-year');
-
-		return hash({ schools, academicYears });
+		const store = this.get('store');
+		return new Promise(resolve => {
+			this.get('currentUser.model').then(user => {
+				const schools = user.get('schools');
+				const academicYears = store.findAll('academic-year');
+				resolve(hash({ schools, academicYears }));
+			});
+		});
+		
 	},
 
 	setupController() {
