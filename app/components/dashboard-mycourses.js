42465ca718662f2a774ac990c83dd421947604c7
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-11-17 11:13:56 -0800

    Use courses query to load a users courses

--- app/components/dashboard-mycourses.js
+++ app/components/dashboard-mycourses.js
@@ -1,19 +1,26 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
-export default Ember.Component.extend({
-  currentUser: Ember.inject.service(),
+const { Component, computed, inject, RSVP } = Ember;
+const { service }= inject;
+
+export default Component.extend({
+  store: service(),
+  currentUser: service(),
   tagName: 'div',
   classNames: ['dashboard-block'],
   courseSorting: ['startDate:desc'],
-  sortedListOfCourses: Ember.computed.sort('listOfCourses', 'courseSorting'),
-  listOfCourses: Ember.computed('currentUser.model.allRelatedCourses.[]', function(){
-    let defer = Ember.RSVP.defer();
+  sortedListOfCourses: computed.sort('listOfCourses', 'courseSorting'),
+  listOfCourses: computed('currentUser.model', function(){
+    let defer = RSVP.defer();
     this.get('currentUser').get('model').then( user => {
-      user.get('allRelatedCourses').then( courses => {
-        let filteredCourses = courses.filter(course => {
-          return (!course.get('locked') && !course.get('archived'));
-        });
+      this.get('store').query('course', {
+        filters: {
+          users: [user.get('id')],
+          locked: false,
+          archived: false
+        }
+      }).then(filteredCourses => {
         defer.resolve(filteredCourses);
       });
     });
