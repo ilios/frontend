19ead3b66b0930c51e96d97e5f3d9a9d938909a2
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-11-19 10:44:06 -0800

    Move relatedCourses into the currentUser
    This makes the component testable.

--- app/components/dashboard-mycourses.js
+++ app/components/dashboard-mycourses.js
@@ -1,31 +1,24 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
-const { Component, computed, inject, RSVP } = Ember;
+const { Component, computed, inject } = Ember;
 const { service }= inject;
 
 export default Component.extend({
-  store: service(),
   currentUser: service(),
   tagName: 'div',
   classNames: ['dashboard-block'],
   courseSorting: ['startDate:desc'],
   sortedListOfCourses: computed.sort('listOfCourses', 'courseSorting'),
-  listOfCourses: computed('currentUser.model', function(){
-    let defer = RSVP.defer();
-    this.get('currentUser').get('model').then( user => {
-      this.get('store').query('course', {
-        filters: {
-          users: [user.get('id')],
-          locked: false,
-          archived: false
-        }
-      }).then(filteredCourses => {
-        defer.resolve(filteredCourses);
+  listOfCourses: computed('currentUser.relatedCourses.[]', function(){
+    let promise = new Ember.RSVP.Promise(resolve => {
+      this.get('currentUser').get('relatedCourses').then(courses => {
+        let filteredCourses = courses.filter(course=>!course.get('locked') && !course.get('archived'));
+        resolve(filteredCourses);
       });
     });
     return DS.PromiseArray.create({
-      promise: defer.promise
+      promise: promise
     });
   }),
 });
