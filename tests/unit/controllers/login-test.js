44bf0cd4d7730a526a6f0e78db0ebbef31e2254c
Thomas Lee <djvoa12@gmail.com>
2015-11-23 15:18:37 -0800

    final changes

--- tests/unit/controllers/login-test.js
+++ tests/unit/controllers/login-test.js
@@ -5,9 +5,3 @@ moduleFor('controller:login', 'Unit | Controller | Login ' + testgroup, {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
-
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var controller = this.subject();
-  assert.ok(controller);
-});
