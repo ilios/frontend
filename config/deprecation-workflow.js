469020facc23dee320cbf7bb94e57230d75f9205
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-11-25 13:07:13 -0800

    Add deprecation busting methods to application adapter
    Specifying the ember-data 2.x defaults for these functions clears up deprecation messages and prepare us to update.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,9 +3,6 @@ window.deprecationWorkflow.config = {
   workflow: [
     //this is thrown by ember simple auth which wont be fixed until 1.0
     { handler: "silence", matchMessage: "`lookup` was called on a Registry. The `initializer` API no longer receives a container, and you should use an `instanceInitializer` to look up objects from the container." },
-    { handler: "silence", matchMessage:  /You modified [a-z\(\)\.\s]+ twice in a single render/i},
-    { handler: "silence", matchMessage: "The default behavior of `shouldBackgroundReloadRecord` will change in Ember Data 2.0 to always return true. If you would like to preserve the current behavior please override `shouldBackgroundReloadRecord` in your adapter:application and return false." },
-    { handler: "silence", matchMessage: /The default behavior of shouldReloadAll will change in Ember Data 2.0 to always return false when there is at least one ["a-z\-]+ record in the store. If you would like to preserve the current behavior please override shouldReloadAll in your adapter:application and return true./ },
     { handler: "silence", matchMessage: /A property (.*) was modified inside the didInsertElement hook. You should never change properties on components, services or models during didInsertElement because it causes significant performance degradation./ },
     { handler: "silence", matchMessage: "this.append() is deprecated. Please use this.render() or this.$() instead." },
     { handler: "silence", matchMessage: "Using the same function as getter and setter is deprecated." },
@@ -14,5 +11,6 @@ window.deprecationWorkflow.config = {
     { handler: "silence", matchMessage: "Using Ember.HTMLBars._registerHelper is deprecated. Helpers (even dashless ones) are automatically resolved."},
     { handler: "silence", matchMessage: "Using `Ember.HTMLBars.makeBoundHelper` is deprecated. Please refactor to using `Ember.Helper` or `Ember.Helper.helper`."},
     { handler: "silence", matchMessage: /Depending on arrays using a dependent key/},
+    { handler: "silence", matchId: "ember-uploader.files-attr"},
   ]
 };
