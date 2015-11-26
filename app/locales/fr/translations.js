f9c096baed31a4f9a720192c34e98ed58b267935
sascha benjamin cohen <sascha.cohen@ucsf.edu>
2015-11-25 19:29:10 -0800

    Update translations.js

--- app/locales/fr/translations.js
+++ app/locales/fr/translations.js
@@ -111,7 +111,7 @@ export default {
     'transitionErrorMessage': "Désolé, nous ne pouvons pas vous faire demander où vous aller : quelque chose semble être cassé.",
     'errorDisplayMessage': "Désolé! Quelque chose s'est passé mal avec votre demande. Essayez S'il vous plaît de rafraîchir ou vérifier en arrière plus tard.",
     'backToDashboard': "Retour à tableau de bord",
-    'showMore': "Afficher plus'
+    'showMore': "Afficher plus",
   },
   'programs': {
     'programTitle': "Titre de Diplôme",
