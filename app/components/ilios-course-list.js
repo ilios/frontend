41c5f2ebc850449e3a04817a7cce6ece023490ca
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-12-02 00:13:45 -0800

    Limit delete course
    Only show course delete option to users with that privilege:
    - Developer
    - Director in that course

--- app/components/ilios-course-list.js
+++ app/components/ilios-course-list.js
@@ -1,15 +1,45 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
-export default Ember.Component.extend({
+const { computed, inject, RSVP, ObjectProxy, Component } = Ember;
+const { service } = inject;
+const { PromiseObject } = DS;
+
+const CourseProxy = ObjectProxy.extend({
+  content: null,
+  currentUser: null,
+  showRemoveConfirmation: false,
+  userCanDelete: computed('content', 'currentUser.model.directedCourses.[]', function(){
+    let defer = RSVP.defer();
+    const course = this.get('content');
+    this.get('currentUser.userIsDeveloper').then(isDeveloper => {
+      if(isDeveloper){
+        defer.resolve(true);
+      } else {
+        this.get('currentUser.model').then(user => {
+          user.get('directedCourses').then(directedCourses => {
+            defer.resolve(directedCourses.contains(course));
+          });
+        });
+      }
+    });
+    
+    return PromiseObject.create({
+      promise: defer.promise
+    });
+  })
+});
+export default Component.extend({
+  currentUser: service(),
   courses: [],
-  proxiedCourses: function(){
-    return this.get('courses').map(function(course){
-      return Ember.ObjectProxy.create({
+  proxiedCourses: computed('courses.[]', function(){
+    return this.get('courses').map(course => {
+      return CourseProxy.create({
         content: course,
-        showRemoveConfirmation: false
+        currentUser: this.get('currentUser')
       });
     });
-  }.property('courses.@each'),
+  }),
   actions: {
     edit: function(courseProxy){
       this.sendAction('edit', courseProxy.get('content'));
