4e6ab6a543541c63c294397f4b69a421ff085882
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-12-03 14:55:37 -0800

    Update ember-cli-deploy
    Updated to v 0.5.1 including plugins.  This version doesn’t limit us to
    10 deploys and has many other major improvements.  As part of this move
    I also consolidated our assets into a single bucket so it is easy to
    CDN and versioned the index files by API version.

--- config/deploy.js
+++ config/deploy.js
@@ -1,31 +1,38 @@
 /* jshint node: true */
 
-module.exports = {
-  production: {
-    store: {
-      type: "S3",
-      bucket: "iliosindex",
-      acl: 'public-read',
-      indexMode: "indirect",
-    },
-    assets: {
-      type: "s3",
-      bucket: "ilioscdn",
-      exclude: ['.DS_Store', '*-test.js']
-    }
-  },
-  staging: {
-    buildEnv: 'staging',
-    store: {
-      type: "S3",
-      bucket: "dev-iliosindex",
-      acl: 'public-read',
-      indexMode: "indirect",
-    },
-    assets: {
-      type: "s3",
-      bucket: "dev-ilioscdn",
-      exclude: ['.DS_Store', '*-test.js']
-    }
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {},
+    exclude: ['.DS_Store', '*-test.js']
+    // include other plugin configuration that applies to all deploy targets here
+  };
+  
+  ENV.s3 = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    acl: 'public-read',
+    region: 'us-west-2',
+    bucket: 'ilios-frontend-assets'
+  };
+  
+  ENV['s3-index'] = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'us-west-2'
+  };
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    ENV['s3-index'].bucket = 'frontend-apiv1.0-index-stage';
   }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    ENV['s3-index'].bucket = 'frontend-apiv1.0-index-prod';
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
 };
