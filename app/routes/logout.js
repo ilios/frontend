d89c845eda9693f91cb25ec44b334f52f44daa28
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-12-02 20:27:49 -0800

    Another try at working shibboleth logout
    This way should both remove the users session and log the user out of
    shibboleth.

--- app/routes/logout.js
+++ app/routes/logout.js
@@ -1,26 +1,10 @@
 import Ember from 'ember';
-import ajax from 'ic-ajax';
-import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
-const { inject } = Ember;
-const { service } = inject;
-
-export default Ember.Route.extend(UnauthenticatedRouteMixin, {
-  flashMessages: service(),
+export default Ember.Route.extend({
   beforeModel(){
-    let logoutUrl = '/auth/logout';
-    return ajax(logoutUrl).then(response => {
-      const session = this.get('session');
-      if(session.isAuthenticated){
-        session.invalidate();
-      }
-      if(response.status === 'redirect'){
-        window.location.replace(response.logoutUrl);
-      } else {
-        this.get('flashMessages').success('auth.confirmLogout');
-        this.transitionTo('login');
-      }
-      return false;
-    });
+    const session = this.get('session');
+    if(session.isAuthenticated){
+      return session.invalidate();
+    }
   }
 });
