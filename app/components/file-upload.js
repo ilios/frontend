e7bda7cf32de44c91f92bf472e5506a9b43febb5
Thomas Lee <djvoa12@gmail.com>
2015-12-08 15:06:44 -0800

    removed file-upload deprecation

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -1,25 +1,21 @@
 import Ember from 'ember';
 import EmberUploader from 'ember-uploader';
 
-const { observer } = Ember;
-const { FileField } = EmberUploader;
+const { FileField, Uploader } = EmberUploader;
 
 export default FileField.extend({
   url: '',
-  filesDidChange: observer('files', function() {
-    var uploadUrl = this.get('url');
-    var files = this.get('files');
 
-    var uploader = EmberUploader.Uploader.create({
-      url: uploadUrl
-    });
+  filesDidChange(files) {
+    const uploadUrl = this.get('url');
+    const uploader = Uploader.create({ url: uploadUrl });
 
-    uploader.on('didUpload', e=> {
+    uploader.on('didUpload', (e) => {
       this.sendAction('finishedUploading', e);
     });
 
     if (!Ember.isEmpty(files)) {
       uploader.upload(files[0]);
     }
-  })
+  }
 });
