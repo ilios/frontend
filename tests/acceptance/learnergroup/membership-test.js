092fee635a91a25a2ce580497572d6cf0f24a819
Thomas Lee <djvoa12@gmail.com>
2015-12-10 19:01:26 -0800

    commented out toggle for release

--- tests/acceptance/learnergroup/membership-test.js
+++ tests/acceptance/learnergroup/membership-test.js
@@ -1,8 +1,5 @@
 import destroyApp from '../../helpers/destroy-app';
-import {
-  module,
-  test
-} from 'qunit';
+import { module, skip, test } from 'qunit';
 import startApp from 'ilios/tests/helpers/start-app';
 import {b as testgroup} from 'ilios/tests/helpers/test-groups';
 
@@ -207,7 +204,7 @@ test('remove group member back to cohort', function(assert) {
   });
 });
 
-test('multi-edit save (bulk-saving) works properly and knows when to trigger', function(assert) {
+skip('multi-edit save (bulk-saving) works properly and knows when to trigger', function(assert) {
   const toggleSwitch = '.switch-label';
   const checkAllBox = '.check-all-input';
   const selectInputField = '.ff-select-field';
