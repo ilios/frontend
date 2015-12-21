6a4390f2cd0731a55bc6f65745ca16156fa74a94
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-12-20 16:58:17 -0800

    Fix ilm-session serializer test dependancies

--- tests/unit/serializers/ilm-session-test.js
+++ tests/unit/serializers/ilm-session-test.js
@@ -2,7 +2,13 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('ilm-session', 'Unit | Serializer | ilm session', {
   // Specify the other units that are required for this test.
-  needs: ['serializer:ilm-session']
+  needs: [
+    'model:session',
+    'model:learner-group',
+    'model:instructor-group',
+    'model:user',
+    'serializer:ilm-session'
+  ]
 });
 
 // Replace this with your real tests.
