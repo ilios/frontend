ee9bafc127c84dad9b11f3f97d5c6f13a7f76732
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-01-12 15:50:55 -0800

    Send authorization headers with upload requests
    Our new simple auth library doesn’t do this automatically so we have to
    hand it on our own.

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -2,13 +2,38 @@ import Ember from 'ember';
 import EmberUploader from 'ember-uploader';
 
 const { FileField, Uploader } = EmberUploader;
+const { inject, computed } = Ember;
+const { service } = inject;
+
+let IliosUploader = Uploader.extend({
+  iliosHeaders: [],
+  ajaxSettings: function() {
+    let settings = this._super(...arguments);
+    settings.headers = this.get('iliosHeaders');
+    
+    return settings;
+  }
+  
+});
 
 export default FileField.extend({
+  session: service(),
   url: '',
+  headers: computed('session.isAuthenticated', function(){
+    let headers = {};
+    this.get('session').authorize('authorizer:token', (headerName, headerValue) => {
+      headers[headerName] = headerValue;
+    });
+    
+    return headers;
+  }),
 
   filesDidChange(files) {
     const uploadUrl = this.get('url');
-    const uploader = Uploader.create({ url: uploadUrl });
+    const uploader = IliosUploader.create({
+      url: uploadUrl,
+      iliosHeaders: this.get('headers')
+    });
     
     this.sendAction('startUploading');
     
