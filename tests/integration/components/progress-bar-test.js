35bab25654c270c1a7112eedf9cb6049f7c0d39e
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-01-18 10:08:59 -0800

    Add tests for progress-bar component

--- tests/integration/components/progress-bar-test.js
+++ tests/integration/components/progress-bar-test.js
@@ -5,21 +5,33 @@ moduleForComponent('progress-bar', 'Integration | Component | progress bar', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
-
+test('it renders at default 0%', function(assert) {
   this.render(hbs`{{progress-bar}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), '0%');
+});
 
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#progress-bar}}
-      template block text
-    {{/progress-bar}}
-  `);
+test('changing percentage changes width', function(assert) {
+  
+  this.set('passedValue', 42);
+  
+  this.render(hbs`{{progress-bar percentage=passedValue}}`);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.meter').attr('style').trim(), 'width: 42%');
+  
+  this.set('passedValue', 12);
+  assert.equal(this.$('.meter').attr('style').trim(), 'width: 12%');
+});
+
+test('changing percentage changes the displayvalue', function(assert) {
+  
+  this.set('passedValue', 42);
+  
+  this.render(hbs`{{progress-bar percentage=passedValue}}`);
+
+  assert.equal(this.$().text().trim(), '42%');
+  
+  this.set('passedValue', 11);
+  assert.equal(this.$().text().trim(), '11%');
+  
 });
