b3c76a227888b808f46d244e109ec23e9bd50c1d
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-02-01 23:45:10 -0800

    Add collapsed state for session objective details

--- app/controllers/session/index.js
+++ app/controllers/session/index.js
@@ -4,6 +4,21 @@ const { Controller, inject } = Ember;
 const { controller } = inject;
 
 export default Controller.extend({
+  queryParams: ['sessionObjectiveDetails'],
+  sessionObjectiveDetails: false,
+
   sessionController: controller('session'),
-  courseController: controller('course')
+  courseController: controller('course'),
+
+  actions: {
+    setSessionOffset(offset){
+      this.set('sessionOffset', offset);
+    },
+    setSessionLimit(limit){
+      this.set('sessionLimit', limit);
+    },
+    toggleSessionObjectiveDetails() {
+      this.set('sessionObjectiveDetails', !this.get('sessionObjectiveDetails'));
+    }
+  }
 });
