92d7ea98a20cafcd97bb9827f3ab10db9cde8f2d
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-02-04 00:29:56 -0800

    Move inflector setup to an initializer
    Wasn’t working any more in app.js, maybe a bug

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-Ember.Inflector.inflector.uncountable('aamc-pcrs');
 
 let App;
 
