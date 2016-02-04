8c7111abb5b383eb7df54bd782c91f8baaf924ac
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-02-04 00:30:27 -0800

    Add Ember 2.3.0 deprecations to workflow

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,5 +1,10 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
+    { handler: "silence", matchId: "ember-resolver.legacy-shims"},
+    { handler: "silence", matchId: "ember-application.app-initializer-initialize-arguments"},
+    { handler: "silence", matchId: "ember-application.injected-container"},
+    { handler: "silence", matchId: "ember-application.app-instance-container"},
+    { handler: "silence", matchId: "ember-views.render-double-modify"},
   ]
 };
