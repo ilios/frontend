49128c4ff99b7d7497b4c3baaea31218203401b5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-02-17 11:03:18 -0800

    added intersection-count helper.

---
+++ app/helpers/intersection-count.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+const { isEmpty, isArray, Helper, observer } = Ember;
+
+export function intersectionCount([a, b]/*, hash*/) {
+  if (! isArray(a) || ! isArray(b) || isEmpty(a) || isEmpty(b)) {
+    return 0;
+  }
+
+  if (a.length > b.length) {
+    let tmp = a;
+    a = b;
+    b = tmp;
+  }
+
+  let count = 0;
+  a.forEach((item) => {
+    if (b.contains(item)) {
+      count++;
+    }
+  });
+
+  return count;
+}
+
+export default Helper.extend({
+  a: [],
+  b: [],
+
+  compute([a, b]) {
+    this.set('a', a);
+    this.set('b', b);
+    return intersectionCount([a, b]);
+  },
+
+  recomputeOnArrayChange: observer('a.[]', 'b.[]', function() {
+    this.recompute();
+  })
+});
---
+++ tests/unit/helpers/intersection-count-test.js
@@ -0,0 +1,10 @@
+import { intersectionCount } from '../../../helpers/intersection-count';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | intersection count');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = intersectionCount([42]);
+  assert.ok(result);
+});
