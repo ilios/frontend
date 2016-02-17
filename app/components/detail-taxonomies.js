f574b7253d9b44b5906800217e5c1acb26787e7a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-02-17 11:50:56 -0800

    fleshing out taxo details component

--- app/components/detail-taxonomies.js
+++ app/components/detail-taxonomies.js
@@ -1,4 +1,37 @@
 import Ember from 'ember';
+import scrollTo from '../utils/scroll-to';
+import config from 'ilios/config/environment';
 
-export default Ember.Component.extend({
+const { Component, computed, inject } = Ember;
+const { service } = inject;
+const { or, notEmpty, alias } = computed;
+
+export default Component.extend({
+    store: service(),
+    i18n: service(),
+    flashMessages: service(),
+    subject: null,
+    terms:  alias('subject.terms'),
+    classNames: ['detail-taxonomies'],
+    isCourse: false,
+    isSession: false,
+    isProgramYear: false,
+    isManaging: false,
+    editable: true,
+
+    showCollapsible: computed('isManaging', 'terms', function(){
+        const isManaging = this.get('isManaging');
+        const terms = this.get('terms');
+        return terms.get('length') && ! isManaging;
+    }),
+
+    actions: {
+        collapse(){
+            this.get('terms').then(terms => {
+                if(terms.length){
+                    this.attrs.collapse();
+                }
+            });
+        },
+    }
 });
