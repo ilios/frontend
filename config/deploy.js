223679a7a6d08ce03d5d4460cdf96ab4d5627559
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-02-16 19:42:51 -0800

    Working redis deploy strategy
    Currently only setup for development on localhost.

--- config/deploy.js
+++ config/deploy.js
@@ -10,7 +10,18 @@ module.exports = function(deployTarget) {
   ENV.s3 = {
     acl: 'public-read',
     region: 'us-west-2',
-    bucket: 'ilios-frontend-assets'
+    // bucket: 'ilios-frontend-assets'
+  };
+
+  ENV.redis = {
+    // host: '<your-redis-host>',
+    // port: '<your-redis-port>',
+    // password: '<your-redis-password>',
+    filePattern: 'index.json'
+  };
+
+  ENV.gzip = {
+    filePattern: '**/*.{js,css,ico,map,xml,txt,svg,eot,ttf,woff,woff2}'
   };
 
   if (deployTarget === 'staging') {
@@ -19,6 +30,14 @@ module.exports = function(deployTarget) {
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
+  }
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'production';
+    ENV.s3.region = 'us-west-1';
+    ENV.s3.bucket = 'dev-ilioscdn';
+    ENV.redis.host = '192.168.99.100';
+    ENV.redis.allowOverwrite = true;
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
