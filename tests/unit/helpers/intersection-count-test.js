c6e6792a4dcc4fb15e8176ce36cf14e7232feb48
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-02-17 11:18:22 -0800

    fleshed out test coverage for intersection-count helper.

--- tests/unit/helpers/intersection-count-test.js
+++ tests/unit/helpers/intersection-count-test.js
@@ -1,10 +1,26 @@
 import { intersectionCount } from '../../../helpers/intersection-count';
 import { module, test } from 'qunit';
+import {a as testgroup} from 'ilios/tests/helpers/test-groups';
 
-module('Unit | Helper | intersection count');
+module('Unit | Helper | intersection count + testgroup');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = intersectionCount([42]);
-  assert.ok(result);
+test('correctly calculates intersection count between two arrays', function(assert) {
+  let result = intersectionCount([[42], [42]]);
+  assert.equal(result, 1);
+  result = intersectionCount([[3, 2, 1], [2, 3]]);
+  assert.equal(result, 2);
+  result = intersectionCount([[2, 3], [3, 2, 1]]);
+  assert.equal(result, 2);
+  result = intersectionCount([[], [1, 2, 3]]);
+  assert.equal(result, 0);
+  result = intersectionCount(['not an array', [1, 2]]);
+  assert.equal(result, 0);
+
+  let objA = {}, objB = {}, objC = 'a', objD = [];
+  result = intersectionCount([[objA], [objA]]);
+  assert.equal(result, 1);
+  result = intersectionCount([[objA], [objB]]);
+  assert.equal(result, 0);
+  result = intersectionCount([[objA, objD], [objA, objB, objC, objD]]);
+  assert.equal(result, 2);
 });
