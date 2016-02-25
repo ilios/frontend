c67ce58400a927e5c8a264121a8d54b226b64f29
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-02-24 18:27:19 -0800

    sorting and display improvements.

--- app/components/selectable-terms-list.js
+++ app/components/selectable-terms-list.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+const { sort } = computed;
+
 export default Ember.Component.extend({
   classNames: ['selectable-terms-list'],
   tagName: 'ul',
   selectedTerms: [],
   terms: [],
+  sortOrder: ['title'],
+  sortedTerms: sort('terms', 'sortOrder'),
   actions: {
     add: function(term) {
       this.sendAction('add', term);
