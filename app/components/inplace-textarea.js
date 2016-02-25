9b600e5e6b9c56a4594498d8b474a70e9b5d5d25
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-02-25 13:53:04 -0800

    fixed inplace texarea component.

--- app/components/inplace-textarea.js
+++ app/components/inplace-textarea.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 import InPlaceValidation from 'ilios/mixins/inplace-validation';
+import ValidationError from 'ilios/mixins/validation-error';
+import EmberValidations from 'ember-validations';
 
 const { Component, isBlank } = Ember;
 
-export default Component.extend(InPlaceValidation, {
+export default Component.extend(InPlaceValidation, EmberValidations, ValidationError, {
   classNames: ['editinplace', 'inplace-textarea'],
 
   init() {
