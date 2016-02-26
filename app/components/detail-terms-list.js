ec248f712abca42bac488529258edbcaf4339fa5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-02-26 10:33:11 -0800

    selected terms list is finally sorting tags on title with parent titles.

--- app/components/detail-terms-list.js
+++ app/components/detail-terms-list.js
@@ -1,20 +1,86 @@
 import Ember from 'ember';
 
 const { Component, computed } = Ember;
-const { sort } = computed;
 
+/**
+ * Displays all given terms that belong to a given vocabulary as a list of tags.
+ */
 export default Component.extend({
-  canEdit: false,
-  vocabulary: null,
-  terms: null,
+
   classNames: ['detail-taxonomies'],
 
-  termsSorting: [
-    //'titleWithParentTitles.content', // @todo does not work, sorting on 'title instead. Revisit [ST 2016/02/19]
-    'title',
-  ],
-  sortedTerms: sort('filteredTerms', 'termsSorting'),
+  /**
+   * Flag indicating whether terms can be removed from the list or not.
+   *
+   * @property canEdit
+   * @type {boolean}
+   * @public
+   */
+  canEdit: false,
 
+  /**
+   * A vocabulary model.
+   *
+   * @property vocabulary
+   * @type {DS.Model}
+   */
+  vocabulary: null,
+
+  /**
+   * A list of term models.
+   *
+   * @property terms
+   * @type {Array}
+   * @public
+   */
+  terms: [],
+
+  /**
+   * A sorted list of the filtered terms.
+   * Terms are sorted by title including parent titles.
+   *
+   * @property sortedTerms
+   * @type {Ember.computed}
+   * @public
+   */
+  sortedTerms: computed('filteredTerms.[]', function() {
+    let terms = this.get('filteredTerms');
+    let promises = [];
+    let temp = [];
+    terms.forEach(term => {
+      let promise = new Ember.RSVP.Promise(resolve => {
+        term.get('titleWithParentTitles').then(title => {
+          temp.pushObject({
+            'term': term,
+            'title': title
+          });
+          resolve();
+        });
+      });
+      promises.pushObject(promise);
+    });
+
+    let deferred = Ember.RSVP.defer();
+    Ember.RSVP.all(promises).then(() => {
+      let sorted = temp.sort(function(a, b) {
+        return (a.title > b.title ? 1 : (a.title < b.title ? -1 : 0));
+      });
+      deferred.resolve(sorted.mapBy('term'));
+    });
+
+    return DS.PromiseArray.create({
+      promise: deferred.promise
+    });
+  }),
+
+  /**
+   * A filtered list of the given terms.
+   * Terms are filtered by the given vocabulary.
+   *
+   * @property filteredTerms
+   * @type {Ember.computed}
+   * @public
+   */
   filteredTerms: computed('terms.[]', 'vocabulary', function () {
     let terms = this.get('terms');
     let vocab = this.get('vocabulary');
@@ -29,7 +95,9 @@ export default Component.extend({
 
   actions: {
     remove: function (term) {
-      this.sendAction('remove', term);
+      if (this.get('canEdit')) {
+        this.sendAction('remove', term);
+      }
     }
   }
 });
