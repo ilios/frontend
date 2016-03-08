42f7ff538dc21a06f0484a0c7f4df838c2505e2a
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-03-07 23:12:16 -0800

    Basic school list test

--- tests/integration/components/school-list-test.js
+++ tests/integration/components/school-list-test.js
@@ -1,15 +1,25 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import startMirage from '../../helpers/start-mirage';
+import Ember from 'ember';
+
+const { Object } = Ember;
 
 moduleForComponent('school-list', 'Integration | Component | school list', {
-  integration: true
+  integration: true,
+  setup(){
+    startMirage(this.container);
+  }
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });"
+  let school1 = server.create('school');
+  let school2 = server.create('school');
 
-  this.render(hbs`{{school-list}}`);
+  const schools = [school1, school2].map(obj => Object.create(obj));
 
-  assert.equal(this.$().text().trim(), '');
+  this.set('schools', schools);
+  this.render(hbs`{{school-list schools=schools}}`);
+  assert.equal(this.$('tr:eq(1) td:eq(0)').text().trim(), 'school 0');
+  assert.equal(this.$('tr:eq(2) td:eq(0)').text().trim(), 'school 1');
 });
