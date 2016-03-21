c57b10cc0a28164d3458fd1eda21519a48a77e3b
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-03-20 23:25:01 -0700

    Use a validation error display mixin to handle common tasks
    Standard API for components to display validation errors.

---
+++ app/mixins/validation-error-display.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+const { Mixin } = Ember;
+
+export default Mixin.create({
+  didReceiveAttrs(){
+    this._super(...arguments);
+    this.set('showErrorsFor', []);
+  },
+  showErrorsFor: [],
+  actions: {
+    addErrorDisplayFor(field){
+      this.get('showErrorsFor').pushObject(field);
+    },
+    removeErrorDisplayFor(field){
+      this.get('showErrorsFor').removeObject(field);
+    },
+  }
+});
