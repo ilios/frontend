03787fb00a3a787a100ba8644a16281ad3cc5ae1
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-03-21 00:52:21 -0700

    Replace validation and inplace-text in course-header

--- app/components/course-header.js
+++ app/components/course-header.js
@@ -1,29 +1,57 @@
 import Ember from 'ember';
 import Publishable from 'ilios/mixins/publishable';
+import { validator, buildValidations } from 'ember-cp-validations';
+import ValidationErrorDisplay from 'ilios/mixins/validation-error-display';
 
-const { Component, computed } = Ember;
+const { Component, computed, RSVP } = Ember;
 const { alias, not } = computed;
+const { Promise } = RSVP;
 
-export default Component.extend(Publishable, {
+const Validations = buildValidations({
+  courseTitle: [
+    validator('presence', true),
+    validator('length', {
+      min: 3,
+      max: 200
+    }),
+  ],
+});
+
+export default Component.extend(Validations, Publishable, ValidationErrorDisplay, {
+  didReceiveAttrs(){
+    this._super(...arguments);
+    this.set('courseTitle', this.get('course.title'));
+  },
   course: null,
-
+  courseTitle: null,
   publishTarget: alias('course'),
 
   editable: not('course.locked'),
 
-  titleValidations: {
-    'validationBuffer': {
-      presence: true,
-      length: { minimum: 3, maximum: 200 }
-    }
-  },
-
   actions: {
-    changeTitle(newTitle) {
+    changeTitle() {
       const course = this.get('course');
-
-      course.set('title', newTitle);
-      course.save();
-    }
+      const newTitle = this.get('courseTitle');
+      this.send('addErrorDisplayFor', 'courseTitle');
+      return new Promise((resolve, reject) => {
+        this.validate().then(({validations}) => {
+          if (validations.get('isValid')) {
+            this.send('removeErrorDisplayFor', 'courseTitle');
+            course.set('title', newTitle);
+            course.save().then((newCourse) => {
+              this.set('courseTitle', newCourse.get('title'));
+              this.set('course', newCourse);
+              resolve();
+            });
+          } else {
+            reject();
+          }
+        });
+      });
+    },
+    revertTitleChanges(){
+      const course = this.get('course');
+      this.set('courseTitle', course.get('title'));
+    },
   }
 });
