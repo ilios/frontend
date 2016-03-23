6f5d250389d38596ae5d8fd951b4951c093a27a8
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-03-23 10:17:12 -0700

    replaced validation handling in session overview component.

--- app/components/session-overview.js
+++ app/components/session-overview.js
@@ -1,12 +1,39 @@
 import moment from 'moment';
 import Ember from 'ember';
+import DS from 'ember-data';
 import Publishable from 'ilios/mixins/publishable';
+import { validator, buildValidations } from 'ember-cp-validations';
+import ValidationErrorDisplay from 'ilios/mixins/validation-error-display';
 
-const { Component, computed } = Ember;
+const { Component, computed, RSVP } = Ember;
 const { oneWay, sort } = computed;
+const { Promise } = RSVP;
 
-export default Component.extend(Publishable, {
+const Validations = buildValidations({
+  title: [
+    validator('presence', true),
+    validator('length', {
+      min: 3,
+      max: 200
+    }),
+  ],
+  hours: [
+    validator('presence', true),
+    validator('number', {
+      allowString: true,
+      integer: true,
+      gt: 0
+    }),
+  ],
+});
+
+export default Component.extend(Publishable, Validations, ValidationErrorDisplay, {
+  didReceiveAttrs(){
+    this._super(...arguments);
+    this.set('title', this.get('session.title'));
+  },
   session: null,
+  title: null,
   publishTarget: oneWay('session'),
   editable: true,
   classNames: ['session-overview'],
@@ -14,20 +41,7 @@ export default Component.extend(Publishable, {
   sessionTypes: [],
   sortedSessionTypes: sort('sessionTypes', 'sortTypes'),
   showCheckLink: true,
-
-  titleValidations: {
-    'validationBuffer': {
-      presence: true,
-      length: { minimum: 3, maximum: 200 }
-    }
-  },
-
-  hoursValidations: {
-    'validationBuffer': {
-      presence: true,
-      numericality: { greaterThan: 0 }
-    }
-  },
+  isSaving: false,
 
   actions: {
     saveIndependentLearning: function(value){
@@ -51,10 +65,31 @@ export default Component.extend(Publishable, {
         });
       }
     },
-    changeTitle: function(value){
-      this.get('session').set('title', value);
-      this.get('session').save();
+    changeTitle(){
+      const title = this.get('title');
+      const session = this.get('session');
+      this.send('addErrorDisplayFor', 'title');
+      return new Promise((resolve, reject) => {
+        this.validate({ on: ['title'] }).then(({validations}) => {
+          if (validations.get('isValid')) {
+            this.send('removeErrorDisplayFor', 'title');
+            session.set('title', title);
+            session.save().then((newSession) => {
+              this.set('title', newSession.get('title'));
+              this.set('session', newSession);
+              resolve();
+            });
+          } else {
+            reject();
+          }
+        });
+      });
     },
+    revertTitleChanges(){
+      const session = this.get('session');
+      this.set('title', session.get('title'));
+    },
+
     changeSessionType: function(newId){
       var session = this.get('session');
       var type = this.get('sessionTypes').findBy('id', newId);
