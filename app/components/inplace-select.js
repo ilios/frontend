9534ae5d5cb67a7ead6b3cc1dd4db5b6b375ef56
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-03-23 19:41:37 -0700

    Add i18n service to inplace-select since it is needed

--- app/components/inplace-select.js
+++ app/components/inplace-select.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 import InPlace from 'ilios/mixins/inplace';
 
-const { Component, computed, ObjectProxy } = Ember;
+const { Component, computed, ObjectProxy, inject } = Ember;
 const { notEmpty } = computed;
+const { service } = inject;
 
 export default Component.extend(InPlace, {
+  i18n: service(),
   classNames: ['editinplace', 'inplace-select'],
 
   selectPromptTranslation: null,
