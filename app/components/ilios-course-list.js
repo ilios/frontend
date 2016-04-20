0b617c6f818c00071fecd82153eb6307eff8a708
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-04-20 10:28:56 -0700

    moved check for user course to useCanDelete comp prop.

--- app/components/ilios-course-list.js
+++ app/components/ilios-course-list.js
@@ -6,38 +6,47 @@ const { service } = inject;
 const { PromiseObject } = DS;
 
 const CourseProxy = ObjectProxy.extend({
-  userCoursesOnly: false,
+  isUserCourse: false,
   content: null,
   currentUser: null,
   showRemoveConfirmation: false,
-  userCanDelete: computed('content', 'currentUser.model.directedCourses.[]', function(){
+
+  userCanDelete: computed('isUserCourse', 'content', 'currentUser.model.directedCourses.[]', function(){
     let defer = RSVP.defer();
-    const course = this.get('content');
-    this.get('currentUser.userIsDeveloper').then(isDeveloper => {
-      if(isDeveloper){
-        defer.resolve(true);
-      } else {
-        this.get('currentUser.model').then(user => {
-          user.get('directedCourses').then(directedCourses => {
-            defer.resolve(directedCourses.contains(course));
+
+    if (this.get('isUserCourse')) {
+      defer.resolve(false);
+    } else {
+      const course = this.get('content');
+      this.get('currentUser.userIsDeveloper').then(isDeveloper => {
+        if (isDeveloper) {
+          defer.resolve(true);
+        } else {
+          this.get('currentUser.model').then(user => {
+            user.get('directedCourses').then(directedCourses => {
+              defer.resolve(directedCourses.contains(course));
+            });
           });
-        });
-      }
-    });
+        }
+      });
+    }
 
     return PromiseObject.create({
       promise: defer.promise
     });
   })
 });
+
 export default Component.extend({
+  userCoursesOnly: false,
   currentUser: service(),
   courses: [],
   proxiedCourses: computed('courses.[]', function(){
     return this.get('courses').map(course => {
       return CourseProxy.create({
         content: course,
-        currentUser: this.get('currentUser')
+        currentUser: this.get('currentUser'),
+        isUserCourse: this.get('userCoursesOnly'),
       });
     });
   }),
