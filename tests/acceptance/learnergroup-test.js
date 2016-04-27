d6901dbfec83ee4d3cfdd9be09389cba7cfcb6cd
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-04-26 21:44:26 -0700

    Re-add test for generating multiple learner groups
    This is way too hard to do as an integration test

---
+++ tests/acceptance/learnergroup-test.js
@@ -0,0 +1,79 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'ilios/tests/helpers/module-for-acceptance';
+import startApp from 'ilios/tests/helpers/start-app';
+import destroyApp from '../helpers/destroy-app';
+import setupAuthentication from 'ilios/tests/helpers/setup-authentication';
+
+let application;
+
+moduleForAcceptance('Acceptance | learnergroup', {
+  beforeEach: function() {
+    application = startApp();
+    setupAuthentication(application);
+  },
+
+  afterEach: function() {
+    destroyApp(application);
+  }
+});
+
+
+test('generate new subgroups', function(assert) {
+
+  server.create('learnerGroup', {
+    cohort: 1,
+    parent: 1,
+    children: [2,3],
+  });
+  server.create('learnerGroup', {
+    cohort: 1,
+    parent: 1,
+    children: [4,5],
+    users: [2,3]
+  });
+  server.create('learnerGroup', {
+    cohort: 1,
+    parent: 1
+  });
+  server.createList('learnerGroup', 2, {
+    cohort: 1,
+    parent: 2
+  });
+
+  assert.expect(11);
+
+  const expandButton = '.expand-button';
+  const input = '.new-learnergroup input';
+  const done = '.new-learnergroup .done';
+  const multiGroupsButton = '.second-button';
+  const parentLearnergroupTitle = 'learner group 0';
+
+  visit('/learnergroups/1');
+  // add five subgroups
+  click(expandButton);
+  click(multiGroupsButton);
+  fillIn(input, '5');
+  click(done);
+  function getCellData(row, cell) {
+    return find(`.resultslist-list tbody tr:eq(${row}) td:eq(${cell})`).text().trim();
+  }
+  andThen(() => {
+    assert.equal(find('.resultslist-list tbody tr').length, 7, 'all subgroups are displayed.');
+    for (let i = 0; i < 5; i++) {
+      assert.equal(getCellData(i, 0), `${parentLearnergroupTitle} ${i + 1}`, 'new learnergroup title is ok.');
+    }
+    assert.equal(getCellData(5, 0), 'learner group 1');
+    assert.equal(getCellData(6, 0), 'learner group 2');
+
+    // add two more subgroups
+    click(expandButton);
+    click(multiGroupsButton);
+    fillIn(input, '2');
+    click(done);
+    andThen(() => {
+      assert.equal(find('.resultslist-list tbody tr').length, 9, 'all subgroups are still displayed.');
+      assert.equal(getCellData(5, 0), `${parentLearnergroupTitle} 6`, 'consecutively new learnergroup title is ok.');
+      assert.equal(getCellData(6, 0), `${parentLearnergroupTitle} 7`, 'consecutively new learnergroup title is ok.');
+    });
+  });
+});
