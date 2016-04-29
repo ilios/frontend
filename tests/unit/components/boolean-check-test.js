50cbfd449efe610f969e4d683caf7b7645e7b573
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-04-29 10:02:52 -0700

    rm obsolete test.

--- tests/unit/components/boolean-check-test.js
+++ tests/unit/components/boolean-check-test.js
@@ -16,16 +16,3 @@ test('default properties are correct', function(assert) {
   assert.equal(tagName, 'span', 'span contains input helper');
   assert.equal(value, false, 'checkbox is unchecked by default');
 });
-
-test('`click` event triggers primary action', function(assert) {
-  assert.expect(1);
-
-  const component = this.subject();
-
-  component.click = () => {
-    assert.ok(true, 'click triggers primary action');
-  };
-
-  this.render();
-  component.click();
-});
