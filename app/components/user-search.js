ed10ac94148f4ee45d9c6014ff6d02f72554585a
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-05-04 23:36:24 -0700

    Update user-search component
    More idiomatic and better tested

--- app/components/user-search.js
+++ app/components/user-search.js
@@ -1,14 +1,35 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
 
 const { Component, computed } = Ember;
 const { notEmpty, oneWay } = computed;
 
+let userProxy = Ember.ObjectProxy.extend({
+  isUser: true,
+  currentlyActiveUsers: [],
+  isActive: computed('content', 'currentlyActiveUsers.[]', function(){
+    let user = this.get('content');
+    if(!user.get('enabled')){
+      return false;
+    }
+    return !this.get('currentlyActiveUsers').contains(user);
+  }),
+  sortTerm: computed('content.firstName', 'content.lastName', function(){
+    return this.get('content.lastName')+this.get('content.firstName');
+  }),
+});
+let instructorGroupProxy = Ember.ObjectProxy.extend({
+  isInstructorGroup: true,
+  currentlyActiveInstructorGroups: [],
+  isActive: computed('content', 'currentlyActiveInstructorGroups.[]', function(){
+    return !this.get('currentlyActiveInstructorGroups').contains(this.get('content'));
+  }),
+  sortTerm: oneWay('content.title'),
+});
+
 export default Component.extend({
   store: Ember.inject.service(),
   classNames: ['user-search'],
-  results: [],
-  searchTerms: null,
-  searching: false,
   showMoreInputPrompt: false,
   searchReturned: false,
   currentlyActiveUsers: [],
@@ -16,90 +37,55 @@ export default Component.extend({
   roles: '',
   availableInstructorGroups: [],
   currentlyActiveInstructorGroups: [],
-  currentlySearchingForTerm: false,
   integrateInstructorGroups: notEmpty('availableInstructorGroups'),
-  actions: {
-    search: function(searchTerms){
-      if(this.get('currentlySearchingForTerm') === searchTerms){
-        return;
-      }
-      this.set('currentlySearchingForTerm', searchTerms);
-      this.set('showMoreInputPrompt', false);
-      this.set('searchReturned', false);
-      let noWhiteSpaceTerm = searchTerms.replace(/ /g,'');
-      if(noWhiteSpaceTerm.length === 0){
-        this.set('results', []);
-        this.set('searching', false);
-        return;
-      } else if(noWhiteSpaceTerm.length < 3){
-        this.set('results', []);
-        this.set('showMoreInputPrompt', true);
-        return;
-      }
-      this.set('searching', true);
-      let userProxy = Ember.ObjectProxy.extend({
-        isUser: true,
-        currentlyActiveUsers: [],
-        isActive: computed('content', 'currentlyActiveUsers.[]', function(){
-          let user = this.get('content');
-          if(!user.get('enabled')){
-            return false;
-          }
-          return !this.get('currentlyActiveUsers').contains(user);
-        }),
-        sortTerm: computed('content.firstName', 'content.lastName', function(){
-          return this.get('content.lastName')+this.get('content.firstName');
-        }),
-      });
-      let instructorGroupProxy = Ember.ObjectProxy.extend({
-        isInstructorGroup: true,
-        currentlyActiveInstructorGroups: [],
-        isActive: computed('content', 'currentlyActiveInstructorGroups.[]', function(){
-          return !this.get('currentlyActiveInstructorGroups').contains(this.get('content'));
-        }),
-        sortTerm: oneWay('content.title'),
-      });
-      let query = {
-        q: searchTerms,
-        limit: 100
+  search: task(function * (searchTerms = '') {
+    this.set('showMoreInputPrompt', false);
+    this.set('searchReturned', false);
+    let noWhiteSpaceTerm = searchTerms.replace(/ /g,'');
+    if(noWhiteSpaceTerm.length === 0){
+      return [];
+    } else if(noWhiteSpaceTerm.length < 3){
+      this.set('showMoreInputPrompt', true);
+      return [];
+    }
+    let query = {
+      q: searchTerms,
+      limit: 100
+    };
+    if (this.get('roles')) {
+      query.filters = {
+        roles: this.get('roles').split(',')
       };
-      if (this.get('roles')) {
-        query.filters = {
-          roles: this.get('roles').split(',')
-        };
-      }
-      this.get('store').query('user', query).then(users => {
-        let results = users.uniq().map(user => {
-          return userProxy.create({
-            content: user,
-            currentlyActiveUsers: this.get('currentlyActiveUsers'),
-          });
-        });
-
-        if(this.get('integrateInstructorGroups')){
-          let exp = new RegExp(searchTerms, 'gi');
-
-          let filteredGroups = this.get('availableInstructorGroups').filter(group => {
-            return group.get('title') && group.get('title').match(exp);
-          });
-          let instructorGroupProxies = filteredGroups.map(group => {
-            return instructorGroupProxy.create({
-              content: group,
-              currentlyActiveInstructorGroups: this.get('currentlyActiveInstructorGroups'),
-            });
-          });
-
-          results.pushObjects(instructorGroupProxies);
-          results.sortBy('sortTerm');
-        }
-
-        this.set('results', results);
-        this.set('searching', false);
-        this.set('searchReturned', true);
+    }
+    let users = yield this.get('store').query('user', query);
+    let results = users.map(user => {
+      return userProxy.create({
+        content: user,
+        currentlyActiveUsers: this.get('currentlyActiveUsers'),
       });
-    },
+    });
+
+    if(this.get('integrateInstructorGroups')){
+      let exp = new RegExp(searchTerms, 'gi');
+
+      let filteredGroups = this.get('availableInstructorGroups').filter(group => {
+        return group.get('title') && group.get('title').match(exp);
+      });
+      let instructorGroupProxies = filteredGroups.map(group => {
+        return instructorGroupProxy.create({
+          content: group,
+          currentlyActiveInstructorGroups: this.get('currentlyActiveInstructorGroups'),
+        });
+      });
+
+      results.pushObjects(instructorGroupProxies);
+      results.sortBy('sortTerm');
+    }
+    this.set('searchReturned', true);
+    return results;
+  }).restartable(),
+  actions: {
     addUser: function(user){
-      this.set('searchTerms', '');
       //don't send actions to the calling component if the user is already in the list
       //prevents a complicated if/else on the template.
       if(!this.get('currentlyActiveUsers').contains(user)){
@@ -107,7 +93,6 @@ export default Component.extend({
       }
     },
     addInstructorGroup: function(group){
-      this.set('searchTerms', '');
       //don't send actions to the calling component if the user is already in the list
       //prevents a complicated if/else on the template.
       if(!this.get('currentlyActiveInstructorGroups').contains(group)){
---
+++ tests/integration/components/user-search-test.js
@@ -0,0 +1,124 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+const { Service, RSVP, Object } = Ember;
+const { resolve } = RSVP;
+
+moduleForComponent('user-search', 'Integration | Component | user search', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.render(hbs`{{user-search}}`);
+
+  assert.equal(this.$('input').length, 1);
+});
+
+test('less than 3 charecters triggers warning', function(assert) {
+  this.render(hbs`{{user-search}}`);
+
+  this.$('input').val('ab').trigger('change');
+  assert.equal(this.$('ul').text().trim(), 'keep typing...');
+});
+
+test('input triggers search', function(assert) {
+  let storeMock = Service.extend({
+    query(what, {q, limit}){
+
+      assert.equal('user', what);
+      assert.equal(100, limit);
+      assert.equal('search words', q);
+      return resolve([Object.create({fullName: 'test person', email: 'testemail'})]);
+    }
+  });
+  this.register('service:store', storeMock);
+  this.render(hbs`{{user-search}}`);
+
+  this.$('input').val('search words').trigger('change');
+  assert.equal(this.$('li:eq(0)').text().trim(), '1 Results');
+  assert.equal(this.$('li:eq(1)').text().replace(/[\t\n\s]+/g, ""), 'testpersontestemail');
+});
+
+test('no results displayes messages', function(assert) {
+  let storeMock = Service.extend({
+    query(what, {q, limit}){
+
+      assert.equal('user', what);
+      assert.equal(100, limit);
+      assert.equal('search words', q);
+      return resolve([]);
+    }
+  });
+  this.register('service:store', storeMock);
+  this.render(hbs`{{user-search}}`);
+
+  this.$('input').val('search words').trigger('change');
+  assert.equal(this.$('li:eq(0)').text().trim(), 'no results');
+});
+
+test('search for groups', function(assert) {
+  let storeMock = Service.extend({
+    query(){
+      return resolve([]);
+    }
+  });
+  this.register('service:store', storeMock);
+  let group1 = Object.create({
+    title: 'test1'
+  });
+  let group2 = Object.create({
+    title: 'test2'
+  });
+  this.set('availableInstructorGroups', [group1, group2]);
+  this.render(hbs`{{user-search availableInstructorGroups=availableInstructorGroups}}`);
+
+  this.$('input').val('test').trigger('change');
+  assert.equal(this.$('li:eq(0)').text().trim(), '2 Results');
+  assert.equal(this.$('li:eq(1)').text().trim(), 'test1');
+  assert.equal(this.$('li:eq(2)').text().trim(), 'test2');
+});
+
+test('click user fires add user', function(assert) {
+  let user1 = Object.create({
+    fullName: 'test person',
+    email: 'testemail'
+  });
+  let storeMock = Service.extend({
+    query(){
+      return resolve([user1]);
+    }
+  });
+  this.register('service:store', storeMock);
+
+  this.on('action', function(user){
+    assert.equal(user1, user);
+  });
+  this.render(hbs`{{user-search addUser=(action 'action')}}`);
+
+  this.$('input').val('test').trigger('change');
+  assert.equal(this.$('li:eq(1)').text().replace(/[\t\n\s]+/g, ""), 'testpersontestemail');
+  this.$('li:eq(1)').click();
+});
+
+test('click group fires add group', function(assert) {
+  let storeMock = Service.extend({
+    query(){
+      return resolve([]);
+    }
+  });
+  this.register('service:store', storeMock);
+
+  let group1 = Object.create({
+    title: 'test1'
+  });
+  this.on('action', function(group){
+    assert.equal(group1, group);
+  });
+  this.set('availableInstructorGroups', [group1]);
+  this.render(hbs`{{user-search availableInstructorGroups=availableInstructorGroups addInstructorGroup=(action 'action')}}`);
+
+  this.$('input').val('test').trigger('change');
+  assert.equal(this.$('li:eq(1)').text().trim(), 'test1');
+  this.$('li:eq(1)').click();
+});
