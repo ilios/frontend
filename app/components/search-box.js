d52f6fcddf9fc2acc5235fca5890d4054bb0bd03
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-05-04 22:34:45 -0700

    Update search-box component
    More idiomatic style and use of the concurrency system will hopefully
    make this easier to use.  Added tests.

--- app/components/search-box.js
+++ app/components/search-box.js
@@ -1,31 +1,33 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
-const { Component, observer } = Ember;
+const { Component } = Ember;
+const DEBOUNCE_TIMEOUT = 250;
 
 export default Component.extend({
   classNames: ['search-box'],
-  value: null,
+  value: '',
   liveSearch: true,
-  watchValue: observer('value', 'liveSearch', function(){
-    if(this.get('liveSearch')){
-      this.send('search');
-    }
-  }),
+  searchTask: task(function * () {
+    const value = this.get('value');
+    yield this.get('search')(value);
+    yield timeout(DEBOUNCE_TIMEOUT);
+  }).restartable(),
   actions: {
-    clear: function() {
-      this.set('value', '');
-      this.sendAction('clear');
+    update(value){
+      const liveSearch = this.get('liveSearch');
+      this.set('value', value);
+      if (liveSearch) {
+        this.get('searchTask').perform();
+      }
     },
-    search: function(){
+    clear() {
+      this.set('value', '');
+      this.get('clear')();
+    },
+    focus(){
       //place focus into the search box when search icon is clicked
       this.$('input[type="search"]').focus();
-      if(this.get('value').length === 0 || !this.get('liveSearch')){
-        this.sendAction('search', this.get('value'));
-      } else {
-        Ember.run.debounce(this, function(){
-          this.sendAction('search', this.get('value'));
-        }, 1000);
-      }
     },
   }
 });
