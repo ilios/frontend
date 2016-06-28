39ad4b39c0c5fdeb3f2ae49d0b5fc2c9f0651e37
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-06-28 16:19:50 -0700

    long-handed comp props b/c shorthand chaining did not work.

--- app/models/program.js
+++ app/models/program.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 import PublishableModel from 'ilios/mixins/publishable-model';
 
 const { computed } = Ember;
-const { mapBy, sum } = computed;
 
 export default DS.Model.extend(PublishableModel,{
   title: DS.attr('string'),
@@ -15,10 +14,68 @@ export default DS.Model.extend(PublishableModel,{
     inverse: 'program'
   }),
   curriculumInventoryReports: DS.hasMany('curriculum-inventory-report', {async: true}),
-  cohorts: mapBy('programYears', 'cohort'),
-  courses: mapBy('cohorts', 'courses'),
-  courseCounts: mapBy('courses', 'length'),
-  courseCount: sum('courseCounts'),
+
+  cohorts: computed('programYears.[]', function() {
+    let defer = Ember.RSVP.defer();
+    let allCohorts = [];
+    let promises = [];
+    promises.pushObject(new Ember.RSVP.Promise(resolve => {
+      this.get('programYears').then(programYears => {
+        if(!programYears.length){
+          resolve();
+        }
+        let promises = [];
+        programYears.forEach(programYear => {
+          promises.pushObject(programYear.get('cohort').then(cohort =>{
+            allCohorts.pushObject(cohort);
+          }));
+        });
+        Ember.RSVP.all(promises).then(()=>{
+          resolve();
+        });
+      });
+    }));
+
+    Ember.RSVP.all(promises).then(()=>{
+      defer.resolve(allCohorts);
+    });
+
+    return DS.PromiseArray.create({
+      promise: defer.promise
+    });
+  }),
+
+  courses: computed('cohorts.[]', function() {
+    let defer = Ember.RSVP.defer();
+    let allCourses = [];
+    let promises = [];
+    promises.pushObject(new Ember.RSVP.Promise(resolve => {
+      this.get('cohorts').then(cohorts => {
+        if(!cohorts.length){
+          resolve();
+        }
+        let promises = [];
+        cohorts.forEach(cohort => {
+          promises.pushObject(cohort.get('courses').then(courses =>{
+            courses.forEach(course => {
+              allCourses.pushObject(course);
+            });
+          }));
+        });
+        Ember.RSVP.all(promises).then(()=>{
+          resolve();
+        });
+      });
+    }));
+
+    Ember.RSVP.all(promises).then(()=>{
+      defer.resolve(allCourses.uniq());
+    });
+
+    return DS.PromiseArray.create({
+      promise: defer.promise
+    });
+  }),
   requiredPublicationSetFields: ['title', 'shortTitle', 'duration'],
   optionalPublicationLengthFields: ['programYears'],
   requiredPublicationIssues: computed('title', 'shortTitle', 'duration', function(){
