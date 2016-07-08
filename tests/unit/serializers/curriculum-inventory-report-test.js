3335e746540884760bb05c720571c39f3ee8bdc7
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-07-08 15:41:28 -0700

    serializer test coverage.

--- tests/unit/serializers/curriculum-inventory-report-test.js
+++ tests/unit/serializers/curriculum-inventory-report-test.js
@@ -1,19 +1,33 @@
-import { moduleForModel } from 'ember-qunit';
-import { skip } from 'qunit';
+import { moduleForModel, test } from 'ember-qunit';
+import {a as testgroup} from 'ilios/tests/helpers/test-groups';
+import Ember from 'ember';
 
-moduleForModel('curriculum-inventory-report', 'Unit | Serializer | curriculum inventory report', {
+moduleForModel('curriculum-inventory-report', 'Unit | Serializer | curriculum inventory report' + testgroup, {
   // Specify the other units that are required for this test.
   needs: [
+    'model:curriculum-inventory-academic-level',
     'model:curriculum-inventory-report',
-    'serializer:curriculum-inventory-report'
+    'model:curriculum-inventory-export',
+    'model:curriculum-inventory-sequence',
+    'model:curriculum-inventory-sequence-block',
+    'model:program',
+    'serializer:curriculum-inventory-report',
   ]
 });
 
-// Replace this with your real tests.
-skip('it serializes records', function(assert) {
+test('it serializes records', function(assert) {
   let record = this.subject();
-
   let serializedRecord = record.serialize();
-
   assert.ok(serializedRecord);
+});
+
+test('it removes all non postable fields', function(assert) {
+  let record = this.subject();
+  Ember.run(()=> {
+    var uri = "/abandon/all/hope/ye/who/enter/here";
+    record.set('absoluteFileUri', uri);
+    assert.equal(record.get('absoluteFileUri'), uri);
+    var serializedRecord = record.serialize();
+    assert.ok(!('absoluteFileUri' in serializedRecord));
+  });
 });
