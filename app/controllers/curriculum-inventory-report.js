8039f4bf50ea4bef39a2d41cb37a1afe47ef5dab
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-07-19 16:34:45 -0700

    added rm seq block action to ci report controller.

--- app/controllers/curriculum-inventory-report.js
+++ app/controllers/curriculum-inventory-report.js
@@ -3,4 +3,9 @@ import Ember from 'ember';
 const { Controller } = Ember;
 
 export default Controller.extend({
+  actions: {
+    removeSequenceBlock(block) {
+      return block.destroyRecord();
+    },
+  }
 });
