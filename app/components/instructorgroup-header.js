8cbc35ad5e0abde95331b5bc932aad93ed39afbf
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-08-02 10:56:50 -0700

    refactored instructorgroup header out into its own component.

---
+++ app/components/instructorgroup-header.js
@@ -0,0 +1,54 @@
+import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
+import ValidationErrorDisplay from 'ilios/mixins/validation-error-display';
+
+const { inject, Component, RSVP } = Ember;
+const { Promise } = RSVP;
+const { service } = inject;
+
+const Validations = buildValidations({
+  title: [
+    validator('presence', true),
+    validator('length', {
+      min: 3,
+      max: 200
+    }),
+  ],
+});
+
+export default Component.extend(Validations, ValidationErrorDisplay, {
+  didReceiveAttrs(){
+    this._super(...arguments);
+    this.set('title', this.get('instructorGroup.title'));
+  },
+  store: service(),
+  title: null,
+  classNames: ['instructorgroup-header'],
+  actions: {
+    changeTitle(){
+      const group = this.get('instructorGroup');
+      const newTitle = this.get('title');
+      this.send('addErrorDisplayFor', 'title');
+      return new Promise((resolve, reject) => {
+        this.validate().then(({validations}) => {
+          if (validations.get('isValid')) {
+            this.send('removeErrorDisplayFor', 'title');
+            group.set('title', newTitle);
+            group.save().then((newGroup) => {
+              this.set('title', newGroup.get('title'));
+              this.set('instructorGroup', newGroup);
+              resolve();
+            });
+          } else {
+            reject();
+          }
+        });
+      });
+    },
+
+    revertTitleChanges(){
+      const group = this.get('instructorGroup');
+      this.set('title', group.get('title'));
+    },
+  }
+});
