d0b86a55b092f41ec564d13c934fc704f5294263
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-08-11 14:00:14 -0700

    Remove ui-select and use a standard select element
    The ui-select component is gone we were only using it to pick the start
    date for a program year which can be done with a select list just as
    well.

--- tests/integration/components/new-programyear-test.js
+++ tests/integration/components/new-programyear-test.js
@@ -12,11 +12,9 @@ moduleForComponent('new-programyear', 'Integration | Component | new programyear
 
 test('it renders', function(assert) {
   assert.expect(1);
+  this.set('nothing', parseInt);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{new-programyear}}`);
+  this.render(hbs`{{new-programyear save=(action nothing) cancel=(action nothing)}}`);
 
   assert.ok(this.$().text().search(/New Program Year/) === 0);
 });
