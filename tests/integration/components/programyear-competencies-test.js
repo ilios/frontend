1115d8809b4bf354c5aa102ec898188a65ff482b
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-08-11 00:05:38 -0700

    Remove unused wait import

--- tests/integration/components/programyear-competencies-test.js
+++ tests/integration/components/programyear-competencies-test.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import wait from 'ember-test-helpers/wait';
 
 const { Object, RSVP } = Ember;
 const { resolve } = RSVP;
