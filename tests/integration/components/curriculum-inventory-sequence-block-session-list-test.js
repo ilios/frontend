df18dace7fb73e2db53c58cb58a4866f56214b4f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-08-19 11:27:19 -0700

    skipped tests.

--- tests/integration/components/curriculum-inventory-sequence-block-session-list-test.js
+++ tests/integration/components/curriculum-inventory-sequence-block-session-list-test.js
@@ -1,11 +1,12 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import { skip } from 'qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('curriculum-inventory-sequence-block-session-list', 'Integration | Component | curriculum inventory sequence block session list', {
   integration: true
 });
 
-test('it renders', function(assert) {
+skip('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
