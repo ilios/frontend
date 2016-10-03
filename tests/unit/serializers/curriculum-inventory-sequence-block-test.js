ae0c4bf49591b337ffd13a79f13e092d7e71e39a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-10-03 09:35:40 -0700

    properly apply TZ conversion when serializing/normalizing start/end dates in CI seq blocks.

---
+++ tests/unit/serializers/curriculum-inventory-sequence-block-test.js
@@ -0,0 +1,21 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('curriculum-inventory-sequence-block', 'Unit | Serializer | curriculum inventory sequence block', {
+  // Specify the other units that are required for this test.
+  needs: [
+    'serializer:curriculum-inventory-sequence-block',
+    'model:curriculum-inventory-report',
+    'model:curriculum-inventory-academic-level',
+    'model:course',
+    'model:session',
+  ],
+});
+
+// Replace this with your real tests.
+test('it serializes records', function(assert) {
+  let record = this.subject();
+
+  let serializedRecord = record.serialize();
+
+  assert.ok(serializedRecord);
+});
