ed4cc568989eea85e1829b3465a55243f1666f83
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-10-05 13:48:52 -0700

    ES6 syntax, everywhere.

--- app/components/learningmaterial-search.js
+++ app/components/learningmaterial-search.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
 
 const { Component, inject } = Ember;
 const { service } = inject;
@@ -21,67 +22,73 @@ export default Component.extend({
   searching: false,
   searchReturned: false,
 
+  addLearningMaterial: task(function * (proxy) {
+    let lm = proxy.content;
+    if (! this.get('currentMaterials').contains(lm)) {
+      proxy.set('isActive', false);
+      this.sendAction('add', lm);
+    }
+  }).enqueue(),
+
   actions: {
-    search: function(query){
-      var self = this;
+    search(query){
       if (Ember.$.trim(query) === '') {
-        self.set('searchReturned', false);
-        self.set('searching', false);
-        self.set('searchPage', 1);
-        self.set('hasMoreSearchResults', false);
-        self.set('searchResults', []);
+        this.set('searchReturned', false);
+        this.set('searching', false);
+        this.set('searchPage', 1);
+        this.set('hasMoreSearchResults', false);
+        this.set('searchResults', []);
         return;
       }
       this.set('searchReturned', false);
       this.set('searching', true);
       this.set('query', query);
-      var currentMaterials = this.get('currentMaterials');
+      const currentMaterials = this.get('currentMaterials');
       this.get('store').query('learningMaterial', {
         q: query,
         limit: this.get('searchResultsPerPage') + 1,
         'order_by[title]': 'ASC',
-      }).then(function(lms){
-        let results = lms.map(function(lm){
+      }).then(lms => {
+        let results = lms.map(lm => {
           return ProxiedMaterials.create({
             content: lm,
             isActive: ! currentMaterials.contains(lm)
           });
         });
-        self.set('searchReturned', true);
-        self.set('searching', false);
-        self.set('searchPage', 1);
-        self.set('hasMoreSearchResults', (results.length > self.get('searchResultsPerPage')));
-        if (self.get('hasMoreSearchResults')) {
+        this.set('searchReturned', true);
+        this.set('searching', false);
+        this.set('searchPage', 1);
+        this.set('hasMoreSearchResults', (results.length > this.get('searchResultsPerPage')));
+        if (this.get('hasMoreSearchResults')) {
           results.pop();
         }
-        self.set('searchResults', results);
+        this.set('searchResults', results);
       });
     },
-    searchMore: function() {
-      var self = this;
-      var currentMaterials = this.get('currentMaterials');
-      var query = this.get('query');
+    searchMore() {
+      const currentMaterials = this.get('currentMaterials');
+      const query = this.get('query');
       this.get('store').query('learningMaterial', {
         q: query,
         limit: this.get('searchResultsPerPage') + 1,
         offset: this.get('searchPage') * this.get('searchResultsPerPage'),
         'order_by[title]': 'ASC',
-      }).then(function(lms){
-        let results = lms.map(function(lm){
+      }).then(lms => {
+        let results = lms.map(lm => {
           return ProxiedMaterials.create({
             content: lm,
             isActive: ! currentMaterials.contains(lm)
           });
         });
-        self.set('searchPage', self.get('searchPage') + 1);
-        self.set('hasMoreSearchResults', (results.length > self.get('searchResultsPerPage')));
-        if (self.get('hasMoreSearchResults')) {
+        this.set('searchPage', this.get('searchPage') + 1);
+        this.set('hasMoreSearchResults', (results.length > this.get('searchResultsPerPage')));
+        if (this.get('hasMoreSearchResults')) {
           results.pop();
         }
-        self.get('searchResults').pushObjects(results);
+        this.get('searchResults').pushObjects(results);
       });
     },
-    clear: function(){
+    clear(){
       this.set('searchResults', []);
       this.set('searchReturned', false);
       this.set('searching', false);
@@ -89,13 +96,5 @@ export default Component.extend({
       this.set('hasMoreSearchResults', false);
       this.set('query', '');
     },
-
-    add(proxy) {
-      let lm = proxy.content;
-      if (! this.get('currentMaterials').contains(lm)) {
-        proxy.set('isActive', false);
-        this.sendAction('add', lm);
-      }
-    }
   }
 });
