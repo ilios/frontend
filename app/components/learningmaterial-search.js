02b203fe447c3e95f25b1e13149ebbef6896f0bb
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-10-05 09:40:14 -0700

    rm unused var.

--- app/components/learningmaterial-search.js
+++ app/components/learningmaterial-search.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { Component, computed, inject } = Ember;
+const { Component, inject } = Ember;
 const { service } = inject;
 
 var ProxiedMaterials = Ember.ObjectProxy.extend({
