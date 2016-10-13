de9d6fab8a4ed13861ef062dee8e4c8fc55f43a4
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-10-13 10:17:32 -0700

    Add template linting to catch common errors
    This will make it easier to catch issues and stay within structured
    development patterns.
    For now all rules are disabled so they can be enabled and fixed
    individually.

---
+++ .template-lintrc.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+
+  rules: {
+    'bare-strings': false,
+    'block-indentation': false,
+    'html-comments': false,
+    'nested-interactive': false,
+    'self-closing-void-elements': false,
+    'img-alt-attributes': false,
+    'link-rel-noopener': false,
+    'invalid-interactive': false,
+    'inline-link-to': false,
+    'style-concatenation': false,
+    'triple-curlies': false,
+    'deprecated-each-syntax': false,
+  }
+};
