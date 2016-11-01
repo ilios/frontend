523273e62e8990856d27b6c05b15ef06cc52acc2
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-11-01 10:58:42 -0700

    Improve ilios-event component
    Cleaned up the computed properties, passing values instead of promises
    to the calendar, remove all the CPs that were for passing translations
    those are done in the template correctly now.

--- app/components/ilios-event.js
+++ app/components/ilios-event.js
@@ -1,317 +1,249 @@
 import Ember from 'ember';
-import DS from 'ember-data';
-import momentFormat from 'ember-moment/computeds/format';
 
 const { Component, computed, inject, RSVP, isEmpty} = Ember;
-const { PromiseObject, PromiseArray } = DS;
 const { notEmpty } = computed;
 const { service } = inject;
+const { Promise, map } = RSVP;
 
 export default Component.extend({
   store: service(),
   i18n: service(),
   event: null,
   classNames: ['ilios-event'],
-  description: computed('offering.session.description.description', function(){
-    let defer = RSVP.defer();
-    this.get('thesession').then(session=>{
-      session.get('sessionDescription').then(description=>{
-        if(isEmpty(description)){
-          defer.resolve(null);
-        } else {
-          defer.resolve(description.get('description'));
-        }
+  description: computed('session.description.description', function(){
+    return new Promise(resolve => {
+      this.get('session').then(session=>{
+        session.get('sessionDescription').then(description=>{
+          if(isEmpty(description)){
+            resolve(null);
+          } else {
+            resolve(description.get('description'));
+          }
+        });
       });
-    });
-    return PromiseObject.create({
-      promise: defer.promise
     });
   }),
   isOffering: notEmpty('event.offering'),
-  niceStartTime: momentFormat('event.startDate', 'dddd, MMMM Do YYYY, h:mm a'),
-  offeredAt: computed('niceStartTime', function(){
-    return this.get('i18n').t('general.offeredAt', {date: this.get('niceStartTime')});
+  taughtBy: computed('i18n.locale', 'event.instructors', function(){
+    const instructors = this.get('event.instructors');
+    if (isEmpty(instructors)) {
+      return '';
+    } else {
+      return this.get('i18n').t('general.taughtBy', {instructors});
+    }
   }),
-  instructorList: computed('event.instructors.[]', function(){
-    return RSVP.resolve(this.get('event.instructors'));
-  }),
-  taughtBy: computed('i18n.locale', 'instructorList', function(){
-    let defer = RSVP.defer();
-
-    this.get('instructorList').then((instructors) => {
-      if (isEmpty(instructors)) {
-        defer.resolve('');
-      } else {
-        defer.resolve(this.get('i18n').t('general.taughtBy', {instructors}));
-      }
-    });
-
-    return PromiseObject.create({
-      promise: defer.promise
-    });
-  }),
-  sessionIs: computed('offering.session.sessionType', function(){
-    let defer = RSVP.defer();
-
-    this.get('thesession').then(session => {
-      session.get('sessionType').then(sessionType => {
-        defer.resolve(this.get('i18n').t('general.sessionIs', {type: sessionType.get('title')}));
+  sessionIs: computed('session.sessionType', 'i18n.locale', function(){
+    return new Promise(resolve => {
+      const i18n = this.get('i18n');
+      this.get('session').then(session => {
+        session.get('sessionType').then(sessionType => {
+          resolve(i18n.t('general.sessionIs', {type: sessionType.get('title')}));
+        });
       });
-    });
-
-    return PromiseObject.create({
-      promise: defer.promise
     });
   }),
   offering: computed('event.offering', function(){
-    let offeringId = this.get('event.offering');
-    if(!offeringId){
-      return null;
-    }
-    return PromiseObject.create({
-      promise: this.get('store').findRecord('offering', offeringId)
+    const offeringId = this.get('event.offering');
+    const store = this.get('store');
+    return new Promise(resolve => {
+      if(!offeringId){
+        resolve(null);
+      } else {
+        store.findRecord('offering', offeringId).then(offering => {
+          resolve(offering);
+        })
+      }
     });
   }),
   ilmSession: computed('event.ilmSession', function(){
-    let ilmSessionId = this.get('event.ilmSession');
-    if(!ilmSessionId){
-      return null;
-    }
-    return PromiseObject.create({
-      promise: this.get('store').findRecord('ilm-session', ilmSessionId)
+    const ilmSessionId = this.get('event.ilmSession');
+    const store = this.get('store');
+    return new Promise(resolve => {
+      if(!ilmSessionId){
+        resolve(null);
+      } else {
+        store.findRecord('ilm-session', ilmSessionId).then(ilmSession => {
+          resolve(ilmSession);
+        })
+      }
     });
   }),
-  noContentPhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.none');
-  }),
-  coursePhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.courseTitle');
-  }),
-  courseObjectivesPhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.courseObjectives');
-  }),
-  courseLearningMaterialsPhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.courseLearningMaterials');
-  }),
-  courseObjectives: computed('i18n.locale', 'thesession.course.objectives.[]', function(){
-    let defer = RSVP.defer();
-
-    this.get('thesession').then(session => {
-      session.get('course').then(course => {
+  courseObjectives: computed('i18n.locale', 'course.objectives.[]', function(){
+    const i18n = this.get('i18n');
+    return new Promise(resolve => {
+      this.get('course').then(course => {
         course.get('objectives').then(objectives => {
-          let promises = [];
-          let mappedObjectives = [];
-          objectives.forEach(objective => {
-            promises.pushObject(objective.get('topParents').then(parents => {
-              let parent = parents.get('firstObject');
-              promises.pushObject(parent.get('competency').then(competency => {
-                //strip all HTML
-                let title = objective.get('title').replace(/(<([^>]+)>)/ig,"");
-                if(isEmpty(competency)){
-                  mappedObjectives.pushObject({
-                    title,
-                    domain: this.get('i18n').t('general.noAssociatedCompetencies')
-                  });
-                } else {
-                  promises.pushObject(competency.get('domain').then(domain => {
-                    mappedObjectives.pushObject({
+          map(objectives.toArray(), objective => {
+            return new Promise(resolve => {
+              objective.get('topParents').then(parents => {
+                let parent = parents.get('firstObject');
+                parent.get('competency').then(competency => {
+                  //strip all HTML
+                  let title = objective.get('title').replace(/(<([^>]+)>)/ig,"");
+                  if(isEmpty(competency)){
+                    resolve({
                       title,
-                      domain: competency.get('title') + ' (' + domain.get('title') + ')'
+                      domain: i18n.t('general.noAssociatedCompetencies')
                     });
-                  }));
-                }
-              }));
-            }));
-          });
-          RSVP.all(promises).then(()=>{
-            defer.resolve(mappedObjectives);
+                  } else {
+                    competency.get('domain').then(domain => {
+                      resolve({
+                        title,
+                        domain: competency.get('title') + ' (' + domain.get('title') + ')'
+                      });
+                    });
+                  }
+                });
+              });
+            });
+          }).then(mappedObjectives => {
+            resolve(mappedObjectives);
           });
         });
       });
     });
-
-    return PromiseArray.create({
-      promise: defer.promise
-    });
   }),
-  courseLearningMaterials: computed('i18n.locale', 'session.course', function() {
-    let defer = Ember.RSVP.defer();
-
-    this.get('thesession').then(session => {
-      session.get('course').then(course => {
-        this.get('store').query('courseLearningMaterial', {
+  courseLearningMaterials: computed('i18n.locale', 'course', function() {
+    return new Promise(resolve => {
+      const store = this.get('store');
+      this.get('course').then(course => {
+        store.query('courseLearningMaterial', {
           filters: {
             course: course.get('id')
           },
           limit: 1000
         }).then((courseLearningMaterials) => {
-          let promises = [];
-          let mappedLearningMaterials = [];
-          courseLearningMaterials.forEach(courseLearningMaterial => {
+          map(courseLearningMaterials.toArray(), clm => {
+            return new Promise(resolve => {
+              clm.get('learningMaterial').then(learningMaterial => {
+                const { required, storedNotes, publicNotes } = clm.getProperties('required', 'notes', 'publicNotes');
+                const notes = publicNotes?storedNotes:'';
+                const {title, description, url, type, mimetype, filesize, citation} = learningMaterial.getProperties([
+                  'title', 'description', 'url', 'type', 'mimetype', 'filesize', 'citation'
+                ]);
 
-            promises.pushObject(courseLearningMaterial.get('learningMaterial').then((learningMaterial) => {
-              let notes = '';
-
-              if (courseLearningMaterial.get('publicNotes')) {
-                notes = courseLearningMaterial.get('notes');
-              }
-
-              mappedLearningMaterials.pushObject({
-                title: learningMaterial.get('title'),
-                description: learningMaterial.get('description'),
-                required: courseLearningMaterial.get('required'),
-                notes,
-                url: learningMaterial.get('url'),
-                type: learningMaterial.get('type'),
-                mimetype: learningMaterial.get('mimetype'),
-                filesize: learningMaterial.get('filesize'),
-                citation: learningMaterial.get('citation'),
-              });
-            }));
-          });
-          RSVP.all(promises).then(()=>{
-            defer.resolve(mappedLearningMaterials);
-          });
-        });
-      });
-    });
-    return PromiseArray.create({
-      promise: defer.promise
-    });
-  }),
-  sessionPhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.session');
-  }),
-  sessionObjectivesPhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.sessionObjectives');
-  }),
-  sessionLearningMaterialsPhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.sessionLearningMaterials');
-  }),
-  sessionObjectives: computed('i18n.locale', 'offering.session.objectives.@each.topParents', function(){
-    let defer = RSVP.defer();
-
-    this.get('thesession').then(session => {
-      session.get('objectives').then(objectives => {
-        let promises = [];
-        let mappedObjectives = [];
-        objectives.forEach(objective => {
-          promises.pushObject(objective.get('topParents').then(parents => {
-            let parent = parents.get('firstObject');
-            promises.pushObject(parent.get('competency').then(competency => {
-              //strip all HTML
-              let title = objective.get('title').replace(/(<([^>]+)>)/ig,"");
-              if(isEmpty(competency)){
-                mappedObjectives.pushObject({
+                let obj = {
                   title,
-                  domain: this.get('i18n').t('general.noAssociatedCompetencies')
-                });
-              } else {
-                promises.pushObject(competency.get('domain').then(domain => {
-                  mappedObjectives.pushObject({
-                    title,
-                    domain: competency.get('title') + ' (' + domain.get('title') + ')'
-                  });
-                }));
-              }
-            }));
-          }));
-        });
-        RSVP.all(promises).then(()=>{
-          defer.resolve(mappedObjectives);
+                  description,
+                  required,
+                  notes,
+                  url,
+                  type,
+                  mimetype,
+                  filesize,
+                  citation,
+                };
+
+                resolve(obj);
+              });
+            });
+          }).then(mappedLearningMaterials => {
+            resolve(mappedLearningMaterials);
+          });
         });
       });
     });
-
-    return PromiseArray.create({
-      promise: defer.promise
+  }),
+  sessionObjectives: computed('i18n.locale', 'session.objectives.[]', function(){
+    return new Promise(resolve => {
+      this.get('session').then(session => {
+        session.get('objectives').then(objectives => {
+          map(objectives.toArray(), objective => {
+            return new Promise(resolve => {
+              objective.get('topParents').then(parents => {
+                let parent = parents.get('firstObject');
+                parent.get('competency').then(competency => {
+                  //strip all HTML
+                  let title = objective.get('title').replace(/(<([^>]+)>)/ig,"");
+                  if(isEmpty(competency)){
+                    resolve({
+                      title,
+                      domain: this.get('i18n').t('general.noAssociatedCompetencies')
+                    });
+                  } else {
+                    competency.get('domain').then(domain => {
+                      resolve({
+                        title,
+                        domain: competency.get('title') + ' (' + domain.get('title') + ')'
+                      });
+                    });
+                  }
+                });
+              });
+            });
+          }).then(mappedObjectives => {
+            resolve(mappedObjectives);
+          });
+        });
+      });
     });
   }),
 
   sessionLearningMaterials: computed('i18n.locale', 'session', function() {
-    let defer = Ember.RSVP.defer();
+    return new Promise(resolve => {
+      this.get('session').then(session => {
+        this.get('store').query('sessionLearningMaterial', {
+          filters: {
+            session: session.get('id')
+          },
+          limit: 1000
+        }).then((sessionLearningMaterials) => {
+          map(sessionLearningMaterials.toArray(), slm => {
+            return new Promise(resolve => {
+              slm.get('learningMaterial').then(learningMaterial => {
+                const { required, storedNotes, publicNotes } = slm.getProperties('required', 'notes', 'publicNotes');
+                const notes = publicNotes?storedNotes:'';
+                const {title, description, url, type, mimetype, filesize, citation} = learningMaterial.getProperties([
+                  'title', 'description', 'url', 'type', 'mimetype', 'filesize', 'citation'
+                ]);
 
-    this.get('thesession').then(session => {
-      this.get('store').query('sessionLearningMaterial', {
-        filters: {
-          session: session.get('id')
-        },
-        limit: 1000
-      }).then((sessionLearningMaterials) => {
-        let promises = [];
-        let mappedLearningMaterials = [];
-        sessionLearningMaterials.forEach(sessionLearningMaterials => {
+                let obj = {
+                  title,
+                  description,
+                  required,
+                  notes,
+                  url,
+                  type,
+                  mimetype,
+                  filesize,
+                  citation,
+                };
 
-          promises.pushObject(sessionLearningMaterials.get('learningMaterial').then((learningMaterial) => {
-            let notes = '';
-
-            if (sessionLearningMaterials.get('publicNotes')) {
-              notes = sessionLearningMaterials.get('notes');
-            }
-
-            mappedLearningMaterials.pushObject({
-              title: learningMaterial.get('title'),
-              description: learningMaterial.get('description'),
-              required: sessionLearningMaterials.get('required'),
-              notes,
-              url: learningMaterial.get('url'),
-              type: learningMaterial.get('type'),
-              mimetype: learningMaterial.get('mimetype'),
-              filesize: learningMaterial.get('filesize'),
-              citation: learningMaterial.get('citation'),
+                resolve(obj);
+              });
             });
-          }));
-        });
-        RSVP.all(promises).then(()=>{
-          defer.resolve(mappedLearningMaterials);
+          }).then(lmObjects => {
+            resolve(lmObjects);
+          });
         });
       });
     });
-    return PromiseArray.create({
-      promise: defer.promise
-    });
-  }),
-  requiredPhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.required');
   }),
   course: computed('session.course', function(){
-    let defer = RSVP.defer();
-    this.get('thesession').then(session=>{
-      session.get('course').then(course=>{
-        defer.resolve(course);
+    return new Promise(resolve => {
+      this.get('session').then(session=>{
+        session.get('course').then(course=>{
+          resolve(course);
+        });
       });
     });
-    return PromiseObject.create({
-      promise: defer.promise
-    });
   }),
-  sessionTitle: computed('thesession.title', 'event.isOffering', function(){
-    let defer = RSVP.defer();
-    let prefix = this.get('isOffering')?'':'ILM: ';
-    this.get('thesession').then(session => {
-      defer.resolve(prefix + session.get('title'));
-    });
-    return PromiseObject.create({
-      promise: defer.promise
-    });
-  }),
-  //session name is already taken by ember-simple-auth
-  thesession: computed('offering.session', 'ilmSession.session', function(){
-    let defer = RSVP.defer();
-    let relationship = this.get('isOffering')?'offering':'ilmSession';
-    this.get(relationship).then(related => {
-      related.get('session').then(session=>{
-        defer.resolve(session);
+  sessionTitle: computed('session.title', 'isOffering', function(){
+    return new Promise(resolve => {
+      let prefix = this.get('isOffering')?'':'ILM: ';
+      this.get('session').then(session => {
+        resolve(prefix + session.get('title'));
       });
     });
-    return PromiseObject.create({
-      promise: defer.promise
+  }),
+  session: computed('offering.session', 'ilmSession.session', function(){
+    const relationship = this.get('isOffering')?'offering':'ilmSession';
+    return new Promise(resolve => {
+      this.get(relationship).then(related => {
+        related.get('session').then(session=>{
+          resolve(session);
+        });
+      });
     });
-  }),
-  attireRequiredPhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.specialAttireIsRequired');
-  }),
-  equipmentRequiredPhrase: computed('i18n.locale', function(){
-    return this.get('i18n').t('general.specialEquipmentIsRequired');
   }),
 });
