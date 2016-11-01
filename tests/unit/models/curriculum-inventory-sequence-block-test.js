7147773f8899e69aa182232286c8eb83b5b6b4fa
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-11-01 16:50:05 -0700

    implemented unit test coverage for sequence block model.

--- tests/unit/models/curriculum-inventory-sequence-block-test.js
+++ tests/unit/models/curriculum-inventory-sequence-block-test.js
@@ -1,16 +1,39 @@
-import {
-  moduleForModel,
-  test
-} from 'ember-qunit';
+import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 import modelList from '../../helpers/model-list';
 
 moduleForModel('curriculum-inventory-sequence-block', 'Unit | Model | CurriculumInventorySequenceBlock ', {
   needs: modelList
 });
 
-
 test('it exists', function(assert) {
-  var model = this.subject();
-  // var store = this.store();
+  let model = this.subject();
   assert.ok(!!model);
+});
+
+test('get all ancestors of nested sequence block', function(assert) {
+  assert.expect(3);
+  let model = this.subject();
+  let store = this.store();
+  Ember.run(() => {
+    let parent = store.createRecord('curriculumInventorySequenceBlock', { 'children': [ model ] });
+    let grandParent = store.createRecord('curriculumInventorySequenceBlock', { 'children': [ parent ] });
+    parent.set('parent', grandParent);
+    model.set('parent', parent);
+    model.get('allParents').then(ancestors => {
+      assert.equal(ancestors.length, 2);
+      assert.equal(ancestors[0], parent);
+      assert.equal(ancestors[1], grandParent);
+    });
+  });
+});
+
+test('list of ancestors is empty for top-level sequence block', function(assert) {
+  assert.expect(1);
+  let model = this.subject();
+  Ember.run(() => {
+    model.get('allParents').then(ancestors => {
+      assert.equal(ancestors.length, 0);
+    });
+  });
 });
