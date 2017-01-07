8145bab1cb71439fa9aef21f3f2091aa66328550
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-01-07 10:03:02 -0800

    make currently managed course parent objective sticky

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -3,10 +3,17 @@ import Ember from 'ember';
 const { Controller } = Ember;
 
 export default Controller.extend({
-  queryParams: ['details', 'courseObjectiveDetails', 'courseTaxonomyDetails', 'courseCompetencyDetails'],
+  queryParams: [
+    'details',
+    'courseObjectiveDetails',
+    'courseTaxonomyDetails',
+    'courseCompetencyDetails',
+    'currentlyManagedObjective',
+  ],
 
   details: false,
   courseObjectiveDetails: false,
   courseTaxonomyDetails: false,
   courseCompetencyDetails: false,
+  currentlyManagedObjective: 0,
 });
