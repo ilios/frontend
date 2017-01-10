e7a4bbe0c4d1a9d54ec71a5f94b7d0bb44aa9d61
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-01-09 22:58:00 -0800

    Send all required models to publish menu
    We were only sending the programYear.  In some contexts ember was able
    to interpret this, but not aways.  Instead we need to send both the
    program and the program year to the publish menu so it can render the
    review link correctly.

--- app/components/publish-menu.js
+++ app/components/publish-menu.js
@@ -14,6 +14,7 @@ export default Component.extend({
   showUnPublish: false,
   reviewRoute: null,
   reviewObject: null,
+  secondReviewObject: null,
   publishTranslation: '',
   unpublishTranslation: '',
 });
