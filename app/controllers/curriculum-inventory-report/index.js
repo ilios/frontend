b76c913a5d2992e25a6d7ec096ba312172730aa4
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-01-11 20:04:33 -0800

    moved action to correct controller.

--- app/controllers/curriculum-inventory-report/index.js
+++ app/controllers/curriculum-inventory-report/index.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    removeSequenceBlock(block) {
+      let report = this.get('model');
+      block.destroyRecord().then(() => {
+        report.reload();
+      });
+    },
+  }
 });
