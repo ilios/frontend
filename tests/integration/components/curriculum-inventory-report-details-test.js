6ff224aa66ab341615788494a0c009025023ccd0
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-01-19 13:36:46 -0800

    added integration test for ci report details component.

--- tests/integration/components/curriculum-inventory-report-details-test.js
+++ tests/integration/components/curriculum-inventory-report-details-test.js
@@ -1,25 +1,176 @@
+import Ember from 'ember';
 import { moduleForComponent } from 'ember-qunit';
-import { skip } from 'qunit';
+import { test } from 'qunit';
 import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
+import moment from 'moment';
+import tHelper from "ember-i18n/helper";
+
+const { RSVP, Object, Service } = Ember;
+const { resolve } = RSVP;
 
 moduleForComponent('curriculum-inventory-report-details', 'Integration | Component | curriculum inventory report details', {
-  integration: true
+  integration: true,
+
+  beforeEach: function () {
+    this.container.lookup('service:i18n').set('locale', 'en');
+    this.registry.register('helper:t', tHelper);
+  }
 });
 
-skip('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it renders', function(assert) {
+  assert.expect(2);
 
-  this.render(hbs`{{curriculum-inventory-report-details}}`);
+  let school = Object.create({ id() { return 1; }});
 
-  assert.equal(this.$().text().trim(), '');
+  let academicLevels = [];
+  for (let i = 0; i < 10; i++) {
+    academicLevels.pushObject(Object.create({ id: i, name: `Year ${i + 1}` }));
+  }
 
-  // Template block usage:
-  this.render(hbs`
-    {{#curriculum-inventory-report-details}}
-      template block text
-    {{/curriculum-inventory-report-details}}
-  `);
+  let program = Object.create({
+    belongsTo() {
+      return school;
+    }
+  });
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  let report = Object.create({
+    academicLevels,
+    year: '2016',
+    program: resolve(program),
+    linkedCourses: resolve([]),
+    isFinalized: false,
+    name: 'Lorem Ipsum',
+    startDate: moment('2015-06-12').toDate(),
+    endDate: moment('2016-04-11').toDate(),
+    description: 'Lorem Ipsum',
+    sequenceBlocks: resolve([])
+  });
+
+  this.set('report', report);
+
+  this.render(hbs`{{curriculum-inventory-report-details report=report}}`);
+
+  return wait().then(() => {
+    assert.equal(this.$('.curriculum-inventory-report-header .title').text().trim(), report.get('name'),
+      'Report name is visible in header.'
+    );
+    assert.equal(this.$('.curriculum-inventory-report-overview .description .editable').text().trim(), report.get('description'),
+      'Report description is visible in overview.'
+    );
+  });
+});
+
+test('finalize report', function(assert) {
+  assert.expect(8);
+
+  let school = Object.create({ id() { return 1; }});
+
+  let academicLevels = [];
+  for (let i = 0; i < 10; i++) {
+    academicLevels.pushObject(Object.create({ id: i, name: `Year ${i + 1}` }));
+  }
+
+  let program = Object.create({
+    belongsTo() {
+      return school;
+    }
+  });
+
+  let report = Object.create({
+    academicLevels,
+    year: '2016',
+    program: resolve(program),
+    linkedCourses: resolve([]),
+    isFinalized: false,
+    name: 'Lorem Ipsum',
+    startDate: moment('2015-06-12').toDate(),
+    endDate: moment('2016-04-11').toDate(),
+    description: 'Lorem Ipsum',
+    sequenceBlocks: resolve([])
+  });
+
+  let storeMock = Service.extend({
+    createRecord(what, params) {
+      assert.equal(what, 'curriculumInventoryExport', 'createRecord() got invoked for export.');
+      assert.equal(params.report, report, 'Report gets passed to correctly.');
+      return Object.create({
+        save() {
+          return resolve(this);
+        }
+      });
+    }
+  });
+
+  this.set('report', report);
+  this.register('service:store', storeMock);
+
+  this.render(hbs`{{curriculum-inventory-report-details report=report}}`);
+
+  return wait().then(() => {
+    assert.equal(this.$('.confirm-finalize').length, 0, 'Confirmation dialog is initially not visible.');
+    this.$('.curriculum-inventory-report-header .finalize').click();
+    assert.equal(this.$('.confirm-finalize').length, 1, 'Confirmation dialog is visible.');
+    assert.ok(
+      this.$('.confirm-finalize .confirm-message').text().trim().indexOf('By finalizing this report') === 0,
+      'Finalize confirmation message is visible'
+    );
+    this.$('.confirm-finalize .confirm-buttons .finalize').click();
+    return wait().then(() => {
+      assert.equal(this.$('.confirm-finalize').length, 0, 'Confirmation dialog is not visible post-finalization.');
+      assert.equal(this.$('.curriculum-inventory-report-header .title .fa-lock').length, 1,
+        'Lock icon is visible next to title post-finalization.'
+      );
+      assert.equal(this.$('.curriculum-inventory-report-header .finalize').length, 0,
+        'Finalize button is not visible post-finalization.'
+      );
+    });
+  });
+});
+
+test('start finalizing report, then cancel', function(assert){
+  assert.expect(3);
+  let school = Object.create({ id() { return 1; }});
+
+  let academicLevels = [];
+  for (let i = 0; i < 10; i++) {
+    academicLevels.pushObject(Object.create({ id: i, name: `Year ${i + 1}` }));
+  }
+
+  let program = Object.create({
+    belongsTo() {
+      return school;
+    }
+  });
+
+  let report = Object.create({
+    academicLevels,
+    year: '2016',
+    program: resolve(program),
+    linkedCourses: resolve([]),
+    isFinalized: false,
+    name: 'Lorem Ipsum',
+    startDate: moment('2015-06-12').toDate(),
+    endDate: moment('2016-04-11').toDate(),
+    description: 'Lorem Ipsum',
+    sequenceBlocks: resolve([])
+  });
+
+  this.set('report', report);
+
+  this.render(hbs`{{curriculum-inventory-report-details report=report}}`);
+
+  return wait().then(() => {
+    this.$('.curriculum-inventory-report-header .finalize').click();
+    this.$('.confirm-finalize .confirm-buttons .done').click();
+    return wait().then(() => {
+      assert.equal(this.$('.confirm-finalize').length, 0, 'Confirmation dialog is not visible post-cancellation.');
+      assert.equal(this.$('.curriculum-inventory-report-header .title .fa-lock').length, 0,
+        'Lock icon is not visible post-cancellation.'
+      );
+      assert.equal(this.$('.curriculum-inventory-report-header .finalize').length, 1,
+        'Finalize button is visible post-cancellation.'
+      );
+    });
+  });
 });
