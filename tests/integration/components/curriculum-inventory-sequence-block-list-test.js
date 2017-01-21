ce94e4377d23e7f47d16b9d8a51e0354f52921d6
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-01-20 17:55:32 -0800

    integration test for seq block list component.

--- tests/integration/components/curriculum-inventory-sequence-block-list-test.js
+++ tests/integration/components/curriculum-inventory-sequence-block-list-test.js
@@ -1,25 +1,349 @@
+import Ember from 'ember';
 import { moduleForComponent } from 'ember-qunit';
-import { skip } from 'qunit';
+import { test } from 'qunit';
 import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
+import moment from 'moment';
+import tHelper from "ember-i18n/helper";
+
+const { RSVP, Object } = Ember;
+const { resolve } = RSVP;
 
 moduleForComponent('curriculum-inventory-sequence-block-list', 'Integration | Component | curriculum inventory sequence block list', {
-  integration: true
+  integration: true,
+
+  beforeEach: function () {
+    this.container.lookup('service:i18n').set('locale', 'en');
+    this.registry.register('helper:t', tHelper);
+  }
 });
 
-skip('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it renders with top-level sequence blocks', function(assert) {
+  assert.expect(25);
 
-  this.render(hbs`{{curriculum-inventory-sequence-block-list}}`);
+  let school = Object.create({ id() { return 1; }});
 
-  assert.equal(this.$().text().trim(), '');
+  let program = Object.create({
+    belongsTo() {
+      return school;
+    }
+  });
 
-  // Template block usage:
-  this.render(hbs`
-    {{#curriculum-inventory-sequence-block-list}}
-      template block text
-    {{/curriculum-inventory-sequence-block-list}}
-  `);
+  let course = Object.create({
+    id: 1,
+    title: "Life Lessons",
+  });
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  let academicLevel1 = Object.create({ id: 1, level: '2'});
+  let academicLevel2 = Object.create({ id: 2, level: '1'});
+
+  let block1 = Object.create({
+    id: 1,
+    academicLevel: resolve(academicLevel1),
+    title: 'Foo',
+    startDate: new Date('2015-02-23'),
+    endDate: new Date('2016-12-03'),
+    course: resolve(course),
+    orderInSequence: 0,
+  });
+
+  let block2 = Object.create({
+    id: 2,
+    academicLevel: resolve(academicLevel2),
+    title: 'Bar',
+    startDate: null,
+    endDate: null,
+    course: resolve(null),
+    orderInSequence: 0,
+  });
+
+  let blocks = [block1, block2];
+
+  let report = Object.create({
+    academicLevels: resolve([]),
+    year: '2016',
+    program: resolve(program),
+    linkedCourses: resolve([]),
+    isFinalized: false,
+    name: 'Lorem Ipsum',
+    startDate: moment('2015-06-12').toDate(),
+    endDate: moment('2016-04-11').toDate(),
+    description: 'Lorem Ipsum',
+    topLevelSequenceBlocks: resolve(blocks)
+  });
+
+  this.set('report', report);
+  this.set('removeSequenceBlock', function() {});
+  this.render(hbs`{{curriculum-inventory-sequence-block-list report=report remove='removeSequenceBlock'}}`);
+  return wait().then(() => {
+    assert.equal(this.$('.detail-title').text().trim(), `Sequence Blocks (${blocks.length})`,
+      'Component title is correct, and show the correct number of blocks.'
+    );
+    assert.equal(this.$('.detail-actions .expand-button').length, 1, 'Add new button is visible.');
+
+    assert.equal(this.$('thead th:eq(0)').text().trim(), 'Sequence Block', 'Table column header has correct label.');
+    assert.equal(this.$('thead th:eq(1)').text().trim(), 'Level', 'Table column header has correct label.');
+    assert.equal(this.$('thead th:eq(2)').text().trim(), 'Sequence #', 'Table column header has correct label.');
+    assert.equal(this.$('thead th:eq(3)').text().trim(), 'Start Date', 'Table column header has correct label.');
+    assert.equal(this.$('thead th:eq(4)').text().trim(), 'End Date', 'Table column header has correct label.');
+    assert.equal(this.$('thead th:eq(5)').text().trim(), 'Course', 'Table column header has correct label.');
+    assert.equal(this.$('thead th:eq(6)').text().trim(), 'Actions', 'Table column header has correct label.');
+
+    assert.equal(this.$('tbody tr:eq(0) td:eq(0)').text().trim(), block2.get('title'));
+    assert.equal(this.$('tbody tr:eq(0) td:eq(1)').text().trim(), academicLevel2.get('level'));
+    assert.equal(this.$('tbody tr:eq(0) td:eq(2)').text().trim(), 'n/a');
+    assert.equal(this.$('tbody tr:eq(0) td:eq(3)').text().trim(), 'n/a');
+    assert.equal(this.$('tbody tr:eq(0) td:eq(4)').text().trim(), 'n/a');
+    assert.equal(this.$('tbody tr:eq(0) td:eq(5)').text().trim(), 'n/a');
+    assert.equal(this.$('tbody tr:eq(0) td:eq(6) .edit').length, 1, 'Edit link is visible.');
+    assert.equal(this.$('tbody tr:eq(0) td:eq(6) .remove').length, 1, 'Remove link is visible.');
+
+    assert.equal(this.$('tbody tr:eq(1) td:eq(0)').text().trim(), block1.get('title'));
+    assert.equal(this.$('tbody tr:eq(1) td:eq(1)').text().trim(), academicLevel1.get('level'));
+    assert.equal(this.$('tbody tr:eq(1) td:eq(2)').text().trim(), 'n/a');
+    assert.equal(this.$('tbody tr:eq(1) td:eq(3)').text().trim(), moment(block1.get('startDate')).format('MM/DD/YY'));
+    assert.equal(this.$('tbody tr:eq(1) td:eq(4)').text().trim(), moment(block1.get('endDate')).format('MM/DD/YY'));
+    assert.equal(this.$('tbody tr:eq(1) td:eq(5)').text().trim(), course.get('title'));
+    assert.equal(this.$('tbody tr:eq(1) td:eq(6) .edit').length, 1, 'Edit link is visible.');
+    assert.equal(this.$('tbody tr:eq(1) td:eq(6) .remove').length, 1, 'Remove link is visible.');
+
+  });
+});
+
+test('it renders with nested blocks', function(assert) {
+  assert.expect(18);
+
+  let school = Object.create({ id() { return 1; }});
+
+  let program = Object.create({
+    belongsTo() {
+      return school;
+    }
+  });
+
+  let course = Object.create({
+    id: 1,
+    title: "Life Lessons",
+  });
+
+  let academicLevel1 = Object.create({ id: 1, level: '2'});
+  let academicLevel2 = Object.create({ id: 2, level: '1'});
+
+
+  let block1 = Object.create({
+    id: 2,
+    academicLevel: resolve(academicLevel1),
+    title: 'Foo',
+    startDate: new Date('2015-02-23'),
+    endDate: new Date('2016-12-03'),
+    course: resolve(course),
+    orderInSequence: 1,
+  });
+
+  let block2 = Object.create({
+    id: 3,
+    academicLevel: resolve(academicLevel2),
+    title: 'Bar',
+    startDate: null,
+    endDate: null,
+    course: resolve(null),
+    orderInSequence: 2,
+  });
+
+  let nestedBlocks = [block1, block2];
+
+  let parentBlock = Object.create({
+    id: 1,
+    academicLevel: resolve(academicLevel1),
+    title: 'Parent Block',
+    course: resolve(null),
+    isOrdered: true,
+    children: resolve(nestedBlocks),
+  });
+
+  let report = Object.create({
+    academicLevels: resolve([]),
+    year: '2016',
+    program: resolve(program),
+    linkedCourses: resolve([]),
+    isFinalized: false,
+    name: 'Lorem Ipsum',
+    startDate: moment('2015-06-12').toDate(),
+    endDate: moment('2016-04-11').toDate(),
+    description: 'Lorem Ipsum',
+    topLevelSequenceBlocks: resolve([parentBlock]),
+    sequenceBlocks: resolve([parentBlock, block1, block2])
+  });
+
+  parentBlock.set('report', resolve(report));
+  block1.set('report', resolve(report));
+  block1.set('parent', resolve(parentBlock));
+  block2.set('report', resolve(report));
+  block2.set('parent', resolve(parentBlock));
+
+  this.set('parent', parentBlock);
+  this.set('removeSequenceBlock', function() {});
+  this.render(hbs`{{curriculum-inventory-sequence-block-list parent=parent remove='removeSequenceBlock'}}`);
+  return wait().then(() => {
+    assert.equal(this.$('.detail-title').text().trim(), `Sequence Blocks (${nestedBlocks.length})`,
+      'Component title is correct, and show the correct number of nested blocks.'
+    );
+    assert.equal(this.$('.detail-actions .expand-button').length, 1, 'Add new button is visible.');
+
+    assert.equal(this.$('tbody tr:eq(0) td:eq(0)').text().trim(), block1.get('title'));
+    assert.equal(this.$('tbody tr:eq(0) td:eq(1)').text().trim(), academicLevel1.get('level'));
+    assert.equal(this.$('tbody tr:eq(0) td:eq(2)').text().trim(), block1.get('orderInSequence'));
+    assert.equal(this.$('tbody tr:eq(0) td:eq(3)').text().trim(), moment(block1.get('startDate')).format('MM/DD/YY'));
+    assert.equal(this.$('tbody tr:eq(0) td:eq(4)').text().trim(), moment(block1.get('endDate')).format('MM/DD/YY'));
+    assert.equal(this.$('tbody tr:eq(0) td:eq(5)').text().trim(), course.get('title'));
+    assert.equal(this.$('tbody tr:eq(0) td:eq(6) .edit').length, 1, 'Edit link is visible.');
+    assert.equal(this.$('tbody tr:eq(0) td:eq(6) .remove').length, 1, 'Remove link is visible.');
+
+    assert.equal(this.$('tbody tr:eq(1) td:eq(0)').text().trim(), block2.get('title'));
+    assert.equal(this.$('tbody tr:eq(1) td:eq(1)').text().trim(), academicLevel2.get('level'));
+    assert.equal(this.$('tbody tr:eq(1) td:eq(2)').text().trim(), block2.get('orderInSequence'));
+    assert.equal(this.$('tbody tr:eq(1) td:eq(3)').text().trim(), 'n/a');
+    assert.equal(this.$('tbody tr:eq(1) td:eq(4)').text().trim(), 'n/a');
+    assert.equal(this.$('tbody tr:eq(1) td:eq(5)').text().trim(), 'n/a');
+    assert.equal(this.$('tbody tr:eq(1) td:eq(6) .edit').length, 1, 'Edit link is visible.');
+    assert.equal(this.$('tbody tr:eq(1) td:eq(6) .remove').length, 1, 'Remove link is visible.');
+
+  });
+});
+
+test('finalized/read-only mode', function(assert){
+  assert.expect(3);
+
+  let school = Object.create({ id() { return 1; }});
+
+  let program = Object.create({
+    belongsTo() {
+      return school;
+    }
+  });
+
+  let block1 = Object.create({
+    id: 1,
+    academicLevel: resolve(Object.create({ id: 1, level: '2'})),
+    title: 'Bar',
+    startDate: new Date('2015-02-23'),
+    endDate: new Date('2016-12-03'),
+    course: resolve(null),
+    orderInSequence: 0,
+  });
+  let report = Object.create({
+    academicLevels: resolve([]),
+    year: '2016',
+    program: resolve(program),
+    linkedCourses: resolve([]),
+    isFinalized: true,
+    name: 'Lorem Ipsum',
+    startDate: moment('2015-06-12').toDate(),
+    endDate: moment('2016-04-11').toDate(),
+    description: 'Lorem Ipsum',
+    topLevelSequenceBlocks: resolve([ block1 ])
+  });
+
+  this.set('report', report);
+  this.set('removeSequenceBlock', function() {});
+  this.render(hbs`{{curriculum-inventory-sequence-block-list report=report remove='removeSequenceBlock'}}`);
+  return wait().then(() => {
+    assert.equal(this.$('.detail-actions .expand-button').length, 0, 'Add new button is not visible.');
+    assert.equal(this.$('tbody tr:eq(0) td:eq(6) .edit').length, 1, 'Edit link is visible.');
+    assert.equal(this.$('tbody tr:eq(0) td:eq(6) .remove').length, 0, 'Remove link is not visible.');
+  });
+});
+
+test('delete', function(assert){
+  assert.expect(4);
+
+  let school = Object.create({ id() { return 1; }});
+
+  let program = Object.create({
+    belongsTo() {
+      return school;
+    }
+  });
+
+  let block1 = Object.create({
+    id: 1,
+    academicLevel: resolve(Object.create({ id: 1, level: '2'})),
+    title: 'Foo',
+    startDate: new Date('2015-02-23'),
+    endDate: new Date('2016-12-03'),
+    course: resolve(null),
+    orderInSequence: 0,
+  });
+
+  let report = Object.create({
+    academicLevels: resolve([]),
+    year: '2016',
+    program: resolve(program),
+    linkedCourses: resolve([]),
+    isFinalized: false,
+    name: 'Lorem Ipsum',
+    startDate: moment('2015-06-12').toDate(),
+    endDate: moment('2016-04-11').toDate(),
+    description: 'Lorem Ipsum',
+    topLevelSequenceBlocks: resolve([ block1 ])
+  });
+
+  this.set('report', report);
+  this.on('removeSequenceBlock', function(block) {
+    assert.equal(block, block1, 'Remove action was invoked, and sequence block was passed.');
+    report.set('toLevelSequenceBlocks', resolve([ block1 ])); // fake deletion.
+  });
+  this.render(hbs`{{curriculum-inventory-sequence-block-list report=report remove='removeSequenceBlock'}}`);
+  return wait().then(() => {
+    this.$('tbody tr:eq(0) td:eq(6) .remove').click();
+    assert.equal(this.$('tbody tr:eq(1) .confirm-message').text().trim().indexOf('Are you sure you want to delete'), 0,
+      'Confirmation message is visible.');
+    assert.equal(this.$('tbody tr:eq(1) .confirm-buttons .remove').length, 1,'Delete button is visible.');
+    assert.equal(this.$('tbody tr:eq(1) .confirm-buttons .done').length, 1,'Cancel button is visible.');
+    this.$('tbody tr:eq(1) .confirm-buttons .remove').click();
+  });
+});
+
+test('cancel delete', function(assert){
+  assert.expect(2);
+
+  let school = Object.create({ id() { return 1; }});
+
+  let program = Object.create({
+    belongsTo() {
+      return school;
+    }
+  });
+
+  let block1 = Object.create({
+    id: 1,
+    academicLevel: resolve(Object.create({ id: 1, level: '2'})),
+    title: 'Foo',
+    startDate: new Date('2015-02-23'),
+    endDate: new Date('2016-12-03'),
+    course: resolve(null),
+    orderInSequence: 0,
+  });
+
+  let report = Object.create({
+    academicLevels: resolve([]),
+    year: '2016',
+    program: resolve(program),
+    linkedCourses: resolve([]),
+    isFinalized: false,
+    name: 'Lorem Ipsum',
+    startDate: moment('2015-06-12').toDate(),
+    endDate: moment('2016-04-11').toDate(),
+    description: 'Lorem Ipsum',
+    topLevelSequenceBlocks: resolve([ block1 ])
+  });
+
+  this.set('report', report);
+  this.render(hbs`{{curriculum-inventory-sequence-block-list report=report}}`);
+  return wait().then(() => {
+    this.$('tbody tr:eq(0) td:eq(6) .remove').click();
+    assert.equal(this.$('tbody .confirm-message').length, 1,'Confirmation dialog is visible.');
+    this.$('tbody tr:eq(1) .confirm-buttons .done').click();
+    assert.equal(this.$('tbody .confirm-message').length, 0,'Confirmation dialog is not visible after cancelling.');
+  });
 });
