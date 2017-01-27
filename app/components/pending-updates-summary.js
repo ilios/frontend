9f4ff49a791889f935bc013e3ee67368c533fdfa
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-01-27 15:53:15 -0800

    replaced DS.PromiseArray/Object with RSVP.Promise.

--- app/components/pending-updates-summary.js
+++ app/components/pending-updates-summary.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
-import DS from 'ember-data';
-
-const { PromiseArray, PromiseObject } = DS;
 
 const { Component, inject, RSVP, computed, isPresent } = Ember;
 const { service } = inject;
+const { Promise } = RSVP;
 const { reads, gt, sort } = computed;
 
 export default Component.extend({
@@ -16,51 +14,57 @@ export default Component.extend({
   schoolId: null,
   hasMoreThanOneSchool: gt('currentUser.model.schools.length', 1),
   schools: reads('currentUser.model.schools'),
-  selectedSchool: computed('currentUser', 'schoolId', function(){
-    let defer = RSVP.defer();
 
-    this.get('currentUser').get('model').then(user => {
-      user.get('schools').then(schools => {
-        if(isPresent(this.get('schoolId'))){
-          let school =  schools.find(school => {
-            return school.get('id') === this.get('schoolId');
-          });
-          if(school){
-            defer.resolve(school);
-            return;
+  /**
+   * The currently selected school, defaults to the current-user's primary school if none is selected.
+   * @property selectedSchool
+   * @type {Ember.computed}
+   * @public
+   */
+  selectedSchool: computed('currentUser', 'schoolId', function(){
+    return new Promise(resolve => {
+      this.get('currentUser').get('model').then(user => {
+        user.get('schools').then(schools => {
+          if(isPresent(this.get('schoolId'))){
+            let school =  schools.find(school => {
+              return school.get('id') === this.get('schoolId');
+            });
+            if(school){
+              resolve(school);
+              return;
+            }
           }
-        }
-        user.get('school').then(school => {
-          defer.resolve(school);
+          user.get('school').then(school => {
+            resolve(school);
+          });
         });
       });
     });
-
-    return PromiseObject.create({
-      promise: defer.promise
-    });
-
   }),
   sortSchoolsBy:['title'],
   sortedSchools: sort('schools', 'sortSchoolsBy'),
 
+  /**
+   * A list of pending user updates.
+   * @property updates
+   * @type {Ember.computed}
+   * @public
+   */
   updates: computed('selectedSchool', function(){
-    let defer = RSVP.defer();
-    this.get('selectedSchool').then(school => {
-      this.get('store').query('pending-user-update', {
-        limit: 1000,
-        filters: {
-          schools: [school.get('id')]
-        }
-      }).then(updates => {
-        defer.resolve(updates);
+    return new Promise(resolve => {
+      this.get('selectedSchool').then(school => {
+        this.get('store').query('pending-user-update', {
+          limit: 1000,
+          filters: {
+            schools: [school.get('id')]
+          }
+        }).then(updates => {
+          resolve(updates);
+        });
       });
     });
-
-    return PromiseArray.create({
-      promise: defer.promise
-    });
   }),
+
   actions: {
     changeSelectedSchool: function(schoolId){
       this.set('schoolId', schoolId);
