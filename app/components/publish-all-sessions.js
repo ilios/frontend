ab7a72319daf112713159c26febe6b9778ea971b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-07 10:34:57 -0800

    adjusted actions and dependent comp props to handle promises.

--- app/components/publish-all-sessions.js
+++ app/components/publish-all-sessions.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 const { Component, computed, RSVP, inject } = Ember;
 const { equal } = computed;
 const { service } = inject;
-const { Promise } = RSVP;
+const { all, Promise } = RSVP;
 
 export default Component.extend({
   store: service(),
@@ -16,8 +16,18 @@ export default Component.extend({
   unPublishableCollapsed: true,
   totalSessionsToSave: null,
   currentSessionsSaved: null,
+
+  /**
+   * @property allSessionsAsIs
+   * @type {Ember.computed}
+   * @public
+   */
   allSessionsAsIs: computed('sessionsToOverride.[]', 'overridableSessions.[]', function(){
-    return this.get('sessionsToOverride').get('length') === this.get('overridableSessions').get('length');
+    return new Promise(resolve => {
+      this.get('overridableSessions').then(overridableSessions => {
+        resolve(this.get('sessionsToOverride').get('length') === overridableSessions.length);
+      });
+    });
   }),
 
   /**
@@ -71,27 +81,44 @@ export default Component.extend({
     });
   }),
 
-  publishCount: computed(
-    'publishableSessions.length',
-    'sessionsToOverride.length',
-    function(){
-      return parseInt(this.get('publishableSessions.length')) + parseInt(this.get('sessionsToOverride.length'));
-    }
-  ),
-  scheduleCount: computed(
-    'sessionsToOverride.length',
-    'overridableSessions.length',
-    function(){
-      return parseInt(this.get('overridableSessions.length')) -
-             parseInt(this.get('sessionsToOverride.length'));
-    }
-  ),
-  ignoreCount: computed(
-    'unPublishableSessions.length',
-    function(){
-      return parseInt(this.get('unPublishableSessions.length'));
-    }
-  ),
+  /**
+   * @property publishCount
+   * @type {Ember.computed}
+   * @public
+   */
+  publishCount: computed('publishableSessions.[]','sessionsToOverride.length', function() {
+    return new Promise(resolve => {
+      this.get('publishableSessions').then(publishableSessions => {
+        resolve(publishableSessions.length + parseInt(this.get('sessionsToOverride.length'), 10));
+      });
+    });
+  }),
+
+  /**
+   * @property scheduleCount
+   * @type {Ember.computed}
+   * @public
+   */
+  scheduleCount: computed('overridableSessions.[]', 'sessionsToOverride.length', function() {
+    return new Promise(resolve => {
+      this.get('overridableSessions').then(overridableSessions => {
+        resolve(overridableSessions.length - parseInt(this.get('sessionsToOverride.length'), 10));
+      });
+    });
+  }),
+
+  /**
+   * @property ignoreCount
+   * @type {Ember.computed}
+   * @public
+   */
+  ignoreCount: computed('unPublishableSessions.[]', function() {
+    return new Promise(resolve => {
+      this.get('unPublishableSessions').then(unPublishableSessions => {
+        resolve(unPublishableSessions.length);
+      });
+    });
+  }),
 
   actions: {
     toggleSession(session){
@@ -102,56 +129,77 @@ export default Component.extend({
       }
     },
     publishAllAsIs(){
-      this.get('overridableSessions').forEach(session =>{
-        if(!this.get('sessionsToOverride').includes(session)){
-          this.get('sessionsToOverride').pushObject(session);
-        }
+      this.get('overridableSessions').then(overridableSessions => {
+        overridableSessions.forEach(session => {
+          if (!this.get('sessionsToOverride').includes(session)) {
+            this.get('sessionsToOverride').pushObject(session);
+          }
+        });
       });
     },
     publishNoneAsIs(){
-      this.get('overridableSessions').forEach(session =>{
-        if(this.get('sessionsToOverride').includes(session)){
-          this.get('sessionsToOverride').removeObject(session);
-        }
+      this.get('overridableSessions').then(overridableSessions => {
+        overridableSessions.forEach(session => {
+          if(this.get('sessionsToOverride').includes(session)){
+            this.get('sessionsToOverride').removeObject(session);
+          }
+        });
       });
     },
     save(){
       this.set('isSaving', true);
       let asIsSessions = this.get('sessionsToOverride');
       let sessionsToSave = [];
-      this.get('overridableSessions').forEach(session =>{
-        session.set('publishedAsTbd', !asIsSessions.includes(session));
-        session.set('published', true);
-        sessionsToSave.pushObject(session);
+      let promises = [];
+
+      promises.pushObject(
+        new Promise(resolve => {
+          this.get('overridableSessions').then(overridableSessions => {
+            overridableSessions.forEach(session => {
+              session.set('publishedAsTbd', !asIsSessions.includes(session));
+              session.set('published', true);
+              sessionsToSave.pushObject(session);
+            });
+            resolve();
+          });
+        })
+      );
+
+      promises.pushObject(
+        new Promise(resolve => {
+          this.get('publishableSessions').then(publishableSessions => {
+            publishableSessions.forEach(session => {
+              session.set('published', true);
+              sessionsToSave.pushObject(session);
+            });
+            resolve();
+          });
+        })
+      );
+
+      all(promises).then(() => {
+        this.set('totalSessionsToSave', sessionsToSave.length);
+        this.set('currentSessionsSaved', 0);
+        this.set('isSaving', true);
+
+        let saveSomeSessions = (sessions) => {
+          let chunk = sessions.splice(0, 6);
+
+          all(chunk.invoke('save')).then(() => {
+            if (sessions.length){
+              this.set('currentSessionsSaved', this.get('currentSessionsSaved') + chunk.length);
+              saveSomeSessions(sessions);
+            } else {
+              this.set('isSaving', false);
+              this.sendAction('saved');
+              this.get('flashMessages').success('general.savedSuccessfully');
+            }
+          });
+        };
+        saveSomeSessions(sessionsToSave);
       });
-      this.get('publishableSessions').forEach(session => {
-        session.set('published', true);
-        sessionsToSave.pushObject(session);
-      });
-
-      this.set('totalSessionsToSave', sessionsToSave.length);
-      this.set('currentSessionsSaved', 0);
-      this.set('isSaving', true);
-
-
-      let saveSomeSessions = (sessions) => {
-        let chunk = sessions.splice(0, 6);
-
-        RSVP.all(chunk.invoke('save')).then(() => {
-          if (sessions.length){
-            this.set('currentSessionsSaved', this.get('currentSessionsSaved') + chunk.length);
-            saveSomeSessions(sessions);
-          } else {
-            this.set('isSaving', false);
-            this.sendAction('saved');
-            this.get('flashMessages').success('general.savedSuccessfully');
-          }
-        });
-      };
-
-      saveSomeSessions(sessionsToSave);
-
     },
+
     togglePublishableCollapsed(){
       this.set('publishableCollapsed', !this.get('publishableCollapsed'));
     },
