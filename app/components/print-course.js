c7332caf2eb73031212fad747a064848070038e6
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-10 14:20:30 -0800

    sort learning materials in course print view by position.

--- app/components/print-course.js
+++ app/components/print-course.js
@@ -15,6 +15,25 @@ export default Component.extend({
   sortedDirectors: computed.sort('course.directors', 'sortDirectorsBy'),
   sortedMeshDescriptors: computed.sort('course.meshDescriptors', 'sortTitle'),
 
+  learningMaterialSortingCallback(lm1, lm2) {
+    let pos1 = lm1.get('position');
+    let pos2 = lm2.get('position');
+    if (pos1 > pos2) {
+      return 1;
+    } else if (pos1 < pos2) {
+      return -1;
+    }
+
+    let id1 = lm1.get('id');
+    let id2 = lm2.get('id');
+    if (id1 > id2) {
+      return -1;
+    } else if (id1 < id2) {
+      return 1;
+    }
+    return 0;
+  },
+
   /**
    * A list of proxied course sessions, sorted by title.
    * @property sortedSessionProxies
@@ -34,11 +53,15 @@ export default Component.extend({
         sortTitle: ['title'],
         sortedMeshDescriptors: computed.sort('content.meshDescriptors', 'sortTitle'),
         sessionLearningMaterials: computed('content', function(){
-          let session = this.get('content').get('id');
-          return this.get('store').query('sessionLearningMaterial', {
-            filters: {
-              session
-            }
+          return new Promise(resolve => {
+            let session = this.get('content').get('id');
+            this.get('store').query('sessionLearningMaterial', {
+              filters: {
+                session
+              }
+            }).then(learningMaterials => {
+              resolve(learningMaterials.toArray().sort(this.get('learningMaterialSortingCallback')));
+            });
           });
         })
       });
@@ -58,12 +81,16 @@ export default Component.extend({
   }),
 
   courseLearningMaterials: computed('course', function(){
-    let course = this.get('course').get('id');
-    return this.get('store').query('courseLearningMaterial', {
-      filters: {
-        course
-      }
+    return new Promise(resolve => {
+      let course = this.get('course').get('id');
+      this.get('store').query('courseLearningMaterial', {
+        filters: {
+          course
+        }
+      }).then(learningMaterials => {
+        resolve(learningMaterials.toArray().sort(this.get('learningMaterialSortingCallback')));
+      });
     });
-  }),
+  })
 
 });
