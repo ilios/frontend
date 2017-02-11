ffbe0836c845cee011bf84bddcfa6eb6bcf06801
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-11 10:38:09 -0800

    refactored sorting callback out into mixin for reusability.

--- app/components/learning-materials-sort-manager.js
+++ app/components/learning-materials-sort-manager.js
@@ -1,29 +1,15 @@
 import Ember from 'ember';
+import SortableByPosition from 'ilios/mixins/sortable-by-position';
+
 
 const { Component } = Ember;
 
-export default Component.extend({
+export default Component.extend(SortableByPosition, {
   sortableObjectList: null,
   learningMaterials: null,
   didReceiveAttrs() {
-    this.set('sortableObjectList', this.get('learningMaterials').toArray().sort((lm1, lm2) => {
-      let pos1 = lm1.get('position');
-      let pos2 = lm2.get('position');
-      if (pos1 > pos2) {
-        return 1;
-      } else if (pos1 < pos2) {
-        return -1;
-      }
-
-      let id1 = lm1.get('id');
-      let id2 = lm2.get('id');
-      if (id1 > id2) {
-        return -1;
-      } else if (id1 < id2) {
-        return 1;
-      }
-      return 0;
-    }));
+    this.set('sortableObjectList',
+      this.get('learningMaterials').toArray().sort(this.get('learningMaterialSortingCallback')));
   },
   actions: {
     sortEndAction(){
