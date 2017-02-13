c219849e5410c27b43ca2652e407fe29fe544610
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-13 09:34:31 -0800

    wired up lm sort saving.

--- app/components/learning-materials-sort-manager.js
+++ app/components/learning-materials-sort-manager.js
@@ -1,23 +1,31 @@
 import Ember from 'ember';
 import SortableByPosition from 'ilios/mixins/sortable-by-position';
-
+import { task } from 'ember-concurrency';
 
 const { Component } = Ember;
 
 export default Component.extend(SortableByPosition, {
   sortableObjectList: null,
-  learningMaterials: null,
+  subject: null,
+
   didReceiveAttrs() {
     this._super(...arguments);
-    this.set('sortableObjectList',
-      this.get('learningMaterials').toArray().sort(this.get('learningMaterialSortingCallback')));
+    let subject = this.get('subject');
+    this.get('loadAttr').perform(subject);
+
   },
+
+  loadAttr: task(function * (subject) {
+    let learningMaterials = yield subject.get('learningMaterials');
+    this.set('sortableObjectList', learningMaterials.toArray().sort(this.get('learningMaterialSortingCallback')));
+  }),
+
   actions: {
     cancel(){
       this.sendAction('cancel');
     },
     save() {
-      this.sendAction('save', this.get('learningMaterials'));
+      this.sendAction('save', this.get('sortableObjectList'));
     }
   }
 });
