eed29d38514123ef901ad6d048b26d30c6dafe39
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-13 20:01:39 -0800

    integration tests for sort manager.

--- tests/integration/components/learning-materials-sort-manager-test.js
+++ tests/integration/components/learning-materials-sort-manager-test.js
@@ -1,25 +1,135 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
+import { moduleForComponent } from 'ember-qunit';
+import { test, skip } from 'qunit';
 import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
+import tHelper from "ember-i18n/helper";
+const { Object, RSVP } = Ember;
+const { resolve } = RSVP;
 
 moduleForComponent('learning-materials-sort-manager', 'Integration | Component | learning materials sort manager', {
-  integration: true
+
+  integration: true,
+
+  beforeEach: function () {
+    this.container.lookup('service:i18n').set('locale', 'en');
+    this.registry.register('helper:t', tHelper);
+  }
 });
 
 test('it renders', function(assert) {
+  assert.expect(5);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  let lm1 = Object.create({
+    title: 'Lorem Ipsum'
+  });
 
-  this.render(hbs`{{learning-materials-sort-manager}}`);
+  let lm2 = Object.create({
+    title: 'Baba Booey'
+  });
 
-  assert.equal(this.$().text().trim(), '');
+  let clm1 = Object.create({
+    id: 1,
+    learningMaterial: resolve(lm1),
+    position: 1,
+  });
 
-  // Template block usage:
-  this.render(hbs`
-    {{#learning-materials-sort-manager}}
-      template block text
-    {{/learning-materials-sort-manager}}
-  `);
+  let clm2 = Object.create({
+    id: 2,
+    learningMaterial: resolve(lm2),
+    position: 0,
+  });
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  let clms = [ clm1, clm2 ];
+
+  let subject = Object.create({
+    learningMaterials: resolve(clms)
+  });
+
+  this.set('subject', subject);
+
+  this.render(hbs`{{learning-materials-sort-manager subject=subject}}`);
+
+  return wait().then(() => {
+    assert.equal(this.$('.draggable-object').length, 2);
+    assert.equal(this.$('.draggable-object:eq(0)').text().trim(), ':: ' + lm2.get('title'));
+    assert.equal(this.$('.draggable-object:eq(1)').text().trim(), ':: ' + lm1.get('title'));
+    assert.equal(this.$('.actions .bigadd').length, 1);
+    assert.equal(this.$('.actions .bigcancel').length, 1);
+  });
+});
+
+test('cancel', function(assert) {
+  assert.expect(1);
+  let subject = Object.create({
+    learningMaterials: resolve([
+      Object.create({
+        id: 1,
+        position: 1,
+        learningMaterial: resolve(Object.create({
+          title: 'First'
+        }))
+      }),
+      Object.create({
+        id: 2,
+        position: 2,
+        learningMaterial: resolve(Object.create({
+          title: 'Second'
+        }))
+      })
+    ]),
+  });
+  this.set('subject', subject);
+  this.on('cancel', function(){
+    assert.ok(true, 'Cancel action was invoked correctly.');
+  });
+
+  this.render(hbs`{{learning-materials-sort-manager subject=subject cancel=(action 'cancel')}}`);
+
+  return wait().then(() => {
+    this.$('.actions .bigcancel').click();
+  });
+});
+
+test('save', function(assert) {
+  assert.expect(3);
+
+  let clm1 = Object.create({
+    id: 1,
+    position: 1,
+    learningMaterial: resolve(Object.create({
+      title: 'First'
+    }))
+  });
+
+  let clm2 = Object.create({
+    id: 2,
+    position: 2,
+    learningMaterial: resolve(Object.create({
+      title: 'Second'
+    }))
+  });
+
+  let clms = [ clm1, clm2 ];
+
+  let subject = Object.create({
+    learningMaterials: resolve(clms),
+  });
+  this.set('subject', subject);
+  this.on('save', function(data){
+    assert.equal(data.length, clms.length);
+    assert.ok(data.contains(clm1));
+    assert.ok(data.contains(clm2));
+  });
+
+  this.render(hbs`{{learning-materials-sort-manager subject=subject save=(action 'save')}}`);
+
+  return wait().then(() => {
+    this.$('.actions .bigadd').click();
+  });
+});
+
+skip('reorder and save', function(assert) {
+  assert.ok(false);
+  // @todo figure out how to simulate drag and drop and implement this test [ST 2017/02/13]
 });
