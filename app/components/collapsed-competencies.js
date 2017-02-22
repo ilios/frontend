244ea05928d2de4462bf6b745d4fde1fbcbcaae3
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-22 15:00:30 -0800

    replaced promise implementation.

--- app/components/collapsed-competencies.js
+++ app/components/collapsed-competencies.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
-import DS from 'ember-data';
 
 const { Component, computed, RSVP } = Ember;
-const { all, defer } = RSVP;
-const { PromiseArray } = DS;
+const { all, Promise } = RSVP;
 
 export default Component.extend({
   tagName: 'section',
@@ -25,31 +23,27 @@ export default Component.extend({
    * @public
    */
   summary: computed('subject.competencies.[]', function(){
-    let deferred = defer();
-
-    this.get('subject.competencies').then(competencies => {
-      let promises = [];
-      let schools = [];
-      competencies.forEach(competency => {
-        promises.pushObject(competency.get('school').then(school => {
-          schools.pushObject(school);
-        }));
-      });
-      all(promises).then(() => {
-        let schoolIds = schools.mapBy('id').uniq();
-        let summary = [];
-        schoolIds.forEach(id => {
-          summary.pushObject({
-            school: schools.findBy('id', id),
-            competencies: schools.filterBy('id', id)
-          });
+    return new Promise(resolve => {
+      this.get('subject.competencies').then(competencies => {
+        let promises = [];
+        let schools = [];
+        competencies.forEach(competency => {
+          promises.pushObject(competency.get('school').then(school => {
+            schools.pushObject(school);
+          }));
         });
-        deferred.resolve(summary);
+        all(promises).then(() => {
+          let schoolIds = schools.mapBy('id').uniq();
+          let summary = [];
+          schoolIds.forEach(id => {
+            summary.pushObject({
+              school: schools.findBy('id', id),
+              competencies: schools.filterBy('id', id)
+            });
+          });
+          resolve(summary);
+        });
       });
-    });
-
-    return PromiseArray.create({
-      promise: deferred.promise
     });
   })
 });
