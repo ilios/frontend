f21b96d0d58396c50e98a4f42879512b63198211
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-22 13:52:50 -0800

    replaced promise implementations.

--- app/components/session-objective-manager.js
+++ app/components/session-objective-manager.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
-import DS from 'ember-data';
 
 const { Component, computed, observer, on, ObjectProxy, RSVP, run } = Ember;
 const { debounce } = run;
-const { defer } = RSVP;
-const { PromiseArray, PromiseObject } = DS;
+const { Promise } = RSVP;
 
 const objectiveProxy = ObjectProxy.extend({
   sessionObjective: null,
@@ -18,43 +16,42 @@ export default Component.extend({
   sessionObjective: null,
   showObjectiveList: false,
   course: computed('sessionObjective.courses.[]', function(){
-    let sessionObjective = this.get('sessionObjective');
-    if(!sessionObjective){
-      return null;
-    }
-    let deferred = defer();
-    sessionObjective.get('sessions').then(function(sessions){
-      let session =  sessions.get('firstObject');
-      session.get('course').then(function(course){
-        deferred.resolve(course);
+    return new Promise(resolve => {
+      let sessionObjective = this.get('sessionObjective');
+      if(!sessionObjective){
+        resolve(null);
+        return;
+      }
+      sessionObjective.get('sessions').then(function(sessions){
+        let session =  sessions.get('firstObject');
+        session.get('course').then(function(course){
+          resolve(course);
+        });
       });
-    });
-    return PromiseObject.create({
-      promise:deferred.promise
     });
   }),
   proxiedObjectives: computed('course', 'course.objectives.[]', function(){
-    let sessionObjective = this.get('sessionObjective');
-    if(!sessionObjective){
-      return [];
-    }
-    let deferred = defer();
-    this.get('course').then(function(course){
-      if(!course){
-        deferred.resolve([]);
+    return new Promise(resolve => {
+      let sessionObjective = this.get('sessionObjective');
+      if(!sessionObjective){
+        resolve([]);
+        return;
       }
-      course.get('objectives').then(function(objectives){
-        let objectiveProxies = objectives.map(function(objective){
-          return objectiveProxy.create({
-            content: objective,
-            sessionObjective: sessionObjective,
+      this.get('course').then(function(course){
+        if(!course){
+          resolve([]);
+          return;
+        }
+        course.get('objectives').then(function(objectives){
+          let objectiveProxies = objectives.map(function(objective){
+            return objectiveProxy.create({
+              content: objective,
+              sessionObjective: sessionObjective,
+            });
           });
+          resolve(objectiveProxies.sortBy('id'));
         });
-        deferred.resolve(objectiveProxies.sortBy('id'));
       });
-    });
-    return PromiseArray.create({
-      promise: deferred.promise
     });
   }),
   watchProxiedObjectives: on('init', observer('proxiedObjectives.length', function(){
