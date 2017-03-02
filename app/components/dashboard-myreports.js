c83375501e2b3421eba499f7a4a00dcc9be9e29c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-03-01 17:26:03 -0800

    work with current user model directly to catch changes to its reports list

--- app/components/dashboard-myreports.js
+++ app/components/dashboard-myreports.js
@@ -1,24 +1,38 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
+
 
 const { computed, inject, Component, RSVP } = Ember;
 const { service } = inject;
 const { Promise, resolve } = RSVP;
 
 export default Component.extend({
+  currentUser: service(),
   reporting: service(),
   tagName: 'div',
   classNames: ['dashboard-block', 'dashboard-double-block', 'dashboard-myreports'],
   myReportEditorOn: false,
   selectedReport: null,
+  user: null,
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.get('loadAttr').perform();
+  },
+
+  loadAttr: task(function * () {
+    const user = yield this.get('currentUser').get('model');
+    this.set('user', user);
+  }),
 
   /**
    * @property sortedReports
    * @type {Ember.computed}
    * @public
    */
-  sortedReports: computed('reporting.reportsList.[]', function(){
+  sortedReports: computed('user.reports.[]', function(){
     return new Promise(resolve => {
-      this.get('reporting').get('reportsList').then(reports => {
+      this.get('user').get('reports').then(reports => {
         resolve(reports.sortBy('title'));
       });
     });
