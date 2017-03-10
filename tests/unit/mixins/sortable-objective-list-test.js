daed244c40f7b92e34dcfe25a60c117645e03904
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-03-10 12:42:14 -0800

    initial check-in of generated mixin test.

---
+++ tests/unit/mixins/sortable-objective-list-test.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import SortableObjectiveListMixin from 'ilios/mixins/sortable-objective-list';
+import { module, test } from 'qunit';
+
+module('Unit | Mixin | sortable objective list');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let SortableObjectiveListObject = Ember.Object.extend(SortableObjectiveListMixin);
+  let subject = SortableObjectiveListObject.create();
+  assert.ok(subject);
+});
