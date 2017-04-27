936041ce579977f646842c46d9da78bae3771bc8
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-04-27 16:11:32 -0700

    show session attrs in "week at a glance"

---
+++ app/components/week-glance-session-attrs.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import { task } from 'ember-concurrency';
+
+const { Component, inject } = Ember;
+const { service } = inject;
+
+export default Component.extend({
+
+  tagName: 'span',
+  classNames: ['session-attributes'],
+  userEvents: service(),
+
+  event: null,
+  session: null,
+
+  didReceiveAttrs() {
+      let event = this.get('event');
+      this.get('loadAttr').perform(event);
+ },
+
+  loadAttr: task(function * (event) {
+    let userEvents = this.get('userEvents');
+    let session = yield userEvents.getSessionForEvent(event);
+    this.set('session', session);
+  })
+});
