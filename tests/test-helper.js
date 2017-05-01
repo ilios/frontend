0c436b142ff16ba46b1ac368ca4147983d43c053
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-01 10:47:57 -0700

    Ran `ember init` and updated blueprints for v2.13

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,10 +1,11 @@
 import resolver from './helpers/resolver';
-
 import {
   setResolver
 } from 'ember-qunit';
+import { start } from 'ember-cli-qunit';
 
 import './helpers/flash-message';
 import './helpers/custom-helpers';
 
 setResolver(resolver);
+start();
