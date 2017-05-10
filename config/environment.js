0868f91f10e4ffac1a2669ee88daa8b8b9704c3f
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-10 14:42:47 -0700

    Enable prototype extensions
    Extending the array prototype is a good idea for us, it provides a much
    better interface.  Since we're the only ones who will be using this
    addon we don't need to be too careful about that.

--- config/environment.js
+++ config/environment.js
@@ -2,5 +2,14 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return { };
+  return {
+    EmberENV: {
+      EXTEND_PROTOTYPES: {
+        String: true,
+        Array: true,
+        Function: false,
+        Date: false,
+      }
+    }
+  };
 };
