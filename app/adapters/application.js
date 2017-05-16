f04dfa326bfe3e73e0a7ae77a99dc611275276cc
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-16 13:29:54 -0700

    Use the ilios-common adapter for all ember data needs

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,42 +1,6 @@
-import Ember from 'ember';
-import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import IliosAdapter from 'ilios/adapters/ilios';
 
-const { inject, computed } = Ember;
-const { service } = inject;
-const { reads } = computed;
-const { RESTAdapter } = DS;
-
-export default RESTAdapter.extend(DataAdapterMixin, {
-  iliosConfig: service(),
-
-  host: reads('iliosConfig.apiHost'),
-  namespace: reads('iliosConfig.apiNameSpace'),
-
-  coalesceFindRequests: true,
-
-  // Ember Data 2.0 Reload behavior
-  shouldReloadRecord: function() { return true; },
-  shouldReloadAll: function() { return true; },
-  shouldBackgroundReloadRecord: function() { return true; },
-  shouldBackgroundReloadAll: function() { return true; },
-
-  findMany(store, type, ids, snapshots) {
-    let url = this.urlForFindMany(ids, type.modelName, snapshots);
-
-    return this.ajax(url, 'GET', {
-      data: {
-        filters: { id: ids },
-        limit: 1000000
-      }
-    });
-  },
-
-  pathForType(type) {
-    return Ember.String.pluralize(type.camelize().toLowerCase());
-  },
-
-  sortQueryParams: false,
-
+export default IliosAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:token'
 });
