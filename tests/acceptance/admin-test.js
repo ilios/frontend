33c963e5d037eaa786c244ad65b4e2e803200419
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-16 10:49:37 -0700

    Improve admin-test
    While debugging some test issues I rewrote this test as async / await.

--- tests/acceptance/admin-test.js
+++ tests/acceptance/admin-test.js
@@ -10,7 +10,6 @@ module('Acceptance: Admin', {
   beforeEach() {
     application = startApp();
     setupAuthentication(application);
-    server.create('school');
   },
 
   afterEach() {
@@ -30,8 +29,7 @@ test('can transition to `users` route', function(assert) {
   });
 });
 
-test('can search for users', function(assert) {
-
+test('can search for users', async function(assert) {
   server.createList('user', 20, { email: 'user@example.edu' });
   server.createList('authentication', 20);
 
@@ -41,17 +39,14 @@ test('can search for users', function(assert) {
   const secondResultEmail = `${secondResult} .email`;
   const name = '.user-display-name';
 
-  visit(url);
-  fillIn(userSearch, 'son');
-  triggerEvent(userSearch, 'keyup');
-  andThen(() => {
-    assert.equal(find(secondResultUsername).text().trim(), '1 guy M. Mc1son', 'user name is correct');
-    assert.equal(find(secondResultEmail).text().trim(), 'user@example.edu', 'user email is correct');
-  });
+  await visit(url);
+  await fillIn(userSearch, 'son');
+  await triggerEvent(userSearch, 'keyup');
 
-  click(secondResultUsername);
-  andThen(() => {
-    assert.equal(currentURL(), '/users/2', 'new user profile is shown');
-    assert.equal(find(name).text().trim(), '1 guy M. Mc1son', 'user name is shown');
-  });
+  assert.equal(find(secondResultUsername).text().trim(), '1 guy M. Mc1son', 'user name is correct');
+  assert.equal(find(secondResultEmail).text().trim(), 'user@example.edu', 'user email is correct');
+
+  await click(secondResultUsername);
+  assert.equal(currentURL(), '/users/2', 'new user profile is shown');
+  assert.equal(find(name).text().trim(), '1 guy M. Mc1son', 'user name is shown');
 });
