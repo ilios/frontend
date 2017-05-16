7dde799aa4e81886523ccd36e28c20087b206ba6
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-16 14:44:09 -0700

    Narrow our testing range and test ember-data
    Ember-data needs to be part of our future testing process, which right
    now means we have to keep this file up to date manually with fixed
    version numbers. That is probably ok since we will need to lock and
    support things for each consuming app.

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,66 +2,29 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-lts-2.8',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-8'
-        },
-        resolutions: {
-          'ember': 'lts-2-8'
-        }
-      },
+      name: 'ember-2.12',
       npm: {
         devDependencies: {
-          'ember-source': null
+          'ember-source': '~2.12',
+          'ember-data': '~2.12',
         }
       }
     },
     {
       name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      },
       npm: {
         devDependencies: {
-          'ember-source': null
+          'ember-source': '^2',
+          'ember-data': '^2',
         }
       }
     },
     {
       name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
+          'ember-source': '^2.14.0-beta.1',
+          'ember-data': '^2.14.0-beta.1',
         }
       }
     },
