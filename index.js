5284fbf38d4c95fc39665ba0fdf462bb486b4554
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-16 13:50:57 -0700

    It isn't really possible to override these defaults
    We will just have to deal with the uncertainty in our own apps that
    serverVariables must be setup each time.

--- index.js
+++ index.js
@@ -3,18 +3,4 @@
 
 module.exports = {
   name: 'ilios-common',
-  config: function() {
-    let ENV = {
-      serverVariables: {
-        tagPrefix: 'iliosconfig',
-        vars: ['api-host', 'api-name-space'],
-        defaults: {
-          'api-name-space': process.env.ILIOS_API_NAMESPACE || 'api/v1',
-          'api-host': process.env.ILIOS_API_HOST || null,
-        }
-      },
-    };
-
-    return ENV;
-  },
 };
