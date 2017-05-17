8655ea6c26773fa3927c2315cdd574696c6fdf0d
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-17 12:59:14 -0700

    Use mixins from ilios-common for dashboard route and controller

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,25 +1,9 @@
 import Ember from "ember";
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import DashboardRoute from 'ilios-common/mixins/dashboard-route';
 
-const { inject, Route, RSVP } = Ember;
-const { service } = inject;
-const { hash, Promise } = RSVP;
+const { Route } = Ember;
 
-export default Route.extend(AuthenticatedRouteMixin, {
-  i18n: service(),
-  currentUser: service(),
-  store: service(),
-  today: new Date(),
+export default Route.extend(DashboardRoute, AuthenticatedRouteMixin, {
   titleToken: 'general.dashboard',
-
-  model() {
-    const store = this.get('store');
-    return new Promise(resolve => {
-      this.get('currentUser.model').then(user => {
-        const schools = user.get('schools');
-        const academicYears = store.findAll('academic-year');
-        resolve(hash({ schools, academicYears }));
-      });
-    });
-  },
 });
