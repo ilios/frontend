b83da69812e7e243a07d89141a3d8686afb48a83
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-23 21:26:39 -0700

    Destructure Ember.String

--- addon/adapters/ilios.js
+++ addon/adapters/ilios.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
-const { inject, computed } = Ember;
+const { inject, computed, String:EmberString } = Ember;
 const { service } = inject;
 const { reads } = computed;
 const { RESTAdapter } = DS;
+const { pluralize } = EmberString;
 
 export default RESTAdapter.extend({
   iliosConfig: service(),
@@ -26,7 +27,7 @@ export default RESTAdapter.extend({
   },
 
   pathForType(type) {
-    return Ember.String.pluralize(type.camelize().toLowerCase());
+    return pluralize(type.camelize().toLowerCase());
   },
 
   sortQueryParams: false,
