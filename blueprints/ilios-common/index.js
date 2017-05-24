b2e835941ccb99303a064981343d7875ce8419a2
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-05-23 21:35:15 -0700

    Require ember-simple-auth in containing apps
    We need the session service and it should be provided by our containing
    application.

---
+++ blueprints/ilios-common/index.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  description: 'Add project dependencies',
+  normalizeEntityName: function() {
+    // allows us to run ember -g ilios-frontend and not blow up
+    // because ember cli normally expects the format
+    // ember generate <entitiyName> <blueprint>
+  },
+  afterInstall: function(/* options */) {
+    return this.addAddonsToProject({ packages: [
+      'ember-simple-auth',
+    ]});
+  }
+};
