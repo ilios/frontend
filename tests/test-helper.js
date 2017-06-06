23dc6d1a3158e21601368fb4d777c0b8018aacbe
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-06-06 08:39:14 -0400

    Restore test splitting for travis
    Better, faster, moar awesome! Ember-exam 0.7 has restored this functionality
    so we can use it to speed up our CI.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,9 +3,11 @@ import {
   setResolver
 } from 'ember-qunit';
 import { start } from 'ember-cli-qunit';
+import loadEmberExam from 'ember-exam/test-support/load';
 
 import './helpers/flash-message';
 import './helpers/custom-helpers';
 
 setResolver(resolver);
+loadEmberExam();
 start();
