d659a3fb4492ebe6a782ada2407030fc47853487
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-06-22 22:06:19 -0700

    Fix bad test for back-link helper

--- tests/integration/components/back-link-test.js
+++ tests/integration/components/back-link-test.js
@@ -6,20 +6,7 @@ moduleForComponent('back-link', 'Integration | Component | back link', {
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{back-link}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#back-link}}
-      template block text
-    {{/back-link}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Back');
 });
