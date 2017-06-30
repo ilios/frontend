6e69c23596685f18da7242390a36eab3184479b9
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-06-30 14:24:24 -0700

    Preload school relationships
    Pulling these early eases pop-in effect on the school.

--- app/routes/school.js
+++ app/routes/school.js
@@ -1,8 +1,20 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-const { Route } = Ember;
+const { Route, RSVP } = Ember;
+const { hash } = RSVP;
 
 export default Route.extend(AuthenticatedRouteMixin, {
   titleToken: 'general.schools',
+  afterModel(model){
+    //preload relationships to improve the user experience
+    return hash(model.getProperties(
+      'administrators',
+      'competencies',
+      'configurations',
+      'directors',
+      'sessionTypes',
+      'vocabularies')
+    );
+  }
 });
