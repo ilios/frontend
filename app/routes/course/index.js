07a2d8b18b888b9ffd1c4f77828e88109d1e0a17
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-07-14 12:32:36 -0700

    Pre-load some course data to speed up rendering
    By pre-loading the things we need on the session list we can avoid
    several expensive round trip network calls.

--- app/routes/course/index.js
+++ app/routes/course/index.js
@@ -1,7 +1,26 @@
 import Ember from 'ember';
 
-export default  Ember.Route.extend({
-  model: function() {
-    return this.modelFor('course');
+const { Route, RSVP, inject } = Ember;
+const { all } = RSVP;
+const { service } = inject;
+
+export default  Route.extend({
+  store: service(),
+  /**
+   * Prefetch related data to limit network requests
+  */
+  afterModel(model) {
+    const store = this.get('store');
+    const courses = [model.get('id')];
+    const course = model.get('id');
+    const sessions = model.hasMany('sessions').ids();
+
+    return all([
+      store.query('session', {filters: {course}, limit: 1000}),
+      store.query('offering', {filters: {courses}, limit: 1000}),
+      store.query('ilm-session', {filters: {courses}, limit: 1000}),
+      store.query('objective', {filters: {courses}, limit: 1000}),
+      store.query('objective', {filters: {sessions}, limit: 1000}),
+    ]);
   }
 });
--- config/api-version.js
+++ config/api-version.js
@@ -1,3 +1,3 @@
 /* eslint-env node */
 
-module.exports = 'v1.20';
+module.exports = 'v1.21';
