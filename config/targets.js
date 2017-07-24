790b051f536065f97da5699f9d9332391d4ec97a
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-07-23 19:27:34 -0700

    Drop support for older browsers
    By only targeting modern evergreen browsers in our build we can
    significantly reduce the amount of extra code we generate to be
    compatible with older browsers. This will make our application smaller
    to download and faster to parse.

--- config/targets.js
+++ config/targets.js
@@ -1,9 +1,13 @@
 /* eslint-env node */
+
+// Which browsers are returned can be found at http://browserl.ist/
 module.exports = {
   browsers: [
-    'ie 9',
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions'
+    'last 1 edge versions',
+    'last 1 chrome versions',
+    'firefox esr', //actually points to the last 2 ESR releases as they overlap
+    'last 1 safari versions',
+    'last 1 ios versions',
+    'last 1 android versions',
   ]
 };
