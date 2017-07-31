f5cafccf38310906ee15ef185c0e19edddb2d71b
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-07-30 21:34:03 -0700

    Allow other versions of ember-getowner-polyfill
    The 2.0 update simply removes a deprecated way of calling this addon. We
    can allow both since it seems like addons have updated to not use the
    bad syntax.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   allowedVersions: {
-    'ember-getowner-polyfill': '^1.0.0',
+    'ember-getowner-polyfill': '^1.0.0 || ^2.0.0',
     'ember-inflector': '^1.0.0 || ^2.0.0',
     'ember-runtime-enumerable-includes-polyfill': '^1.0.0 || ^2.0.0',
     'ember-require-module': '^0.1',
