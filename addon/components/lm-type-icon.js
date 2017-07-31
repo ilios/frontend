24a2f94aadfb5884679b0125cf93239f98cef7f6
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-07-31 15:05:52 -0700

    don't destructure Ember.Component.

--- addon/components/lm-type-icon.js
+++ addon/components/lm-type-icon.js
@@ -1,10 +1,9 @@
 import Ember from 'ember';
 import layout from '../templates/components/lm-type-icon';
 
+const { computed } = Ember;
 
-const { Component, computed } = Ember;
-
-export default Component.extend({
+export default Ember.Component.extend({
   layout,
   listItem: false,
   lm: null,
