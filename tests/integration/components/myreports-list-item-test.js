c81f758c70b564525bc8e0fe7c7c4795f650971f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-08-09 09:38:15 -0700

    implemented myreports-list-item component integration tests

--- tests/integration/components/myreports-list-item-test.js
+++ tests/integration/components/myreports-list-item-test.js
@@ -1,25 +1,140 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+import wait from 'ember-test-helpers/wait';
+
+const { Service, RSVP, Object:EmberObject } = Ember;
+const { resolve } = RSVP;
 
 moduleForComponent('myreports-list-item', 'Integration | Component | myreports list item', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('custom title', function(assert) {
+  assert.expect(3);
+  const report = EmberObject.create({
+    'title': 'Lorem Ipsum'
+  });
+  this.set('report', report);
+  this.on('selectReport', (param) => {
+    assert.equal(param, report);
+  });
+  this.render(hbs`{{myreports-list-item report=report selectReport='selectReport'}}`);
+  return wait().then(()=>{
+    assert.equal(this.$().text().trim(), report.get('title'));
+    assert.equal(this.$('.clickable').length, 1);
+    this.$('.clickable').click();
+  });
+});
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('all competencies in all schools', function(assert) {
+  assert.expect(3);
+  const report = EmberObject.create({
+    'prepositionalObject': null,
+    'school': null,
+    'subject': 'competency',
+    'title': null,
+  });
+  const storeMock = Service.extend({});
+  this.register('service:store', storeMock);
+  this.set('report', report);
+  this.on('selectReport', (param) => {
+    assert.equal(param, report);
+  });
+  this.render(hbs`{{myreports-list-item report=report selectReport='selectReport'}}`);
+  return wait().then(()=>{
+    assert.equal(this.$().text().trim(), 'All Competencies in All Schools');
+    assert.equal(this.$('.clickable').length, 1);
+    this.$('.clickable').click();
+  });
+});
 
-  this.render(hbs`{{myreports-list-item}}`);
+test('all competencies in school X', function(assert) {
+  assert.expect(2);
+  const school = EmberObject.create({
+    'title': 'School of Schools'
+  });
+  const report = EmberObject.create({
+    'prepositionalObject': null,
+    'school': resolve(school),
+    'subject': 'competency',
+    'title': null,
+  });
+  const storeMock = Service.extend({});
+  this.register('service:store', storeMock);
+  this.set('report', report);
+  this.render(hbs`{{myreports-list-item report=report}}`);
+  return wait().then(()=>{
+    assert.equal(this.$().text().trim(), 'All Competencies in ' + school.get('title'));
+    assert.equal(this.$('.clickable').length, 1);
+  });
+});
 
-  assert.equal(this.$().text().trim(), '');
+test('all competencies for user X in school Y', function(assert) {
+  assert.expect(4);
+  const school = EmberObject.create({
+    'title': 'School of Schools'
+  });
+  const pObject = EmberObject.create({
+    dasherize(){
+      return 'user';
+    }
+  });
+  const userRecord = EmberObject.create({
+    fullName: 'Chip Whitley',
+  });
+  const report = EmberObject.create({
+    'prepositionalObject': pObject,
+    'school': resolve(school),
+    'subject': 'competency',
+    'title': null,
+    'prepositionalObjectTableRowId': 1,
+  });
 
-  // Template block usage:
-  this.render(hbs`
-    {{#myreports-list-item}}
-      template block text
-    {{/myreports-list-item}}
-  `);
+  const storeMock = Service.extend({
+    query(model, params) {
+      assert.equal(model, 'user');
+      assert.equal(params.filter.id, 1);
+      return resolve(new Ember.A([userRecord]));
+    }
+  });
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.register('service:store', storeMock);
+  this.set('report', report);
+  this.render(hbs`{{myreports-list-item report=report}}`);
+  return wait().then(()=>{
+    assert.equal(this.$().text().trim(), 'All Competencies for ' + userRecord.get('fullName') +  ' in ' + school.get('title'));
+    assert.equal(this.$('.clickable').length, 1);
+  });
+});
+
+test('broken report', function(assert) {
+  assert.expect(2);
+  const school = EmberObject.create({
+    'title': 'School of Schools'
+  });
+  const pObject = EmberObject.create({
+    dasherize(){
+      return 'user';
+    }
+  });
+  const report = EmberObject.create({
+    'prepositionalObject': pObject,
+    'school': resolve(school),
+    'subject': 'competency',
+    'title': null,
+    'prepositionalObjectTableRowId': 1,
+  });
+  const storeMock = Service.extend({
+    query() {
+      return resolve(new Ember.A());
+    }
+  });
+  this.register('service:store', storeMock);
+  this.set('report', report);
+  this.render(hbs`{{myreports-list-item report=report}}`);
+  return wait().then(()=>{
+    assert.equal(this.$().text().trim(), 'This report is no longer available.');
+    assert.equal(this.$('.clickable').length, 0);
+  });
 });
