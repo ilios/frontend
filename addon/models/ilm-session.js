f5064c199aa8d80ce74dd816af3def6262b5ecf1
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-08-31 22:58:39 -0700

    rm obsolete allInstructors CP.

--- addon/models/ilm-session.js
+++ addon/models/ilm-session.js
@@ -1,9 +1,4 @@
-import Ember from 'ember';
 import DS from 'ember-data';
-
-const { computed, RSVP } = Ember;
-const { PromiseArray } = DS;
-const { Promise } = RSVP;
 
 export default DS.Model.extend({
   session: DS.belongsTo('session', {async: true}),
@@ -18,23 +13,5 @@ export default DS.Model.extend({
   learners: DS.hasMany('user', {
     async: true,
     inverse: 'learnerIlmSessions'
-  }),
-  allInstructors: computed('instructors.[]', 'instructorGroups.@each.users', function(){
-    let promise = new Promise(resolve => {
-      this.get('instructorGroups').then(instructorGroups => {
-        let promises = instructorGroups.getEach('users');
-        promises.pushObject(this.get('instructors'));
-        RSVP.all(promises).then(trees => {
-          let instructors = trees.reduce((array, set) => {
-            return array.pushObjects(set.toArray());
-          }, []);
-          instructors = instructors.uniq().sortBy('lastName', 'firstName');
-          resolve(instructors);
-        });
-      });
-    });
-    return PromiseArray.create({
-      promise: promise
-    });
   }),
 });
