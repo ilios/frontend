2caf92ea3ceb49124a7c01e63cedc71f2fd0be25
Geoffrey Boushey <geoffrey.boushey@ucsf.edu>
2017-09-11 10:27:26 -0700

    include offering events related to current session

--- app/components/offering-calendar.js
+++ app/components/offering-calendar.js
@@ -17,6 +17,8 @@ export default Component.extend({
     const learnerGroups = this.get('learnerGroups');
     const startDate = this.get('startDate');
     const endDate = this.get('endDate');
+	
+	let flat = []
 
     let data = await map(learnerGroups, async learnerGroup => {
       let offerings = await learnerGroup.get('offerings');	  
@@ -32,15 +34,62 @@ export default Component.extend({
           location: offering.get("location"),
           color: "#fefefe"
         };
-        return event;
+		flat.pushObject(event);
       });
-      return events;
     });
+				
+	const session = this.get("session");
 	
-    let flat = data.reduce((flattened, obj) => {
-      return flattened.pushObjects(obj.toArray());
-    }, []);
-	
+	let offerings = session.get('offerings');
+
+    let events = await map(offerings.toArray(), async offering => {
+      let session = await offering.get('session');
+      let course = await session.get('course');
+      let event = {
+        startDate: offering.get('startDate'),
+		endDate:  moment(offering.get('endDate'), "DD-MM-YYYY").add(1, 'hours'),
+        courseTitle: course.get('title'),
+        name: session.get('title'),
+        offering: offering.get("id"),
+        location: offering.get("location"),
+        color: "#fefefe"
+      };
+	  flat.pushObject(event);
+	  
+	  // hangs on this
+	  /*
+	  let offeringLearnerGroups = await offering.get('learnerGroups');
+	  	  debugger
+	  
+	      let data2 = await map(offeringLearnerGroups, async learnerGroup2 => {
+	        let offerings2 = await learnerGroup2.get('offerings');	  
+	        let events2 = await map(offerings2.toArray(), async offering2 => {
+	          let session2 = await offering2.get('session');
+	          let course2 = await session2.get('course');
+	          let event2 = {
+	            startDate: offering2.get('startDate'),
+	            endDate: offering2.get('endDate'),
+	            courseTitle: course2.get('title'),
+	            name: session2.get('title'),
+	            offering: offering2.get("id"),
+	            location: offering2.get("location"),
+	            color: "#fefefe"
+	          };
+	  		flat.pushObject(event2);
+	        });
+	      });
+	  
+	  debugger
+	  */
+	  
+	  let offeringInstructorGroups = await offering.get('instructorGroups');
+	  
+	  
+	  
+	  debugger
+	  
+    });
+	/*
     flat.pushObject({
       startDate, 
       endDate, 
@@ -51,8 +100,8 @@ export default Component.extend({
       location: "Parnassus2", 
       color:"#00617f"
     });
-	
-    //debugger	
+	*/
+		
     return flat;
   }),
 
