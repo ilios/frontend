d9ce75ff0bd54f288c0b6fbabbe4d186cfea5395
Geoffrey Boushey <geoffrey.boushey@ucsf.edu>
2017-09-13 11:24:35 -0700

    breaking calendar data feeds into separate categories

--- app/components/offering-calendar.js
+++ app/components/offering-calendar.js
@@ -13,97 +13,83 @@ export default Component.extend({
   namespace: reads('iliosConfig.apiNameSpace'),
   offering: null,
   learnerGroup: null,
-  calendarEvents: computed('learnerGroups.[]', 'startDate', 'endDate', 'session', async function(){
-    const learnerGroups = this.get('learnerGroups');
-    const startDate = this.get('startDate');
-    const endDate = this.get('endDate');
-	
-	let flat = []
+	learnerGroupEvents: computed('learnerGroups.[]', 'startDate', 'endDate', async function () {
+	    const learnerGroups = this.get('learnerGroups');
+	    let data = await map(learnerGroups, async learnerGroup => {
+	      let offerings = await learnerGroup.get('offerings');
+	      let events = await map(offerings.toArray(), async offering => {
+	        let session = await offering.get('session');
+	        let course = await session.get('course');
+	        let event = {
+	          startDate: offering.get('startDate'),
+	          endDate: offering.get('endDate'),
+	          courseTitle: course.get('title'),
+	          name: session.get('title'),
+	          offering: offering.get("id"),
+	          location: offering.get("location"),
+	          color: "#84c444"
+	        };
 
-    let data = await map(learnerGroups, async learnerGroup => {
-      let offerings = await learnerGroup.get('offerings');	  
-      let events = await map(offerings.toArray(), async offering => {
-        let session = await offering.get('session');
-        let course = await session.get('course');
-        let event = {
-          startDate: offering.get('startDate'),
-          endDate: offering.get('endDate'),
-          courseTitle: course.get('title'),
-          name: session.get('title'),
-          offering: offering.get("id"),
-          location: offering.get("location"),
-          color: "#fefefe"
-        };
-		flat.pushObject(event);
-      });
-    });
-				
-	const session = this.get("session");
-	
-	let offerings = session.get('offerings');
-
-    let events = await map(offerings.toArray(), async offering => {
-      let session = await offering.get('session');
-      let course = await session.get('course');
-      let event = {
-        startDate: offering.get('startDate'),
-		endDate:  moment(offering.get('endDate'), "DD-MM-YYYY").add(1, 'hours'),
-        courseTitle: course.get('title'),
-        name: session.get('title'),
-        offering: offering.get("id"),
-        location: offering.get("location"),
-        color: "#fefefe"
-      };
-	  flat.pushObject(event);
-	  
-	  // hangs on this
-	  /*
-	  let offeringLearnerGroups = await offering.get('learnerGroups');
-	  	  debugger
-	  
-	      let data2 = await map(offeringLearnerGroups, async learnerGroup2 => {
-	        let offerings2 = await learnerGroup2.get('offerings');	  
-	        let events2 = await map(offerings2.toArray(), async offering2 => {
-	          let session2 = await offering2.get('session');
-	          let course2 = await session2.get('course');
-	          let event2 = {
-	            startDate: offering2.get('startDate'),
-	            endDate: offering2.get('endDate'),
-	            courseTitle: course2.get('title'),
-	            name: session2.get('title'),
-	            offering: offering2.get("id"),
-	            location: offering2.get("location"),
-	            color: "#fefefe"
-	          };
-	  		flat.pushObject(event2);
-	        });
+	        return event;
 	      });
-	  
-	  debugger
-	  */
-	  
-	  let offeringInstructorGroups = await offering.get('instructorGroups');
-	  
-	  
-	  
-	  debugger
-	  
-    });
-	/*
-    flat.pushObject({
-      startDate, 
-      endDate, 
-      courseTitle: "courseTitle2", 
-      name: 'title', 
-      isPublished: true, 
-      offering: 1, 
-      location: "Parnassus2", 
-      color:"#00617f"
-    });
-	*/
-		
-    return flat;
-  }),
+
+	      return events;
+	    });
+
+	    let flat = data.reduce((flattened, obj) => {
+	      return flattened.pushObjects(obj.toArray());
+	    }, []);
+
+	    return flat;
+	  }),
+
+	  sessionEvents: computed('session', async function () {
+	    const session = this.get('session');
+	    const offerings = await session.get('offerings');
+	    let events = await map(offerings.toArray(), async offering => {
+	      let course = await session.get('course');
+	      let event = {
+	        startDate: offering.get('startDate'),
+	        endDate: offering.get('endDate'),
+	        courseTitle: course.get('title'),
+	        name: session.get('title'),
+	        offering: offering.get("id"),
+	        location: offering.get("location"),
+	        color: "#f6f6f6"
+	      };
+
+	      return event;
+	    });
+
+	    return events;
+	  }),
+
+	  currentEvent: computed('startDate', 'endDate', 'location', 'session', async function () {
+	    const startDate = this.get('startDate');
+	    const endDate = this.get('endDate');
+	    const session = this.get('session');
+	    const location = this.get('location');
+	    const sessionType = await session.get('sessionType');
+	    const course = await session.get('course');
+	    return {
+	      startDate,
+	      endDate,
+	      courseTitle: course.get('title'),
+	      name: session.get('title'),
+	      isPublished: session.get('isPublished'),
+	      offering: 1,
+	      location,
+	      color: sessionType.get('calendarColor')
+	    };
+	  }),
+
+	  calendarEvents: computed('learnerGroupEvents.[]', 'sessionEvents.[]', 'currentEvent', async function(){
+	    const learnerGroupEvents = await this.get('learnerGroupEvents');
+	    const sessionEvents = await this.get('sessionEvents');
+	    const currentEvent = await this.get('currentEvent');
+
+	    return [].concat([currentEvent], learnerGroupEvents, sessionEvents);
+	  }),
 
   actions: {
     goForward(){
