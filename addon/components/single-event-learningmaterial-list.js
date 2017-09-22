ea584507e220cf6bd9e12711ac272d655890b101
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-09-22 16:00:35 -0700

    label blanked materials on dashboard as such.

--- addon/components/single-event-learningmaterial-list.js
+++ addon/components/single-event-learningmaterial-list.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 import layout from '../templates/components/single-event-learningmaterial-list';
 
-const { computed, isEmpty } = Ember;
-
 export default Ember.Component.extend({
   layout: layout,
   classNames: ['single-event-learningmaterial-list'],
