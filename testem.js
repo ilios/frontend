15d1e5a6c78f5186ffab75b7edc968ff7bf06c46
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-09-24 12:45:20 -0700

    Better testem config
    This matches the blueprint for ember-cli 2.15.

--- testem.js
+++ testem.js
@@ -1,18 +1,20 @@
 /* eslint-env node */
+/* eslint camelcase: 0 */
+
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  'parallel': 5,
-  "launch_in_ci": [
-    "Chrome"
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  parallel: 5,
+  launch_in_ci: [
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "Chrome"
+  launch_in_dev: [
+    'Chrome'
   ],
-  "browser_args": {
-    "Chrome": [
-      '--headless',
+  browser_args: {
+    Chrome: [
       '--disable-gpu',
+      '--headless',
       '--remote-debugging-port=9222',
       '--window-size=1440,900'
     ]
