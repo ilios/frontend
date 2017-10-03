9f69c002942284433f9e875989ea23d62094abec
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-02 17:32:35 -0700

    cleaned up user-events service. use async functions as applicable.

--- addon/services/user-events.js
+++ addon/services/user-events.js
@@ -2,70 +2,78 @@ import Ember from 'ember';
 import EventMixin from 'ilios-common/mixins/events';
 import moment from 'moment';
 
-const { inject, computed } = Ember;
+const { inject, computed, Service } = Ember;
 const { service } = inject;
 const { reads } = computed;
 
-export default Ember.Service.extend(EventMixin, {
+export default Service.extend(EventMixin, {
   store: service(),
   currentUser: service(),
   session: service(),
   commonAjax: service(),
   iliosConfig: service(),
-
   namespace: reads('iliosConfig.apiNameSpace'),
 
-  getEvents(from, to){
-    var deferred = Ember.RSVP.defer();
-    this.get('currentUser.model').then(user => {
-      if( user ){
-        let url = '';
-        const namespace = this.get('namespace');
-        if (namespace) {
-          url += '/' + namespace;
-        }
-        url += '/userevents/' + user.get('id') + '?from=' + from + '&to=' + to;
-        const commonAjax = this.get('commonAjax');
-        commonAjax.request(url).then(data => {
-          let events = data.userEvents.map(event => {
-            event.isBlanked = !event.offering && !event.ilmSession;
-            event.slug = this.getSlugForEvent(event);
-            return event;
-          }).sortBy('startDate');
+  /**
+   * Retrieves a list of user-events for the current user that occur in a given date range, sorted by start dates.
+   * @method getEvents
+   * @param {int} from
+   * @param {int} to
+   * @return {Promise.<Array>}
+   */
+  async getEvents(from, to){
+    const user = await this.get('currentUser.model');
+    if (! user) {
+      return [];
+    }
 
-          deferred.resolve(events);
-        });
-      } else {
-        deferred.resolve([]);
+    let url = '';
+    const namespace = this.get('namespace');
+    if (namespace) {
+      url += '/' + namespace;
+    }
+    url += '/userevents/' + user.get('id') + '?from=' + from + '&to=' + to;
+
+    const commonAjax = this.get('commonAjax');
+    const data = await commonAjax.request(url);
+
+    return data.userEvents.map(event => {
+      event.isBlanked = !event.offering && !event.ilmSession;
+      event.slug = this.getSlugForEvent(event);
+      return event;
+    }).sortBy('startDate');
+  },
+
+  /**
+   * Retrieves and event by it's given slug.
+   * @method getEventForSlug
+   * @param {String} slug
+   * @return {Promise.<Object>}
+   */
+  async getEventForSlug(slug){
+    const from = moment(slug.substring(1, 9), 'YYYYMMDD').hour(0);
+    const to = from.clone().hour(24);
+    const type = slug.substring(9, 10);
+    const id = parseInt(slug.substring(10), 10);
+
+    const events = await this.getEvents(from.unix(), to.unix());
+
+    return events.find(event => {
+      if(type === 'O'){
+        return parseInt(event.offering, 10) === id;
       }
-
+      if(type === 'I'){
+        return parseInt(event.ilmSession, 10) === id;
+      }
     });
-
-    return deferred.promise;
   },
-  getEventForSlug(slug){
-    let from = moment(slug.substring(1, 9), 'YYYYMMDD').hour(0);
-    let to = from.clone().hour(24);
-    let type = slug.substring(9, 10);
-    let id = parseInt(slug.substring(10));
 
-    return new Ember.RSVP.Promise(resolved => {
-      this.getEvents(from.unix(), to.unix()).then(events => {
-        let event = events.find( event => {
-          if(type === 'O'){
-            return parseInt(event.offering) === id;
-          }
-          if(type === 'I'){
-            return parseInt(event.ilmSession) === id;
-          }
-        });
-
-        resolved(event);
-      });
-    });
-
-
-  },
+  /**
+   * Generates a slug for a given event.
+   * @method getSlugForEvent
+   * @param {Object} event
+   * @return {String}
+   */
   getSlugForEvent(event){
     let slug = 'U';
     slug += moment(event.startDate).format('YYYYMMDD');
@@ -75,7 +83,6 @@ export default Ember.Service.extend(EventMixin, {
     if(event.ilmSession){
       slug += 'I' + event.ilmSession;
     }
-
     return slug;
   },
 });
--- tests/unit/services/user-events-test.js
+++ tests/unit/services/user-events-test.js
@@ -1,11 +1,174 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
+import moment from 'moment';
+
+const { Object: EmberObject, RSVP, run, Service } = Ember;
+const { resolve } = RSVP;
 
 moduleFor('service:user-events', 'Unit | Service | user events', {
-  needs: ['service:currentUser', 'service:session', 'service:commonAjax', 'service:iliosConfig']
+  integration: true,
+  beforeEach() {
+    const MockCurrentUserService = Service.extend({
+      model: resolve(EmberObject.create({
+        id: 1
+      }))
+    });
+    this.register('service:current-user', MockCurrentUserService);
+    this.inject.service('current-user', { as: 'currentUser' });
+
+    const MockCommonAjaxService = Service.extend({
+      request() {
+        return resolve({ userEvents: [] });
+      }
+    });
+    this.register('service:commonAjax', MockCommonAjaxService);
+    this.inject.service('commonAjax', { as: 'commonAjax' });
+
+    const MockIliosConfigService = Service.extend({
+      apiNameSpace: ''
+    });
+    this.register('service:iliosConfig', MockIliosConfigService);
+    this.inject.service('iliosConfig', { as: 'iliosConfig' });
+  }
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
+test('getEvents', async function(assert){
+  assert.expect(10);
+  let event1 = {
+    offering: 1,
+    startDate: '2011-04-21'
+  };
+  let event2 = {
+    ilmSession: 3,
+    startDate: '2008-09-02'
+  };
+  let event3 = {
+    startDate: '2015-11-20'
+  };
+  this.commonAjax.reopen({
+    request(url) {
+      assert.equal(url, '/userevents/1?from=1425542400000&to=1425628800000');
+      return resolve({ userEvents: [ event1, event2, event3 ] });
+    }
+  });
+  let from = moment('20150305', 'YYYYMMDD').hour(0);
+  let to = from.clone().hour(24);
+  const subject = this.subject();
+  run(async () => {
+    const events = await subject.getEvents(from, to);
+    assert.equal(events.length, 3);
+    assert.equal(events[0], event2);
+    assert.equal(events[0].isBlanked, false);
+    assert.equal(events[0].slug, 'U20080902I3');
+    assert.equal(events[1], event1);
+    assert.equal(events[1].isBlanked, false);
+    assert.equal(events[1].slug, 'U20110421O1');
+    assert.equal(events[2], event3);
+    assert.equal(events[2].isBlanked, true);
+  });
+});
+
+test('getEvents - no user', async function(assert){
+  assert.expect(1);
+  this.currentUser.reopen({
+    model: resolve(null)
+  });
+  this.commonAjax.reopen({
+    request() {
+      assert.ok(false, 'this assertion should never be called.');
+    }
+  });
+  const subject = this.subject();
+  let from = moment('20150305', 'YYYYMMDD').hour(0);
+  let to = from.clone().hour(24);
+  run( async () => {
+    let events = await subject.getEvents(from, to);
+    assert.equal(events.length, 0);
+  });
+});
+
+test('getEvents - with configured namespace', async function(assert){
+  assert.expect(2);
+  this.iliosConfig.reopen({
+    apiNameSpace: 'geflarknik'
+  });
+  this.commonAjax.reopen({
+    request(url) {
+      assert.equal(url, '/geflarknik/userevents/1?from=1425542400000&to=1425628800000');
+      return resolve({ userEvents: [] });
+    }
+  });
+  const subject = this.subject();
+  let from = moment('20150305', 'YYYYMMDD').hour(0);
+  let to = from.clone().hour(24);
+  run( async () => {
+    let events = await subject.getEvents(from, to);
+    assert.equal(events.length, 0);
+  });
+});
+
+test('getEventsForSlug - offering', async function(assert){
+  assert.expect(2);
+  let event1 = {
+    offering: 1,
+    startDate: '2011-04-21'
+  };
+  let event2 = {
+    ilmSession: 3,
+    startDate: '2008-09-02'
+  };
+  this.commonAjax.reopen({
+    request(url) {
+      assert.equal(url, '/userevents/1?from=1358755200&to=1358841600');
+      return resolve({ userEvents: [event1, event2] });
+    }
+  });
+  const subject = this.subject();
+  run( async () => {
+    let event = await subject.getEventForSlug('U20130121O1');
+    assert.equal(event, event1);
+  });
+});
+
+test('getEventsForSlug - ILM', async function(assert){
+  assert.expect(2);
+  let event1 = {
+    offering: 1,
+    startDate: '2011-04-21'
+  };
+  let event2 = {
+    ilmSession: 3,
+    startDate: '2008-09-02'
+  };
+  this.commonAjax.reopen({
+    request(url) {
+      assert.equal(url, '/userevents/1?from=1358755200&to=1358841600');
+      return resolve({ userEvents: [event1, event2] });
+    }
+  });
+  const subject = this.subject();
+  run( async () => {
+    let event = await subject.getEventForSlug('U20130121I3');
+    assert.equal(event, event2);
+  });
+});
+
+test('getSlugForEvent - offering', function(assert) {
+  assert.expect(1);
+  const service = this.subject();
+  const event = {
+    startDate: moment('2013-01-21').toDate(),
+    offering: 1
+  };
+  assert.equal(service.getSlugForEvent(event), 'U20130121O1');
+});
+
+test('getSlugForEvent - ILM', function(assert) {
+  assert.expect(1);
+  const service = this.subject();
+  const event = {
+    startDate: moment('2014-10-30').toDate(),
+    ilmSession: 1
+  };
+  assert.equal(service.getSlugForEvent(event), 'U20141030I1');
 });
