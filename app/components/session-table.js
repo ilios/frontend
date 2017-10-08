8e5f30a0f355f8672ea344b7272695042a8eaf5d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-07 17:41:10 -0700

    minor cleanup.

--- app/components/session-table.js
+++ app/components/session-table.js
@@ -15,9 +15,8 @@ export default Component.extend({
   table: computed('sortedSessions.[]', 'columns.[]', function(){
     const columns = this.get('columns');
     const sortedSessions = this.get('sortedSessions');
-    const table = new Table(columns, sortedSessions);
 
-    return table;
+    return new Table(columns, sortedSessions);
   }),
 
   columns: computed('sortInfo', function(){
@@ -118,7 +117,8 @@ export default Component.extend({
     let filterExpressions = filterBy.split(' ').map(function (string) {
       return new RegExp(string, 'gi');
     });
-    let filtered = sessions.filter(session => {
+
+    return sessions.filter(session => {
       let matchedSearchTerms = 0;
 
       for (let i = 0; i < filterExpressions.length; i++) {
@@ -129,9 +129,8 @@ export default Component.extend({
       //if the number of matching search terms is equal to the number searched, return true
       return (matchedSearchTerms === filterExpressions.length);
     });
-
-    return filtered;
   }),
+
   sortedSessions: computed('filteredSessions.[]', 'sortInfo', function(){
     const sessions = this.get('filteredSessions');
     const sortInfo = this.get('sortInfo');
@@ -141,8 +140,8 @@ export default Component.extend({
     }
 
     return sessions.sortBy(sortInfo.column);
-
   }),
+
   sortInfo: computed('sortBy', function(){
     const sortBy = this.get('sortBy');
     const parts = sortBy.split(':');
@@ -151,6 +150,7 @@ export default Component.extend({
 
     return { column, descending, sortBy };
   }),
+
   filterByDebounced: computed('filterByLocalCache', 'filterBy', function(){
     const filterBy = this.get('filterBy');
     const filterByLocalCache = this.get('filterByLocalCache');
@@ -162,7 +162,7 @@ export default Component.extend({
 
     return filterByLocalCache;
   }),
-  
+
   changeFilterBy: task(function * (value){
     const setFilterBy = this.get('setFilterBy');
     const clean = escapeRegExp(value);
@@ -170,7 +170,7 @@ export default Component.extend({
     yield timeout(250);
     setFilterBy(clean);
   }).restartable(),
-  
+
   removeSession: task(function * (session){
     session.deleteRecord();
     yield session.save();
