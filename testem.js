dd0aa8ff1d4c5e4e95774561a135f7ff033b5041
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-10-11 20:37:09 -0700

    Update blueprints for ember-cli 2.16.2

--- testem.js
+++ testem.js
@@ -4,7 +4,7 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  parallel: 5,
+  parallel: -1,
   launch_in_ci: [
     'Chrome'
   ],
@@ -12,11 +12,14 @@ module.exports = {
     'Chrome'
   ],
   browser_args: {
-    Chrome: [
-      '--disable-gpu',
-      '--headless',
-      '--remote-debugging-port=9222',
-      '--window-size=1440,900'
-    ]
+    Chrome: {
+      mode: 'ci',
+      args: [
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=9222',
+        '--window-size=1440,900'
+      ]
+    },
   }
 };
