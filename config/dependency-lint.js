5d3046e1d1eb4d7cebc12fe82b5ab23e0b2aa6a3
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-10-16 22:00:19 -0700

    Remove no longer needed allowedVersions
    These conflicts have been resolved in the ecosystem and we don't need
    to worry about them anymore.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -6,7 +6,5 @@ module.exports = {
     'ember-cli-string-helpers': '^1.4.0', //temporary workaround for conflict with ember-light-table
     'ember-getowner-polyfill': '^1.0.0 || ^2.0.0',
     'ember-inflector': '^1.0.0 || ^2.0.0',
-    'ember-runtime-enumerable-includes-polyfill': '^1.0.0 || ^2.0.0',
-    'ember-require-module': '^0.1',
   }
 };
