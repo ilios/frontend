f7303651a2ebadbf65a5785dbd0b1c255c2dcbf8
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-23 14:37:21 -0700

    wip: integration tests for program-list component.

---
+++ tests/integration/components/program-list-test.js
@@ -0,0 +1,34 @@
+import EmberObject from '@ember/object';
+import { run } from '@ember/runloop';
+import { moduleForComponent, skip } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
+
+
+moduleForComponent('program-list', 'Integration | Component | program list', {
+  integration: true
+});
+
+skip('it renders', function(){
+  // @todo implement [ST 2017/10/23]
+});
+
+skip('empty list', function(){
+  // @todo implement [ST 2017/10/23]
+});
+
+skip('edit', function(){
+  // @todo implement [ST 2017/10/23]
+});
+
+skip('remove', function(){
+  // @todo implement [ST 2017/10/23]
+});
+
+skip('confirm remove', function(){
+  // @todo implement [ST 2017/10/23]
+});
+
+skip('cancel remove', function(){
+  // @todo implement [ST 2017/10/23]
+});
