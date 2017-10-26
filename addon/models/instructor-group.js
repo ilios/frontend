0ab1a61ed0d799023d1d8696375ca46052c2e290
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-25 21:03:49 -0700

    async/await syntax in instructor group model.

--- addon/models/instructor-group.js
+++ addon/models/instructor-group.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 
 const { attr, belongsTo, hasMany, Model } = DS;
 const { RSVP, computed } = Ember;
-const { Promise, map } = RSVP;
+const { map } = RSVP;
 
 export default Model.extend({
   title: attr('string'),
@@ -13,50 +13,30 @@ export default Model.extend({
   users: hasMany('user', {async: true}),
   offerings: hasMany('offering', {async: true}),
 
-  coursesFromOfferings: computed('offerings.[]', function(){
-    return new Promise(resolve => {
-      this.get('offerings').then(offerings => {
-        map(offerings.toArray(), offering => {
-          return new Promise(resolve => {
-            offering.get('session').then(session => {
-              session.get('course').then(course => {
-                resolve(course);
-              });
-            });
-          });
-        }).then(courses => {
-          resolve(courses.uniq());
-        });
-      });
+  coursesFromOfferings: computed('offerings.[]', async function () {
+    const offerings = await this.get('offerings');
+    const courses = await map(offerings.toArray(), async ilmSession => {
+      const session = await ilmSession.get('session');
+      return session.get('course');
     });
+    return courses.uniq();
   }),
-  coursesFromIlmSessions: computed('ilmSessions.[]', function(){
-    return new Promise(resolve => {
-      this.get('ilmSessions').then(ilmSessions => {
-        map(ilmSessions.toArray(), ilmSession => {
-          return new Promise(resolve => {
-            ilmSession.get('session').then(session => {
-              session.get('course').then(course => {
-                resolve(course);
-              });
-            });
-          });
-        }).then(courses => {
-          resolve(courses.uniq());
-        });
-      });
+
+  coursesFromIlmSessions: computed('ilmSessions.[]', async function () {
+    const ilmSessions = await this.get('ilmSessions');
+    const courses = await map(ilmSessions.toArray(), async ilmSession => {
+      const session = await ilmSession.get('session');
+      return session.get('course');
     });
+    return courses.uniq();
   }),
-  courses: computed('coursesFromOfferings.[]', 'coursesFromIlmSessions.[]', function(){
-    return new Promise(resolve => {
-      this.get('coursesFromOfferings').then(offeringCourses => {
-        this.get('coursesFromIlmSessions').then(ilmCourses => {
-          let courses = [];
-          courses.pushObjects(offeringCourses);
-          courses.pushObjects(ilmCourses);
-          resolve(courses.uniqBy('id'));
-        });
-      });
-    });
-  }),
+
+  courses: computed('coursesFromOfferings.[]', 'coursesFromIlmSessions.[]', async function () {
+    const offeringCourses = await this.get('coursesFromOfferings');
+    const ilmCourses = await this.get('coursesFromIlmSessions');
+    const courses = [];
+    courses.pushObjects(offeringCourses);
+    courses.pushObjects(ilmCourses);
+    return courses.uniqBy('id');
+  })
 });
