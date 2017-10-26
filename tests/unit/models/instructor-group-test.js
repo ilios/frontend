eab094ee8afae89232c890c8da8a1052ae78637b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-25 21:04:14 -0700

    async/await in unit test.

--- tests/unit/models/instructor-group-test.js
+++ tests/unit/models/instructor-group-test.js
@@ -5,8 +5,7 @@ import {
 import Ember from 'ember';
 import modelList from '../../helpers/model-list';
 
-const { RSVP, run } = Ember;
-const { Promise } = RSVP;
+const { run } = Ember;
 
 moduleForModel('instructor-group', 'Unit | Model | InstructorGroup', {
   needs: modelList
@@ -18,33 +17,28 @@ test('it exists', function(assert) {
   assert.ok(!!model);
 });
 
-test('list courses', function(assert) {
+test('list courses', async function(assert) {
   assert.expect(3);
-  return new Promise(resolve => {
-    run(()=> {
-      let model = this.subject();
-      let store = model.store;
-      let course1 = store.createRecord('course', {title:'course1', id: 1});
-      let course2 = store.createRecord('course', {title:'course2', id: 2});
-      let session1 = store.createRecord('session', {course: course1});
-      let session2 = store.createRecord('session', {course: course1});
-      let session3 = store.createRecord('session', {course: course2});
-      model.get('offerings').then(function(offerings){
-        offerings.pushObject(store.createRecord('offering', {session: session1}));
-        offerings.pushObject(store.createRecord('offering', {session: session1}));
-        offerings.pushObject(store.createRecord('offering', {session: session1}));
-        offerings.pushObject(store.createRecord('offering', {session: session2}));
-        offerings.pushObject(store.createRecord('offering', {session: session2}));
-        offerings.pushObject(store.createRecord('offering', {session: session3}));
-      });
-      model.get('courses').then(function(courses){
-        assert.equal(courses.length, 2);
-        assert.equal(courses.objectAt(0).get('title'), 'course1');
-        assert.equal(courses.objectAt(1).get('title'), 'course2');
+  run(async ()=> {
+    const model = this.subject();
+    const store = model.store;
+    const course1 = store.createRecord('course', {title:'course1', id: 1});
+    const course2 = store.createRecord('course', {title:'course2', id: 2});
+    const session1 = store.createRecord('session', {course: course1});
+    const session2 = store.createRecord('session', {course: course1});
+    const session3 = store.createRecord('session', {course: course2});
+    model.get('offerings').pushObjects([
+      store.createRecord('offering', {session: session1}),
+      store.createRecord('offering', {session: session1}),
+      store.createRecord('offering', {session: session1}),
+      store.createRecord('offering', {session: session2}),
+      store.createRecord('offering', {session: session2}),
+      store.createRecord('offering', {session: session3}),
+    ]);
 
-        resolve();
-      });
-    });
+    const courses = await model.get('courses');
+    assert.equal(courses.length, 2);
+    assert.equal(courses.objectAt(0).get('title'), 'course1');
+    assert.equal(courses.objectAt(1).get('title'), 'course2');
   });
-
 });
