422b0bef43d18727a7cf9c95278695987c2126a7
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-26 17:27:23 -0700

    test all the CPs.

--- tests/unit/models/cohort-test.js
+++ tests/unit/models/cohort-test.js
@@ -1,7 +1,5 @@
-import {
-  moduleForModel,
-  test
-} from 'ember-qunit';
+import { moduleForModel,  test, skip } from 'ember-qunit';
+import moment from 'moment';
 import modelList from '../../helpers/model-list';
 import Ember from 'ember';
 
@@ -41,3 +39,117 @@ test('list root level groups', async function(assert) {
     assert.equal(topLevelGroups.objectAt(1).get('title'), 'Top Group 2');
   });
 });
+
+test('competencies', async function(assert){
+  assert.expect(3);
+  const model = this.subject();
+  const store = this.store();
+  run( async () => {
+    const competency1 = store.createRecord('competency');
+    const competency2 = store.createRecord('competency');
+    const programYear = store.createRecord('program-year', { competencies: [ competency1, competency2 ] });
+    model.set('programYear', programYear);
+    const competencies = await model.get('competencies');
+    assert.equal(competencies.length, 2);
+    assert.ok(competencies.includes(competency1));
+    assert.ok(competencies.includes(competency2));
+  });
+});
+
+test('objectives', async function(assert){
+  assert.expect(3);
+  const model = this.subject();
+  const store = this.store();
+  run( async () => {
+    const objective1 = store.createRecord('objective');
+    const objective2 = store.createRecord('objective');
+    const programYear = store.createRecord('program-year', { objectives: [ objective1, objective2 ] });
+    model.set('programYear', programYear);
+    const objectives = await model.get('objectives');
+    assert.equal(objectives.length, 2);
+    assert.ok(objectives.includes(objective1));
+    assert.ok(objectives.includes(objective2));
+  });
+});
+
+test('currentLevel', async function(assert){
+  assert.expect(1);
+  const model = this.subject();
+  const store = this.store();
+  run( async () => {
+    const twoYearsAgo = moment().subtract(2, 'years').year();
+    const programYear = store.createRecord('programYear', { startYear: twoYearsAgo});
+    model.set('programYear', programYear);
+    const currentLevel = await model.get('currentLevel');
+    assert.equal(currentLevel, 2);
+  });
+});
+
+test('currentLevel - no start year', async function(assert){
+  assert.expect(1);
+  const model = this.subject();
+  const store = this.store();
+  run( async () => {
+    const programYear = store.createRecord('programYear');
+    model.set('programYear', programYear);
+    const currentLevel = await model.get('currentLevel');
+    assert.equal(currentLevel, '');
+  });
+});
+
+test('program', async function(assert){
+  assert.expect(1);
+  const model = this.subject();
+  const store = this.store();
+  run( async () => {
+    const program1 = store.createRecord('program');
+    const programYear = store.createRecord('program-year', { program: program1 });
+    model.set('programYear', programYear);
+    const program = await model.get('program');
+    assert.equal(program, program1);
+  });
+});
+
+test('school', async function(assert){
+  assert.expect(1);
+  const model = this.subject();
+  const store = this.store();
+  run( async () => {
+    const school1 = store.createRecord('school');
+    const program = store.createRecord('program', { school: school1 });
+    const programYear = store.createRecord('program-year', { program });
+    model.set('programYear', programYear);
+    const school = await model.get('school');
+    assert.equal(school, school1);
+  });
+});
+
+test('objectives', async function(assert){
+  assert.expect(3);
+  const model = this.subject();
+  const store = this.store();
+  run( async () => {
+    const objective1 = store.createRecord('objective', { position: 10, title: 'Aardvark' });
+    const objective2 = store.createRecord('objective', { position: 1, title: 'Zeppelin' });
+    const programYear = store.createRecord('program-year', { objectives: [ objective1, objective2 ] });
+    model.set('programYear', programYear);
+    const objectives = await model.get('sortedObjectives');
+    assert.equal(objectives.length, 2);
+    assert.equal(objectives[0], objective2);
+    assert.equal(objectives[1], objective1);
+  });
+});
+
+test('classOfYear', async function(assert){
+  assert.expect(1);
+  const model = this.subject();
+  const store = this.store();
+  run( async () => {
+    const program = store.createRecord('program', { duration: 4 });
+    const programYear = store.createRecord('program-year', { program, startYear: 2012 });
+    model.set('programYear', programYear);
+    const classOfYear = await model.get('classOfYear');
+    assert.equal(classOfYear, 2016);
+  });
+});
+
