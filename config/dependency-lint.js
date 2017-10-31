8d5e94ab562572e02c2602733f02a5c521beed16
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-31 13:29:35 -0700

    rm obsolete exclusion.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -3,7 +3,6 @@
 
 module.exports = {
   allowedVersions: {
-    'ember-cli-string-helpers': '^1.4.0', //temporary workaround for conflict with ember-light-table
     'ember-getowner-polyfill': '^1.0.0 || ^2.0.0',
     'ember-inflector': '^1.0.0 || ^2.0.0',
   }
