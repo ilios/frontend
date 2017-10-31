7e385788b822d070bb68aa23e6678674cc2bf5e3
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-31 13:56:51 -0700

    rm obsolete models and tests for them.

--- addon/models/alert-change-type.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  title: DS.attr('string'),
-  alerts: DS.hasMany('alert', {async: true}),
-});
--- tests/helpers/model-list.js
+++ tests/helpers/model-list.js
@@ -5,8 +5,6 @@ const models = [
   'model:aamc-pcrs',
   'model:aamc-resource-type',
   'model:academic-year',
-  'model:alert-change-type',
-  'model:alert',
   'model:assessment-option',
   'model:authentication',
   'model:cohort',
