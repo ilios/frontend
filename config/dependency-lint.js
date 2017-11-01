dff169934dafcd453bf13636653dbabe9ebbda99
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-11-01 08:37:30 -0700

    rm exception for ember-weakmap from dependency linter config.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -5,7 +5,6 @@ module.exports = {
   allowedVersions: {
     'ember-getowner-polyfill': '^1.0.0 || ^2.0.0',
     'ember-inflector': '^1.0.0 || ^2.0.0',
-    'ember-weakmap': '^2.0.0 || ^3.1.1', // workaround for dependency conflict between ember-moment and liquid-tether
     'ember-hash-helper-polyfill': '^0.1.2 || ^0.2.0' // workaround for dep. conflict ember-tooltips/liquid-fire
   }
 };
