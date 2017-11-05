c371d27fb131e21fd47f7f7d5e0e9f16207bbbb3
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-11-05 00:04:31 -0700

    Add target icons for different systems
    Apple requires icons that do not have a transparent background otherwise
    the background gets converted to black when it is added to the home
    screen.
    
    Microsoft has some weird sizes that it requires
    
    Also added support for various favicon sizes so the default favicon.ico
    isn't used.

--- config/manifest.js
+++ config/manifest.js
@@ -1,4 +1,5 @@
 /* eslint-env node */
+/* eslint camelcase: 0 */
 'use strict';
 
 module.exports = function (/* environment, appConfig */) {
@@ -15,42 +16,97 @@ module.exports = function (/* environment, appConfig */) {
     theme_color: "#c60",
     icons: [
       {
-        "src": "assets/icons/ilios-sunburst16.png",
-        "sizes": "16x16"
+        src: "assets/icons/ilios-sunburst16.png",
+        sizes: "16x16",
+        type: "image/png",
+        targets: ['manifest', 'favicon', 'ms'],
       },
       {
-        "src": "assets/icons/ilios-sunburst32.png",
-        "sizes": "32x32"
+        src: "assets/icons/ilios-sunburst32.png",
+        sizes: "32x32",
+        type: "image/png",
+        targets: ['manifest', 'favicon', 'ms'],
       },
       {
-        "src": "assets/icons/ilios-sunburst48.png",
-        "sizes": "48x48"
+        src: "assets/icons/ilios-sunburst48.png",
+        sizes: "48x48",
+        type: "image/png",
+        targets: ['manifest', 'favicon', 'ms'],
       },
       {
-        "src": "assets/icons/ilios-sunburst72.png",
-        "sizes": "72x72"
+        src: "assets/icons/ilios-sunburst96.png",
+        sizes: "96x96",
+        type: "image/png",
+        targets: ['manifest', 'favicon', 'ms'],
       },
       {
-        "src": "assets/icons/ilios-sunburst128.png",
-        "sizes": "128x128"
+        src: "assets/icons/ilios-sunburst128.png",
+        sizes: "128x128",
+        type: "image/png",
+        targets: ['manifest', 'ms'],
       },
       {
-        "src": "assets/icons/ilios-sunburst180.png",
-        "sizes": "180x180"
+        src: "assets/icons/ilios-sunburst256.png",
+        sizes: "256x256",
+        type: "image/png",
+        targets: ['manifest', 'ms'],
       },
       {
-        "src": "assets/icons/ilios-sunburst144.png",
-        "sizes": "144x144"
+        src: "assets/icons/ilios-sunburst512.png",
+        sizes: "512x512",
+        type: "image/png",
+        targets: ['manifest', 'ms'],
+      },
+      // apple requires special icons with a white background
+      {
+        src: "assets/icons/ilios-sunburst-apple120.png",
+        sizes: "120x120",
+        type: "image/png",
+        targets: ['apple'],
       },
       {
-        "src": "assets/icons/ilios-sunburst256.png",
-        "sizes": "256x256"
+        src: "assets/icons/ilios-sunburst-apple152.png",
+        sizes: "152x152",
+        type: "image/png",
+        targets: ['apple'],
       },
       {
-        "src": "assets/icons/ilios-sunburst512.png",
-        "sizes": "512x512"
+        src: "assets/icons/ilios-sunburst-apple167.png",
+        sizes: "167x167",
+        type: "image/png",
+        targets: ['apple'],
+      },
+      {
+        src: "assets/icons/ilios-sunburst-apple180.png",
+        sizes: "180x180",
+        type: "image/png",
+        targets: ['apple'],
+      },
+      // MS requires special icon sizes
+      {
+        src: "assets/icons/ilios-sunburst70.png",
+        sizes: "70x70",
+        type: "image/png",
+        targets: ['ms'],
+      },
+      {
+        src: "assets/icons/ilios-sunburst270.png",
+        sizes: "270x270",
+        type: "image/png",
+        targets: ['ms'],
+      },
+      {
+        src: "assets/icons/ilios-sunburst310.png",
+        sizes: "310x310",
+        type: "image/png",
+        targets: ['ms'],
       },
     ],
+
+    apple: {
+      statusBarStyle: 'black',
+      precomposed: true,
+    },
 
     ms: {
       tileColor: '#c60'
