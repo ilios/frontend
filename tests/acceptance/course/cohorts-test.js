2674059fba9e10246ba28af4775c4ed9075c3bdb
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-11-06 09:59:32 -0800

    Update cohort test for new mirage

--- tests/acceptance/course/cohorts-test.js
+++ tests/acceptance/course/cohorts-test.js
@@ -6,54 +6,41 @@ import {
 import startApp from 'ilios/tests/helpers/start-app';
 import setupAuthentication from 'ilios/tests/helpers/setup-authentication';
 
-var application;
-var fixtures = {};
-var url = '/courses/1?details=true&courseObjectiveDetails=true';
+let application;
+let url = '/courses/1?details=true&courseObjectiveDetails=true';
 module('Acceptance: Course - Cohorts', {
   beforeEach: function() {
     application = startApp();
-    setupAuthentication(application);
-    server.create('school');
+    const school = server.create('school');
+    setupAuthentication(application, {id: 4136, school});
     server.create('academicYear', {id: 2013});
-    server.create('program', {
-      programYears: [1,2]
+    const program = server.create('program', { school });
+    const cohort1 = server.create('cohort');
+    const cohort2 = server.create('cohort');
+    const programYear1 = server.create('programYear', {
+      program,
+      cohort: cohort1,
     });
-    server.create('programYear', {
-      program: 1,
-      cohort: 1,
-      objectives: [1]
+    const programYear2 = server.create('programYear', {
+      program,
+      cohort: cohort2,
     });
-    server.create('programYear', {
-      program: 1,
-      cohort: 2,
-      objectives: [2]
+    const parentObjective1 = server.create('objective', {
+      programYears: [programYear1],
     });
-    fixtures.parentObjective1 = server.create('objective', {
-      programYears: [1],
-      children: [3],
+    const parentObjective2 = server.create('objective', {
+      programYears: [programYear2],
     });
-    fixtures.parentObjective2 = server.create('objective', {
-      programYears: [2],
-      children: [3],
-    });
-    fixtures.courseObjective = server.create('objective', {
-      courses: [1],
-      parents: [1,2]
-    });
-    fixtures.cohorts = [];
-    fixtures.cohorts.pushObject(server.create('cohort', {
-      courses: [1],
-      programYear: 1
-    }));
-    fixtures.cohorts.pushObject(server.create('cohort', {
-      programYear: 2
-    }));
 
-    fixtures.course = server.create('course', {
+    const course = server.create('course', {
       year: 2013,
-      school: 1,
-      cohorts: [1],
-      objectives: [3]
+      school,
+      cohorts: [cohort1],
+    });
+
+    server.create('objective', {
+      courses: [course],
+      parents: [parentObjective1, parentObjective2]
     });
   },
 
@@ -62,18 +49,15 @@ module('Acceptance: Course - Cohorts', {
   }
 });
 
-test('list cohorts', async function(assert) {
+test('list cohorts', async function (assert) {
   assert.expect(4);
   await visit(url);
   var container = find('.detail-cohorts');
   var rows = find('tbody tr', container);
-  assert.equal(rows.length, fixtures.course.cohorts.length);
-  for(let i = 0; i < fixtures.course.cohorts.length; i++){
-    let cohort = fixtures.cohorts[fixtures.course.cohorts[i] - 1];
-    assert.equal(getElementText(find('td:eq(0)', rows[i])), getText('school 0'));
-    assert.equal(getElementText(find('td:eq(1)', rows[i])), getText('program 0'));
-    assert.equal(getElementText(find('td:eq(2)', rows[i])), getText(cohort.title));
-  }
+  assert.equal(rows.length, 1);
+  assert.equal(getElementText(find('td:eq(0)', rows[0])), getText('school 0'));
+  assert.equal(getElementText(find('td:eq(1)', rows[0])), getText('program 0'));
+  assert.equal(getElementText(find('td:eq(2)', rows[0])), getText(`cohort 0`));
 });
 
 test('manage cohorts', async function(assert) {
@@ -96,7 +80,7 @@ test('save cohort chages', async function(assert) {
 
   assert.equal(getElementText(find('tbody tr:eq(0) td:eq(0)', container)), getText('school 0'));
   assert.equal(getElementText(find('tbody tr:eq(0) td:eq(1)', container)), getText('program 0'));
-  assert.equal(getElementText(find('tbody tr:eq(0) td:eq(2)', container)), getText(fixtures.cohorts[1].title));
+  assert.equal(getElementText(find('tbody tr:eq(0) td:eq(2)', container)), getText('cohort 1'));
 });
 
 test('cancel cohort changes', async function(assert) {
@@ -109,7 +93,7 @@ test('cancel cohort changes', async function(assert) {
   await click('button.bigcancel', container);
   assert.equal(getElementText(find('tbody tr:eq(0) td:eq(0)', container)), getText('school 0'));
   assert.equal(getElementText(find('tbody tr:eq(0) td:eq(1)', container)), getText('program 0'));
-  assert.equal(getElementText(find('tbody tr:eq(0) td:eq(2)', container)), getText(fixtures.cohorts[0].title));
+  assert.equal(getElementText(find('tbody tr:eq(0) td:eq(2)', container)), getText('cohort 0'));
 });
 
 test('removing a cohort remove course objectives parents linked to that cohort', async function(assert) {
@@ -122,5 +106,5 @@ test('removing a cohort remove course objectives parents linked to that cohort',
   await click('.detail-cohorts button.bigadd');
   parents = find('.course-objective-list tbody tr:eq(0) td:eq(1) .link');
   assert.equal(parents.length, 1);
-  assert.equal(getElementText(parents), getText(fixtures.parentObjective2.title));
+  assert.equal(getElementText(parents), getText('objective 1'));
 });
