e7d974971e4323a8508ecf7cbdc309569488332e
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-11-13 13:30:00 -0800

    added missing semicolon.

--- app/components/detail-learnergroups-list.js
+++ app/components/detail-learnergroups-list.js
@@ -37,7 +37,7 @@ export default Component.extend({
         return EmberObject.create({
           group,
           sortTitle
-        })
+        });
       });
 
       return EmberObject.create({
