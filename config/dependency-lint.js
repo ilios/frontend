52c592d303fa43837c85c01cad69e787d74c6c93
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-11-13 14:12:25 -0800

    Allow range of ember-get-config versions

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -3,6 +3,7 @@
 
 module.exports = {
   allowedVersions: {
+    'ember-get-config': '^0.2.0', // workaround for dep. conflict ember-cli-mirage/ember-light-table
     'ember-getowner-polyfill': '^1.0.0 || ^2.0.0',
     'ember-inflector': '^1.0.0 || ^2.0.0',
     'ember-hash-helper-polyfill': '^0.1.2 || ^0.2.0' // workaround for dep. conflict ember-tooltips/liquid-fire
