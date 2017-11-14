43aae9099211608126f53fac73be7a3932a542e7
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-11-14 15:40:28 -0800

    ditched getter/readonly from CP.

--- app/components/offering-editor-learnergroups.js
+++ app/components/offering-editor-learnergroups.js
@@ -8,30 +8,28 @@ const { map } = RSVP;
 export default Component.extend({
   classNames: ['offering-editor-learnergroups'],
 
-  revisedLearnerGroups: computed('cohort.filteredAvailableLearnerGroups.[]', {
-    async get() {
-      const cohortId = this.get('cohort.id');
-      let learnerGroups = this.get('learnerGroups')[cohortId];
+  revisedLearnerGroups: computed('cohort.filteredAvailableLearnerGroups.[]', async function() {
+    const cohortId = this.get('cohort.id');
+    let learnerGroups = this.get('learnerGroups')[cohortId];
 
-      if (isPresent(learnerGroups)) {
-        return await map(learnerGroups, async group => {
-          let groupObject = {};
-          let parentTitle = await group.get('allParentTitles');
-          groupObject.group = group;
+    if (isPresent(learnerGroups)) {
+      return await map(learnerGroups, async group => {
+        let groupObject = {};
+        let parentTitle = await group.get('allParentTitles');
+        groupObject.group = group;
 
-          if (isEmpty(parentTitle)) {
-            groupObject.sortName = group.get('title');
-          } else {
-            groupObject.sortName = `${parentTitle[0]} > ${group.get('title')}`;
-          }
+        if (isEmpty(parentTitle)) {
+          groupObject.sortName = group.get('title');
+        } else {
+          groupObject.sortName = `${parentTitle[0]} > ${group.get('title')}`;
+        }
 
-          return groupObject;
-        });
-      }
-
-      return [];
+        return groupObject;
+      });
     }
-  }).readOnly(),
+
+    return [];
+  }),
 
 
   sortedLearnerGroups: computed('revisedLearnerGroups.[]', async function() {
