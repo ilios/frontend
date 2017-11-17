d2470d32327b7880cdb3bcd97a2d0c5b6a087595
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-11-17 13:53:09 -0800

    Clean up the manifest.js file a bit
    DRY up this file instead of needing to repeat each declaration.

--- config/manifest.js
+++ config/manifest.js
@@ -16,52 +16,34 @@ module.exports = function (/* environment, appConfig */) {
     theme_color: "#cc6600",
     icons: [
       //favicon icons get a transparent background
-      {
-        src: "assets/icons/sunburst-transparent16.png",
-        sizes: "16x16",
+      ...[16, 32, 48, 96].map(size => ({
+        src: `/assets/icons/sunburst-transparent${size}.png`,
+        sizes: `${size}x${size}`,
         type: "image/png",
         targets: ['favicon'],
-      },
+      })),
       {
-        src: "assets/icons/sunburst-transparent32.png",
-        sizes: "32x32",
-        type: "image/png",
-        targets: ['favicon'],
-      },
-      {
-        src: "assets/icons/sunburst-transparent48.png",
-        sizes: "48x48",
-        type: "image/png",
-        targets: ['favicon'],
-      },
-      {
-        src: "assets/icons/sunburst-white-background96.png",
-        sizes: "96x96",
-        type: "image/png",
-        targets: ['favicon'],
-      },
-      {
-        src: "assets/icons/sunburst-white-background192.png",
+        src: "/assets/icons/sunburst-white-background192.png",
         sizes: "192x192",
         type: "image/png",
         targets: ['manifest'],
       },
       {
-        src: "assets/icons/sunburst-transparent512.png",
+        src: "/assets/icons/sunburst-transparent512.png",
         sizes: "512x512",
         type: "image/png",
         targets: ['manifest'],
       },
       // apple icons end up in index.html as link tags so we should limit them to specific ones
       {
-        src: "assets/icons/sunburst-white-background192.png",
+        src: "/assets/icons/sunburst-white-background192.png",
         sizes: "192x192",
         type: "image/png",
         targets: ['apple'],
       },
       // MS requires special icon sizes
       {
-        src: "assets/icons/sunburst-transparent150.png",
+        src: "/assets/icons/sunburst-transparent150.png",
         sizes: "150x150",
         type: "image/png",
         element: 'square150x150logo',
