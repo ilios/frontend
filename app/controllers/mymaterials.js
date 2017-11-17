87241dc73d46ab1349c66459d76cfac2b7339a23
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-11-17 10:45:48 -0800

    default filter to blank string so the url param gets yanked on no-search.

--- app/controllers/mymaterials.js
+++ app/controllers/mymaterials.js
@@ -3,6 +3,6 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   queryParams: ['sortBy', 'filter', 'course'],
   sortBy: 'firstOfferingDate:desc',
-  filter: null,
+  filter: "",
   course: "",
 });
