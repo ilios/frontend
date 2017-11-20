337e4174858ade843cb625c2b0b99467705e73c3
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-11-19 20:46:06 -0800

    refactored tests to set up session with loaded JWT by default.

--- tests/integration/services/current-user-test.js
+++ tests/integration/services/current-user-test.js
@@ -1,21 +1,25 @@
 import { moduleFor, test } from 'ember-qunit';
+import { getOwner } from '@ember/application';
 
 moduleFor('service:current-user', 'CurrentUserService', {
   integration: true,
+
   beforeEach() {
+    const session = getOwner(this).lookup('service:session');
+    session.reopen({
+      data: {
+        authenticated: {
+          // this token de-serializes to object with "user_id:100" property/value
+          jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTA5Njg4NDEsImV4cCI6MTUxMDk3MjQ3NiwidXNlcl9pZCI6MTAwLCJqdGkiOiI5YzYxZDdjZS1jZjliLTQxZDgtYjQ5YS0zMWFmNWQ4Y2MzY2UifQ.P1QY8zDSi8IAVaJ0YHX_KzYsIfZP_bvBdocZ9V9JUb0"
+        }
+      }
+    });
     this.inject.service('session', { as: 'session' });
     this.inject.service('store', { as: 'store' });
   }
 });
 
 test('currentUserId', function(assert) {
-  this.session.reopen({
-    data: {
-      authenticated: {
-        jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTA5Njg4NDEsImV4cCI6MTUxMDk3MjQ3NiwidXNlcl9pZCI6MTAwLCJqdGkiOiI5YzYxZDdjZS1jZjliLTQxZDgtYjQ5YS0zMWFmNWQ4Y2MzY2UifQ.P1QY8zDSi8IAVaJ0YHX_KzYsIfZP_bvBdocZ9V9JUb0"
-      }
-    }
-  });
   const subject = this.subject();
   const userId = subject.get('currentUserId');
   assert.equal(userId, 100);
@@ -23,6 +27,13 @@ test('currentUserId', function(assert) {
 
 test('no token - no currentUserId', function(assert) {
   assert.expect(1);
+  this.session.reopen({
+    data: {
+      authenticated: {
+        jwt: null
+      }
+    }
+  });
   const subject = this.subject();
   const userId = subject.get('currentUserId');
   assert.equal(userId, null);
@@ -31,21 +42,12 @@ test('no token - no currentUserId', function(assert) {
 
 test('model', async function(assert) {
   assert.expect(3);
-
   const user = {};
-
   this.store.reopen({
     async find(what, id) {
       assert.equal(what, 'user');
       assert.equal(id, 100);
       return user;
-    }
-  });
-  this.session.reopen({
-    data: {
-      authenticated: {
-        jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTA5Njg4NDEsImV4cCI6MTUxMDk3MjQ3NiwidXNlcl9pZCI6MTAwLCJqdGkiOiI5YzYxZDdjZS1jZjliLTQxZDgtYjQ5YS0zMWFmNWQ4Y2MzY2UifQ.P1QY8zDSi8IAVaJ0YHX_KzYsIfZP_bvBdocZ9V9JUb0"
-      }
     }
   });
   const subject = this.subject();
@@ -56,6 +58,15 @@ test('model', async function(assert) {
 test('no token - no model', async function(assert) {
   assert.expect(1);
   const subject = this.subject();
+  this.session.reopen({
+    data: {
+      authenticated: {
+        jwt: null
+      }
+    }
+  });
   const model = await subject.get('model');
   assert.equal(model, null);
 });
+
+
