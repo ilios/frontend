141dbc5c37c35afc91a3015421bd73361fa9d924
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-11-29 16:21:23 -0800

    configured eslint to fail on missing radix.

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   rules: {
     /* Possible Errors http://eslint.org/docs/rules/#possible-errors */
     "comma-dangle": [2, "only-multiline"],
-
+    "radix": [2, "always"],
     /* Stylistic Issues http://eslint.org/docs/rules/#stylistic-issues */
     indent: [2, 2], /* two-space indentation */
     semi: 2, /* require semi-colons */
