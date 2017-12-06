5c37bf75e68fe879b59d536b6f320266a4067284
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-12-05 21:21:57 -0800

    test coverage for assignableVocabularies CP.

--- tests/unit/models/course-test.js
+++ tests/unit/models/course-test.js
@@ -173,3 +173,29 @@ test('schools', async function(assert) {
     assert.ok(schools.includes(school3));
   });
 });
+
+test('assignableVocabularies', async function(assert){
+  assert.expect(5);
+  let course = this.subject();
+  let store = this.store();
+  await run( async () => {
+    const school1 = store.createRecord('school', { title: 'Zylinder' });
+    const school2 = store.createRecord('school', { title: 'Anton' });
+    const vocabulary1 = store.createRecord('vocabulary', { title: 'Sowjetunion',  school: school1 });
+    const vocabulary2 = store.createRecord('vocabulary', { title: 'DDR', school: school1 });
+    const vocabulary3 = store.createRecord('vocabulary', { title: 'Walter Ulbricht', school: school2 });
+    const vocabulary4 = store.createRecord('vocabulary', { title: 'Antifaschistischer Schutzwall', school: school2 });
+    const program = store.createRecord('program', { school: school1 });
+    const programYear = store.createRecord('programYear', { program });
+    const cohort = store.createRecord('cohort', { programYear });
+    course.get('cohorts').pushObject(cohort);
+    course.set('school', school2);
+
+    const vocabularies = await course.get('assignableVocabularies');
+    assert.equal(vocabularies.length, 4);
+    assert.equal(vocabularies[0], vocabulary4);
+    assert.equal(vocabularies[1], vocabulary3);
+    assert.equal(vocabularies[2], vocabulary2);
+    assert.equal(vocabularies[3], vocabulary1);
+  });
+});
