8e7d5aba934dec1593ab8e41ae6d4cb8040340bf
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-12-06 15:24:51 -0800

    test coverage for removeParentWithProgramYears()

--- tests/unit/models/objective-test.js
+++ tests/unit/models/objective-test.js
@@ -23,7 +23,7 @@ test('top parent with no parents should be self', async function(assert) {
   assert.expect(2);
 
   let model = this.subject();
-  run(async () => {
+  await run(async () => {
     let topParents = await model.get('topParents');
     assert.ok(topParents.length === 1);
     assert.equal(topParents.get('firstObject'), model);
@@ -34,7 +34,7 @@ test('current top parents with single parent tree', async function(assert) {
   assert.expect(2);
   let store = this.store();
   let model = this.subject();
-  run(async () => {
+  await run(async () => {
     let parent1 = store.createRecord('objective', {
       children: [model]
     });
@@ -52,7 +52,7 @@ test('current top parents with multi parent tree', async function(assert) {
   assert.expect(3);
   let store = this.store();
   let model = this.subject();
-  run(async () => {
+  await run(async () => {
     let parent1 = store.createRecord('objective', {
       children: [model]
     });
@@ -78,7 +78,7 @@ test('tree competencies', async function(assert) {
   assert.expect(3);
   let store = this.store();
   let model = this.subject();
-  run(async () => {
+  await run(async () => {
     let competency1 = store.createRecord('competency');
     let competency2 = store.createRecord('competency');
 
@@ -118,5 +118,32 @@ test('tree competencies', async function(assert) {
     assert.equal(2, treeCompetencies.length);
     assert.ok(treeCompetencies.includes(competency1));
     assert.ok(treeCompetencies.includes(competency2));
+  });
+});
+
+
+test('removeParentWithProgramYears', async function(assert) {
+  assert.expect(3);
+  let store = this.store();
+  let model = this.subject();
+  await run( async () => {
+    model.reopen({
+      async save() {
+        assert.ok(true, 'save() was called.')
+      }
+    });
+    const programYear1 = store.createRecord('programYear');
+    const parentObjective1 = store.createRecord('objective', { programYears: [ programYear1 ] });
+    const programYear2 = store.createRecord('programYear');
+    const parentObjective2 = store.createRecord('objective', { programYears: [ programYear2 ] });
+    const programYear3 = store.createRecord('programYear');
+    const parentObjective3 = store.createRecord('objective', { programYears: [ programYear3 ] });
+
+    model.get('parents').pushObjects([parentObjective1, parentObjective2, parentObjective3 ]);
+
+    await model.removeParentWithProgramYears([programYear1, programYear2]);
+    const parents = await model.get('parents');
+    assert.equal(parents.length, 1);
+    assert.ok(parents.includes(parentObjective3));
   });
 });
