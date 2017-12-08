3f97f3a91ac13538660fc124cb6d55f7646330fa
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-12-08 13:49:25 -0800

    Simplify rendering of learner groups in offering form
    We're bumping up against an infinite rendering limit in ember that is
    alleviated by simplifying this data and the template.

--- app/components/learnergroup-selection-manager.js
+++ app/components/learnergroup-selection-manager.js
@@ -1,37 +1,11 @@
 /* eslint ember/order-in-components: 0 */
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import RSVP from 'rsvp';
-const { Promise, all } = RSVP;
 
 export default Component.extend({
   i18n: service(),
-  init(){
-    this._super(...arguments);
-    this.set('sortBy', ['title']);
-  },
   classNames: ['learnergroup-selection-manager'],
   filter: '',
-  sortBy: null,
   cohorts: null,
   learnerGroups: null,
-  allLearnerGroups: computed('cohorts.[]', function(){
-    return new Promise(resolve => {
-      let cohorts = this.get('cohorts');
-      all(cohorts.mapBy('rootLevelLearnerGroups')).then(allLearnerGroups => {
-        let flat = allLearnerGroups.reduce((flattened, arr) => {
-          return flattened.pushObjects(arr);
-        }, []);
-
-        resolve(flat);
-      });
-    });
-
-  }),
-  actions: {
-    compareCohorts(cohort1, cohort2){
-      return cohort1.get('id') === cohort2.get('id');
-    }
-  }
 });
