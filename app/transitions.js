3ae2eca1b5d4642b30da1fd058ce7401c9a0a157
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-12-07 22:46:44 -0800

    Remove animations between routes
    These have not been working for a while due to our loading states and
    some route restructuring. They break other stuff so let's get rid of
    them.

--- app/transitions.js
+++ app/transitions.js
@@ -22,24 +22,4 @@ export default function(){
     this.toValue(true),
     this.use('crossFade', {duration: getDuration(1000)})
   );
-  this.transition(
-    this.toRoute(function(){
-      var topRoutes = [];
-      topRoutes.push('dashboard');
-      topRoutes.push('courses');
-      topRoutes.push('learnerGroups');
-      topRoutes.push('learnerGroup');
-      topRoutes.push('instructorGroups');
-      topRoutes.push('instructorGroup');
-      topRoutes.push('programs');
-      return topRoutes.includes(this);
-    }),
-    this.use('crossFade', {duration: getDuration(500)})
-  );
-  this.transition(
-    this.fromRoute('course.index', 'session.index'),
-    this.toRoute('session.index', 'session.publicationCheck'),
-    this.use('toLeft', {duration: getDuration(1000)}),
-    this.reverse('toRight', {duration: getDuration(1000)})
-  );
 }
