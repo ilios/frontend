97a46cffb0c59a26b40e952f1cc36df6c057febd
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-12-13 23:49:43 -0800

    Immediately load new worker if there is an API update
    The user should not need to take any action in this case. We should
    immediately load any new workers that are waiting.

--- app/components/api-version-check.js
+++ app/components/api-version-check.js
@@ -1,22 +1,29 @@
 /* eslint ember/order-in-components: 0 */
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import RSVP from 'rsvp';
-import { computed } from '@ember/object';
 import ENV from 'ilios/config/environment';
-
-const { Promise } = RSVP;
+import serviceWorkerHasUpdate from 'ilios/utils/service-worker-has-update';
 
 const { apiVersion } = ENV.APP;
 
 export default Component.extend({
   iliosConfig: service(),
-  versionMismatch: computed('iliosConfig.apiVersion', function(){
+  versionMismatch: false,
+
+  didInsertElement() {
+    this.loadAttributes();
+  },
+  async loadAttributes() {
     const iliosConfig = this.get('iliosConfig');
-    return new Promise(resolve => {
-      iliosConfig.get('apiVersion').then(serverApiVersion => {
-        resolve(serverApiVersion !== apiVersion);
-      });
-    });
-  }),
+    const serverApiVersion = await iliosConfig.get('apiVersion');
+    const versionMismatch = serverApiVersion !== apiVersion;
+    if (versionMismatch && 'serviceWorker' in navigator) {
+      const hasUpdate = await serviceWorkerHasUpdate();
+      if (hasUpdate) {
+        const reg = await navigator.serviceWorker.getRegistration();
+        reg.waiting.postMessage('skipWaiting');
+      }
+    }
+    this.set('versionMismatch', versionMismatch);
+  },
 });
