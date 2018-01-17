36e5b19d1a0eed2bbb53efd6adce108dfee3e6bd
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-01-17 12:15:47 -0800

    Wait more
    Quick fix for select lists not waiting for all promises to be fulfilled
    before moving on.

--- tests/helpers/selectable.js
+++ tests/helpers/selectable.js
@@ -18,6 +18,7 @@ export default function selectable(selector, options = {}) {
         }
         await wait();
         await wait();
+        await wait();
         return wait();
       };
     }
