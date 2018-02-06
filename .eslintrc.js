cd7a84dc69409c1b0f5e3c3850c97c119730accb
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-02-06 11:53:10 -0800

    Update blueprints for ember-cli 2.18.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,7 +50,8 @@ module.exports = {
       files: [
         'testem.js',
         'ember-cli-build.js',
-        'config/**/*.js'
+        'config/**/*.js',
+        'lib/*/index.js'
       ],
       parserOptions: {
         sourceType: 'script',
