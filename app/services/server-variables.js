eca1d7bc7d42ac3273ff800a2416350f1ecec6a3
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-02-10 09:30:35 -0800

    Ran ember-modules-codemod to update imports

--- app/services/server-variables.js
+++ app/services/server-variables.js
@@ -1,17 +1,15 @@
-import Ember from 'ember';
-
-const { Service, inject, computed } = Ember;
-const { service } = inject;
-const { reads } = computed;
+import Service from '@ember/service';
+import { inject } from '@ember/service';
+import { reads } from '@ember/object/computed';
 
 /**
- * In the LTI app we pull server varialbes out of the session instead
+ * In the LTI app we pull server variables out of the session instead
  * of the index.html where it is in the frontend. Instead of using ember-server-variables
  * we can provide this proxy service instead
  * They are needed in the ilios-config service
 **/
 export default Service.extend({
-  session: service(),
+  session: inject(),
   apiHost: reads('session.data.apiHost'),
   apiNameSpace: reads('session.data.apiNameSpace'),
 });
