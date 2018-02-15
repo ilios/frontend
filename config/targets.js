660281867dac85e4fd72592985b85dd45680cd8a
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-02-15 13:32:56 -0800

    Update blueprints for ember-cli 3.0.0

--- config/targets.js
+++ config/targets.js
@@ -1,11 +1,18 @@
-// Which browsers are returned can be found at http://browserl.ist/
+const browsers = [
+  'last 1 Chrome versions',
+  'last 1 Firefox versions',
+  'last 1 Safari versions'
+];
+
+const isCI = !!process.env.CI;
+const isProduction = process.env.EMBER_ENV === 'production';
+
+if (isCI || isProduction) {
+  browsers.push('last 1 edge versions');
+  browsers.push('firefox esr'); //actually points to the last 2 ESR releases as they overlap
+  browsers.push('last 1 ios versions');
+}
+
 module.exports = {
-  browsers: [
-    'last 1 edge versions',
-    'last 1 chrome versions',
-    'firefox esr', //actually points to the last 2 ESR releases as they overlap
-    'last 1 safari versions',
-    'last 1 ios versions',
-    'last 1 android versions',
-  ]
+  browsers
 };
