8a35f65686804d53c65ea09f346692a6167ca092
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-02-28 22:13:24 -0800

    Fix top level forms and saved results

--- app/components/new-learnergroup-multiple.js
+++ app/components/new-learnergroup-multiple.js
@@ -16,6 +16,7 @@ const Validations = buildValidations({
 });
 
 export default Component.extend(Validations, ValidationErrorDisplay, {
+  tagName: 'form',
   numSubGroups: null,
   isSaving: false,
 
--- app/components/new-learnergroup-single.js
+++ app/components/new-learnergroup-single.js
@@ -14,6 +14,7 @@ const Validations = buildValidations({
 });
 
 export default Component.extend(Validations, ValidationErrorDisplay, {
+  tagName: 'form',
   title: null,
   fillModeSupported: false,
   fillWithCohort: false,
