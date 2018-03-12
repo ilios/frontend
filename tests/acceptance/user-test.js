213ce4c3b56a212ba7a11585e4584b6576fc5ab0
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-03-11 22:26:49 -0700

    Update test to new API and query strings for new helpers

--- tests/acceptance/user-test.js
+++ tests/acceptance/user-test.js
@@ -1,38 +1,33 @@
 import { click, fillIn, currentURL, find, triggerEvent, visit } from '@ember/test-helpers';
-import { run } from '@ember/runloop';
 import { module, test } from 'qunit';
-import startApp from 'ilios/tests/helpers/start-app';
 import setupAuthentication from 'ilios/tests/helpers/setup-authentication';
 
-let application;
-let url = '/users/4136';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance: User', function(hooks) {
-  hooks.beforeEach(function() {
-    application = startApp();
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(async function () {
+    const school = this.server.create('school');
     let userObject = {
-      id: 4136,
+      id: 100,
       campusId: '123',
       email: 'user@example.edu',
       phone: '111-111-1111',
       primaryCohortId: 1,
       cohortIds: [1, 2, 3],
       learnerGroupIds: [3, 5],
-      schoolId: 1
+      school
     };
-
-    this.server.create('school');
-    this.server.create('program');
+    this.server.create('program', { school });
     this.server.createList('programYear', 3, { programId: 1});
     this.server.create('cohort', { title: 'Medicine', programYearId: 1 });
     this.server.create('cohort', { programYearId: 2 });
     this.server.create('cohort', { programYearId: 3 });
     this.server.createList('learnerGroup', 5, { title: 'Group 1', cohortId: 1 });
-    setupAuthentication(application, userObject);
-  });
-
-  hooks.afterEach(function() {
-    run(application, 'destroy');
+    await setupAuthentication( userObject );
   });
 
   test('can search for users', async function(assert) {
@@ -40,12 +35,12 @@ module('Acceptance: User', function(hooks) {
     this.server.createList('authentication', 20);
 
     const userSearch = '.user-search input';
-    const secondResult = '.user-search .results li:eq(2)';
+    const secondResult = '.user-search .results li:nth-of-type(3)';
     const secondResultUsername = `${secondResult} .name`;
     const secondResultEmail = `${secondResult} .email`;
     const name = '.user-display-name';
 
-    await visit(url);
+    await visit('/users/100');
     await fillIn(userSearch, 'son');
     await triggerEvent(userSearch, 'keyup');
     assert.equal(find(secondResultUsername).textContent.trim(), '1 guy M. Mc1son', 'user name is correct');
