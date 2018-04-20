09adf30283d1f1b0e616d9d599d38f9544e07a28
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-04-20 08:28:05 -0700

    Improve flaky test
    This test fails sporadically, just going to try bumping the timeout a
    bit to see if that helps.

--- tests/integration/components/loading-bar-test.js
+++ tests/integration/components/loading-bar-test.js
@@ -11,13 +11,15 @@ test('it renders', async function (assert) {
   const bar = '.bar';
   this.set('isLoading', true);
   this.render(hbs`{{loading-bar isLoading=isLoading}}`);
-  // we need to give the bar a moment to actually change state
+  // we need to give the bar a moment to actually change state and we
+  // cannot use wait here because the task actually keeps running forever preventing
+  // any kind of settled state
   later(() => {
     assert.ok(this.$(bar).attr('value').trim() > 0);
     this.set('isLoading', false);
     later(() => {
       assert.equal(this.$(bar).attr('value').trim(), 0);
     }, 2000);
-  }, 500);
+  }, 1000);
   await wait();
 });
