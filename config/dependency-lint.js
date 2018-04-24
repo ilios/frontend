2f164a13e2ad51845c4df6fe2f9fefef9cee03f2
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-04-24 15:11:21 -0700

    Add dependency lint exception
    There isn't anything new between these versions, we can accept either.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -8,6 +8,7 @@ module.exports = {
     'ember-inflector': '^1.0.0 || ^2.0.0',
     'ember-hash-helper-polyfill': '^0.1.2 || ^0.2.0', // workaround for dep. conflict ember-tooltips/liquid-fire
     'ember-require-module': '^0.1.2 || ^0.2.0', // workaround for dep. conflict ember-cp-validations/ember-validators
-    'ember-compatibility-helpers': '^0.1.3 || ^1.0.0-beta.2' // workaround for dep. conflict ember-light-table/ember-data
+    'ember-compatibility-helpers': '^0.1.3 || ^1.0.0-beta.2', // workaround for dep. conflict ember-light-table/ember-data
+    'ember-concurrency': '^0.8.17' // workaround for dep. conflict ilios-common/ember-simple-charts
   }
 };
