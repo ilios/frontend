f423d43915f5c913d03899245ef55254761b0996
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-04-25 16:19:45 -0700

    Build navigation using permissions from the user token

--- app/components/ilios-navigation.js
+++ app/components/ilios-navigation.js
@@ -1,9 +1,5 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import RSVP from 'rsvp';
-
-const { Promise } = RSVP;
 
 export default Component.extend({
   i18n: service(),
@@ -11,18 +7,4 @@ export default Component.extend({
   tagName: 'nav',
   classNameBindings: [':ilios-navigation', 'expanded'],
   expanded: false,
-  permissions: computed('currentUser.model.roles.[]', function(){
-    return new Promise(resolve => {
-      this.get('currentUser.model').then(user => {
-        user.get('roles').then(roles => {
-          let ids = roles.mapBy('id');
-          let permissions = {
-            isFaculty: ids.includes('1')||ids.includes('2')||ids.includes('3'),
-            isAdmin: ids.includes('2'),
-          };
-          resolve(permissions);
-        });
-      });
-    });
-  }),
 });
