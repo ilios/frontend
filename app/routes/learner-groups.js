5f9acb9658b6ba6de952b0033b62edeceb21adcb
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-04-26 15:42:21 -0700

    replaced calls to removed CP.

--- app/routes/learner-groups.js
+++ app/routes/learner-groups.js
@@ -5,17 +5,14 @@ import RSVP from 'rsvp';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  currentUser: service(),
   store: service(),
   titleToken: 'general.learnerGroups',
   model() {
     let defer = RSVP.defer();
     let model = {};
-    this.get('currentUser.model').then(currentUser=>{
-      currentUser.get('schools').then(schools => {
-        model.schools = schools;
-        defer.resolve(model);
-      });
+    this.get('store').findAll('school', { reload: true }).then(schools => {
+      model.schools = schools;
+      defer.resolve(model);
     });
 
     return defer.promise;
