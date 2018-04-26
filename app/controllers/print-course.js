c6a8697af55e6115fbea3c2458cbaee2a818554e
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-04-25 19:31:09 -0700

    Authorize printing unpublished course with new permissions

--- app/controllers/print-course.js
+++ app/controllers/print-course.js
@@ -1,22 +1,9 @@
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
-import { all } from 'rsvp';
 
 export default Controller.extend({
   currentUser: service(),
   queryParams: ['unpublished'],
   unpublished: false,
-  includeUnpublishedSessions: computed('unpublished', 'currentUser.canPrintUnpublishedCourse', async function(){
-    const unpublished = this.get('unpublished');
-    const currentUser = this.get('currentUser');
-    const hasRole = await all([
-      currentUser.get('userIsCourseDirector'),
-      currentUser.get('userIsFaculty'),
-      currentUser.get('userIsDeveloper'),
-    ]);
-    const canPrintUnpublishedCourse = hasRole.includes(true);
-
-    return unpublished && canPrintUnpublishedCourse;
-  })
+  canViewUnpublished: false,
 });
