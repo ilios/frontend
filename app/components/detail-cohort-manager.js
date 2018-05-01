2ec96393e6bfcc970f4a0e3fe7d94e995058ec9a
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-04-30 22:47:27 -0700

    Refactor cohort sorting
    Simplified with async function to create a sort by string out of the
    cohorts school and program and title.

--- app/components/detail-cohort-manager.js
+++ app/components/detail-cohort-manager.js
@@ -1,29 +1,21 @@
 /* eslint ember/order-in-components: 0 */
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import { all, filter } from 'rsvp';
-import EmberObject, { computed } from '@ember/object';
+import { map, filter } from 'rsvp';
+import { computed } from '@ember/object';
 
 import { translationMacro as t } from "ember-i18n";
-
-const { sort } = computed;
 
 export default Component.extend({
   i18n: service(),
   store: service(),
   permissionChecker: service(),
-  init() {
-    this._super(...arguments);
-    this.set('sortBy', ['title']);
-  },
   tagName: 'section',
   classNames: ['detail-cohort-manager'],
   placeholder: t('general.filterPlaceholder'),
   filter: '',
   school: null,
   selectedCohorts: null,
-  sortBy: null,
-  sortedCohorts: sort('selectedCohorts', 'sortBy'),
 
   allCohorts: computed('school', async function () {
     const store = this.get('store');
@@ -73,69 +65,34 @@ export default Component.extend({
    * @type {Ember.computed}
    * @public
    */
-  sortedAvailableCohorts: computed('availableCohorts.[]', function() {
-    return new Promise(resolve => {
-      this.get('availableCohorts').then(cohorts => {
-        let promises = [];
-        let proxies = [];
-        cohorts.forEach(cohort => {
-          let proxy = EmberObject.create({
-            cohort,
-            schoolTitle: null,
-            programTitle: null
-          });
+  sortedAvailableCohorts: computed('availableCohorts.[]', async function () {
+    const availableCohorts = await this.get('availableCohorts');
+    const objects = await map(availableCohorts, async cohort => {
+      const programYear = await cohort.get('programYear');
+      const program = await programYear.get('program');
+      const school = await program.get('school');
 
-          proxies.pushObject(proxy);
-          promises.pushObject(new Promise(resolve => {
-            cohort.get('program').then(program => {
-              proxy.set('programTitle', program.get('title'));
-              program.get('school').then(school => {
-                proxy.set('schoolTitle', school.get('title'));
-                resolve();
-              });
-            });
-          }));
-        });
-        all(promises).then(() => {
-          let sortedProxies = proxies.sort((proxy1, proxy2) => {
-            // sort by cohort's owning school's title, ascending
-            if (proxy1.get('schoolTitle') > proxy2.get('schoolTitle')) {
-              return 1;
-            } else if (proxy1.get('schoolTitle') < proxy2.get('schoolTitle')) {
-              return -1;
-            }
+      const sortTitle = school.get('title') + program.get('title');
 
-            // sort by cohort's owning program's title, ascending
-            if (proxy1.get('programTitle') > proxy2.get('programTitle')) {
-              return 1;
-            } else if (proxy1.get('programTitle') < proxy2.get('programTitle')) {
-              return -1;
-            }
-
-            let cohort1 = proxy1.get('cohort');
-            let cohort2 = proxy2.get('cohort');
-
-            // sort by cohort title, descending
-            if (cohort1.get('title') > cohort2.get('title')) {
-              return -1;
-            } else if (cohort1.get('title') < cohort2.get('title')) {
-              return 1;
-            }
-
-            // final fallback: sort by id, ascending
-            if (cohort1.get('id') > cohort2.get('id')) {
-              return 1;
-            } else if (cohort1.get('id') < cohort2.get('id')) {
-              return -1;
-            }
-
-            return 0;
-          });
-
-          resolve(sortedProxies.getEach('cohort'));
-        });
-      });
+      return {
+        cohort,
+        sortTitle,
+        cohortTitle: cohort.get('title')
+      };
     });
+
+    objects.sort((obj1, obj2) => {
+      let compare = obj1.sortTitle.localeCompare(obj2.sortTitle);
+      if (compare === 0) {
+        //cohort titles are sorted descending
+        compare = -obj1.cohortTitle.localeCompare(obj2.cohortTitle);
+      }
+
+      return compare;
+
+    });
+
+    return objects.mapBy('cohort');
   }),
 
   actions: {
