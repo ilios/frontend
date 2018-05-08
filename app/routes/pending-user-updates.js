b949fc33692a25e0ddc1aa27519c9490762452d8
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-05-08 13:37:02 -0700

    Lookup schools correctly
    We were still using the old permissions based school lookup here, which
    doesn't work.

--- app/routes/pending-user-updates.js
+++ app/routes/pending-user-updates.js
@@ -1,21 +1,29 @@
 /* eslint ember/order-in-routes: 0 */
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import { filter } from 'rsvp';
 
 export default Route.extend({
   currentUser: service(),
+  store: service(),
+  permissionChecker: service(),
   titleToken: 'general.admin',
-  model(){
-    return this.get('currentUser.model').then(user => {
-      return user.get('schools').then(schools => {
-        return user.get('school').then(primarySchool => {
-          return {
-            primarySchool,
-            schools
-          };
-        });
-      });
+  async model(){
+    const currentUser = this.get('currentUser');
+    const store = this.get('store');
+    const permissionChecker = this.get('permissionChecker');
+    const allSchools = await store.findAll('school');
+    const schools = await filter(allSchools.toArray(), async school => {
+      return permissionChecker.canUpdateUserInSchool(school);
     });
+
+    const user = await currentUser.get('model');
+    const primarySchool = await user.get('school');
+
+    return {
+      primarySchool,
+      schools
+    };
   },
   queryParams: {
     filter: {
