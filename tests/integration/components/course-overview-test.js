fda246fa0da6ffc755f52fc7b9b24b7d06077063
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-05-10 15:16:47 -0700

    Update test to new format and remove mocked store

--- tests/integration/components/course-overview-test.js
+++ tests/integration/components/course-overview-test.js
@@ -1,178 +1,139 @@
 import EmberObject from '@ember/object';
-import Service from '@ember/service';
 import RSVP from 'rsvp';
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, settled, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import wait from 'ember-test-helpers/wait';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
-let storeMock;
-moduleForComponent('course-overview', 'Integration | Component | course overview', {
-  integration: true,
-  beforeEach(){
-    storeMock = Service.extend({});
-    this.register('service:store', storeMock);
-  }
-});
+module('Integration | Component | course overview', function(hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-const { resolve } = RSVP;
+  const { resolve } = RSVP;
 
-test('renders with no course id', function(assert) {
-  storeMock.reopen({
-    findAll(what){
-      assert.equal('course-clerkship-type', what);
-      return resolve([]);
-    }
-  });
-
-  let course = EmberObject.create({
-    clerkshipType: resolve(EmberObject.create())
-  });
-  this.set('course', course);
-  this.render(hbs`{{course-overview course=course editable=true}}`);
-
-  assert.notEqual(this.$('.courseexternalid').text().search(/Course ID:/), -1);
-  assert.notEqual(this.$('.courseexternalid').text().search(/Click to edit/), -1);
-
-});
-
-test('course external id validation fails if value is too short', function(assert) {
-  storeMock.reopen({
-    findAll(what){
-      assert.equal('course-clerkship-type', what);
-      return resolve([]);
-    }
-  });
-
-  let course = EmberObject.create({
-    clerkshipType: resolve(EmberObject.create()),
-  });
-  this.set('course', course);
-  this.render(hbs`{{course-overview course=course editable=true}}`);
-
-  const item = '.courseexternalid';
-  const error = `${item} .validation-error-message`;
-  const button = `${item} .clickable`;
-  const save = `${item} .actions .done`;
-  const input = `${item} input`;
-  this.$(button).click();
-  return wait().then(()=>{
-    assert.equal(this.$(error).length, 0, 'No validation errors shown initially.');
-    this.$(input).val('a').trigger('input');
-    this.$(save).click();
-    wait().then(() => {
-      assert.equal(this.$(error).length, 1, 'Validation failed, error message shows.');
+  test('renders with no course id', async function(assert) {
+    let course = EmberObject.create({
+      clerkshipType: resolve(EmberObject.create())
     });
-  });
-});
+    this.set('course', course);
+    await render(hbs`{{course-overview course=course editable=true}}`);
 
-test('course external id validation fails if value is too long', function(assert) {
-  storeMock.reopen({
-    findAll(what){
-      assert.equal('course-clerkship-type', what);
-      return resolve([]);
-    }
+    assert.notEqual(find('.courseexternalid').textContent.search(/Course ID:/), -1);
+    assert.notEqual(find('.courseexternalid').textContent.search(/Click to edit/), -1);
   });
 
-  let course = EmberObject.create({
-    clerkshipType: resolve(EmberObject.create()),
-  });
-  this.set('course', course);
-  this.render(hbs`{{course-overview course=course editable=true}}`);
-
-  const item = '.courseexternalid';
-  const error = `${item} .validation-error-message`;
-  const button = `${item} .clickable`;
-  const save = `${item} .actions .done`;
-  const input = `${item} input`;
-  this.$(button).click();
-  return wait().then(()=>{
-    assert.equal(this.$(error).length, 0, 'No validation errors shown initially.');
-    this.$(input).val('tooLong'.repeat(50)).trigger('input');
-    this.$(save).click();
-    wait().then(() => {
-      assert.equal(this.$(error).length, 1, 'Validation failed, error message shows.');
+  test('course external id validation fails if value is too short', async function(assert) {
+    let course = EmberObject.create({
+      clerkshipType: resolve(EmberObject.create()),
     });
-  });
-});
+    this.set('course', course);
+    await render(hbs`{{course-overview course=course editable=true}}`);
 
-test('course external id validation passes on changed value within boundaries', function(assert) {
-  storeMock.reopen({
-    findAll(what){
-      assert.equal('course-clerkship-type', what);
-      return resolve([]);
-    }
-  });
-
-  let course = EmberObject.create({
-    clerkshipType: resolve(EmberObject.create()),
-    externalid: 'abcde',
-    save() {
-      let that = this;
-      assert.ok(true, 'Validation passed, course object is being saved.');
-      return new RSVP.Promise(function(resolve) {
-        resolve(that);
+    const item = '.courseexternalid';
+    const error = `${item} .validation-error-message`;
+    const button = `${item} .clickable`;
+    const save = `${item} .actions .done`;
+    const input = `${item} input`;
+    this.$(button).click();
+    return settled().then(()=>{
+      assert.equal(this.$(error).length, 0, 'No validation errors shown initially.');
+      this.$(input).val('a').trigger('input');
+      this.$(save).click();
+      settled().then(() => {
+        assert.equal(this.$(error).length, 1, 'Validation failed, error message shows.');
       });
-    },
-    get(value) {
-      return this[value];
-    }
-  });
-  this.set('course', course);
-  this.render(hbs`{{course-overview course=course}}`);
-
-  const item = '.courseexternalid';
-  const error = `${item} .validation-error-message`;
-  const button = `${item} .clickable`;
-  const save = `${item} .actions .done`;
-  const input = `${item} input`;
-  this.$(button).click();
-  return wait().then(()=>{
-    assert.equal(this.$(error).length, 0, 'No validation errors shown initially.');
-    this.$(input).val('legit').trigger('input');
-    this.$(save).click();
-    wait().then(() => {
-      assert.equal(this.$(error).length, 0, 'No validation errors, no messages shown.');
     });
   });
-});
 
-test('course external id validation passes on empty value', function(assert) {
-  storeMock.reopen({
-    findAll(what){
-      assert.equal('course-clerkship-type', what);
-      return resolve([]);
-    }
-  });
+  test('course external id validation fails if value is too long', async function(assert) {
+    let course = EmberObject.create({
+      clerkshipType: resolve(EmberObject.create()),
+    });
+    this.set('course', course);
+    await render(hbs`{{course-overview course=course editable=true}}`);
 
-  let course = EmberObject.create({
-    clerkshipType: resolve(EmberObject.create()),
-    externalid: 'abcde',
-    save() {
-      let that = this;
-      assert.ok(true, 'Validation passed, course object is being saved.');
-      return new RSVP.Promise(function(resolve) {
-        resolve(that);
+    const item = '.courseexternalid';
+    const error = `${item} .validation-error-message`;
+    const button = `${item} .clickable`;
+    const save = `${item} .actions .done`;
+    const input = `${item} input`;
+    this.$(button).click();
+    return settled().then(()=>{
+      assert.equal(this.$(error).length, 0, 'No validation errors shown initially.');
+      this.$(input).val('tooLong'.repeat(50)).trigger('input');
+      this.$(save).click();
+      settled().then(() => {
+        assert.equal(this.$(error).length, 1, 'Validation failed, error message shows.');
       });
-    },
-    get(value) {
-      return this[value];
-    }
+    });
   });
-  this.set('course', course);
-  this.render(hbs`{{course-overview course=course}}`);
 
-  const item = '.courseexternalid';
-  const error = `${item} .validation-error-message`;
-  const button = `${item} .clickable`;
-  const save = `${item} .actions .done`;
-  const input = `${item} input`;
-  this.$(button).click();
-  return wait().then(()=>{
-    assert.equal(this.$(error).length, 0, 'No validation errors shown initially.');
-    this.$(input).val('').trigger('input');
-    this.$(save).click();
-    wait().then(() => {
-      assert.equal(this.$(error).length, 0, 'No validation errors, no messages shown.');
+  test('course external id validation passes on changed value within boundaries', async function(assert) {
+    let course = EmberObject.create({
+      clerkshipType: resolve(EmberObject.create()),
+      externalid: 'abcde',
+      save() {
+        let that = this;
+        assert.ok(true, 'Validation passed, course object is being saved.');
+        return new RSVP.Promise(function(resolve) {
+          resolve(that);
+        });
+      },
+      get(value) {
+        return this[value];
+      }
+    });
+    this.set('course', course);
+    await render(hbs`{{course-overview course=course}}`);
+
+    const item = '.courseexternalid';
+    const error = `${item} .validation-error-message`;
+    const button = `${item} .clickable`;
+    const save = `${item} .actions .done`;
+    const input = `${item} input`;
+    this.$(button).click();
+    return settled().then(()=>{
+      assert.equal(this.$(error).length, 0, 'No validation errors shown initially.');
+      this.$(input).val('legit').trigger('input');
+      this.$(save).click();
+      settled().then(() => {
+        assert.equal(this.$(error).length, 0, 'No validation errors, no messages shown.');
+      });
+    });
+  });
+
+  test('course external id validation passes on empty value', async function(assert) {
+    let course = EmberObject.create({
+      clerkshipType: resolve(EmberObject.create()),
+      externalid: 'abcde',
+      save() {
+        let that = this;
+        assert.ok(true, 'Validation passed, course object is being saved.');
+        return new RSVP.Promise(function(resolve) {
+          resolve(that);
+        });
+      },
+      get(value) {
+        return this[value];
+      }
+    });
+    this.set('course', course);
+    await render(hbs`{{course-overview course=course}}`);
+
+    const item = '.courseexternalid';
+    const error = `${item} .validation-error-message`;
+    const button = `${item} .clickable`;
+    const save = `${item} .actions .done`;
+    const input = `${item} input`;
+    this.$(button).click();
+    return settled().then(()=>{
+      assert.equal(this.$(error).length, 0, 'No validation errors shown initially.');
+      this.$(input).val('').trigger('input');
+      this.$(save).click();
+      settled().then(() => {
+        assert.equal(this.$(error).length, 0, 'No validation errors, no messages shown.');
+      });
     });
   });
 });
