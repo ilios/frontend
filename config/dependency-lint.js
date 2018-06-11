a5eed88bf8426cfec307de9c5a6452c867a73de2
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-06-11 16:36:39 -0700

    Update ember-tether
    Matches the version we need for ember-simple-charts

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -10,6 +10,5 @@ module.exports = {
     'ember-require-module': '^0.1.2 || ^0.2.0', // workaround for dep. conflict ember-cp-validations/ember-validators
     'ember-compatibility-helpers': '^0.1.3 || ^1.0.0-beta.1', // workaround for dep. conflict ember-light-table/ember-data
     'ember-concurrency': '^0.8.17', // workaround for dep. conflict ilios-common/ember-simple-charts
-    'ember-tether': '0.4.1 || 1.0.0-beta.2' // workaround for dep. conflict ilios-common/ember-simple-charts/ember-tooltips
   }
 };
