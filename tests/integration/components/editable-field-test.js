d58f13271a3d0b17e178d744cfe512b1162ea805
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-07-08 20:16:43 -0700

    Improve test
    Convert to new test structure and use test helpers to trigger the key
    events instead of jquery.

--- tests/integration/components/editable-field-test.js
+++ tests/integration/components/editable-field-test.js
@@ -1,69 +1,69 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, click, findAll, triggerKeyEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('editable-field', 'Integration | Component | editable field', {
-  integration: true
-});
+module('Integration | Component | editable field', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders value', function(assert) {
-  this.render(hbs`{{editable-field value='woot!'}}`);
+  test('it renders value', async function(assert) {
+    await render(hbs`{{editable-field value='woot!'}}`);
 
-  assert.equal(this.$().text().trim(), 'woot!');
-});
-
-test('it renders clickPrompt', function(assert) {
-  this.render(hbs`{{editable-field clickPrompt='click me!'}}`);
-
-  assert.equal(this.$().text().trim(), 'click me!');
-});
-
-test('it renders content', function(assert) {
-  this.render(hbs`
-    {{#editable-field value='text'}}
-       template block text
-     {{/editable-field}}
- `);
-
-  assert.equal(this.$().text().trim(), 'text');
-  this.$('.editable').click();
-  assert.equal(this.$().text().trim(), 'template block text');
-
-
-});
-
-test('it renders an edit icon when it looks empty', function(assert) {
-  const icon = 'i.fa-edit';
-  this.set('value', '<p>&nbsp;</p>');
-  this.render(hbs`{{editable-field value=value}}`);
-
-  assert.equal(this.$().text().trim(), '');
-  assert.equal(this.$(icon).length, 1);
-});
-
-test('save on enter', function(assert) {
-  assert.expect(1);
-  this.set('value', 'lorem');
-  this.on('save', () => {
-    assert.ok(true, 'save action fired.');
+    assert.equal(this.element.textContent.trim(), 'woot!');
   });
-  this.render(hbs`{{#editable-field save=(action 'save') saveOnEnter=true value=value}}<input value={{value}} oninput={{action (mut value) value="target.value"}}>{{/editable-field}}`);
-  this.$('.editable').click();
-  const e = $.Event("keyup");
-  e.which = 13;
-  e.keyCode = 13;
-  this.$('.editinplace input').trigger(e);
-});
 
-test('close on escape', function(assert) {
-  assert.expect(1);
-  this.set('value', 'lorem');
-  this.on('revert', () => {
-    assert.ok(true, 'revert action fired.');
+  test('it renders clickPrompt', async function(assert) {
+    await render(hbs`{{editable-field clickPrompt='click me!'}}`);
+
+    assert.equal(this.element.textContent.trim(), 'click me!');
   });
-  this.render(hbs`{{#editable-field close=(action 'revert') closeOnEscape=true value=value}}<input value={{value}} oninput={{action (mut value) value="target.value"}}>{{/editable-field}}`);
-  this.$('.editable').click();
-  const e = $.Event("keyup");
-  e.which = 27;
-  e.keyCode = 27;
-  this.$('.editinplace input').trigger(e);
+
+  test('it renders content', async function(assert) {
+    await render(hbs`
+      {{#editable-field value='text'}}
+         template block text
+       {{/editable-field}}
+   `);
+
+    assert.equal(this.element.textContent.trim(), 'text');
+    await click('.editable');
+    assert.equal(this.element.textContent.trim(), 'template block text');
+
+
+  });
+
+  test('it renders an edit icon when it looks empty', async function(assert) {
+    const icon = 'i.fa-edit';
+    this.set('value', '<p>&nbsp;</p>');
+    await render(hbs`{{editable-field value=value}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(findAll(icon).length, 1);
+  });
+
+  test('save on enter', async function(assert) {
+    assert.expect(1);
+    this.set('value', 'lorem');
+    this.set('save', () => {
+      assert.ok(true, 'save action fired.');
+    });
+    await render(
+      hbs`{{#editable-field save=(action save) saveOnEnter=true value=value}}<input value={{value}} oninput={{action (mut value) value="target.value"}}>{{/editable-field}}`
+    );
+    await click('.editable');
+    await triggerKeyEvent('.editinplace input', 'keyup', 13);
+  });
+
+  test('close on escape', async function(assert) {
+    assert.expect(1);
+    this.set('value', 'lorem');
+    this.set('revert', () => {
+      assert.ok(true, 'revert action fired.');
+    });
+    await render(
+      hbs`{{#editable-field close=(action revert) closeOnEscape=true value=value}}<input value={{value}} oninput={{action (mut value) value="target.value"}}>{{/editable-field}}`
+    );
+    await click('.editable');
+    await triggerKeyEvent('.editinplace input', 'keyup', 27);
+  });
 });
