7b9a1cd59dc1a93dd43fd71aab3715b259a7cefa
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-07-08 22:54:26 -0700

    Update test
    Update to new test style and test selectors

--- tests/integration/components/api-version-check-test.js
+++ tests/integration/components/api-version-check-test.js
@@ -1,38 +1,35 @@
 /* eslint ember/no-global-jquery: 0 */
 import Service from '@ember/service';
-import RSVP from 'rsvp';
+import { resolve } from 'rsvp';
 import $ from 'jquery';
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import ENV from 'ilios/config/environment';
-import wait from 'ember-test-helpers/wait';
 
 const { apiVersion } = ENV.APP;
 
-const { resolve } = RSVP;
+module('Integration | Component | api version check', function(hooks) {
+  setupRenderingTest(hooks);
 
-moduleForComponent('api-version-check', 'Integration | Component | api version check', {
-  integration: true
-});
-
-test('shows no warning when versions match', async function(assert) {
-  const iliosConfigMock = Service.extend({
-    apiVersion: resolve(apiVersion)
+  test('shows no warning when versions match', async function(assert) {
+    const iliosConfigMock = Service.extend({
+      apiVersion: resolve(apiVersion)
+    });
+    const warningOverlay = '.api-version-check-warning';
+    this.owner.register('service:iliosConfig', iliosConfigMock);
+    await render(hbs`{{api-version-check}}`);
+    assert.equal($(warningOverlay).length, 0);
   });
-  const warningOverlay = '.api-version-check-warning';
-  this.register('service:iliosConfig', iliosConfigMock);
-  this.render(hbs`{{api-version-check}}`);
-  await wait();
-  assert.equal($(warningOverlay).length, 0);
-});
 
-test('shows warning on mismatch', async function(assert) {
-  const iliosConfigMock = Service.extend({
-    apiVersion: resolve('bad')
+  test('shows warning on mismatch', async function(assert) {
+    const iliosConfigMock = Service.extend({
+      apiVersion: resolve('bad')
+    });
+    const warningOverlay = '.api-version-check-warning';
+    this.owner.register('service:iliosConfig', iliosConfigMock);
+    await render(hbs`{{api-version-check}}`);
+    assert.equal($(warningOverlay).length, 1);
   });
-  const warningOverlay = '.api-version-check-warning';
-  this.register('service:iliosConfig', iliosConfigMock);
-  this.render(hbs`{{api-version-check}}`);
-  await wait();
-  assert.equal($(warningOverlay).length, 1);
 });
