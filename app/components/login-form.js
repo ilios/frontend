e06d53d19855cde88714d7d491afd67f6c202c11
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-07-09 22:05:05 -0700

    Make login form accessible
    Add links between labels and inputs and ensure there is sufficient
    contrast for the text in the button.
    
    Since the button text couldn't be green I also added a bit of extra
    border to the button on the sides and modified the hover transition and
    active states to make it a bit smoother.

---
+++ app/components/login-form.js
@@ -0,0 +1,27 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
+
+export default Component.extend({
+  session: service(),
+  classNames: ['login-form'],
+  errors: null,
+  noAccountExistsError: false,
+  noAccountExistsAccount: null,
+  identification: null,
+  password: null,
+  authenticate: task(function* () {
+    try {
+      this.set('errors', []);
+      const credentials = this.getProperties('identification', 'password');
+      const session = this.get('session');
+      const authenticator = 'authenticator:ilios-jwt';
+      yield session.authenticate(authenticator, credentials);
+    } catch (error) {
+      const keys = error.keys.map(key => {
+        return ('general.' + key);
+      });
+      this.set('error', { 'message': error.message, 'keys': keys });
+    }
+  }),
+});
