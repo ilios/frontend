33f0f0a9cad5ffc554c437d49bc12f7320854239
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-07-10 21:46:27 -0700

    Update ember-cli to v3.2.0
    Update all blueprints, run all codemods.

--- config/targets.js
+++ config/targets.js
@@ -1,8 +1,21 @@
+'use strict';
+
+const browsers = [
+  'last 1 Chrome versions',
+  'last 1 Firefox versions',
+  'last 1 Safari versions'
+];
+
+const isCI = !!process.env.CI;
+const isProductionLikeBuild = ['production', 'preview'].includes(process.env.EMBER_ENV);
+
+if (isCI || isProductionLikeBuild) {
+  browsers.push('last 1 edge versions');
+  browsers.push('firefox esr'); //sometimes points to the last 2 ESR releases when they overlap
+  browsers.push('last 1 ios versions');
+  browsers.push('> 1%'); // any browser with more than 1% global market share
+}
+
 module.exports = {
-  browsers: [
-    'ie 9',
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions'
-  ]
+  browsers
 };
