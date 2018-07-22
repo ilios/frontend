32f9fccc75cd823029b9a6399b79d531d08f9c05
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-07-22 16:21:59 -0700

    Update tests to new format
    After converting to the new format I removed the mocked store as that
    doesn't work with a rendering test.

--- tests/integration/components/leadership-manager-test.js
+++ tests/integration/components/leadership-manager-test.js
@@ -1,265 +1,219 @@
-import Service from '@ember/service';
-import RSVP from 'rsvp';
-import EmberObject from '@ember/object';
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, fillIn, click, findAll, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import wait from 'ember-test-helpers/wait';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { run } from '@ember/runloop';
 
-const { resolve } = RSVP;
+module('Integration | Component | leadership manager', function(hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-moduleForComponent('leadership-manager', 'Integration | Component | leadership manager', {
-  integration: true
-});
+  test('it renders with data', async function(assert) {
+    assert.expect(5);
+    this.server.createList('user', 2);
+    const users = await run(() => this.owner.lookup('service:store').findAll('user'));
+    this.set('directors', [users.firstObject]);
+    this.set('administrators', users);
+    this.set('nothing', parseInt);
 
-test('it renders with data', function(assert) {
-  assert.expect(5);
-  let user1 = EmberObject.create({
-    firstName: 'a',
-    lastName: 'person',
-    fullName: 'a b person',
-  });
-  let user2 = EmberObject.create({
-    firstName: 'b',
-    lastName: 'person',
-    fullName: 'b a person',
-  });
-  this.set('directors', [user1]);
-  this.set('administrators', [user2, user1]);
-  this.set('nothing', parseInt);
+    await render(hbs`{{leadership-manager
+      directors=directors
+      administrators=administrators
+      removeDirector=(action nothing)
+      addDirector=(action nothing)
+      removeAdministrator=(action nothing)
+      addAdministrator=(action nothing)
+    }}`);
+    const directors = 'table tbody tr:nth-of-type(1) td:nth-of-type(1) li';
+    const administrators = 'table tbody tr:nth-of-type(1) td:nth-of-type(2) li';
 
-  this.render(hbs`{{leadership-manager
-    directors=directors
-    administrators=administrators
-    removeDirector=(action nothing)
-    addDirector=(action nothing)
-    removeAdministrator=(action nothing)
-    addAdministrator=(action nothing)
-  }}`);
-  const directors = 'table tbody tr:eq(0) td:eq(0) li';
-  const administrators = 'table tbody tr:eq(0) td:eq(1) li';
-
-  assert.equal(this.$(directors).length, 1);
-  assert.equal(this.$(directors).eq(0).text().trim(), 'a b person');
-  assert.equal(this.$(administrators).length, 2);
-  assert.equal(this.$(administrators).eq(0).text().trim(), 'a b person');
-  assert.equal(this.$(administrators).eq(1).text().trim(), 'b a person');
-});
-
-test('it renders without data', function(assert) {
-  assert.expect(2);
-  this.set('directors', []);
-  this.set('administrators', []);
-  this.set('nothing', parseInt);
-
-  this.render(hbs`{{leadership-manager
-    directors=directors
-    administrators=administrators
-    removeDirector=(action nothing)
-    addDirector=(action nothing)
-    removeAdministrator=(action nothing)
-    addAdministrator=(action nothing)
-  }}`);
-  const directors = 'table tbody tr:eq(0) td:eq(0) li';
-  const administrators = 'table tbody tr:eq(0) td:eq(1) li';
-
-  assert.equal(this.$(directors).length, 0);
-  assert.equal(this.$(administrators).length, 0);
-});
-
-test('it remove director', function(assert) {
-  assert.expect(3);
-  let user1 = EmberObject.create({
-    firstName: 'a',
-    lastName: 'person',
-    fullName: 'a b person',
-  });
-  this.set('directors', [user1]);
-  this.set('administrators', []);
-  this.set('nothing', parseInt);
-  this.set('remove', (who) => {
-    assert.equal(who, user1);
+    assert.equal(this.$(directors).length, 1);
+    assert.equal(this.$(directors).eq(0).text().trim(), '0 guy M. Mc0son');
+    assert.equal(this.$(administrators).length, 2);
+    assert.equal(this.$(administrators).eq(0).text().trim(), '0 guy M. Mc0son');
+    assert.equal(this.$(administrators).eq(1).text().trim(), '1 guy M. Mc1son');
   });
 
-  this.render(hbs`{{leadership-manager
-    directors=directors
-    administrators=administrators
-    removeDirector=(action remove)
-    addDirector=(action nothing)
-    removeAdministrator=(action nothing)
-    addAdministrator=(action nothing)
-  }}`);
-  const list = 'table tbody tr:eq(0) td:eq(0) li';
-  const icon = `${list}:eq(0) i`;
+  test('it renders without data', async function(assert) {
+    assert.expect(2);
+    this.set('directors', []);
+    this.set('administrators', []);
+    this.set('nothing', parseInt);
 
-  assert.equal(this.$(list).length, 1);
-  assert.equal(this.$(list).eq(0).text().trim(), 'a b person');
-  this.$(icon).click();
-});
+    await render(hbs`{{leadership-manager
+      directors=directors
+      administrators=administrators
+      removeDirector=(action nothing)
+      addDirector=(action nothing)
+      removeAdministrator=(action nothing)
+      addAdministrator=(action nothing)
+    }}`);
+    const directors = 'table tbody tr:nth-of-type(1) td:nth-of-type(1) li';
+    const administrators = 'table tbody tr:nth-of-type(1) td:nth-of-type(2) li';
 
-test('it remove administrator', function(assert) {
-  assert.expect(3);
-  let user1 = EmberObject.create({
-    firstName: 'a',
-    lastName: 'person',
-    fullName: 'a b person',
-  });
-  this.set('directors', []);
-  this.set('administrators', [user1]);
-  this.set('nothing', parseInt);
-  this.set('remove', (who) => {
-    assert.equal(who, user1);
+    assert.equal(this.$(directors).length, 0);
+    assert.equal(this.$(administrators).length, 0);
   });
 
-  this.render(hbs`{{leadership-manager
-    directors=directors
-    administrators=administrators
-    removeDirector=(action nothing)
-    addDirector=(action nothing)
-    removeAdministrator=(action remove)
-    addAdministrator=(action nothing)
-  }}`);
-  const list = 'table tbody tr:eq(0) td:eq(1) li';
-  const icon = `${list}:eq(0) i`;
-
-  assert.equal(this.$(list).length, 1);
-  assert.equal(this.$(list).eq(0).text().trim(), 'a b person');
-  this.$(icon).click();
-});
-
-test('add director', function(assert) {
-  assert.expect(6);
-  let user1 = EmberObject.create({
-    fullName: 'test person',
-    email: 'testemail'
-  });
-  let storeMock = Service.extend({
-    query(){
-      return resolve([user1]);
-    }
-  });
-  this.register('service:store', storeMock);
-  this.set('directors', []);
-  this.set('administrators', [user1]);
-  this.set('nothing', parseInt);
-  this.set('add', (who) => {
-    assert.equal(who, user1, 'user passed correctly from action');
-    this.set('directors', [who]);
-  });
-
-  this.render(hbs`{{leadership-manager
-    directors=directors
-    administrators=administrators
-    removeDirector=(action nothing)
-    addDirector=(action add)
-    removeAdministrator=(action nothing)
-    addAdministrator=(action nothing)
-  }}`);
-  const directorsList = 'table tbody tr:eq(0) td:eq(0) li';
-  const administratorsList = 'table tbody tr:eq(0) td:eq(1) li';
-  const directorSearch = 'input[type="search"]:eq(0)';
-  const results = '.results:eq(0) li';
-  const firstResult = `${results}:eq(1)`;
-
-  assert.equal(this.$(directorsList).length, 0);
-  assert.equal(this.$(administratorsList).length, 1);
-
-  this.$(directorSearch).val('test').trigger('keyup');
-
-  return wait().then(()=>{
-    assert.equal(this.$(firstResult).text().replace(/[\t\n\s]+/g, ""), 'testpersontestemail');
-    this.$(firstResult).click();
-    return wait().then(()=>{
-      assert.equal(this.$(directorsList).length, 1);
-      assert.equal(this.$(administratorsList).length, 1);
+  test('remove director', async function(assert) {
+    assert.expect(3);
+    this.server.createList('user', 1);
+    const user = await run(() => this.owner.lookup('service:store').find('user', 1));
+    this.set('directors', [user]);
+    this.set('administrators', []);
+    this.set('nothing', parseInt);
+    this.set('remove', (who) => {
+      assert.equal(who, user);
     });
-  });
-});
 
-test('add administrator', function(assert) {
-  assert.expect(6);
-  let user1 = EmberObject.create({
-    fullName: 'test person',
-    email: 'testemail'
-  });
-  let storeMock = Service.extend({
-    query(){
-      return resolve([user1]);
-    }
-  });
-  this.register('service:store', storeMock);
-  this.set('directors', [user1]);
-  this.set('administrators', []);
-  this.set('nothing', parseInt);
-  this.set('add', (who) => {
-    assert.equal(who, user1, 'user passed correctly from action');
-    this.set('administrators', [who]);
+    await render(hbs`{{leadership-manager
+      directors=directors
+      administrators=administrators
+      removeDirector=(action remove)
+      addDirector=(action nothing)
+      removeAdministrator=(action nothing)
+      addAdministrator=(action nothing)
+    }}`);
+    const list = 'table tbody tr:nth-of-type(1) td:nth-of-type(1) li';
+    const icon = `${list}:nth-of-type(1) i`;
+
+    assert.equal(findAll(list).length, 1);
+    assert.equal(findAll(list)[0].textContent.trim(), '0 guy M. Mc0son');
+    await click(icon);
   });
 
-  this.render(hbs`{{leadership-manager
-    directors=directors
-    administrators=administrators
-    removeDirector=(action nothing)
-    addDirector=(action nothing)
-    removeAdministrator=(action nothing)
-    addAdministrator=(action add)
-  }}`);
-  const directorsList = 'table tbody tr:eq(0) td:eq(0) li';
-  const administratorsList = 'table tbody tr:eq(0) td:eq(1) li';
-  const administratorSearch = 'input[type="search"]:eq(1)';
-  const results = '.results:eq(1) li';
-  const firstResult = `${results}:eq(1)`;
-
-  assert.equal(this.$(directorsList).length, 1);
-  assert.equal(this.$(administratorsList).length, 0);
-
-  this.$(administratorSearch).val('test').trigger('keyup');
-
-  return wait().then(()=>{
-    assert.equal(this.$(firstResult).text().replace(/[\t\n\s]+/g, ""), 'testpersontestemail');
-    this.$(firstResult).click();
-    return wait().then(()=>{
-      assert.equal(this.$(directorsList).length, 1);
-      assert.equal(this.$(administratorsList).length, 1);
+  test('remove administrator', async function(assert) {
+    assert.expect(3);
+    this.server.createList('user', 1);
+    const user = await run(() => this.owner.lookup('service:store').find('user', 1));
+    this.set('directors', []);
+    this.set('administrators', [user]);
+    this.set('nothing', parseInt);
+    this.set('remove', (who) => {
+      assert.equal(who, user);
     });
-  });
-});
 
-test('disabled users are indicated with an icon', function(assert) {
-  assert.expect(7);
-  let user1 = EmberObject.create({
-    enabled: true,
-    firstName: 'a',
-    lastName: 'person',
-    fullName: 'a b person',
-  });
-  let user2 = EmberObject.create({
-    enabled: false,
-    firstName: 'b',
-    lastName: 'person',
-    fullName: 'b a person',
-  });
-  this.set('directors', [user1]);
-  this.set('administrators', [user2, user1]);
-  this.set('nothing', parseInt);
+    await render(hbs`{{leadership-manager
+      directors=directors
+      administrators=administrators
+      removeDirector=(action nothing)
+      addDirector=(action nothing)
+      removeAdministrator=(action remove)
+      addAdministrator=(action nothing)
+    }}`);
+    const list = 'table tbody tr:nth-of-type(1) td:nth-of-type(2) li';
+    const icon = `${list}:nth-of-type(1) i`;
 
-  this.render(hbs`{{leadership-manager
-    directors=directors
-    administrators=administrators
-    removeDirector=(action nothing)
-    addDirector=(action nothing)
-    removeAdministrator=(action nothing)
-    addAdministrator=(action nothing)
-  }}`);
-  const directors = 'table tbody tr:eq(0) td:eq(0) li';
-  const administrators = 'table tbody tr:eq(0) td:eq(1) li';
-  const disabledDirectors = `${directors} .fa-user-times`;
-  const disabledAdministrators = `${administrators} .fa-user-times`;
+    assert.equal(findAll(list).length, 1);
+    assert.equal(findAll(list)[0].textContent.trim(), '0 guy M. Mc0son');
+    await click(icon);
+  });
 
-  assert.equal(this.$(directors).length, 1);
-  assert.equal(this.$(disabledDirectors).length, 0);
-  assert.equal(this.$(directors).eq(0).text().trim(), 'a b person');
-  assert.equal(this.$(administrators).length, 2);
-  assert.equal(this.$(disabledAdministrators).length, 1);
-  assert.equal(this.$(administrators).eq(0).text().trim(), 'a b person');
-  assert.equal(this.$(administrators).eq(1).text().trim(), 'b a person');
+  test('add director', async function(assert) {
+    assert.expect(6);
+    this.server.createList('user', 1);
+    const user = await run(() => this.owner.lookup('service:store').find('user', 1));
+    this.set('directors', []);
+    this.set('administrators', [user]);
+    this.set('nothing', parseInt);
+    this.set('add', (who) => {
+      assert.equal(who, user, 'user passed correctly from action');
+      this.set('directors', [who]);
+    });
+
+    await render(hbs`{{leadership-manager
+      directors=directors
+      administrators=administrators
+      removeDirector=(action nothing)
+      addDirector=(action add)
+      removeAdministrator=(action nothing)
+      addAdministrator=(action nothing)
+    }}`);
+    const directorsList = 'table tbody tr:nth-of-type(1) td:nth-of-type(1) li';
+    const administratorsList = 'table tbody tr:nth-of-type(1) td:nth-of-type(2) li';
+    const directorSearch = '[data-test-director-search] input';
+    const firstResult = '[data-test-result-index="1"]';
+
+    assert.equal(findAll(directorsList).length, 0);
+    assert.equal(findAll(administratorsList).length, 1);
+    await fillIn(directorSearch, 'user');
+
+    assert.ok(find(firstResult).textContent.includes('0 guy'));
+    await click(firstResult);
+    assert.equal(findAll(directorsList).length, 1);
+    assert.equal(findAll(administratorsList).length, 1);
+  });
+
+  test('add administrator', async function(assert) {
+    assert.expect(6);
+    this.server.createList('user', 1);
+    const user = await run(() => this.owner.lookup('service:store').find('user', 1));
+    this.set('directors', [user]);
+    this.set('administrators', []);
+    this.set('nothing', parseInt);
+    this.set('add', (who) => {
+      assert.equal(who, user, 'user passed correctly from action');
+      this.set('administrators', [who]);
+    });
+
+    await render(hbs`{{leadership-manager
+      directors=directors
+      administrators=administrators
+      removeDirector=(action nothing)
+      addDirector=(action nothing)
+      removeAdministrator=(action nothing)
+      addAdministrator=(action add)
+    }}`);
+    const directorsList = 'table tbody tr:nth-of-type(1) td:nth-of-type(1) li';
+    const administratorsList = 'table tbody tr:nth-of-type(1) td:nth-of-type(2) li';
+    const administratorSearch = '[data-test-administrator-search] input';
+    const firstResult = '[data-test-result-index="1"]';
+
+    assert.equal(findAll(directorsList).length, 1);
+    assert.equal(findAll(administratorsList).length, 0);
+
+    await fillIn(administratorSearch, 'user');
+
+    assert.ok(find(firstResult).textContent.includes('0 guy'));
+    await click(firstResult);
+    assert.equal(findAll(directorsList).length, 1);
+    assert.equal(findAll(administratorsList).length, 1);
+  });
+
+  test('disabled users are indicated with an icon', async function(assert) {
+    assert.expect(7);
+    this.server.create('user', {
+      enabled: true
+    });
+    this.server.create('user', {
+      enabled: false
+    });
+    const users = await run(() => this.owner.lookup('service:store').findAll('user'));
+
+    this.set('directors', [users.firstObject]);
+    this.set('administrators', users);
+    this.set('nothing', parseInt);
+
+    await render(hbs`{{leadership-manager
+      directors=directors
+      administrators=administrators
+      removeDirector=(action nothing)
+      addDirector=(action nothing)
+      removeAdministrator=(action nothing)
+      addAdministrator=(action nothing)
+    }}`);
+    const directors = 'table tbody tr:nth-of-type(1) td:nth-of-type(1) li';
+    const administrators = 'table tbody tr:nth-of-type(1) td:nth-of-type(2) li';
+    const disabledDirectors = `${directors} .fa-user-times`;
+    const disabledAdministrators = `${administrators} .fa-user-times`;
+
+    assert.equal(findAll(directors).length, 1);
+    assert.equal(findAll(disabledDirectors).length, 0);
+    assert.equal(findAll(directors)[0].textContent.trim(), '0 guy M. Mc0son');
+    assert.equal(findAll(administrators).length, 2);
+    assert.equal(findAll(disabledAdministrators).length, 1);
+    assert.equal(findAll(administrators)[0].textContent.trim(), '0 guy M. Mc0son');
+    assert.equal(findAll(administrators)[1].textContent.trim(), '1 guy M. Mc1son');
+  });
 });
