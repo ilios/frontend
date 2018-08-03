1ade4a2a54cda7a28952e28cfa850a22c380d215
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-08-03 07:11:00 -0700

    Silence Ember.copy deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -6,5 +6,6 @@ window.deprecationWorkflow.config = {
     { handler: "silence", matchId: "ember-cli-page-object.old-collection-api"},
     { handler: "silence", matchId: "ember-metal.run.sync"},
     { handler: "silence", matchId: "ember-routing.route-router"},
+    { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable"},
   ]
 };
