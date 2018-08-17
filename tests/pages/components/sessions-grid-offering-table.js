2817d7ac0c4fb806a50880ef5a771a289512beb3
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-08-17 10:57:52 -0700

    Enforce permissions on grid
    Missed this in the initial refactor, but we need to enforce the
    canUpdate permissions on both the session and the offerings.

--- tests/pages/components/sessions-grid-offering-table.js
+++ tests/pages/components/sessions-grid-offering-table.js
@@ -18,7 +18,7 @@ export default {
     learners: text('td', { at: 2 }),
     learnerGroups: text('td', { at: 3 }),
     instructors: text('td', { at: 4 }),
-    edit: clickable('td:nth-of-type(6)'),
+    edit: clickable('td:nth-of-type(6) .link'),
     offeringForm,
   })
 };
