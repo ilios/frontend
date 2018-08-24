1b69305cd5ba2752ea8fae119d82211a01693ed9
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-08-24 13:35:51 -0700

    Update some tests
    These needed to be the new test type in order to work with new Ember
    data initializers in ilios-common.

--- tests/integration/components/manage-users-summary-test.js
+++ tests/integration/components/manage-users-summary-test.js
@@ -1,44 +1,43 @@
-import { moduleForComponent, test, skip } from 'ember-qunit';
+import { module, skip, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, findAll, find, fillIn, triggerEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import wait from 'ember-test-helpers/wait';
 
-moduleForComponent('manage-users-summary', 'Integration | Component | manage users summary', {
-  integration: true,
-});
+module('Integration | Component | manage users summary', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  this.render(hbs`{{manage-users-summary canCreate=true}}`);
+  test('it renders', async function(assert) {
+    await render(hbs`{{manage-users-summary canCreate=true}}`);
 
-  assert.ok(this.$('h2').text().trim().startsWith('Ilios Users'));
-  assert.ok(this.$('h2').text().includes('View All'));
-  assert.equal(this.$('a:eq(0)').text().trim(), 'View All');
-  assert.equal(this.$('a:eq(2)').text().trim(), 'Upload Multiple Users');
-});
+    assert.ok(find('h2').textContent.trim().startsWith('Ilios Users'));
+    assert.ok(find('h2').textContent.includes('View All'));
+    assert.equal(find('a').textContent.trim(), 'View All');
+    assert.equal(findAll('a')[2].textContent.trim(), 'Upload Multiple Users');
+  });
 
-/**
- * @todo Move the URL tests to an acceptance test so we don't
- * have to inject a working router which blows up ember-simple-auth
- * [JJ 3/2017]
- */
-skip('it renders URLs', function(assert) {
-  this.render(hbs`{{manage-users-summary}}`);
+  /**
+   * @todo Move the URL tests to an acceptance test so we don't
+   * have to inject a working router which blows up ember-simple-auth
+   * [JJ 3/2017]
+   */
+  skip('it renders URLs', function(assert) {
+    this.render(hbs`{{manage-users-summary}}`);
 
-  assert.notEqual(this.$('a:eq(0)').prop('href').search(/\/users$/), -1, `${this.$('a:eq(0)').prop('href')} links to /users`);
-  assert.notEqual(this.$('a:eq(1)').prop('href').search(/\/users\?addUser=true$/), -1, `${this.$('a:eq(1)').prop('href')} links to /users?addUser=true`);
-  assert.notEqual(this.$('a:eq(2)').prop('href').search(/\/users\?addUsers=true$/), -1, `${this.$('a:eq(2)').prop('href')} links to /users?addUsers=true`);
-});
+    assert.notEqual(find('a').href.search(/\/users$/), -1, `${find('a').href} links to /users`);
+    assert.notEqual(findAll('a')[1].href.search(/\/users\?addUser=true$/), -1, `${findAll('a')[1].href} links to /users?addUser=true`);
+    assert.notEqual(findAll('a')[2].href.search(/\/users\?addUsers=true$/), -1, `${findAll('a')[2].href} links to /users?addUsers=true`);
+  });
 
-test('show more input prompt', function(assert) {
-  this.render(hbs`{{manage-users-summary}}`);
+  test('show more input prompt', async function(assert) {
+    await render(hbs`{{manage-users-summary}}`);
 
-  const userSearch = '.user-search input';
-  const results = '.user-search .results li';
+    const userSearch = '.user-search input';
+    const results = '.user-search .results li';
 
-  this.$(userSearch).val('12');
-  this.$(userSearch).trigger('keyup');
+    await fillIn(userSearch, '12');
+    await triggerEvent(userSearch, 'keyup');
 
-  return wait().then(()=>{
-    assert.equal(this.$(results).length, 1);
-    assert.equal(this.$(results).text().trim(), 'keep typing...');
+    assert.equal(findAll(results).length, 1);
+    assert.equal(find(results).textContent.trim(), 'keep typing...');
   });
 });
