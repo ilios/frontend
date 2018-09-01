0ab4d2d1610ba1046a938bdac4e3c0b159016be6
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-08-31 21:36:21 -0700

    Mark new deprecations
    I've created issues to deal with them, but for now they just need to be
    silenced in tests.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -7,5 +7,8 @@ window.deprecationWorkflow.config = {
     { handler: "silence", matchId: "ember-metal.run.sync"},
     { handler: "silence", matchId: "ember-routing.route-router"},
     { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable"},
+    { handler: "silence", matchId: "ember-component.send-action"},
+    { handler: "silence", matchId: "ember-console.deprecate-logger"},
+    { handler: "silence", matchId: "ember-views.event-dispatcher.jquery-event"},
   ]
 };
