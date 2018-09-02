04201f93ab7851d965d1dec7934bb38cae52ddf0
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-09-02 09:29:51 -0700

    Convert report list test to new API
    This test was failing sometimes so I moved it to the new API.

--- tests/integration/components/curriculum-inventory-report-list-test.js
+++ tests/integration/components/curriculum-inventory-report-list-test.js
@@ -1,213 +1,237 @@
-import EmberObject from '@ember/object';
 import { resolve } from 'rsvp';
 import Service from '@ember/service';
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, render, findAll, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import moment from 'moment';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { run } from '@ember/runloop';
 
-moduleForComponent('curriculum-inventory-report-list', 'Integration | Component | curriculum inventory report list', {
-  integration: true,
-});
+module('Integration | Component | curriculum inventory report list', function(hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-test('it renders', function(assert) {
-  let report1 = EmberObject.create({
-    id: 1,
-    name: 'Zeppelin',
-    year: 2017,
-    yearLabel: '2017 - 2018',
-    startDate: moment('2017-07-01').toDate(),
-    endDate: moment('2018-06-30').toDate(),
-    program: EmberObject.create({
-      'title': 'Doctor of Medicine'
-    }),
-    isFinalized: false,
-  });
-  let report2 = EmberObject.create({
-    id: 2,
-    name: 'Aardvark',
-    year: 2016,
-    startDate: moment('2016-07-01').toDate(),
-    endDate: moment('2017-06-30').toDate(),
-    yearLabel: '2016 - 2017',
-    program: EmberObject.create({
-      'title': 'Doctor of Rocket Surgery'
-    }),
-    isFinalized: true,
-  });
+  test('it renders', async function (assert) {
+    const school = this.server.create('school');
+    const program = this.server.create('program', { school });
+    const report1 = this.server.create('curriculum-inventory-report', {
+      program,
+      name: 'Zeppelin',
+      year: 2017,
+      startDate: moment('2017-07-01').toDate(),
+      endDate: moment('2018-06-30').toDate(),
+    });
+    const reportExport = this.server.create('curriculum-inventory-export');
+    const report2 = this.server.create('curriculum-inventory-report', {
+      program,
+      export: reportExport,
+      name: 'Aardvark',
+      year: 2016,
+      startDate: moment('2016-07-01').toDate(),
+      endDate: moment('2017-06-30').toDate(),
+    });
 
-  const permissionCheckerMock = Service.extend({
-    canDeleteCurriculumInventoryReport() {
-      return resolve(true);
-    }
-  });
-  this.register('service:permission-checker', permissionCheckerMock);
+    const permissionCheckerMock = Service.extend({
+      canDeleteCurriculumInventoryReport() {
+        return resolve(true);
+      }
+    });
+    this.owner.register('service:permission-checker', permissionCheckerMock);
 
-  const reports = [ report1, report2 ];
+    const reports = [report1, report2];
+    const programModel = await run(() => this.owner.lookup('service:store').find('program', program.id));
 
-  const program = EmberObject.create({
-    curriculumInventoryReports: resolve(reports)
-  });
+    this.set('program', programModel);
+    await render(hbs`{{curriculum-inventory-report-list program=program}}`);
+    assert.equal(findAll('th').length, 7, 'Table header has seven columns.');
+    assert.equal(find('th').textContent.trim(), 'Report Name', 'First column table header is labeled correctly');
+    assert.equal(findAll('th')[1].textContent.trim(), 'Program', 'Second column table header is labeled correctly');
+    assert.equal(findAll('th')[2].textContent.trim(), 'Academic Year', 'Third column table header is labeled correctly');
+    assert.equal(findAll('th')[3].textContent.trim(), 'Start Date', 'Fourth column table header is labeled correctly');
+    assert.equal(findAll('th')[4].textContent.trim(), 'End Date', 'Fifth column table header is labeled correctly');
+    assert.equal(findAll('th')[5].textContent.trim(), 'Status', 'Sixth column table header is labeled correctly');
+    assert.equal(findAll('th')[6].textContent.trim(), 'Actions', 'Seventh column table header is labeled correctly');
+    assert.equal(findAll('tbody tr').length, reports.length, 'All reports are shown in list.');
 
-  this.set('program', program);
-  this.render(hbs`{{curriculum-inventory-report-list program=program}}`);
-  assert.equal(this.$('th').length, 7, 'Table header has seven columns.');
-  assert.equal(this.$('th:eq(0)').text().trim(), 'Report Name', 'First column table header is labeled correctly');
-  assert.equal(this.$('th:eq(1)').text().trim(), 'Program', 'Second column table header is labeled correctly');
-  assert.equal(this.$('th:eq(2)').text().trim(), 'Academic Year', 'Third column table header is labeled correctly');
-  assert.equal(this.$('th:eq(3)').text().trim(), 'Start Date', 'Fourth column table header is labeled correctly');
-  assert.equal(this.$('th:eq(4)').text().trim(), 'End Date', 'Fifth column table header is labeled correctly');
-  assert.equal(this.$('th:eq(5)').text().trim(), 'Status', 'Sixth column table header is labeled correctly');
-  assert.equal(this.$('th:eq(6)').text().trim(), 'Actions', 'Seventh column table header is labeled correctly');
-  assert.equal(this.$('tbody tr').length, reports.length, 'All reports are shown in list.');
-  for (let i = 0, n = reports.length; i < n; i++) {
-    let report = reports[i];
-    assert.equal(this.$(`tbody tr:eq(${i}) td:eq(0)`).text().trim(), report.get('name'), 'Report name shows.');
+    assert.equal(findAll('tbody tr').length, 2);
+    assert.equal(find(`[data-test-report="0"] [data-test-name]`).textContent.trim(), report1.name, 'Report name shows.');
     assert.equal(
-      this.$(`tbody tr:eq(${i}) td:eq(1)`).text().trim(),
-      report.get('program').get('title'),
+      find(`[data-test-report="0"] [data-test-program]`).textContent.trim(),
+      program.title,
       'Program title shows.'
     );
     assert.equal(
-      this.$(`tbody tr:eq(${i}) td:eq(2)`).text().trim(),
-      report.get('yearLabel'),
+      find(`[data-test-report="0"] [data-test-year]`).textContent.trim(),
+      '2017 - 2018',
       'Academic year shows.'
     );
-    assert.equal(this.$(`tbody tr:eq(${i}) td:eq(3)`).text().trim(),
-      moment(report.get('startDate')).format('L'),
+    assert.equal(find(`[data-test-report="0"] [data-test-start-date]`).textContent.trim(),
+      moment(report1.startDate).format('L'),
       'Start date shows.'
     );
-    assert.equal(this.$(`tbody tr:eq(${i}) td:eq(4)`).text().trim(),
-      moment(report.get('endDate')).format('L'),
+    assert.equal(find(`[data-test-report="0"] [data-test-end-date]`).textContent.trim(),
+      moment(report1.endDate).format('L'),
       'End date shows.'
     );
-    assert.equal(this.$(`tbody tr:eq(${i}) td:eq(5)`).text().trim(),
-      report.get('isFinalized') ? 'Finalized' : 'Draft',
+    assert.equal(find(`[data-test-report="0"] [data-test-status]`).textContent.trim(),
+      'Draft',
       'Status shows.'
     );
-    assert.equal(this.$(`tbody tr:eq(${i}) td:eq(6) .fa-edit`).length, 1, 'Edit button shows.');
-    assert.equal(this.$(`tbody tr:eq(${i}) td:eq(6) .fa-download`).length, 1, 'Download button shows.');
-    if (report.get('isFinalized')) {
-      assert.equal(
-        this.$(`tbody tr:eq(${i}) td:eq(6) .fa-trash`).length,
-        0,
-        'Delete button is not visible for finalized reports.'
-      );
-    } else {
-      assert.equal(
-        this.$(`tbody tr:eq(${i}) td:eq(6) .fa-trash`).length,
-        1,
-        'Delete button shows for reports in draft.'
-      );
-    }
-  }
-});
+    assert.equal(findAll(`[data-test-report="0"] [data-test-actions] .fa-edit`).length, 1, 'Edit button shows.');
+    assert.equal(findAll(`[data-test-report="0"] [data-test-actions] .fa-download`).length, 1, 'Download button shows.');
+    assert.equal(
+      findAll(`[data-test-report="0"] [data-test-actions] .fa-trash`).length,
+      1,
+      'Delete button shows for reports in draft.'
+    );
 
-test('empty list', function(assert) {
-  const program = EmberObject.create({
-    curriculumInventoryReports: resolve([])
+    assert.equal(find(`[data-test-report="1"] [data-test-name]`).textContent.trim(), report2.name, 'Report name shows.');
+    assert.equal(
+      find(`[data-test-report="1"] [data-test-program]`).textContent.trim(),
+      program.title,
+      'Program title shows.'
+    );
+    assert.equal(
+      find(`[data-test-report="1"] [data-test-year]`).textContent.trim(),
+      '2016 - 2017',
+      'Academic year shows.'
+    );
+    assert.equal(find(`[data-test-report="1"] [data-test-start-date]`).textContent.trim(),
+      moment(report2.startDate).format('L'),
+      'Start date shows.'
+    );
+    assert.equal(find(`[data-test-report="1"] [data-test-end-date]`).textContent.trim(),
+      moment(report2.endDate).format('L'),
+      'End date shows.'
+    );
+    assert.equal(find(`[data-test-report="1"] [data-test-status]`).textContent.trim(),
+      'Finalized',
+      'Status shows.'
+    );
+    assert.equal(findAll(`[data-test-report="1"] [data-test-actions] .fa-edit`).length, 1, 'Edit button shows.');
+    assert.equal(findAll(`[data-test-report="1"] [data-test-actions] .fa-download`).length, 1, 'Download button shows.');
+    assert.equal(
+      findAll(`[data-test-report="1"] [data-test-actions] .fa-trash`).length,
+      0,
+      'Delete button is not visible for finalized reports.'
+    );
   });
 
-  this.set('program', program);
-  this.render(hbs`{{curriculum-inventory-report-list program=program}}`);
-  assert.equal(this.$('thead tr').length, 1, 'Table header shows.');
-  assert.equal(this.$('tbody').length, 1, 'Table body shows.');
-  assert.equal(this.$('tbody tr').length, 0, 'Table body is empty.');
-});
+  test('empty list', async function (assert) {
+    const school = this.server.create('school');
+    const program = this.server.create('program', { school });
+    const programModel = await run(() => this.owner.lookup('service:store').find('program', program.id));
 
-test('delete and confirm', function(assert) {
-  assert.expect(3);
-
-  const permissionCheckerMock = Service.extend({
-    canDeleteCurriculumInventoryReport() {
-      return resolve(true);
-    }
-  });
-  this.register('service:permission-checker', permissionCheckerMock);
-
-  let report = EmberObject.create();
-  let removeAction =  function(obj) {
-    assert.equal(report, obj, 'Report is passed to remove action.');
-  };
-  const program = EmberObject.create({
-    curriculumInventoryReports: resolve([ report ])
+    this.set('program', programModel);
+    await render(hbs`{{curriculum-inventory-report-list program=program}}`);
+    assert.equal(findAll('thead tr').length, 1, 'Table header shows.');
+    assert.equal(findAll('tbody').length, 1, 'Table body shows.');
+    assert.equal(findAll('tbody tr').length, 0, 'Table body is empty.');
   });
 
-  this.set('program', program);
-  this.set('removeAction', removeAction);
-  this.render(hbs`{{curriculum-inventory-report-list program=program remove=removeAction}}`);
-  assert.equal(this.$('.confirm-removal').length, 0, 'Confirm dialog is initially not visible.');
-  this.$(`tbody tr:eq(0) .remove`).click();
-  assert.equal(this.$('.confirm-removal').length, 2, 'Confirm dialog shows.');
-  this.$(`.confirm-removal .remove`).click();
-});
+  test('delete and confirm', async function(assert) {
+    assert.expect(3);
+    const school = this.server.create('school');
+    const program = this.server.create('program', { school });
+    const report = this.server.create('curriculum-inventory-report', {
+      program,
+      name: 'Zeppelin',
+    });
+    const programModel = await run(() => this.owner.lookup('service:store').find('program', program.id));
 
-test('delete and cancel', function(assert) {
-  assert.expect(3);
+    const permissionCheckerMock = Service.extend({
+      canDeleteCurriculumInventoryReport() {
+        return resolve(true);
+      }
+    });
+    this.owner.register('service:permission-checker', permissionCheckerMock);
 
-  const permissionCheckerMock = Service.extend({
-    canDeleteCurriculumInventoryReport() {
-      return resolve(true);
-    }
-  });
-  this.register('service:permission-checker', permissionCheckerMock);
-
-  let report = EmberObject.create();
-  let removeAction =  function() {
-    assert.ok(false, 'Remove action should not have been invoked.');
-  };
-  const program = EmberObject.create({
-    curriculumInventoryReports: resolve([ report ])
-  });
-
-  this.set('program', program);
-  this.set('removeAction', removeAction);
-  this.render(hbs`{{curriculum-inventory-report-list program=program remove=removeAction}}`);
-  assert.equal(this.$('.confirm-removal').length, 0, 'Confirm dialog is initially not visible.');
-  this.$(`tbody tr:eq(0) .remove`).click();
-  assert.equal(this.$('.confirm-removal').length, 2, 'Confirm dialog shows.');
-  this.$(`.confirm-removal .done`).click();
-  assert.equal(this.$('.confirm-removal').length, 0, 'Confirm dialog is invisible again.');
-});
-
-test('sorting', function(assert) {
-  assert.expect(4);
-  let report = EmberObject.create();
-  let count = 0;
-  let sortBys = ['name', 'name:desc', 'year', 'year:desc'];
-  const program = EmberObject.create({
-    curriculumInventoryReports: resolve([ report ])
+    this.set('program', programModel);
+    this.set('removeAction', (obj) => {
+      assert.equal(report.id, obj.id, 'Report is passed to remove action.');
+    });
+    await render(hbs`{{curriculum-inventory-report-list program=program remove=removeAction}}`);
+    assert.equal(findAll('.confirm-removal').length, 0, 'Confirm dialog is initially not visible.');
+    await click('[data-test-report="0"] .remove');
+    assert.equal(findAll('.confirm-removal').length, 2, 'Confirm dialog shows.');
+    await click('[data-test-confirm-removal="0"] .remove');
   });
 
-  this.set('program', program);
-  this.set('sortBy', 'id');
-  this.set('setSortBy', (what) => {
-    assert.equal(what, sortBys[count]);
-    this.set('sortBy', what);
-    count++;
-  });
-  this.render(hbs`{{curriculum-inventory-report-list program=program setSortBy=(action setSortBy) sortBy=sortBy}}`);
-  this.$(`th:eq(0)`).click();
-  this.$(`th:eq(0)`).click();
-  this.$(`th:eq(2)`).click();
-  this.$(`th:eq(2)`).click();
-});
+  test('delete and cancel', async function(assert) {
+    assert.expect(3);
+    const school = this.server.create('school');
+    const program = this.server.create('program', { school });
+    this.server.create('curriculum-inventory-report', {
+      program,
+      name: 'Zeppelin',
+    });
+    const permissionCheckerMock = Service.extend({
+      canDeleteCurriculumInventoryReport() {
+        return resolve(true);
+      }
+    });
+    this.owner.register('service:permission-checker', permissionCheckerMock);
+    const programModel = await run(() => this.owner.lookup('service:store').find('program', program.id));
 
-test('edit', function(assert) {
-  assert.expect(5);
-  let report = EmberObject.create({ id: 1 });
-  let editAction =  function(obj) {
-    assert.equal(report, obj, 'Report is passed to edit action.');
-  };
-  const program = EmberObject.create({
-    curriculumInventoryReports: resolve([ report ])
+    this.set('program', programModel);
+    this.set('removeAction', () => {
+      assert.ok(false, 'Remove action should not have been invoked.');
+    });
+    await render(hbs`{{curriculum-inventory-report-list program=program remove=removeAction}}`);
+    assert.equal(findAll('.confirm-removal').length, 0, 'Confirm dialog is initially not visible.');
+    await click('[data-test-report="0"] .remove');
+    assert.equal(findAll('.confirm-removal').length, 2, 'Confirm dialog shows.');
+    await click('[data-test-confirm-removal="0"] .done');
+    assert.equal(findAll('.confirm-removal').length, 0, 'Confirm dialog is invisible again.');
   });
 
-  this.set('program', program);
-  this.set('editAction', editAction);
-  this.render(hbs`{{curriculum-inventory-report-list program=program edit=editAction}}`);
-  this.$(`tbody tr:eq(0) td:eq(1)`).click();
-  this.$(`tbody tr:eq(0) td:eq(2)`).click();
-  this.$(`tbody tr:eq(0) td:eq(3)`).click();
-  this.$(`tbody tr:eq(0) td:eq(4)`).click();
-  this.$(`tbody tr:eq(0) td:eq(5)`).click();
+  test('sorting', async function(assert) {
+    assert.expect(4);
+    const school = this.server.create('school');
+    const program = this.server.create('program', { school });
+    this.server.create('curriculum-inventory-report', {
+      program,
+      name: 'Zeppelin',
+    });
+    const programModel = await run(() => this.owner.lookup('service:store').find('program', program.id));
+
+    let count = 0;
+    let sortBys = ['name', 'name:desc', 'year', 'year:desc'];
+
+    this.set('program', programModel);
+    this.set('sortBy', 'id');
+    this.set('setSortBy', (what) => {
+      assert.equal(what, sortBys[count]);
+      this.set('sortBy', what);
+      count++;
+    });
+    await render(hbs`{{curriculum-inventory-report-list program=program setSortBy=(action setSortBy) sortBy=sortBy}}`);
+    await click(`th:nth-of-type(1)`);
+    await click(`th:nth-of-type(1)`);
+    await click(`th:nth-of-type(3)`);
+    await click(`th:nth-of-type(3)`);
+  });
+
+  test('edit', async function(assert) {
+    assert.expect(5);
+    const school = this.server.create('school');
+    const program = this.server.create('program', { school });
+    const report = this.server.create('curriculum-inventory-report', {
+      program,
+      name: 'Zeppelin',
+    });
+    const programModel = await run(() => this.owner.lookup('service:store').find('program', program.id));
+
+    this.set('program', programModel);
+    this.set('editAction', (obj) => {
+      assert.equal(report.id, obj.id, 'Report is passed to edit action.');
+    });
+    await render(hbs`{{curriculum-inventory-report-list program=program edit=editAction}}`);
+    await click(`tbody tr:nth-of-type(1) td:nth-of-type(2)`);
+    await click(`tbody tr:nth-of-type(1) td:nth-of-type(3)`);
+    await click(`tbody tr:nth-of-type(1) td:nth-of-type(4)`);
+    await click(`tbody tr:nth-of-type(1) td:nth-of-type(5)`);
+    await click(`tbody tr:nth-of-type(1) td:nth-of-type(6)`);
+  });
 });
