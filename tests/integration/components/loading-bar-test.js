c73dcb942114f10d9b7b9ecfaf8c6c1e080d20e6
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-09-23 12:01:31 -0700

    Use the waitUntil helper instead of a timeout
    The timeout in later didn't always work in our travis builds, but this
    method keeps calling the callback until it gets a truthy value.

--- tests/integration/components/loading-bar-test.js
+++ tests/integration/components/loading-bar-test.js
@@ -1,8 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find, waitFor } from '@ember/test-helpers';
+import { render, find, waitFor, waitUntil } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { later } from '@ember/runloop';
 
 module('Integration | Component | loading bar', function(hooks) {
   setupRenderingTest(hooks);
@@ -14,15 +13,14 @@ module('Integration | Component | loading bar', function(hooks) {
 
     // don't `await` this render as the internal task that never stops will keep this test running forever
     render(hbs`{{loading-bar isLoading=isLoading}}`);
+    await waitUntil(() => {
+      const value = parseInt(find(bar).getAttribute('value').trim(), 10);
+      return value > 0;
+    });
     await waitFor(bar);
-    // we need to give the bar a moment to actually change state and we
-    // cannot use wait here because the task actually keeps running forever preventing
-    // any kind of settled state
-    await later(async () => {
-      assert.ok(find(bar).getAttribute('value').trim() > 0);
-      this.set('isLoading', false);
-      await waitFor(emptyBar);
-      assert.equal(find(bar).getAttribute('value').trim(), 0);
-    }, 500);
+    assert.ok(find(bar).getAttribute('value').trim() > 0);
+    this.set('isLoading', false);
+    await waitFor(emptyBar);
+    assert.equal(find(bar).getAttribute('value').trim(), 0);
   });
 });
