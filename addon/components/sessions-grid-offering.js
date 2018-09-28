77e64b114e6ff815bec2652d6e7765ea0b00c76a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-09-28 14:45:39 -0700

    backfilled component that got dropped in transition.

--- addon/components/sessions-grid-offering.js
+++ addon/components/sessions-grid-offering.js
@@ -1,6 +1,74 @@
 import Component from '@ember/component';
+import { validator, buildValidations } from 'ember-cp-validations';
+import ValidationErrorDisplay from 'ilios-common/mixins/validation-error-display';
+import { task, timeout } from 'ember-concurrency';
+import scrollIntoView from 'scroll-into-view';
 import layout from '../templates/components/sessions-grid-offering';
 
-export default Component.extend({
-  layout
+const Validations = buildValidations({
+  room: [
+    validator('presence', {
+      presence: true
+    }),
+    validator('length', {
+      max: 255
+    }),
+  ],
+});
+
+export default Component.extend(ValidationErrorDisplay, Validations, {
+  layout,
+  tagName: 'tr',
+  classNameBindings: [':sessions-grid-offering', 'firstRow', 'even', 'wasUpdated'],
+  canUpdate: false,
+  room: null,
+  firstRow: false,
+  even: false,
+  isEditing: false,
+  wasUpdated: false,
+  'data-test-sessions-grid-offering': true,
+
+  didReceiveAttrs(){
+    this._super(...arguments);
+    const offering = this.get('offering');
+    this.set('room', offering.room);
+  },
+
+  actions: {
+    revertRoomChanges(){
+      const offering = this.get('offering');
+      this.set('room', offering.get('room'));
+    },
+    close() {
+      this.set('isEditing', false);
+      scrollIntoView(this.element);
+    },
+  },
+  changeRoom: task(function * (){
+    yield timeout(10);
+    this.send('addErrorDisplayFor', 'room');
+    const offering = this.get('offering');
+    const room = this.get('room');
+    const { validations } = yield this.validate();
+    if (validations.get('isInvalid')) {
+      return;
+    }
+    this.send('removeErrorDisplayFor', 'room');
+    offering.set('room', room);
+    yield offering.save();
+  }).drop(),
+  save: task(function * (startDate, endDate, room, learnerGroups, instructorGroups, instructors){
+    const offering = this.get('offering');
+    offering.setProperties({startDate, endDate, room, learnerGroups, instructorGroups, instructors});
+
+    yield offering.save();
+    this.updateUi.perform();
+  }).drop(),
+  updateUi: task(function* () {
+    yield timeout(10);
+    this.set('wasUpdated', true);
+    scrollIntoView(this.element);
+    yield timeout(4000);
+    this.set('wasUpdated', false);
+  }).restartable(),
 });
