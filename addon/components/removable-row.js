f261e6fbe2f89242c3743a9f7ecd9d3c8fed4320
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-10-18 10:42:06 -0700

    Remove light-table component
    We don't need this any more

--- addon/components/removable-row.js
+++
@@ -1,5 +0,0 @@
-import Row from 'ember-light-table/components/lt-row';
-
-export default Row.extend({
-  classNameBindings: ['row.confirmDelete:confirm-removal'],
-});
--- app/components/removable-row.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ilios-common/components/removable-row';
