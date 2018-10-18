6eaab5c47035bf1f4f8f139ab9cf4b7d2e030cc7
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-10-18 11:01:53 -0700

    rm obsolete component.

--- app/components/removable-row.js
+++
@@ -1,6 +0,0 @@
-/* eslint ember/order-in-components: 0 */
-import Row from 'ember-light-table/components/lt-row';
-
-export default Row.extend({
-  classNameBindings: ['row.confirmDelete:confirm-removal'],
-});
