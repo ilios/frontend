0d5a19c986f85ff263bddd953a6d8a7ae5168d14
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-10-18 10:39:38 -0700

    Remove light table dependencies
    We were only using this for light table and we're not using that
    anymore.

--- addon/breakpoints.js
+++
@@ -1,6 +0,0 @@
-export default {
-  smallScreen:  '(max-width: 320px)',
-  mediumScreen:  '(min-width: 321px) and (max-width: 640px)',
-  largeScreen:  '(min-width: 641px) and (max-width: 860px)',
-  giantScreen: '(min-width: 861px)'
-};
