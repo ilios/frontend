9950725a7670aa2e00bcd18ee332585429239601
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-12-17 11:35:37 -0800

    converted component unit tests to RFC 232 compliant format.

--- tests/unit/components/recently-updated-display-test.js
+++ tests/unit/components/recently-updated-display-test.js
@@ -1,16 +1,22 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'ember-qunit';
+import { setupTest } from 'ember-qunit';
+
 import moment from 'moment';
 
-moduleForComponent('recently-updated-display', 'Unit | Component | recently updated display', {
-  unit: true,
+module('Unit | Component | recently updated display', function(hooks) {
+  setupTest(hooks);
+
+  test('`recentlyUpdated` computed property works', function(assert) {
+    assert.expect(2);
+    const lastModified = moment().subtract(5, 'day');
+
+    const Factory = this.owner.factoryFor('component:recently-updated-display');
+    const component = Factory.create({ lastModified });
+
+    assert.ok(component.get('recentlyUpdated'), 'last modified within 5 days');
+
+    component.set('lastModified', moment().subtract(7, 'day'));
+    assert.notOk(component.get('recentlyUpdated'), 'last modified more than 5 days');
+  });
 });
 
-test('`recentlyUpdated` computed property works', function(assert) {
-  const lastModified = moment().subtract(5, 'day');
-  const component = this.subject({ lastModified });
-
-  assert.ok(component.get('recentlyUpdated'), 'last modified within 5 days');
-
-  component.set('lastModified', moment().subtract(7, 'day'));
-  assert.notOk(component.get('recentlyUpdated'), 'last modified more than 5 days');
-});
