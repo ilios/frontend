b829016bfb7320a5c21cfa85d17b8f8f3abd226c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-01-10 15:29:31 -0800

    pre-load sibling sessions.

--- addon/mixins/session-route.js
+++ addon/mixins/session-route.js
@@ -1,4 +1,14 @@
 import Mixin from '@ember/object/mixin';
 
 export default Mixin.create({
+  async model(params) {
+    const store = this.get('store');
+    return await store.find('session', params.session_id);
+  },
+
+  async afterModel(model) {
+    const store = this.get('store');
+    const course = await model.get('course');
+    await store.query('session', { filters: { course: course.get('id') } });
+  }
 });
