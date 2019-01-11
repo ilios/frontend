f2734effe415533063b7a9390f90f5f2d5db04a2
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-01-11 15:40:43 -0800

    rm unnecessary model() hook.

--- addon/mixins/session-route.js
+++ addon/mixins/session-route.js
@@ -1,11 +1,6 @@
 import Mixin from '@ember/object/mixin';
 
 export default Mixin.create({
-  async model(params) {
-    const store = this.get('store');
-    return await store.find('session', params.session_id);
-  },
-
   async afterModel(model) {
     const store = this.get('store');
     const course = await model.get('course');
