a494c5360862317a6758186582943ca8bd533145
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-01-28 10:19:53 -0800

    Drop support for unsupported browsers
    This set of browsers that we target did not match our published support
    matrix and was including IE11 which does not have CSS Grid support and
    looks terrible. Instead we should only target browsers that we actually
    support.

--- config/targets.js
+++ config/targets.js
@@ -13,7 +13,6 @@ if (isCI || isProductionLikeBuild) {
   browsers.push('last 1 edge versions');
   browsers.push('firefox esr'); //sometimes points to the last 2 ESR releases when they overlap
   browsers.push('last 1 ios versions');
-  browsers.push('> 1%'); // any browser with more than 1% global market share
 }
 
 module.exports = {
