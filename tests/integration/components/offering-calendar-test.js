66330574d008a32a364fa6829d0159c22c69debe
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-01-30 16:01:24 -0800

    Add pre and post requisite dummy data to offering calendar events
    As we work on the UI here we just need to fake data so things don't
    break.

---
+++ tests/integration/components/offering-calendar-test.js
@@ -0,0 +1,51 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
+import { run } from '@ember/runloop';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Integration | Component | offering-calendar', function(hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  test('shows events', async function(assert) {
+    assert.expect(1);
+    const today = moment().hour(8);
+    const tomorrow = moment().add(1, 'day').hour(8);
+    const course = this.server.create('course');
+    const sessionType = this.server.create('session-type');
+    const session = this.server.create('session', {
+      course,
+      sessionType
+    });
+
+
+    const offering1 = this.server.create('offering', {
+      startDate: today.format(),
+      endDate: today.clone().add('1', 'hour').format(),
+      location: 123,
+      session
+    });
+    const offering2 = this.server.create('offering', {
+      startDate: today.format(),
+      endDate: today.clone().add('1', 'hour').format(),
+      location: 123,
+      session
+    });
+    const learnerGroup = this.server.create('learner-group', {
+      offerings: [offering1, offering2],
+    });
+    const sessionModel = await run(() => this.owner.lookup('service:store').find('session', session.id));
+    const learnerGroupModel = await run(() => this.owner.lookup('service:store').find('learner-group', learnerGroup.id));
+    this.server.logging = true;
+    this.set('startDate', today.toDate());
+    this.set('endDate', tomorrow.toDate());
+    this.set('session', sessionModel);
+    this.set('learnerGroups', [learnerGroupModel]);
+    await render(hbs`{{offering-calendar learnerGroups=learnerGroups session=session startDate=startDate endDate=endDate}}`);
+    const events = '.ilios-calendar-event';
+    assert.dom(events).exists({ count: 4 });
+  });
+});
