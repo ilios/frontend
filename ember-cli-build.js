2d462886aa916446e8253c73f70d19a7d0c74f2b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-02-04 16:13:49 -0800

    re-aligned cli build config with frontend.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,14 +1,26 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const broccoliAssetRevDefaults = require( 'broccoli-asset-rev/lib/default-options' );
 
 module.exports = function(defaults) {
+  let env = EmberApp.env() || 'development';
+  let isProductionLikeBuild = ['production', 'staging', 'preview'].indexOf(env) > -1;
+
   let app = new EmberApp(defaults, {
     fingerprint: {
+      extensions: broccoliAssetRevDefaults.extensions.concat(['webmanifest', 'svg']),
+      enabled: isProductionLikeBuild,
       exclude: ['ilios-icon.png']
     },
-    'ember-cli-babel': {
-      includePolyfill: true
+    sourcemaps: {
+      enabled: true,
+    },
+    minifyCSS: { enabled: isProductionLikeBuild },
+    minifyJS: { enabled: isProductionLikeBuild },
+
+    babel: {
+      plugins: [ require('ember-auto-import/babel-plugin') ],
     },
     'ember-cli-uglify': {
       uglify: {
@@ -17,6 +29,29 @@ module.exports = function(defaults) {
         }
       }
     },
+    postcssOptions: {
+      compile: {
+        extension: 'scss',
+        enabled: true,
+        parser: require('postcss-scss'),
+        plugins: [
+          {
+            module: require('@csstools/postcss-sass'),
+          },
+        ]
+      },
+      filter: {
+        enabled: true,
+        plugins: [
+          {
+            module: require('autoprefixer'),
+          }
+        ]
+      }
+    },
   });
+
+  app.import('node_modules/normalize.css/normalize.css');
+
   return app.toTree();
 };
