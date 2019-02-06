6293e083e6333fac3b2cfde7721707e6d61019b2
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-02-05 23:15:08 -0800

    Remove browser-timezone helper
    This is provided by common now

--- app/helpers/browser-timezone.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import moment from 'moment';
-
-export function browserTimezone() {
-  return moment.tz.guess();
-}
-
-export default helper(browserTimezone);
