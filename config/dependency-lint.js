70680cc822ad5ea24ae9751bd19a42f916b76505
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-02-09 23:53:37 -0800

    Remove Dependency Lint Exceptions
    These are not needed anymore, so.... remove them!

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -3,10 +3,5 @@
 
 module.exports = {
   allowedVersions: {
-    'ember-getowner-polyfill': '^1.0.0 || ^2.0.0',
-    'ember-inflector': '2.3.0 || ^3.0.0', // conflict between ember-data and mirage
-    'ember-hash-helper-polyfill': '^0.1.2 || ^0.2.0', // workaround for dep. conflict ember-tooltips/liquid-fire
-    'ember-require-module': '^0.1.2 || ^0.2.0', // workaround for dep. conflict ember-cp-validations/ember-validators
-    'ember-concurrency': '^0.8.17', // workaround for dep. conflict ilios-common/ember-simple-charts
   }
 };
