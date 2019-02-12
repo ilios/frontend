892b2d09579bc98ed799e288be0ed9618b8ce14e
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-02-11 16:48:29 -0800

    Enable session linking
    For now I just enabled it by default, we'll want to pull out the flag
    code itself once it has been released.

--- config/environment.js
+++ config/environment.js
@@ -18,7 +18,7 @@ module.exports = function(environment /*, appConfig */) {
       }
     },
     featureFlags: {
-      'sessionLinkingAdminUi': false,
+      'sessionLinkingAdminUi': true,
     }
   };
 
