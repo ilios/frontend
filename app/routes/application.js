2a35750739c942f67e7f9fdebb38824865376ec5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-02-12 15:20:21 -0800

    pre-load user-roles.

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,6 +5,7 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 export default Route.extend(ApplicationRouteMixin,{
   intl: service(),
   moment: service(),
+  currentUser: service(),
 
   beforeModel() {
     const intl = this.intl;
@@ -12,5 +13,17 @@ export default Route.extend(ApplicationRouteMixin,{
     const locale = intl.get('locale');
     moment.setLocale(locale);
     window.document.querySelector('html').setAttribute('lang', locale);
+  },
+
+  /**
+   * Preload the user model and the users roles
+   * This makes the initial page rendering (especially the navigation) much smoother
+   */
+  async afterModel() {
+    const currentUser = this.currentUser;
+    const user = await currentUser.get('model');
+    if (user) {
+      await user.get('roles');
+    }
   },
 });
