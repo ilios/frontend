f873cef8e5aef44bd9de833ae2466af46b097e24
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-02-12 15:04:41 -0800

    re-aligns prototype extensions config with frontend.

--- config/environment.js
+++ config/environment.js
@@ -73,8 +73,10 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       },
       EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
+        String: true,
+        Array: true,
+        Function: false,
+        Date: false,
       }
     },
 
