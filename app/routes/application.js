39cc760ca8782d0995c5867d8f2a55501e829124
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-02-12 15:15:07 -0800

    set default locale.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Route.extend(ApplicationRouteMixin);
+export default Route.extend(ApplicationRouteMixin,{
+  intl: service(),
+  moment: service(),
+
+  beforeModel() {
+    const intl = this.intl;
+    const moment = this.moment;
+    const locale = intl.get('locale');
+    moment.setLocale(locale);
+    window.document.querySelector('html').setAttribute('lang', locale);
+  },
+});
