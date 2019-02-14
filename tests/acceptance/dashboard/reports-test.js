664359c977c9586c0b5c3d8fc4dd76a3eb70a2fe
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-02-13 16:30:57 -0800

    Fixup page imports and tests
    New version of common doesn't ship with this page object component
    anymore, it's in frontend. The new version here uses a new collection
    API so the test needed to get cleaned up to match.

--- tests/acceptance/dashboard/reports-test.js
+++ tests/acceptance/dashboard/reports-test.js
@@ -4,7 +4,7 @@ import {
   test
 } from 'qunit';
 import setupAuthentication from 'ilios/tests/helpers/setup-authentication';
-import page from 'ilios-common/page-objects/dashboard';
+import page from 'ilios/tests/pages/dashboard';
 
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
@@ -66,53 +66,53 @@ module('Acceptance | Dashboard Reports', function(hooks) {
 
   test('shows reports', async function(assert) {
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
-    assert.equal(page.myReports.reports(0).title, 'All Sessions for term 0 in school 0');
-    assert.equal(page.myReports.reports(1).title, 'my report 0');
+    assert.equal(page.myReports.reports.length, 2);
+    assert.equal(page.myReports.reports[0].title, 'All Sessions for term 0 in school 0');
+    assert.equal(page.myReports.reports[1].title, 'my report 0');
   });
 
   test('first report works', async function(assert) {
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
-    await page.myReports.reports(1).select();
+    assert.equal(page.myReports.reports.length, 2);
+    await page.myReports.reports[1].select();
     assert.equal(page.myReports.selectedReport.title, 'my report 0');
-    assert.equal(page.myReports.selectedReport.results().count, 1);
-    assert.equal(page.myReports.selectedReport.results(0).text, '2015 - 2016 course 0 session 0');
+    assert.equal(page.myReports.selectedReport.results.length, 1);
+    assert.equal(page.myReports.selectedReport.results[0].text, '2015 - 2016 course 0 session 0');
   });
 
   test('no year filter on reports with a course prepositional object', async function(assert) {
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
-    await page.myReports.reports(1).select();
+    assert.equal(page.myReports.reports.length, 2);
+    await page.myReports.reports[1].select();
     assert.equal(page.myReports.selectedReport.title, 'my report 0');
     assert.notOk(page.myReports.selectedReport.yearsFilterExists);
   });
 
   test('second report works', async function (assert) {
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
-    await page.myReports.reports(0).select();
+    assert.equal(page.myReports.reports.length, 2);
+    await page.myReports.reports[0].select();
     assert.equal(page.myReports.selectedReport.title, 'All Sessions for term 0 in school 0');
-    assert.equal(page.myReports.selectedReport.results().count, 2);
-    assert.equal(page.myReports.selectedReport.results(0).text, '2015 - 2016 course 0 session 0');
-    assert.equal(page.myReports.selectedReport.results(1).text, '2016 - 2017 course 1 session 1');
+    assert.equal(page.myReports.selectedReport.results.length, 2);
+    assert.equal(page.myReports.selectedReport.results[0].text, '2015 - 2016 course 0 session 0');
+    assert.equal(page.myReports.selectedReport.results[1].text, '2016 - 2017 course 1 session 1');
   });
 
   test('year filter works', async function (assert) {
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
-    await page.myReports.reports(0).select();
+    assert.equal(page.myReports.reports.length, 2);
+    await page.myReports.reports[0].select();
     assert.equal(page.myReports.selectedReport.title, 'All Sessions for term 0 in school 0');
     assert.ok(page.myReports.selectedReport.yearsFilterExists);
-    assert.equal(page.myReports.selectedReport.results().count, 2);
+    assert.equal(page.myReports.selectedReport.results.length, 2);
     await page.myReports.selectedReport.chooseYear('2016');
-    assert.equal(page.myReports.selectedReport.results().count, 1);
-    assert.equal(page.myReports.selectedReport.results(0).text, '2016 - 2017 course 1 session 1');
+    assert.equal(page.myReports.selectedReport.results.length, 1);
+    assert.equal(page.myReports.selectedReport.results[0].text, '2016 - 2017 course 1 session 1');
   });
 
   test('create new report', async function (assert) {
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
+    assert.equal(page.myReports.reports.length, 2);
     await page.myReports.addNewReport();
     await page.myReports.newReport.setTitle('New Report');
     await page.myReports.newReport.chooseSchool('1');
@@ -121,17 +121,17 @@ module('Acceptance | Dashboard Reports', function(hooks) {
     await page.myReports.newReport.chooseObject('1');
     await page.myReports.newReport.save();
 
-    assert.equal(page.myReports.reports().count, 3);
-    await page.myReports.reports(2).select();
+    assert.equal(page.myReports.reports.length, 3);
+    await page.myReports.reports[2].select();
 
     assert.equal(page.myReports.selectedReport.title, 'New Report');
-    assert.equal(page.myReports.selectedReport.results().count, 1);
-    assert.equal(page.myReports.selectedReport.results(0).text, '2015 - 2016 course 0 session 0');
+    assert.equal(page.myReports.selectedReport.results.length, 1);
+    assert.equal(page.myReports.selectedReport.results[0].text, '2015 - 2016 course 0 session 0');
   });
 
   test('filter courses by year in new report form', async function (assert) {
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
+    assert.equal(page.myReports.reports.length, 2);
     await page.myReports.addNewReport();
 
     await page.myReports.newReport.chooseSchool('1');
@@ -143,17 +143,17 @@ module('Acceptance | Dashboard Reports', function(hooks) {
     await page.myReports.newReport.chooseObject('2');
     await page.myReports.newReport.save();
 
-    assert.equal(page.myReports.reports().count, 3);
-    await page.myReports.reports(1).select();
+    assert.equal(page.myReports.reports.length, 3);
+    await page.myReports.reports[1].select();
 
     assert.equal(page.myReports.selectedReport.title, 'All Sessions for course 1 in school 0');
-    assert.equal(page.myReports.selectedReport.results().count, 1);
-    assert.equal(page.myReports.selectedReport.results(0).text, '2016 - 2017 course 1 session 1');
+    assert.equal(page.myReports.selectedReport.results.length, 1);
+    assert.equal(page.myReports.selectedReport.results[0].text, '2016 - 2017 course 1 session 1');
   });
 
   test('filter session by year in new report form', async function (assert) {
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
+    assert.equal(page.myReports.reports.length, 2);
     await page.myReports.addNewReport();
 
     await page.myReports.newReport.chooseSchool('1');
@@ -165,12 +165,12 @@ module('Acceptance | Dashboard Reports', function(hooks) {
     await page.myReports.newReport.chooseObject('2');
     await page.myReports.newReport.save();
 
-    assert.equal(page.myReports.reports().count, 3);
-    await page.myReports.reports(1).select();
+    assert.equal(page.myReports.reports.length, 3);
+    await page.myReports.reports[1].select();
 
     assert.equal(page.myReports.selectedReport.title, 'All Terms for session 1 in school 0');
-    assert.equal(page.myReports.selectedReport.results().count, 1);
-    assert.equal(page.myReports.selectedReport.results(0).text, 'Vocabulary 1 > term 0');
+    assert.equal(page.myReports.selectedReport.results.length, 1);
+    assert.equal(page.myReports.selectedReport.results[0].text, 'Vocabulary 1 > term 0');
   });
 
   test('get all courses associated with mesh term #3419', async function (assert) {
@@ -179,7 +179,7 @@ module('Acceptance | Dashboard Reports', function(hooks) {
       courseIds: [1, 2]
     });
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
+    assert.equal(page.myReports.reports.length, 2);
     await page.myReports.addNewReport();
 
     await page.myReports.newReport.chooseSchool('1');
@@ -187,18 +187,18 @@ module('Acceptance | Dashboard Reports', function(hooks) {
     await page.myReports.newReport.chooseObjectType('mesh term');
     await page.myReports.newReport.fillMeshSearch('0');
     await page.myReports.newReport.runMeshSearch();
-    assert.equal(page.myReports.newReport.meshSearchResults().count, 1);
-    assert.equal(page.myReports.newReport.meshSearchResults(0).text, 'descriptor 0 D1234');
-    await page.myReports.newReport.meshSearchResults(0).pick();
+    assert.equal(page.myReports.newReport.meshSearchResults.length, 1);
+    assert.equal(page.myReports.newReport.meshSearchResults[0].text, 'descriptor 0 D1234');
+    await page.myReports.newReport.meshSearchResults[0].pick();
     await page.myReports.newReport.save();
 
-    assert.equal(page.myReports.reports().count, 3);
-    await page.myReports.reports(1).select();
+    assert.equal(page.myReports.reports.length, 3);
+    await page.myReports.reports[1].select();
 
     assert.equal(page.myReports.selectedReport.title, 'All Courses for descriptor 0 in school 0');
-    assert.equal(page.myReports.selectedReport.results().count, 2);
-    assert.equal(page.myReports.selectedReport.results(0).text, '2015 - 2016 course 0 (Theoretical Phys Ed)');
-    assert.equal(page.myReports.selectedReport.results(1).text, '2016 - 2017 course 1');
+    assert.equal(page.myReports.selectedReport.results.length, 2);
+    assert.equal(page.myReports.selectedReport.results[0].text, '2015 - 2016 course 0 (Theoretical Phys Ed)');
+    assert.equal(page.myReports.selectedReport.results[1].text, '2016 - 2017 course 1');
   });
 
   test('Prepositional object resets when a new type is selected', async function (assert) {
@@ -207,7 +207,7 @@ module('Acceptance | Dashboard Reports', function(hooks) {
       schoolId: 1
     });
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
+    assert.equal(page.myReports.reports.length, 2);
     await page.myReports.addNewReport();
 
     await page.myReports.newReport.chooseSchool('1');
@@ -217,8 +217,8 @@ module('Acceptance | Dashboard Reports', function(hooks) {
     await page.myReports.newReport.chooseObjectType('course');
     await page.myReports.newReport.save();
 
-    assert.equal(page.myReports.reports().count, 3);
-    await page.myReports.reports(1).select();
+    assert.equal(page.myReports.reports.length, 3);
+    await page.myReports.reports[1].select();
     assert.equal(page.myReports.selectedReport.title, 'All Terms for course 0 in school 0');
   });
 
@@ -228,7 +228,7 @@ module('Acceptance | Dashboard Reports', function(hooks) {
       schoolId: 1
     });
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
+    assert.equal(page.myReports.reports.length, 2);
     await page.myReports.addNewReport();
 
     await page.myReports.newReport.chooseSchool('1');
@@ -237,25 +237,25 @@ module('Acceptance | Dashboard Reports', function(hooks) {
     await page.myReports.newReport.chooseAcademicYear('2016');
     await page.myReports.newReport.save();
 
-    assert.equal(page.myReports.reports().count, 3);
-    await page.myReports.reports(1).select();
+    assert.equal(page.myReports.reports.length, 3);
+    await page.myReports.reports[1].select();
     assert.equal(page.myReports.selectedReport.title, 'All Terms for course 1 in school 0');
   });
 
   test('course external Id in report', async function (assert) {
     await page.visit();
-    assert.equal(page.myReports.reports().count, 2);
+    assert.equal(page.myReports.reports.length, 2);
     await page.myReports.addNewReport();
     await page.myReports.newReport.chooseSchool('All Schools');
     await page.myReports.newReport.chooseSubject('course');
     await page.myReports.newReport.save();
 
-    assert.equal(page.myReports.reports().count, 3);
-    await page.myReports.reports(1).select();
+    assert.equal(page.myReports.reports.length, 3);
+    await page.myReports.reports[1].select();
 
     assert.equal(page.myReports.selectedReport.title, 'All Courses in All Schools');
-    assert.equal(page.myReports.selectedReport.results().count, 2);
-    assert.equal(page.myReports.selectedReport.results(0).text, '2015 - 2016 course 0 (Theoretical Phys Ed)');
-    assert.equal(page.myReports.selectedReport.results(1).text, '2016 - 2017 course 1');
+    assert.equal(page.myReports.selectedReport.results.length, 2);
+    assert.equal(page.myReports.selectedReport.results[0].text, '2015 - 2016 course 0 (Theoretical Phys Ed)');
+    assert.equal(page.myReports.selectedReport.results[1].text, '2016 - 2017 course 1');
   });
 });
