180eca98504142a4c4da11ae886a8b9d948e68da
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-02-17 01:41:13 -0800

    Improve our error reporting
    The default of VERSION-tag isn't working for our sentry github
    integration so instead this just sends the tag name.  I've also
    configured travis to upload source maps and commit data to sentry to
    better attach it to the release itself.  Since our production sourcemaps
    are often behind an SSO firewall this ensures the sourcemap is attached
    to the error.

--- app/services/raven.js
+++ app/services/raven.js
@@ -1,5 +1,7 @@
 import RavenService from 'ember-cli-sentry/services/raven';
 import { inject as service } from '@ember/service';
+import config from '../config/environment';
+import { versionRegExp } from 'ember-cli-app-version/utils/regexp';
 
 export default RavenService.extend({
   iliosConfig: service(),
@@ -8,7 +10,9 @@ export default RavenService.extend({
    * Override default service to add configuration check
    * before attempting to capture anything.
    */
-  ignoreError(){
+  ignoreError() {
     return !this.iliosConfig.errorCaptureEnabled;
   },
+
+  release: config.APP.version.match(versionRegExp),
 });
