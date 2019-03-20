24afd962dd5ee405501ac8eae0ad45ff034bbe6f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-03-20 12:09:47 -0700

    rm runloop from serializer tests.

--- tests/unit/serializers/curriculum-inventory-report-test.js
+++ tests/unit/serializers/curriculum-inventory-report-test.js
@@ -1,4 +1,3 @@
-import { run } from '@ember/runloop';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
@@ -6,31 +5,27 @@ module('Unit | Serializer | curriculum inventory report', function(hooks) {
   setupTest(hooks);
 
   test('it serializes records', function(assert) {
-    let record = run(() => this.owner.lookup('service:store').createRecord('curriculum-inventory-report'));
+    let record = this.owner.lookup('service:store').createRecord('curriculum-inventory-report');
     let serializedRecord = record.serialize();
     assert.ok(serializedRecord);
   });
 
   test('it removes all non postable fields', function(assert) {
-    let record = run(() => this.owner.lookup('service:store').createRecord('curriculum-inventory-report'));
-    run(()=> {
-      let uri = "/abandon/all/hope/ye/who/enter/here";
-      record.set('absoluteFileUri', uri);
-      assert.equal(record.get('absoluteFileUri'), uri);
-      let serializedRecord = record.serialize();
-      assert.ok(!('absoluteFileUri' in serializedRecord));
-    });
+    let record = this.owner.lookup('service:store').createRecord('curriculum-inventory-report');
+    let uri = "/abandon/all/hope/ye/who/enter/here";
+    record.set('absoluteFileUri', uri);
+    assert.equal(record.get('absoluteFileUri'), uri);
+    let serializedRecord = record.serialize();
+    assert.ok(!('absoluteFileUri' in serializedRecord));
   });
 
   test('start and end date are formatted during serialization', function(assert) {
-    let record = run(() => this.owner.lookup('service:store').createRecord('curriculum-inventory-report'));
-    run(()=> {
-      record.set('startDate', new Date());
-      record.set('endDate', new Date());
-      let serializedRecord = record.serialize();
-      const pattern = /\d{4}-\d{2}-\d{2}/;
-      assert.ok(serializedRecord.startDate.match(pattern));
-      assert.ok(serializedRecord.endDate.match(pattern));
-    });
+    let record = this.owner.lookup('service:store').createRecord('curriculum-inventory-report');
+    record.set('startDate', new Date());
+    record.set('endDate', new Date());
+    let serializedRecord = record.serialize();
+    const pattern = /\d{4}-\d{2}-\d{2}/;
+    assert.ok(serializedRecord.startDate.match(pattern));
+    assert.ok(serializedRecord.endDate.match(pattern));
   });
 });
--- tests/unit/serializers/report-test.js
+++ tests/unit/serializers/report-test.js
@@ -1,16 +1,14 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-import { run } from '@ember/runloop';
-
 module('Unit | Serializer | report', function(hooks) {
   setupTest(hooks);
 
   test('it removes all non postable fields', function(assert) {
-    var record = run(() => this.owner.lookup('service:store').createRecord('offering'));
+    var record = this.owner.lookup('service:store').createRecord('offering');
 
     var serializedRecord = record.serialize();
-    
+
     assert.ok(!("createdAt" in serializedRecord));
   });
 });
