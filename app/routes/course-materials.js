28277d0f79a10b2770ddb1d61dcd3dbda611c383
Thomas Lee <djvoa12@gmail.com>
2019-03-25 15:55:31 -0400

    minor refactor

--- app/routes/course-materials.js
+++ app/routes/course-materials.js
@@ -2,5 +2,4 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import CourseMaterialsRouteMixin from 'ilios-common/mixins/course-materials-route';
 
-export default Route.extend(AuthenticatedRouteMixin, CourseMaterialsRouteMixin, {
-});
+export default Route.extend(AuthenticatedRouteMixin, CourseMaterialsRouteMixin);
