ab93fd2b6f58e86495a00711e19a10beee986bbd
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-03-27 12:57:48 -0700

    yank that obsolete publishing target attr.

--- app/components/programyear-header.js
+++ app/components/programyear-header.js
@@ -1,13 +1,9 @@
 /* eslint ember/order-in-components: 0 */
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-
-const { alias } = computed;
 
 export default Component.extend({
   programYear: null,
   classNames: ['programyear-header'],
-  publishTarget: alias('programYear'),
   canUpdate: false,
   actions: {
     async activate(programYear) {
