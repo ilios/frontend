95317f48224e6d0fc78cc040324c055f9c1409cd
Thomas Lee <djvoa12@gmail.com>
2019-04-05 12:42:08 -0500

    added courses column and updated control flow for trash icon

--- app/components/learnergroup-list.js
+++ app/components/learnergroup-list.js
@@ -1,33 +1,37 @@
-/* eslint ember/order-in-components: 0 */
 import Component from '@ember/component';
 
 export default Component.extend({
-  init(){
-    this._super(...arguments);
-    this.set('learnerGroupsForRemovalConfirmation', []);
-    this.set('learnerGroupsForCopy', []);
-  },
-  learnerGroups: null,
-  canDelete: false,
+  classNames: ['learnergroup-list'],
+
   canCreate: false,
-  learnerGroupsForRemovalConfirmation: null,
-  learnerGroupsForCopy: null,
+  canDelete: false,
+  learnerGroups: null,
+  copy() {},
+  remove() {},
+
+  learnerGroupsForCopy: Object.freeze([]),
+  learnerGroupsForRemovalConfirmation: Object.freeze([]),
+
   actions: {
     cancelRemove(learnerGroup) {
       this.learnerGroupsForRemovalConfirmation.removeObject(learnerGroup);
     },
+
     confirmRemove(learnerGroup) {
       const canDelete = this.canDelete;
       if (canDelete) {
         this.learnerGroupsForRemovalConfirmation.pushObject(learnerGroup);
       }
     },
+
     cancelCopy(learnerGroup) {
       this.learnerGroupsForCopy.removeObject(learnerGroup);
     },
+
     startCopy(learnerGroup) {
       this.learnerGroupsForCopy.pushObject(learnerGroup);
     },
+
     async copy(withLearners, learnerGroup) {
       const copy = this.copy;
       await copy(withLearners, learnerGroup);
