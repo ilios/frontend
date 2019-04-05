c0807ce2598fa85f4a36ca205f416e173693563a
Thomas Lee <djvoa12@gmail.com>
2019-04-05 12:58:31 -0500

    Revert "added courses column and updated control flow for trash icon"
    This reverts commit 66fd0068041c84a6a665d5f9a0403d148788a229.

--- app/components/learnergroup-list.js
+++ app/components/learnergroup-list.js
@@ -1,37 +1,33 @@
+/* eslint ember/order-in-components: 0 */
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['learnergroup-list'],
-
-  canCreate: false,
-  canDelete: false,
+  init(){
+    this._super(...arguments);
+    this.set('learnerGroupsForRemovalConfirmation', []);
+    this.set('learnerGroupsForCopy', []);
+  },
   learnerGroups: null,
-  copy() {},
-  remove() {},
-
-  learnerGroupsForCopy: Object.freeze([]),
-  learnerGroupsForRemovalConfirmation: Object.freeze([]),
-
+  canDelete: false,
+  canCreate: false,
+  learnerGroupsForRemovalConfirmation: null,
+  learnerGroupsForCopy: null,
   actions: {
     cancelRemove(learnerGroup) {
       this.learnerGroupsForRemovalConfirmation.removeObject(learnerGroup);
     },
-
     confirmRemove(learnerGroup) {
       const canDelete = this.canDelete;
       if (canDelete) {
         this.learnerGroupsForRemovalConfirmation.pushObject(learnerGroup);
       }
     },
-
     cancelCopy(learnerGroup) {
       this.learnerGroupsForCopy.removeObject(learnerGroup);
     },
-
     startCopy(learnerGroup) {
       this.learnerGroupsForCopy.pushObject(learnerGroup);
     },
-
     async copy(withLearners, learnerGroup) {
       const copy = this.copy;
       await copy(withLearners, learnerGroup);
