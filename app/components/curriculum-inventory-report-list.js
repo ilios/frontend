d8eeca322c5bc397c30f69eb0a4782a358e0d134
Thomas Lee <djvoa12@gmail.com>
2019-04-08 17:05:23 -0500

    refactors and adjustments

--- app/components/curriculum-inventory-report-list.js
+++ app/components/curriculum-inventory-report-list.js
@@ -1,84 +1,94 @@
-/* eslint ember/order-in-components: 0 */
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
-import ObjectProxy from '@ember/object/proxy';
 import Component from '@ember/component';
-const { alias, not } = computed;
+import ObjectProxy from '@ember/object/proxy';
+import { computed } from '@ember/object';
+import { alias, not } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 const ReportProxy = ObjectProxy.extend({
   content: null,
   currentUser: null,
+  intl: null,
+  isScheduled: false,
   permissionChecker: null,
   showRemoveConfirmation: false,
-  intl: null,
-  isPublished: alias('isFinalized'),
-  isScheduled: false,
-  isNotPublished: not('isPublished'),
 
-  userCanDelete: computed('content', 'content.isFinalized', 'currentUser.model', async function(){
-    const permissionChecker = this.permissionChecker;
-    const report = this.content;
-    if (report.get('isFinalized')) {
-      return false;
+  isNotPublished: not('isPublished'),
+  isPublished: alias('isFinalized'),
+
+  userCanDelete: computed(
+    'content',
+    'content.{isFinalized,model}', async function(){
+      const permissionChecker = this.permissionChecker;
+      const report = this.content;
+      if (report.get('isFinalized')) {
+        return false;
+      }
+      return permissionChecker.canDeleteCurriculumInventoryReport(report);
     }
-    return permissionChecker.canDeleteCurriculumInventoryReport(report);
-  })
+  )
 });
 
 export default Component.extend({
   currentUser: service(),
   intl: service(),
   permissionChecker: service(),
+
   program: null,
+  sortBy: 'title',
+  edit() {},
+  remove() {},
+  setSortBy() {},
 
   /**
    * @property proxiedReports
    * @type {Ember.computed}
    * @public
    */
-  proxiedReports: computed('program.curriculumInventoryReports.[]', async function () {
-    const currentUser = this.currentUser;
-    const intl = this.intl;
-    const permissionChecker = this.permissionChecker;
-    const program = this.program;
-
-    const reports = await program.get('curriculumInventoryReports');
-    return reports.map(report => {
-      return ReportProxy.create({
-        content: report,
-        intl,
-        currentUser,
-        permissionChecker
+  proxiedReports: computed(
+    'program.curriculumInventoryReports.[]', async function () {
+      const currentUser = this.currentUser;
+      const intl = this.intl;
+      const permissionChecker = this.permissionChecker;
+      const program = this.program;
+      const reports = await program.get('curriculumInventoryReports');
+      return reports.map(report => {
+        return ReportProxy.create({
+          content: report,
+          intl,
+          currentUser,
+          permissionChecker
+        });
       });
-    });
-  }),
+    }
+  ),
 
-  sortBy: 'title',
-
-  sortedAscending: computed('sortBy', function(){
-    const sortBy = this.sortBy;
-    return sortBy.search(/desc/) === -1;
+  sortedAscending: computed('sortBy', function() {
+    return this.sortBy.search(/desc/) === -1;
   }),
 
   actions: {
     edit(proxy) {
       this.edit(proxy.get('content'));
     },
+
     remove(proxy) {
       this.remove(proxy.get('content'));
     },
+
     cancelRemove(proxy) {
       proxy.set('showRemoveConfirmation', false);
     },
+
     confirmRemove(proxy) {
       proxy.set('showRemoveConfirmation', true);
     },
+
     sortBy(what){
       const sortBy = this.sortBy;
       if(sortBy === what){
         what += ':desc';
       }
       this.setSortBy(what);
-    },
+    }
   }
 });
