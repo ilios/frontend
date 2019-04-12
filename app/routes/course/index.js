7e23ab74eed06fc5cd4424393b90ad23215b3601
Thomas Lee <djvoa12@gmail.com>
2019-04-12 11:01:48 -0500

    added code for allowing/resetting preserved scroll

--- app/routes/course/index.js
+++ app/routes/course/index.js
@@ -1,6 +1,22 @@
-/* eslint ember/order-in-routes: 0 */
 import Route from '@ember/routing/route';
+import { get } from '@ember/object';
+import { inject as service } from '@ember/service';
 import IndexRouteMixin from 'ilios-common/mixins/course/index-route';
 
 export default Route.extend(IndexRouteMixin, {
+  preserveScroll: service(),
+
+  beforeModel(transition) {
+    const isFromSessionIndex = get(transition, 'from.name') === 'session.index';
+    this.preserveScroll.set('shouldScrollDown', isFromSessionIndex);
+  },
+
+  actions: {
+    willTransition(transition) {
+      this.preserveScroll.set('isListenerOn', false);
+      if (transition.targetName !== 'session.index') {
+        this.preserveScroll.set('yPos', null);
+      }
+    }
+  }
 });
