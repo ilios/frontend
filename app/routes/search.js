4238645a009c04d455da3774ab8f76f9ca041431
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-04-22 10:37:12 -0700

    Protect search route with feature flag

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  features: service(),
+  beforeModel() {
+    if (!this.features.get('globalSearch')) {
+      this.transitionTo('dashboard');
+    }
+  },
 });
