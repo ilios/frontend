9bd5e3305aaebe6639c2bc51542c2d05224e91e2
Thomas Lee <djvoa12@gmail.com>
2019-05-02 16:44:03 -0500

    course missing items unlink link work

--- addon/components/course-publicationcheck.js
+++ addon/components/course-publicationcheck.js
@@ -1,8 +1,27 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { isEmpty } from '@ember/utils';
 import layout from '../templates/components/course-publicationcheck';
 
 export default Component.extend({
+  router: service(),
+
   layout,
+
   classNames: ['course-publicationcheck'],
-  course: null
+
+  course: null,
+
+  showUnlinkIcon: computed('course.objectives.[]', function() {
+    const objectives = this.course.objectives;
+    return objectives.any((objective) => isEmpty(objective.parents));
+  }),
+
+  actions: {
+    transitionToCourse() {
+      const queryParams = { courseObjectiveDetails: true, details: true };
+      this.router.transitionTo('course', this.course, { queryParams });
+    }
+  }
 });
