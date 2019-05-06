ea64edacc0155cc4312e72b8fce94f66476b8106
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-05-06 15:35:12 -0700

    pull pre-work ILMs out of activities list into own display.

--- addon/components/dashboard-agenda.js
+++ addon/components/dashboard-agenda.js
@@ -8,6 +8,7 @@ export default Component.extend({
   userEvents: service(),
 
   layout,
+  areEventsSelectable: true,
   /**
    * Days in advance of the current date.
    * @property daysInAdvance
@@ -19,11 +20,27 @@ export default Component.extend({
   classNames: ['dashboard-agenda'],
 
   sixDaysAgo: moment().hour(0).minute(0).subtract(6, 'days'),
-  weeksEvents: computed('daysInAdvance', function() {
+  weeksEvents: computed('daysInAdvance', async function() {
     const daysInAdvance = this.get('daysInAdvance');
     const from = moment().hour(0).minute(0).unix();
     const to = moment().hour(23).minute(59).add(daysInAdvance, 'days').unix();
 
-    return this.get('userEvents').getEvents(from, to);
+    return await this.get('userEvents').getEvents(from, to);
+  }),
+
+  ilmPreWorkEvents: computed('weeksEvents.[]', async function () {
+    const events = await this.get('weeksEvents');
+    const preWork =  events.reduce((arr, eventObject) => {
+      return arr.pushObjects(eventObject.prerequisites);
+    }, []);
+
+    return preWork.filter(ev => ev.ilmSession);
+  }),
+
+  nonIlmPreWorkEvents: computed('weeksEvents.[]', async function () {
+    const events = await this.get('weeksEvents');
+    return events.filter(ev => {
+      return ev.postrequisites.length === 0 || !ev.ilmSession;
+    });
   }),
 });
