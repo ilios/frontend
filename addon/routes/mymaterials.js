a7d1c349e4ceac4a69df302f65d931296ba35c65
Thomas Lee <djvoa12@gmail.com>
2019-05-08 13:43:01 -0500

    PR review changes

--- addon/routes/mymaterials.js
+++ addon/routes/mymaterials.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import { task } from 'ember-concurrency';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   commonAjax: service(),
@@ -12,17 +11,13 @@ export default Route.extend(AuthenticatedRouteMixin, {
   host: reads('iliosConfig.apiHost'),
   namespace: reads('iliosConfig.apiNameSpace'),
 
-  model() {
-    return this.fetchModelData.perform();
-  },
-
-  fetchModelData: task(function* () {
+  async model() {
     const commonAjax = this.commonAjax;
     const host = this.host;
     const namespace = this.namespace;
-    const user = yield this.currentUser.model;
+    const user = await this.currentUser.model;
     const url = `${host}/${namespace}/usermaterials/${user.id}`;
-    const data = yield commonAjax.request(url);
+    const data = await commonAjax.request(url);
     return data.userMaterials;
-  }).cancelOn('deactivate')
+  }
 });
