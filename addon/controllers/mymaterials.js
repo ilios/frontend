231daecc76eaff8145587f08e033c27d9b1f347c
Thomas Lee <djvoa12@gmail.com>
2019-05-17 12:17:25 -0500

    better loading state display for mymaterials route

--- addon/controllers/mymaterials.js
+++ addon/controllers/mymaterials.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
+import { reads } from '@ember/object/computed';
 
 export default Controller.extend({
   queryParams: ['course', 'filter', 'sortBy'],
 
   course: '',
   filter: '',
-  sortBy: 'firstOfferingDate:desc'
+  sortBy: 'firstOfferingDate:desc',
+
+  materials: reads('model.materials')
 });
