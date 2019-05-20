3ba2c83e68b1b4dde793136da15b44c770256e7e
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-05-20 11:43:35 -0700

    rm course route/template/controller.

--- app/routes/course/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint ember/order-in-routes: 0 */
-import Route from '@ember/routing/route';
-import IndexRouteMixin from 'ilios-common/mixins/course/index-route';
-
-export default Route.extend(IndexRouteMixin, {
-});
