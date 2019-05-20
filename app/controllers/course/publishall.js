351ed0625aa62c502938a4dea7d072634d051732
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-05-20 12:52:08 -0700

    rm obsolete course rollover/publish controllers.

--- app/controllers/course/publishall.js
+++
@@ -1,5 +0,0 @@
-import Controller from '@ember/controller';
-import PublishAllControllerMixin from 'ilios-common/mixins/course/publish-all-controller';
-
-export default Controller.extend(PublishAllControllerMixin, {
-});
