582a1b4575375c966b91ef647d18d089dc04be0b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-05-20 18:16:51 -0700

    rm obsolete routes/controllers/templates.

--- app/controllers/course-materials.js
+++
@@ -1,4 +0,0 @@
-import Controller from '@ember/controller';
-import MaterialsController from 'ilios-common/mixins/course-materials-controller';
-
-export default Controller.extend(MaterialsController);
--- app/controllers/weeklyevents.js
+++
@@ -1,4 +0,0 @@
-import Controller from '@ember/controller';
-import WeeklyeventsController from 'ilios-common/mixins/weeklyevents-controller';
-
-export default Controller.extend(WeeklyeventsController);
