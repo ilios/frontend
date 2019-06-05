fc857ebb3392aea14a87566cd8d686e31a21cfbc
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-06-05 10:50:54 -0700

    ember 3.10.1 upgrade.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'ember'
+  ],
   extends: [
     '@ilios/eslint-config-ember-addon'
   ],
