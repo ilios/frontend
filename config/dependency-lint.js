e10a3a02b815f5a4c132b986fa0cc0ac6c4a5642
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-06-06 11:35:04 -0700

    expand version range of ember-concurrency exclusion.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -3,6 +3,6 @@
 
 module.exports = {
   allowedVersions: {
-    'ember-concurrency': '^0.9.0 || ^0.10.0', //conflict with ember-cli-new-version and ilios-common
+    'ember-concurrency': '^0.9.0 || ^0.10.0 || ^1.0.0', //conflict with ember-cli-new-version and ilios-common
   }
 };
