672bc325752be56e22316f2339aeaa56cd32270c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-06-10 11:42:43 -0700

    yank overrides and new rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,47 +11,6 @@ module.exports = {
     'ember/no-capital-letters-in-routes': 0,
     'ember/no-unnecessary-route-path-option': 0,
     'ember/routes-segments-snake-case': 0,
-    'sort-imports': 0,
-    "node/no-extraneous-require": ["error", {
-      "allowModules": [
-        "broccoli-file-creator",
-        "broccoli-merge-trees",
-        "browserslist",
-        "caniuse-db",
-        "dotenv",
-        "postcss-scss"
-      ]
-    }]
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js',
-        'server/**/*.js'
-      ],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2018
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      })
-    }
-  ]
+    'sort-imports': 0
+  }
 };
