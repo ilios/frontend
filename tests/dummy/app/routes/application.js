68bd647f5b1815206c53314c763644380aaf314f
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-06-20 11:26:15 -0700

    Set the locale in dummy app
    For internationalization in dates and translations I've set the default
    locale to english.

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,5 +1,12 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(ApplicationRouteMixin, {
+  intl: service(),
+  moment: service(),
+  beforeModel() {
+    this.intl.setLocale('en-us');
+    this.moment.setLocale('en');
+  }
 });
