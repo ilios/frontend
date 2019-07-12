800ca009865be333c24955313ece85af8ebc5834
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-07-12 07:24:53 -0700

    whitelist dependency mismatches for ember-popper and ember-maybe-in-element.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -4,5 +4,7 @@
 module.exports = {
   allowedVersions: {
     'ember-concurrency': '^0.9.0 || ^0.10.0 || ^1.0.0', //conflict with ember-cli-new-version and ilios-common
+    'ember-popper': '^0.9.2 || ^0.10.1', // conflict with ember-popper version in ilios-common
+    'ember-maybe-in-element': '^0.1.3 || ^0.2.0' //sub-dependency of ember-popper, see comment on ember-popper
   }
 };
