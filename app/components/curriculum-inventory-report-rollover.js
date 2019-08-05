efbc94a0f1b64ed304ec8faa51d1793f6878d5a8
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-08-05 15:17:07 -0700

    adds input options for start/end-date to ci rollover form.

--- app/components/curriculum-inventory-report-rollover.js
+++ app/components/curriculum-inventory-report-rollover.js
@@ -14,6 +14,18 @@ const Validations = buildValidations({
       min: 3,
       max: 200
     })
+  ],
+  startDate: [
+    validator('date', {
+      dependentKeys: ['model.endDate'],
+      onOrBefore: reads('model.endDate'),
+    }),
+  ],
+  endDate: [
+    validator('date', {
+      dependentKeys: ['model.startDate'],
+      onOrAfter: reads('model.startDate'),
+    })
   ]
 });
 
@@ -31,6 +43,8 @@ export default Component.extend(ValidationErrorDisplay, Validations, {
   report: null,
   selectedYear: null,
   years: null,
+  startDate: null,
+  endDate: null,
 
   host: reads('iliosConfig.apiHost'),
   namespace: reads('iliosConfig.apiNameSpace'),
@@ -40,6 +54,8 @@ export default Component.extend(ValidationErrorDisplay, Validations, {
     const report = this.report;
     const thisYear = parseInt(moment().format('YYYY'), 10);
     const reportYear = parseInt(report.get('year'), 10);
+    const reportStartDate = report.get('startDate');
+    const reportEndDate = report.get('endDate');
     const startYear = Math.min(thisYear, reportYear);
     const endYear = Math.max(thisYear, reportYear) + 5;
     let years = [];
@@ -55,7 +71,20 @@ export default Component.extend(ValidationErrorDisplay, Validations, {
     if (! selectedYear) {
       selectedYear = years.findBy('id', reportYear + 1);
     }
+
+    const newReportYear = selectedYear.id;
+    const yearDiff = moment(reportEndDate).get('year') - moment(reportStartDate).get('year');
+    const startDate = moment(reportStartDate).set('year', newReportYear).toDate();
+    const endDate = moment(reportEndDate).set('year', newReportYear + yearDiff).toDate();
+
+    console.log(reportStartDate);
+    console.log(reportEndDate);
+    console.log(startDate);
+    console.log(endDate);
+
     this.set('name', report.get('name'));
+    this.set('startDate', startDate);
+    this.set('endDate', endDate);
     this.set('description', report.get('description'));
     this.set('selectedYear', selectedYear);
     this.set('years', years);
@@ -64,7 +93,7 @@ export default Component.extend(ValidationErrorDisplay, Validations, {
   actions: {
     changeName(newName){
       this.set('name', newName);
-    }
+    },
   },
 
   save: task(function* () {
@@ -82,10 +111,14 @@ export default Component.extend(ValidationErrorDisplay, Validations, {
     const year = this.selectedYear.get('id');
     const description = this.description;
     const name = this.name;
+    const startDate = this.startDate;
+    const endDate = this.endDate;
     let data = {
       name,
       description,
       year,
+      startDate,
+      endDate
     };
     const host = this.host ? this.host : '';
     const namespace = this.namespace;
