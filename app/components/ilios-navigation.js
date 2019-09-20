e0112a8341d94eb56b21475778bc46ef705fb6b5
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-09-19 23:33:38 -0700

    Label navigation roles
    Each navigation landmark on the page needs it's own label so it can be
    read by assistive technologies.

--- app/components/ilios-navigation.js
+++ app/components/ilios-navigation.js
@@ -1,10 +1,17 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   intl: service(),
   currentUser: service(),
   tagName: 'nav',
   classNameBindings: [':ilios-navigation', 'expanded'],
-  expanded: false
+  expanded: false,
+  attributeBindings: [
+    'ariaLabel:aria-label',
+  ],
+  ariaLabel: computed('intl.locale', function () {
+    return this.intl.t('general.primary');
+  }),
 });
