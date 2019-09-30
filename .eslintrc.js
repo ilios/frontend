a6bf19c4a61c41d18ad8f85d352a5b2dc3a380cf
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-09-30 08:59:30 -0700

    whitelist required modules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,7 +37,9 @@ module.exports = {
       plugins: ['node'],
       rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
         // add your custom rules and overrides for node files here
-
+        "node/no-extraneous-require": ["error", {
+            "allowModules": ["ember-auto-import", "postcss-scss"]
+        }],
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
         'node/no-unpublished-require': 'off'
