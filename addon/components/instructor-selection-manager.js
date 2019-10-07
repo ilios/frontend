1c0f8fda852829658868659fec04095359f76655
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-10-07 15:59:32 -0700

    display instructor group members.

--- addon/components/instructor-selection-manager.js
+++ addon/components/instructor-selection-manager.js
@@ -1,5 +1,7 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { all } from 'rsvp';
 import layout from '../templates/components/instructor-selection-manager';
 
 export default Component.extend({
@@ -11,6 +13,14 @@ export default Component.extend({
   tagName: 'section',
   instructorGroups: null,
   'data-test-instructor-selection-manager': true,
+
+  instructorGroupMembers: computed('instructorGroups.[]', async function() {
+    const groupsOfInstructors = await all(this.instructorGroups.mapBy('users'));
+    return groupsOfInstructors.reduce((array, set) => {
+      array.pushObjects(set.toArray());
+      return array;
+    }, []).uniq().sortBy('fullName');
+  }),
 
   actions: {
     addInstructor(user){
