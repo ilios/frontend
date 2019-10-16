0be2ff710ff8d442da88f3ef051d3812219c0e71
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-10-16 11:47:33 -0700

    test coverage for selectedSchool CP.

--- tests/unit/controllers/curriculum-inventory-reports-test.js
+++ tests/unit/controllers/curriculum-inventory-reports-test.js
@@ -1,12 +1,41 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { resolve } from 'rsvp';
+import Service from '@ember/service';
+import EmberObject from '@ember/object';
 
 module('Unit | Controller | curriculum-inventory-reports', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('selectedSchool', async function(assert) {
+    assert.expect(4);
+
+    const schoolId =  10;
+    const currentUsersSchoolMock = EmberObject.create();
+    const schoolMock = EmberObject.create();
+    const userMock = EmberObject.create({
+      school: resolve(currentUsersSchoolMock),
+    });
+    const currentUserMock = Service.extend({
+      model: resolve(userMock),
+    });
+    const modelMock = EmberObject.create({
+      findBy: function(attr, id) {
+        assert.equal(attr, 'id');
+        assert.equal(id, schoolId);
+        return schoolMock;
+      },
+    });
+
+    this.owner.register('service:currentUser', currentUserMock);
     let controller = this.owner.lookup('controller:curriculum-inventory-reports');
-    assert.ok(controller);
+
+    controller.set('model', modelMock);
+    let selectedSchool = await controller.get('selectedSchool');
+    assert.equal(selectedSchool, currentUsersSchoolMock);
+
+    controller.set('schoolId', schoolId);
+    selectedSchool = await controller.get('selectedSchool');
+    assert.equal(selectedSchool, schoolMock);
   });
 });
