810ca4626d3b621cd840a28b2b42c2936a40eea2
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-11-12 13:49:43 -0800

    Use fetch to make ember-data requests
    Once we turn off jquery this will be the default, but for now I've
    forced it on.
    
    Changes for fetch based networking
    
    With jQuery removed ember-data reverts to using fetch to get data, but
    the simple auth mixin doesn't work for this so we have to set the
    headers ourselves.

--- addon/adapters/ilios.js
+++ addon/adapters/ilios.js
@@ -1,14 +1,34 @@
 import RESTAdapter from '@ember-data/adapter/rest';
 import { inject as service } from '@ember/service';
 import { reads } from '@ember/object/computed';
-import TokenAuthorizerMixin from 'ember-simple-auth-token/mixins/token-authorizer';
 import { pluralize } from 'ember-inflector';
 
-export default RESTAdapter.extend(TokenAuthorizerMixin, {
+export default RESTAdapter.extend({
   iliosConfig: service(),
+  session: service(),
 
   host: reads('iliosConfig.apiHost'),
   namespace: reads('iliosConfig.apiNameSpace'),
+
+  /**
+   * Force Fetch since we're still using jQuery
+   * https://github.com/emberjs/data/issues/6759
+   */
+  useFetch: true,
+
+  init() {
+    this._super(...arguments);
+
+    let headers = {};
+    if (this.session && this.session.isAuthenticated) {
+      const { jwt } = this.session.data.authenticated;
+      if (jwt) {
+        headers['X-JWT-Authorization'] = `Token ${jwt}`;
+      }
+    }
+
+    this.set('headers', headers);
+  },
 
   coalesceFindRequests: true,
 
