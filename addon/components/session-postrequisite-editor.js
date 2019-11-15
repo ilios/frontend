488bc5668a1a58e9d27d8754364467473aa73378
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-11-14 23:19:23 -0800

    Add filter for finding session postrequisites

--- addon/components/session-postrequisite-editor.js
+++ addon/components/session-postrequisite-editor.js
@@ -2,16 +2,26 @@ import Component from '@ember/component';
 import { task } from 'ember-concurrency';
 import { computed } from '@ember/object';
 import layout from '../templates/components/session-postrequisite-editor';
+import escapeRegExp from 'ilios-common/utils/escape-reg-exp';
 
 export default Component.extend({
   layout,
   tagName: '',
+  filter: '',
 
   selectedPostrequisite: null,
   linkablePostrequisites: computed('session.course.sessions.[]', async function () {
     const course = await this.session.course;
     const sessions = await course.sessions;
     return sessions.sortBy("title").filter(sessionInCourse => sessionInCourse.id !== this.session.id);
+  }),
+  filteredPostrequisites: computed('linkablePostrequisites.[]', 'filter', async function () {
+    const linkablePostrequisites = await this.linkablePostrequisites;
+    if (!this.filter) {
+      return linkablePostrequisites;
+    }
+    let exp = new RegExp(escapeRegExp(this.filter), 'gi');
+    return linkablePostrequisites.filter(session => session.title.match(exp));
   }),
   didReceiveAttrs(){
     this._super(...arguments);
@@ -22,6 +32,9 @@ export default Component.extend({
   actions: {
     setPostrequisite(postrequisite) {
       this.set('selectedPostrequisite', postrequisite);
+    },
+    setFilter(filter) {
+      this.set('filter', filter);
     }
   },
   save: task(function* () {
