9cfed9a6ec9fb13b54944fa977d4597f581ff56a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-11-22 15:23:32 -0800

    invoke method, not action.

--- addon/components/week-glance-event.js
+++ addon/components/week-glance-event.js
@@ -1,9 +1,7 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 
 export default class WeekGlanceEvent extends Component {
 
-  @action
   sortString(a, b){
     return a.localeCompare(b);
   }
