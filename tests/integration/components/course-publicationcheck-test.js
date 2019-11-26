a99443347125479be6180780f52009636144798b
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-11-25 23:00:57 -0800

    Convert <CoursePublicationcheck> into a glimmer component

--- tests/integration/components/course-publicationcheck-test.js
+++ tests/integration/components/course-publicationcheck-test.js
@@ -2,30 +2,31 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import EmberObject from '@ember/object';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Integration | Component | course-publicationcheck', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-  const parent = EmberObject.create();
-  const objective1 = EmberObject.create({ parents: [parent] });
-
-  test('it shows unlink icon', async function(assert) {
-    const objective2 = EmberObject.create({ parents: [] });
-    const course = EmberObject.create({
-      objectives: [objective1, objective2]
+  test('it shows unlink icon', async function (assert) {
+    const parent = this.server.create('objective');
+    const objectiveWithParent = this.server.create('objective', { parents: [parent] });
+    const objectiveWithoutParent = this.server.create('objective');
+    const course = this.server.create('course', {
+      objectives: [objectiveWithParent, objectiveWithoutParent]
     });
-    this.set('model', course);
+    const courseModel = await this.owner.lookup('service:store').find('course', course.id);
+    this.set('model', courseModel);
     await render(hbs`<CoursePublicationcheck @course={{model}} />`);
     assert.ok(!!find('.fa-unlink'));
   });
 
   test('it does not shows unlink icon', async function(assert) {
-    const objective2 = EmberObject.create({ parents: [parent] });
-    const course = EmberObject.create({
-      objectives: [objective1, objective2]
-    });
-    this.set('model', course);
+    const parent = this.server.create('objective');
+    const objectives = this.server.createList('objective', 2, { parents: [parent] });
+    const course = this.server.create('course', { objectives });
+    const courseModel = await this.owner.lookup('service:store').find('course', course.id);
+    this.set('model', courseModel);
     await render(hbs`<CoursePublicationcheck @course={{model}} />`);
     assert.notOk(!!find('.fa-unlink'));
   });
