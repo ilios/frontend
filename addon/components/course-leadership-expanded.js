0967194b623fd2e51e2c40f5aa9435ed982877fe
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-11-25 16:10:43 -0800

    Convert <CourseLeadershipExpanded> into a glimmer component

--- addon/components/course-leadership-expanded.js
+++ addon/components/course-leadership-expanded.js
@@ -1,68 +1,51 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { task, timeout } from 'ember-concurrency';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { timeout } from 'ember-concurrency';
+import { dropTask } from 'ember-concurrency-decorators';
 
-export default Component.extend({
-  course: null,
-  editable: false,
-  classNames: ['course-leadership-expanded'],
-  directors: null,
-  administrators: null,
-  isManaging: false,
-  'data-test-course-leadership-expanded': true,
-  isCollapsible: computed('isManaging', 'course.directors.length', 'course.administrators.length', function(){
-    const course = this.get('course');
-    const isManaging = this.get('isManaging');
-    const administratorIds = course.hasMany('administrators').ids();
-    const directorIds = course.hasMany('directors').ids();
+export default class CoutseLeadershipExpanded extends Component {
+  @tracked directors = null;
+  @tracked administrators = null;
+  get isCollapsible() {
+    const administratorIds = this.args.course.hasMany('administrators').ids();
+    const directorIds = this.args.course.hasMany('directors').ids();
 
-    return (administratorIds.length > 0 || directorIds.length > 0) && !isManaging;
-
-  }),
-  didReceiveAttrs(){
-    this._super(...arguments);
-    const course = this.get('course');
-    if (course) {
-      course.get('directors').then(directors => {
-        this.set('directors', directors.toArray());
-      });
-      course.get('administrators').then(administrators => {
-        this.set('administrators', administrators.toArray());
-      });
-    }
-  },
-  actions: {
-    addDirector(user){
-      this.add('directors', user);
-    },
-    removeDirector(user){
-      this.remove('directors', user);
-    },
-    addAdministrator(user){
-      this.add('administrators', user);
-    },
-    removeAdministrator(user){
-      this.remove('administrators', user);
-    },
-  },
-  save: task(function * (){
+    return (administratorIds.length > 0 || directorIds.length > 0) && !this.args.isManaging;
+  }
+  @action
+  addDirector(user) {
+    this.directors = [...this.directors, user];
+  }
+  @action
+  removeDirector(user){
+    this.directors = this.directors.filter(obj => obj !== user);
+  }
+  @action
+  addAdministrator(user) {
+    this.administrators = [...this.administrators, user];
+  }
+  @action
+  removeAdministrator(user){
+    this.administrators = this.administrators.filter(obj => obj !== user);
+  }
+  @dropTask
+  *manage() {
+    const administrators = yield this.args.course.administrators;
+    const directors = yield this.args.course.directors;
+    this.administrators = administrators.toArray();
+    this.directors = directors.toArray();
+    this.args.setIsManaging(true);
+  }
+  @dropTask
+  *save(){
     yield timeout(10);
-    const directors = this.get('directors');
-    const administrators = this.get('administrators');
-    const course = this.get('course');
-    course.setProperties({directors, administrators});
-    this.get('expand')();
-    yield course.save();
-    this.get('setIsManaging')(false);
-  }),
-  add(where, user){
-    const arr = this.get(where).toArray();
-    arr.pushObject(user);
-    this.set(where, arr);
-  },
-  remove(where, user){
-    const arr = this.get(where).toArray();
-    arr.removeObject(user);
-    this.set(where, arr);
-  },
-});
+    this.args.course.setProperties({
+      directors: this.directors,
+      administrators: this.administrators
+    });
+    this.args.expand();
+    yield this.args.course.save();
+    this.args.setIsManaging(false);
+  }
+}
--- tests/integration/components/course-leadership-expanded-test.js
+++ tests/integration/components/course-leadership-expanded-test.js
@@ -1,156 +1,124 @@
-import EmberObject from '@ember/object';
-import RSVP from 'rsvp';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-const { resolve } = RSVP;
+import { component } from 'ilios-common/page-objects/components/course-leadership-expanded';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Integration | Component | course leadership expanded', function(hooks) {
   setupRenderingTest(hooks);
-
+  setupMirage(hooks);
 
   test('it renders', async function(assert) {
     assert.expect(6);
-
-    const user1 = EmberObject.create({
-      firstName: 'a',
-      lastName: 'person',
-      fullName: 'a b person',
-      enabled: true,
+    const users = this.server.createList('user', 2);
+    const course = this.server.create('course', {
+      directors: [users[0]],
+      administrators: users,
     });
-    const user2 = EmberObject.create({
-      firstName: 'b',
-      lastName: 'person',
-      fullName: 'b a person',
-      enabled: true,
-    });
-    const course = EmberObject.create({
-      directors: resolve([user1]),
-      administrators: resolve([user1, user2]),
-      hasMany(what){
-        if (what === 'directors') {
-          return {
-            ids(){
-              return [1];
-            }
-          };
-        }
-        if (what === 'administrators') {
-          return {
-            ids(){
-              return [1, 2];
-            }
-          };
-        }
-      }
-    });
-
-    this.set('course', course);
-    this.set('nothing', parseInt);
+    const courseModel = await this.owner.lookup('service:store').find('course', course.id);
+    this.set('course', courseModel);
+    this.set('nothing', () => {});
     await render(hbs`<CourseLeadershipExpanded
       @course={{course}}
       @editable={{true}}
-      @collapse={{action nothing}}
-      @expand={{action nothing}}
+      @collapse={{this.nothing}}
+      @expand={{this.nothing}}
       @isManaging={{false}}
-      @setIsManaging={{action nothing}}
+      @setIsManaging={{this.nothing}}
     />`);
-    const title = '.title';
-    const table = 'table';
-    const directors = `${table} tbody tr:nth-of-type(1) td:nth-of-type(1) li`;
-    const firstDirector = `${directors}:nth-of-type(1)`;
-    const administrators = `${table} tbody tr:nth-of-type(1) td:nth-of-type(2) li`;
-    const firstAdministrator = `${administrators}:nth-of-type(1)`;
-    const secondAdministrator = `${administrators}:nth-of-type(2)`;
 
-    assert.dom(title).hasText('Course Leadership');
-    assert.dom(directors).exists({ count: 1 });
-    assert.dom(firstDirector).hasText('a b person');
-    assert.dom(administrators).exists({ count: 2 });
-    assert.dom(firstAdministrator).hasText('a b person');
-    assert.dom(secondAdministrator).hasText('b a person');
+    assert.equal(component.title, 'Course Leadership');
+    assert.equal(component.leadershipList.directors.length, 1);
+    assert.equal(component.leadershipList.directors[0].text, '0 guy M. Mc0son');
+    assert.equal(component.leadershipList.administrators.length, 2);
+    assert.equal(component.leadershipList.administrators[0].text, '0 guy M. Mc0son');
+    assert.equal(component.leadershipList.administrators[1].text, '1 guy M. Mc1son');
   });
 
-  test('clicking the header collapses', async function(assert) {
+  test('clicking the header collapses when there are administrators', async function(assert) {
     assert.expect(1);
-    const course = EmberObject.create({
-      directors: resolve([]),
-      administrators: resolve([]),
-      hasMany(what){
-        if (what === 'directors') {
-          return {
-            ids(){
-              return [];
-            }
-          };
-        }
-        if (what === 'administrators') {
-          return {
-            ids(){
-              return [];
-            }
-          };
-        }
-      }
+    const administrators = this.server.createList('user', 1);
+    const course = this.server.create('course', {
+      administrators
     });
-
-    this.set('course', course);
+    const courseModel = await this.owner.lookup('service:store').find('course', course.id);
+    this.set('course', courseModel);
     this.set('click', () => {
       assert.ok(true, 'Action was fired');
     });
-    this.set('nothing', parseInt);
+    this.set('nothing', () => {});
     await render(hbs`<CourseLeadershipExpanded
       @course={{course}}
       @editable={{true}}
       @collapse={{action click}}
-      @expand={{action nothing}}
+      @expand={{this.nothing}}
       @isManaging={{false}}
-      @setIsManaging={{action nothing}}
+      @setIsManaging={{this.nothing}}
     />`);
-    const title = '.title';
+    await component.collapse();
+  });
 
-    await click(title);
+  test('clicking the header collapses when there are directors', async function(assert) {
+    assert.expect(1);
+    const directors = this.server.createList('user', 1);
+    const course = this.server.create('course', {
+      directors
+    });
+    const courseModel = await this.owner.lookup('service:store').find('course', course.id);
+    this.set('course', courseModel);
+    this.set('click', () => {
+      assert.ok(true, 'Action was fired');
+    });
+    this.set('nothing', () => {});
+    await render(hbs`<CourseLeadershipExpanded
+      @course={{course}}
+      @editable={{true}}
+      @collapse={{action click}}
+      @expand={{this.nothing}}
+      @isManaging={{false}}
+      @setIsManaging={{this.nothing}}
+    />`);
+    await component.collapse();
+  });
+
+  test('clicking the header does not collapse where there are no linked leaders', async function(assert) {
+    assert.expect(0);
+    const course = this.server.create('course');
+    const courseModel = await this.owner.lookup('service:store').find('course', course.id);
+    this.set('course', courseModel);
+    this.set('click', () => {
+      assert.ok(false);
+    });
+    this.set('nothing', () => {});
+    await render(hbs`<CourseLeadershipExpanded
+      @course={{course}}
+      @editable={{true}}
+      @collapse={{action click}}
+      @expand={{this.nothing}}
+      @isManaging={{false}}
+      @setIsManaging={{this.nothing}}
+    />`);
+    await component.collapse();
   });
 
   test('clicking manage fires action', async function(assert) {
     assert.expect(1);
-    const course = EmberObject.create({
-      directors: resolve([]),
-      administrators: resolve([]),
-      hasMany(what){
-        if (what === 'directors') {
-          return {
-            ids(){
-              return [];
-            }
-          };
-        }
-        if (what === 'administrators') {
-          return {
-            ids(){
-              return [];
-            }
-          };
-        }
-      }
-    });
-
-    this.set('course', course);
+    const course = this.server.create('course');
+    const courseModel = await this.owner.lookup('service:store').find('course', course.id);
+    this.set('course', courseModel);
     this.set('click', () => {
       assert.ok(true, 'Action was fired');
     });
-    this.set('nothing', parseInt);
+    this.set('nothing', () => {});
     await render(hbs`<CourseLeadershipExpanded
       @course={{course}}
       @editable={{true}}
-      @collapse={{action nothing}}
-      @expand={{action nothing}}
+      @collapse={{this.nothing}}
+      @expand={{this.nothing}}
       @isManaging={{false}}
       @setIsManaging={{action click}}
     />`);
-    const manage = '.actions button';
-
-    await click(manage);
+    await component.manage();
   });
 });
