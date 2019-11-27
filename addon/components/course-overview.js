392880c7a71325539e1fc1510f8ceaf6bd8de3b7
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-11-26 23:46:15 -0800

    Convert <CourseOverview> to a glimmer component
    With validations removed (to be added back soon)

--- addon/components/course-overview.js
+++ addon/components/course-overview.js
@@ -1,244 +1,193 @@
-import Component from '@ember/component';
-import EmberObject, { computed } from '@ember/object';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { isEmpty } from '@ember/utils';
-import { reject } from 'rsvp';
-import { task } from 'ember-concurrency';
-import { validator, buildValidations } from 'ember-cp-validations';
-import ValidationErrorDisplay from 'ilios-common/mixins/validation-error-display';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import scrollTo from 'ilios-common/utils/scroll-to';
+import { restartableTask } from 'ember-concurrency-decorators';
 
-const { reads } = computed;
+export default class BackLink extends Component {
+  @service currentUser;
+  @service intl;
+  @service permissionChecker;
+  @service router;
+  @service('-routing') routing;
+  @service store;
 
-const Validations = buildValidations({
-  externalId: [
-    validator('length', {
-      allowBlank: true,
-      min: 2,
-      max: 255
-    }),
-  ],
-  startDate: [
-    validator('date', {
-      dependentKeys: ['model.endDate'],
-      onOrBefore: reads('model.endDate'),
-    }),
-  ],
-  endDate: [
-    validator('date', {
-      dependentKeys: ['model.startDate'],
-      onOrAfter: reads('model.startDate'),
-    }),
-  ],
-});
+  universalLocator = 'ILIOS';
 
-export default Component.extend(Validations, ValidationErrorDisplay, {
-  currentUser: service(),
-  intl: service(),
-  permissionChecker: service(),
-  router: service(),
-  routing: service('-routing'),
-  store: service(),
+  @tracked externalId = null;
+  @tracked startDate = null;
+  @tracked level = null;
+  @tracked levelOptions = null;
+  @tracked clerkshipTypeId = null;
+  @tracked manageDirectors = false;
+  @tracked showDirectorManagerLoader = true;
+  @tracked currentRoute = '';
 
-  editable: false,
-  universalLocator: 'ILIOS',
-  'data-test-course-overview': true,
-  course: null,
-  externalId: null,
-  startDate: null,
-  level: null,
-  levelOptions: null,
-  classNames: ['course-overview'],
-  tagName: 'section',
-  clerkshipTypeId: null,
-  manageDirectors: false,
-  showDirectorManagerLoader: true,
-  currentRoute: '',
-  selectedClerkshipType: computed('clerkshipTypeId', 'clerkshipTypeOptions.[]', async function() {
-    const id = this.get('clerkshipTypeId');
-    const clerkshipTypeOptions = await this.get('clerkshipTypeOptions');
-    if (isEmpty(id)) {
+  @tracked clerkshipTypeOptions;
+
+  @tracked showRollover = false;
+  @tracked school = null;
+
+  constructor() {
+    super(...arguments);
+    const levelOptions = [];
+    for(let i=1;i<=5; i++){
+      levelOptions.pushObject({
+        id: i,
+        title: i
+      });
+    }
+    this.levelOptions = levelOptions;
+  }
+
+  @restartableTask
+  *load(element, [clerkshipType]) {
+    this.clerkshipTypeOptions = yield this.store.findAll('course-clerkship-type');
+    this.externalId = this.args.course.externalId;
+    this.startDate = this.args.course.startDate;
+    this.endDate = this.args.course.endDate;
+    this.level = this.args.course.level;
+    this.school = yield this.args.course.school;
+    yield this.directorsToPassToManager.perform();
+    if (clerkshipType) {
+      this.clerkshipTypeId = clerkshipType.id;
+    } else {
+      this.clerkshipTypeId = null;
+    }
+  }
+
+  @restartableTask
+  *directorsToPassToManager() {
+    const users = yield this.args.course.directors;
+    this.showDirectorManagerLoader = false;
+    return users;
+  }
+
+  get selectedClerkshipType() {
+    if (!this.clerkshipTypeId) {
       return null;
     }
 
-    return clerkshipTypeOptions.findBy('id', id);
-  }),
-  showRollover: computed('course', 'routing.currentRouteName', async function () {
-    const routing = this.get('routing');
-    if (routing.get('currentRouteName') === 'course.rollover') {
+    return this.clerkshipTypeOptions.findBy('id', this.clerkshipTypeId);
+  }
+
+  get showRollover(){
+    if (this.routing.currentRouteName === 'course.rollover') {
       return false;
     }
 
-    const permissionChecker = this.get('permissionChecker');
-    const course = this.get('course');
-    const school = await course.get('school');
-    return permissionChecker.canCreateCourse(school);
-  }),
+    return this.permissionChecker.canCreateCourse(this.school);
+  }
 
-  clerkshipTypeTitle: computed('selectedClerkshipType', async function () {
-    const selectedClerkshipType = await this.get('selectedClerkshipType');
-    const intl = this.get('intl');
-    if (!selectedClerkshipType) {
-      return intl.t('general.notAClerkship');
+  get clerkshipTypeTitle() {
+    if (!this.selectedClerkshipType) {
+      return this.intl.t('general.notAClerkship');
     }
 
-    return selectedClerkshipType.title;
-  }),
+    return this.selectedClerkshipType.title;
+  }
 
-  clerkshipTypeOptions: computed(async function () {
-    const store = this.get('store');
-    return store.findAll('course-clerkship-type');
-  }),
+  @action
+  async saveDirectors(newDirectors){
+    this.args.course.set('directors', newDirectors.toArray());
+    await this.args.course.save();
+    this.directorsToPassToManager.perform();
+    return this.manageDirectors = false;
+  }
+  @action
+  async changeClerkshipType() {
+    this.args.course.set('clerkshipType', this.selectedClerkshipType);
+    return this.args.course.save();
+  }
 
-  init(){
-    this._super(...arguments);
-
-    const levelOptions = [];
-    for(let i=1;i<=5; i++){
-      levelOptions.pushObject(EmberObject.create({
-        id: i,
-        title: i
-      }));
+  @action
+  setCourseClerkshipType(id){
+    //convert the string 'null' to a real null
+    if (id === 'null') {
+      id = null;
     }
-    this.set('levelOptions', levelOptions);
-  },
-  didReceiveAttrs(){
-    this._super(...arguments);
-    this.get('directorsToPassToManager').perform();
-    const course = this.get('course');
-    this.set('externalId', course.get('externalId'));
-    this.set('startDate', course.get('startDate'));
-    this.set('endDate', course.get('endDate'));
-    this.set('level', course.get('level'));
+    this.clerkshipTypeId = id;
+  }
 
-    course.get('clerkshipType').then(clerkshipType => {
-      if (isEmpty(clerkshipType)) {
-        this.set('clerkshipTypeId', null);
-      } else {
-        this.set('clerkshipTypeId', clerkshipType.get('id'));
-      }
-    });
-  },
-  actions: {
-    saveDirectors(newDirectors){
-      const course = this.get('course');
-      course.set('directors', newDirectors.toArray());
-      return course.save().then(()=>{
-        this.get('directorsToPassToManager').perform();
-        return this.set('manageDirectors', false);
-      });
-    },
-    async changeClerkshipType(){
-      const course = this.get('course');
-      const selectedClerkshipType = await this.get('selectedClerkshipType');
-      course.set('clerkshipType', selectedClerkshipType);
-      return course.save();
-    },
-
-    setCourseClerkshipType(id){
-      //convert the string 'null' to a real null
-      if (id === 'null') {
-        id = null;
-      }
-      this.set('clerkshipTypeId', id);
-    },
-
-    revertClerkshipTypeChanges(){
-      const course = this.get('course');
-      course.get('clerkshipType').then(clerkshipType => {
-        if (isEmpty(clerkshipType)) {
-          this.set('clerkshipTypeId', null);
-        } else {
-          this.set('clerkshipTypeId', clerkshipType.get('id'));
-        }
-      });
-    },
-
-    async changeStartDate() {
-      const { course, startDate } = this.getProperties('course', 'startDate');
-      this.send('addErrorDisplayFor', 'startDate');
-      const { validations } = await this.validate();
-
-      if (validations.isValid) {
-        this.send('removeErrorDisplayFor', 'startDate');
-        course.set('startDate', startDate);
-        const newCourse = await course.save();
-        this.set('startDate', newCourse.startDate);
-        this.set('course', newCourse);
-      } else {
-        await reject();
-      }
-    },
-
-    revertStartDateChanges(){
-      const course = this.get('course');
-      this.set('startDate', course.get('startDate'));
-    },
-
-    async changeEndDate() {
-      const { course, endDate } = this.getProperties('course', 'endDate');
-      this.send('addErrorDisplayFor', 'endDate');
-      const { validations } = await this.validate();
-
-      if (validations.isValid) {
-        this.send('removeErrorDisplayFor', 'endDate');
-        course.set('endDate', endDate);
-        const newCourse = await course.save();
-        this.set('endDate', newCourse.endDate);
-        this.set('course', newCourse);
-      } else {
-        await reject();
-      }
-    },
-
-    revertEndDateChanges(){
-      const course = this.get('course');
-      this.set('endDate', course.get('endDate'));
-    },
-
-    async changeExternalId() {
-      const { course, externalId } = this.getProperties('course', 'externalId');
-      this.send('addErrorDisplayFor', 'externalId');
-      const { validations } = await this.validate();
-
-      if (validations.isValid) {
-        this.send('removeErrorDisplayFor', 'externalId');
-        course.set('externalId', externalId);
-        const newCourse = await course.save();
-        this.set('externalId', newCourse.externalId);
-        this.set('course', newCourse);
-      } else {
-        await reject();
-      }
-    },
-
-    revertExternalIdChanges(){
-      const course = this.get('course');
-      this.set('externalId', course.get('externalId'));
-    },
-
-    changeLevel(){
-      this.get('course').set('level', this.get('level'));
-      this.get('course').save();
-    },
-
-    revertLevelChanges(){
-      this.set('level', this.get('course').get('level'));
-    },
-
-    transitionToRollover() {
-      this.router.transitionTo('course.rollover', this.course);
-      scrollTo('.rollover-form');
+  @restartableTask
+  *revertClerkshipTypeChanges() {
+    const clerkshipType = yield this.args.course.clerkshipType;
+    if (clerkshipType) {
+      this.clerkshipTypeId = clerkshipType.id;
+    } else {
+      this.clerkshipTypeId = null;
     }
-  },
+  }
 
-  directorsToPassToManager: task(function * () {
-    const course = this.get('course');
+  @restartableTask
+  *changeStartDate() {
+    this.addErrorDisplayFor('startDate');
+    const isValid = yield this.isValid('startDate');
+    if (!isValid) {
+      return false;
+    }
+    this.removeErrorDisplayFor('startDate');
+    this.args.course.set('startDate', this.startDate);
+    yield this.args.course.save();
+    this.startDate = this.args.course.startDate;
+  }
 
-    const users = yield course.get('directors');
+  @action
+  revertStartDateChanges(){
+    this.startDate = this.args.course.startDate;
+  }
 
-    this.set('showDirectorManagerLoader', false);
-    return users;
-  }).restartable(),
+  @restartableTask
+  *changeEndDate() {
+    this.addErrorDisplayFor('endDate');
+    const isValid = yield this.isValid('endDate');
+    if (!isValid) {
+      return false;
+    }
+    this.removeErrorDisplayFor('endDate');
+    this.args.course.set('endDate', this.endDate);
+    yield this.args.course.save();
+    this.endDate = this.args.course.endDate;
+  }
 
-});
+  @action
+  revertEndDateChanges(){
+    this.endDate = this.args.course.endDate;
+  }
+
+  @restartableTask
+  *changeExternalId() {
+    this.addErrorDisplayFor('externalId');
+    const isValid = yield this.isValid('externalId');
+    if (!isValid) {
+      return false;
+    }
+    this.removeErrorDisplayFor('externalId');
+    this.args.course.set('externalId', this.externalId);
+    yield this.args.course.save();
+    this.externalId = this.args.course.externalId;
+  }
+
+  @action
+  revertExternalIdChanges() {
+    this.externalId = this.args.course.externalId;
+  }
+
+  @restartableTask
+  async changeLevel() {
+    this.args.course.set('level', this.level);
+    return this.args.course.save();
+  }
+
+  @action
+  revertLevelChanges(){
+    this.level = this.args.course.level;
+  }
+
+  @action
+  transitionToRollover() {
+    this.router.transitionTo('course.rollover', this.course);
+    scrollTo('.rollover-form');
+  }
+}
