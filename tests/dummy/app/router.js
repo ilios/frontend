19269438be3a3841e3daddc2387960835fd5916e
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-12-04 15:08:25 -0800

    adds routes, controllers and templates for course visualizations to dummy app.
    copied and pasted from frontend, then adjusted to eslint rules.

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -36,5 +36,27 @@ Router.map(function() {
   this.route('course-visualize-objectives', {
     path: 'data/courses/:course_id/objectives'
   });
+  this.route('course-visualize-session-types', {
+    path: 'data/courses/:course_id/session-types'
+  });
+  this.route('course-visualize-vocabularies', {
+    path: 'data/courses/:course_id/vocabularies'
+  });
+  this.route('course-visualize-vocabulary', {
+    path: 'data/courses/:course_id/vocabularies/:vocabulary_id'
+  });
+  this.route('course-visualize-term', {
+    path: 'data/courses/:course_id/terms/:term_id'
+  });
+  /* eslint ember/routes-segments-snake-case: 0 */
+  this.route('course-visualize-session-type', {
+    path: 'data/courses/:course_id/session-types/:session-type_id'
+  });
+  this.route('course-visualize-instructors', {
+    path: 'data/courses/:course_id/instructors'
+  });
+  this.route('course-visualize-instructor', {
+    path: 'data/courses/:course_id/instructors/:user_id'
+  });
   this.route('mymaterials');
 });
