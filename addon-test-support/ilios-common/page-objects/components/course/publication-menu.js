d54a723734c5beb0cd3fd85998c88fb6132efc5f
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-12-10 23:27:08 -0800

    Created <Course::PublicationMenu> component
    This extracts functionality from the shared PublishMenu component. That shared component requires a mixin on CourseHeader to work, so pulling this out into a single purpose component allows that mixin to be removed.
    
    Needed to allow for a11y testing failures for the color contracst on scheduled courses which will have to be addressed at a later date.

---
+++ addon-test-support/ilios-common/page-objects/components/course/publication-menu.js
@@ -0,0 +1,26 @@
+import {
+  create,
+  triggerable,
+  isVisible,
+  isHidden
+} from 'ember-cli-page-object';
+
+const definition = {
+  scope: '[data-test-course-publication-menu]',
+  toggle: {
+    scope: '[data-test-toggle]',
+    enter: triggerable('keydown', '', { eventProperties: { key: 'Enter' } }),
+    down: triggerable('keydown', '', { eventProperties: { key: 'ArrowDown' } }),
+    esc: triggerable('keydown', '', { eventProperties: { key: 'Escape' } }),
+  },
+  menuClosed: isHidden('[data-test-menu]'),
+  menuOpen: isVisible('[data-test-menu]'),
+  hasPublishAsIs: isVisible('[data-test-publish-as-is]'),
+  hasPublish: isVisible('[data-test-publish]'),
+  hasReview: isVisible('[data-test-review]'),
+  hasTbd: isVisible('[data-test-tbd]'),
+  hasUnPublish: isVisible('[data-test-un-publish]'),
+};
+
+export default definition;
+export const component = create(definition);
---
+++ tests/integration/components/course/publication-menu-test.js
@@ -0,0 +1,186 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { component } from 'ilios-common/page-objects/components/course/publication-menu';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Integration | Component | course/publication-menu', function(hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  test('it renders and is accessible for draft course', async function (assert) {
+    this.server.create('course');
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+
+    await a11yAudit(this.element, {
+      rules: {
+        'color-contrast': {
+          enabled: false
+        }
+      }
+    });
+    assert.equal(component.text, 'Not Published');
+    await component.toggle.click();
+    await a11yAudit(this.element, {
+      rules: {
+        'color-contrast': {
+          enabled: false
+        }
+      }
+    });
+    assert.ok(true, 'no a11y errors found!');
+  });
+
+  test('it renders and is accessible for scheduled course', async function (assert) {
+    this.server.create('course', {
+      published: true,
+      publishedAsTbd: true,
+    });
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+
+    await a11yAudit(this.element);
+    assert.equal(component.text, 'Scheduled');
+    await component.toggle.click();
+    await a11yAudit(this.element);
+    assert.ok(true, 'no a11y errors found!');
+  });
+
+  test('it renders and is accessible for published course', async function (assert) {
+    this.server.create('course', {
+      published: true,
+      publishedAsTbd: false,
+    });
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+
+    await a11yAudit(this.element);
+    assert.equal(component.text, 'Published');
+    await component.toggle.click();
+    await a11yAudit(this.element);
+    assert.ok(true, 'no a11y errors found!');
+  });
+
+  test('click opens menu', async function(assert) {
+    this.server.create('course');
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+    assert.ok(component.menuClosed);
+    await component.toggle.click();
+    assert.ok(component.menuOpen);
+  });
+
+  test('correct actions for unpublished course', async function(assert) {
+    this.server.create('course');
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+    await component.toggle.click();
+    assert.ok(component.menuOpen);
+    assert.notOk(component.hasPublishAsIs);
+    assert.notOk(component.hasPublish);
+    assert.ok(component.hasReview);
+    assert.ok(component.hasTbd);
+    assert.notOk(component.hasUnPublish);
+  });
+
+  test('correct actions for unpublished course with required cohort', async function (assert) {
+    const cohort = this.server.create('cohort');
+    this.server.create('course', {
+      cohorts: [cohort],
+    });
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+    await component.toggle.click();
+    assert.ok(component.menuOpen);
+    assert.ok(component.hasPublishAsIs);
+    assert.notOk(component.hasPublish);
+    assert.ok(component.hasReview);
+    assert.ok(component.hasTbd);
+    assert.notOk(component.hasUnPublish);
+  });
+
+  test('correct actions for scheduled course', async function(assert) {
+    this.server.create('course', {
+      published: true,
+      publishedAsTbd: true,
+    });
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+    await component.toggle.click();
+    assert.ok(component.menuOpen);
+    assert.notOk(component.hasPublishAsIs);
+    assert.notOk(component.hasPublish);
+    assert.ok(component.hasReview);
+    assert.notOk(component.hasTbd);
+    assert.ok(component.hasUnPublish);
+  });
+
+  test('correct actions for scheduled course with required cohort', async function (assert) {
+    const cohort = this.server.create('cohort');
+    this.server.create('course', {
+      cohorts: [cohort],
+      published: true,
+      publishedAsTbd: true,
+    });
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+    await component.toggle.click();
+    assert.ok(component.menuOpen);
+    assert.ok(component.hasPublishAsIs);
+    assert.notOk(component.hasPublish);
+    assert.ok(component.hasReview);
+    assert.notOk(component.hasTbd);
+    assert.ok(component.hasUnPublish);
+  });
+
+  test('correct actions for published course', async function(assert) {
+    this.server.create('course', {
+      published: true,
+      publishedAsTbd: false,
+    });
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+    await component.toggle.click();
+    assert.ok(component.menuOpen);
+    assert.notOk(component.hasPublishAsIs);
+    assert.notOk(component.hasPublish);
+    assert.ok(component.hasReview);
+    assert.ok(component.hasTbd);
+    assert.ok(component.hasUnPublish);
+  });
+
+  test('down opens menu', async function(assert) {
+    this.server.create('course');
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+
+    assert.ok(component.menuClosed);
+    await component.toggle.down();
+    assert.ok(component.menuOpen);
+  });
+
+  test('escape closes menu', async function(assert) {
+    this.server.create('course');
+    const courseModel = await this.owner.lookup('service:store').find('course', 1);
+    this.set('course', courseModel);
+    await render(hbs`<Course::PublicationMenu @course={{this.course}} />`);
+
+    await component.toggle.down();
+    assert.ok(component.menuOpen);
+    await component.toggle.esc();
+    assert.ok(component.menuClosed);
+  });
+});
