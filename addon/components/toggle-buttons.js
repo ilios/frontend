18d096d45b788948f42a41d2f1c7f8a4ee8664bc
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-12-11 17:43:01 -0800

    replace elementId with glimmer-compliant getter.
    the answer can be found in the Q&A section of this ticket:
    https://github.com/emberjs/ember.js/issues/16301

--- addon/components/toggle-buttons.js
+++ addon/components/toggle-buttons.js
@@ -1,13 +1,19 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
 
 export default class ToggleButtons extends Component {
+
+  get uniqueId() {
+    return guidFor(this);
+  }
 
   @action
   firstChoice(){
     if (! this.args.firstOptionSelected) {
       this.args.toggle(true);
     }
+    console.log(this.elementId);
   }
 
   @action
--- addon/components/toggle-icons.js
+++ addon/components/toggle-icons.js
@@ -1,7 +1,12 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
 
 export default class ToggleIcons extends Component {
+
+  get uniqueId() {
+    return guidFor(this);
+  }
 
   @action
   firstChoice(){
