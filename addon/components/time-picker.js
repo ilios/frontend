c9a3e4672ed248f643ccdfa3baf2d87be039f465
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-12-11 16:37:53 -0800

    glimmerized TimePicker component.

--- addon/components/time-picker.js
+++ addon/components/time-picker.js
@@ -1,54 +1,58 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import moment from 'moment';
-import momentFormat from 'ember-moment/computeds/format';
+import { action } from '@ember/object';
 
-export default Component.extend({
-  classNames: ['time-picker'],
-  date: null,
+export default class TimePicker extends Component {
 
-  hours: null,
-  minutes: null,
-  ampms: null,
+  constructor() {
+    super(...arguments);
+    this.now = new Date();
+    this.hours = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+    this.minutes = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59'];
+    this.ampms  = ['am', 'pm'];
+  }
 
-  hour: momentFormat('date', 'h'),
-  minute: momentFormat('date', 'mm'),
-  ampm: momentFormat('date', 'a'),
+  get hour() {
+    return moment(this.args.date).format('h');
+  }
 
-  init() {
-    this._super(...arguments);
-    this.set('hours', ['1','2','3','4','5','6','7','8','9','10','11','12']);
-    this.set('minutes', ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59']);
-    this.set('ampms', ['am', 'pm']);
-  },
+  get minute() {
+    return moment(this.args.date).format('mm');
+  }
 
-  actions: {
-    changeHour(string) {
-      let hour = parseInt(string, 10);
-      const ampm = this.get('ampm');
+  get ampm() {
+    return moment(this.args.date).format('a');
+  }
 
-      if (ampm === 'pm') {
-        hour += 12;
-      }
+  @action
+  changeHour(string) {
+    let hour = parseInt(string, 10);
+    const ampm = this.ampm;
 
-      this.action(hour, 'hour');
-    },
+    if (ampm === 'pm') {
+      hour += 12;
+    }
 
-    changeMinute(string) {
-      const minute = parseInt(string, 10);
-      this.action(minute, 'minute');
-    },
+    this.args.action(hour, 'hour');
+  }
 
-    changeAmPm(value) {
-      const currentValue = this.get('ampm');
-      const hour = moment(this.get('date')).hours();
+  @action
+  changeMinute(string) {
+    const minute = parseInt(string, 10);
+    this.args.action(minute, 'minute');
+  }
 
-      if (value != currentValue) {
-        if (value === 'am') {
-          this.action(hour - 12, 'hour');
-        } else {
-          this.action(hour + 12, 'hour');
-        }
+  @action
+  changeAmPm(value) {
+    const currentValue = this.ampm;
+    const hour = moment(this.args.date).hours();
+
+    if (value !== currentValue) {
+      if (value === 'am') {
+        this.args.action(hour - 12, 'hour');
+      } else {
+        this.args.action(hour + 12, 'hour');
       }
     }
   }
-});
+}
