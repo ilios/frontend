1d136ccb73cf9289b2738a13deeadb9562176cba
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-12-20 11:22:12 -0800

    Fix component class names

--- addon/components/back-link.js
+++ addon/components/back-link.js
@@ -1,7 +1,7 @@
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
-export default class BackLink extends Component {
+export default class BackLinkComponent extends Component {
   @service intl;
   back() {
     window.history.back();
