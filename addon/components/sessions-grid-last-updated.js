7c47b7e39ad6a9beabc936c1ee668944d65ceec9
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-12-23 15:33:03 +0100

    glimmerizes SessionsGridLastUpdated component.

--- addon/components/sessions-grid-last-updated.js
+++ addon/components/sessions-grid-last-updated.js
@@ -1,12 +1,12 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+
 import moment from 'moment';
 
-export default Component.extend({
-  classNames: ['sessions-grid-last-updated'],
-  session: null,
-  updatedAt: null,
-  didReceiveAttrs(){
-    this._super(...arguments);
-    this.set('updatedAt', moment(this.get('session.updatedAt')).format("L LT"));
+export default class SessionsGridOfferingTable extends Component {
+  get updatedAt() {
+    if (this.args.session) {
+      return moment(this.args.session.updatedAt).format("L LT");
+    }
+    return '';
   }
-});
+}
