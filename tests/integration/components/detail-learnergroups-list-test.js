6623f3e5aa9482e126310bfc3948d212d4713dc8
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-12-22 21:37:43 -0800

    Octaneify <DetailLearnergroupsList> component

--- tests/integration/components/detail-learnergroups-list-test.js
+++ tests/integration/components/detail-learnergroups-list-test.js
@@ -1,12 +1,12 @@
-import EmberObject from '@ember/object';
-import { resolve } from 'rsvp';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, settled, find } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Integration | Component | detail learnergroups list', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
   test('it renders', async function(assert) {
     const set1 = 'fieldset:nth-of-type(1)';
@@ -19,83 +19,44 @@ module('Integration | Component | detail learnergroups list', function(hooks) {
     const set2Legend = set2 + ' legend';
     const set2Group1 = set2 + ' li:nth-of-type(1)';
     const set2Group2 = set2 + ' li:nth-of-type(2)';
+    const users = this.server.createList('user', 5);
 
-    const tlg1 = EmberObject.create({
-      allParentTitles: [],
+    const tlg1 = this.server.create('learner-group', {
       title: 'tlg1',
-      hasMany(){
-        return {
-          ids(){
-            return [1,2];
-          }
-        };
-      }
+      users: [users[0], users[1]],
     });
-    tlg1.set('topLevelGroup', resolve(tlg1));
-    const subGroup1 = EmberObject.create({
-      allParentTitles: ['tlg1'],
-      topLevelGroup: resolve(tlg1),
+    const subGroup1 = this.server.create('learner-group', {
       title: 'sub group 1',
-      hasMany(){
-        return {
-          ids(){
-            return [1, 2, 3];
-          }
-        };
-      }
+      parent: tlg1,
+      users: [users[0], users[1], users[2]],
     });
-    const subSubGroup1 = EmberObject.create({
-      allParentTitles: ['tlg1', 'sub group 1'],
-      topLevelGroup: resolve(tlg1),
+    const subSubGroup1 = this.server.create('learner-group', {
       title: 'sub sub group 1',
-      hasMany(){
-        return {
-          ids(){
-            return [1];
-          }
-        };
-      }
+      parent: subGroup1,
+      users: [users[0]],
     });
-    const tlg2 = EmberObject.create({
-      allParentTitles: [],
+
+    const tlg2 = this.server.create('learner-group', {
       title: 'tlg2',
-      hasMany(){
-        return {
-          ids(){
-            return [1,2];
-          }
-        };
-      }
+      users: [users[0], users[1]],
     });
-    tlg2.set('topLevelGroup', resolve(tlg2));
-    const subGroup2 = EmberObject.create({
-      topLevelGroup: resolve(tlg2),
-      allParentTitles: ['tlg2'],
+    const subGroup2 = this.server.create('learner-group', {
       title: 'sub group 2',
-      hasMany(){
-        return {
-          ids(){
-            return [];
-          }
-        };
-      }
+      parent: tlg2,
     });
+    const store = this.owner.lookup('service:store');
+    const tlg1Model = await store.find('learner-group', tlg1.id);
+    const subGroup1Model = await store.find('learner-group', subGroup1.id);
+    const subSubGroupModel = await store.find('learner-group', subSubGroup1.id);
+    const subGroup2Model = await store.find('learner-group', subGroup2.id);
 
-    tlg1.set('allDescendants', resolve([subGroup1, subSubGroup1]));
-    subGroup1.set('allDescendants', resolve([subSubGroup1]));
-    subSubGroup1.set('allDescendants', resolve([]));
-    tlg2.set('allDescendants', resolve([subGroup2]));
-    subGroup2.set('allDescendants', resolve([]));
-
-
-    this.set('learnerGroups', [tlg1, subGroup1, subSubGroup1, subGroup2]);
-    this.set('nothing', parseInt);
+    this.set('learnerGroups', [ tlg1Model, subGroup1Model, subSubGroupModel, subGroup2Model ]);
+    this.set('nothing', () => {});
 
     await render(hbs`<DetailLearnergroupsList
       @learnerGroups={{learnerGroups}}
       @remove={{action nothing}}
     />`);
-    await settled();
 
     assert.dom(set1Legend).hasText('tlg1 ( )');
     assert.dom(set1Group1).hasText('tlg1 (2)');
