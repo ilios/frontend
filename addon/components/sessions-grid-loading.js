a4d2e24f2f1b88a872c47e964beaa7a0310fc4dd
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-12-23 14:53:41 +0100

    glimmerizes SessionsGridLoading component.

--- addon/components/sessions-grid-loading.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-
-export default Component.extend({
-  classNames: ['sessions-grid-loading'],
-  attributeBindings: ['ariaHidden:aria-hidden', 'role'],
-  ariaHidden: 'true',
-  role: 'presentation',
-});
