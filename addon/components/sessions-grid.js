915c3819a4c912ca3de7af4d6c53ce963b2b46dc
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-12-23 18:11:36 +0100

    glimmerizes sessions-grid component.
    dropping the window scroll tracking via lifeline in the process,
    the pieces are not fitting back together.

--- addon/components/sessions-grid.js
+++ addon/components/sessions-grid.js
@@ -1,37 +1,29 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { next } from '@ember/runloop';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { isEmpty } from '@ember/utils';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import escapeRegExp from '../utils/escape-reg-exp';
-import { task } from 'ember-concurrency';
-import { addEventListener, runDisposables } from 'ember-lifeline';
+import { dropTask } from "ember-concurrency-decorators";
 
-export default Component.extend({
-  preserveScroll: service(),
-  router: service(),
+export default class SessionsGrid extends Component {
+  @service router;
+  @tracked confirmDeleteSessionIds = [];
 
-  classNames: ['sessions-grid'],
-
-  'data-test-sessions-grid': true,
-  confirmDeleteSessionIds: null,
-
-  filteredSessions: computed('sessions.[]', 'filterBy', function(){
-    const sessions = this.get('sessions');
-    const filterBy = this.get('filterBy');
-    if (isEmpty(sessions)) {
+  get filteredSessions() {
+    if (isEmpty(this.args.sessions)) {
       return [];
     }
-    if (isEmpty(filterBy)) {
-      return sessions;
+    if (isEmpty(this.args.filterBy)) {
+      return this.args.sessions;
     }
 
-    const filterExpressions = filterBy.split(' ').map(function (string) {
+    const filterExpressions = this.args.filterBy.split(' ').map(function (string) {
       const clean = escapeRegExp(string);
       return new RegExp(clean, 'gi');
     });
 
-    return sessions.filter(session => {
+    return this.args.sessions.filter(session => {
       let matchedSearchTerms = 0;
 
       for (let i = 0; i < filterExpressions.length; i++) {
@@ -42,81 +34,43 @@ export default Component.extend({
       //if the number of matching search terms is equal to the number searched, return true
       return (matchedSearchTerms === filterExpressions.length);
     });
-  }),
+  }
 
-  sortedSessions: computed('filteredSessions.[]', 'sortInfo', function(){
-    const sessions = this.get('filteredSessions');
-    const sortInfo = this.get('sortInfo');
-
-    if (sortInfo.descending) {
-      return sessions.sortBy(sortInfo.column).reverse();
+  get sortedSessions() {
+    if (this.sortInfo.descending) {
+      return this.args.sessions.sortBy(this.sortInfo.column).reverse();
     }
-    return sessions.sortBy(sortInfo.column);
-  }),
+    return this.args.sessions.sortBy(this.sortInfo.column);
+  }
 
-  sortInfo: computed('sortBy', function(){
-    const sortBy = this.get('sortBy');
-    const parts = sortBy.split(':');
+  get sortInfo() {
+    const parts = this.args.sortBy.split(':');
     const column = parts[0];
     const descending = parts.length > 1 && parts[1] === 'desc';
 
-    return { column, descending, sortBy };
-  }),
+    return { column, descending, sortBy: this.args.sortBy };
+  }
 
-  init() {
-    this._super(...arguments);
-    this.set('confirmDeleteSessionIds', []);
-  },
+  @action
+  confirmDelete(sessionId) {
+    this.confirmDeleteSessionIds.pushObject(sessionId);
+  }
 
-  didInsertElement() {
-    this._super(...arguments);
-    this.scrollDown();
-  },
+  @action
+  cancelDelete(sessionId) {
+    this.confirmDeleteSessionIds.removeObject(sessionId);
+  }
 
-  didRender() {
-    this._super(...arguments);
-    addEventListener(this, window, 'scroll', () => {
-      const isCourseRoute = this.router.currentRouteName === 'course.index';
-      const preserveScroll = this.preserveScroll;
-
-      if (isCourseRoute) {
-        const yPos = window.scrollY;
-        preserveScroll.set('yPos', yPos === 0 ? null : yPos);
-      }
-    });
-  },
-
-  actions: {
-    confirmDelete(sessionId) {
-      this.confirmDeleteSessionIds.pushObject(sessionId);
-    },
-    cancelDelete(sessionId) {
-      this.confirmDeleteSessionIds.removeObject(sessionId);
-    },
-    expandSession(sessionObject) {
-      if (sessionObject.offeringCount) {
-        this.expandSession(sessionObject.session);
-      }
+  @action
+  expandSession(sessionObject) {
+    if (sessionObject.offeringCount) {
+      this.args.expandSession(sessionObject.session);
     }
-  },
+  }
 
-  destroy() {
-    runDisposables(this);
-    this._super(...arguments);
-  },
-
-  scrollDown() {
-    const preserveScroll = this.preserveScroll;
-    const { shouldScrollDown, yPos } = preserveScroll;
-    next(() => {
-      if (shouldScrollDown && yPos) {
-        window.scroll(0, yPos);
-      }
-    });
-  },
-
-  removeSession: task(function * (session){
+  @dropTask
+  *removeSession(session) {
     session.deleteRecord();
     yield session.save();
-  }).drop(),
-});
+  }
+}
