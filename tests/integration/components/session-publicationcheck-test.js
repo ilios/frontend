49da9394159679b80cc0ab436cef589a1a14b8fe
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-01-10 15:19:57 -0800

    glimmerize SessionOverview component.
    refactors publication menu out into its own subcomponent.
    adds Gte (Greater Than or Equal) validator.

--- tests/integration/components/session-publicationcheck-test.js
+++ tests/integration/components/session-publicationcheck-test.js
@@ -3,6 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { setupMirage } from 'ember-cli-mirage/test-support';
+import { setupAuthentication } from 'ilios-common';
 
 module('Integration | Component | session-publicationcheck', function(hooks) {
   setupRenderingTest(hooks);
@@ -12,9 +13,13 @@ module('Integration | Component | session-publicationcheck', function(hooks) {
     const parent = this.server.create('objective');
     const objective1 = this.server.create('objective', { parents: [ parent ] });
     const objective2 = this.server.create('objective');
+    const school = this.server.create('school');
+    const course = this.server.create('course', { school });
     const session = this.server.create('session', {
       objectives: [ objective1, objective2 ],
+      course
     });
+    await setupAuthentication({ school, administeredSchools: [ school ]});
     const sessionModel = await this.owner.lookup('service:store').find('session', session.id);
     this.set('model', sessionModel);
     await render(hbs`<SessionPublicationcheck @session={{model}} />`);
@@ -25,9 +30,13 @@ module('Integration | Component | session-publicationcheck', function(hooks) {
     const parent = this.server.create('objective');
     const objective1 = this.server.create('objective', { parents: [ parent ] });
     const objective2 = this.server.create('objective', { parents: [ parent ] });
+    const school = this.server.create('school');
+    const course  = this.server.create('course', { school });
     const session = this.server.create('session', {
       objectives: [ objective1, objective2 ],
+      course
     });
+    await setupAuthentication({ school, administeredSchools: [ school ]});
     const sessionModel = await this.owner.lookup('service:store').find('session', session.id);
     this.set('model', sessionModel);
     await render(hbs`<SessionPublicationcheck @session={{model}} />`);
