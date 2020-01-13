453e9d60492514e9bd7c0667df5637d82f7f0c40
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-01-13 12:08:37 -0800

    uses mirage instead of emberobject for test fixtures.

--- tests/integration/components/session-publicationcheck-test.js
+++ tests/integration/components/session-publicationcheck-test.js
@@ -2,39 +2,34 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import EmberObject from '@ember/object';
-import { resolve } from 'rsvp';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Integration | Component | session-publicationcheck', function(hooks) {
   setupRenderingTest(hooks);
-
-  const parent = EmberObject.create();
-  const objective1 = EmberObject.create({ parents: [parent] });
+  setupMirage(hooks);
 
   test('it shows unlink icon', async function(assert) {
-    const objective2 = EmberObject.create({ parents: [] });
-    const session = EmberObject.create({
-      ilmSession: resolve(),
-      objectives: [objective1, objective2],
-      postrequisite: resolve(),
-      sessionDescription: resolve(),
-      sessionType: resolve()
+    const parent = this.server.create('objective');
+    const objective1 = this.server.create('objective', { parents: [ parent ] });
+    const objective2 = this.server.create('objective');
+    const session = this.server.create('session', {
+      objectives: [ objective1, objective2 ],
     });
-    this.set('model', session);
+    const sessionModel = await this.owner.lookup('service:store').find('session', session.id);
+    this.set('model', sessionModel);
     await render(hbs`<SessionPublicationcheck @session={{model}} />`);
     assert.ok(!!find('.fa-unlink'));
   });
 
   test('it does not shows unlink icon', async function(assert) {
-    const objective2 = EmberObject.create({ parents: [parent] });
-    const session = EmberObject.create({
-      ilmSession: resolve(),
-      objectives: [objective1, objective2],
-      postrequisite: resolve(),
-      sessionDescription: resolve(),
-      sessionType: resolve()
+    const parent = this.server.create('objective');
+    const objective1 = this.server.create('objective', { parents: [ parent ] });
+    const objective2 = this.server.create('objective', { parents: [ parent ] });
+    const session = this.server.create('session', {
+      objectives: [ objective1, objective2 ],
     });
-    this.set('model', session);
+    const sessionModel = await this.owner.lookup('service:store').find('session', session.id);
+    this.set('model', sessionModel);
     await render(hbs`<SessionPublicationcheck @session={{model}} />`);
     assert.notOk(!!find('.fa-unlink'));
   });
