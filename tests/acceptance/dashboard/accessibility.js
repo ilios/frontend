344961d904db368b1ec9198be2227f6662a413ec
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-01-13 16:03:37 -0800

    Activate a11y tests for the whole dashboard!
    All of the individual pieces are in place, we can start testing the
    entire thing together.

---
+++ tests/acceptance/dashboard/accessibility.js
@@ -0,0 +1,23 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
+import setupAuthentication from 'ilios/tests/helpers/setup-authentication';
+
+module('Acceptance | dashboard accessibility', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(async function () {
+    const school = this.server.create('school');
+    await setupAuthentication( { school } );
+  });
+
+  test('meets a11y standards in default view', async function(assert) {
+    await visit('/dashboard');
+    assert.equal(currentURL(), '/dashboard');
+    await a11yAudit();
+    assert.ok(true);
+  });
+});
