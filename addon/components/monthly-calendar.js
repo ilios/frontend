aec5f1a14218c2c1fd10cf431932b73b05155629
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-01-31 14:21:31 -0800

    Add features from old calendar
    Added monthly events with their color coded blocks, the show more link,
    clicking a day moves to that day and then title of the Month/Year on the
    top.

--- addon/components/monthly-calendar.js
+++ addon/components/monthly-calendar.js
@@ -10,8 +10,12 @@ export default class MonthlyCalendarComponent extends Component {
     return this.args.events.sortBy('startDate', 'endDate', 'name');
   }
 
+  get firstDayOfMonth() {
+    return moment(this.args.date).startOf('month');
+  }
+
   get eventDays() {
-    const date = moment(this.args.date).startOf('month');
+    const date = this.firstDayOfMonth;
     const lastDayOfMonth = moment(this.args.date).endOf('month');
     const firstDayOfWeek = date.clone().startOf('week');
     const offset = date.diff(firstDayOfWeek, 'days');
@@ -19,6 +23,7 @@ export default class MonthlyCalendarComponent extends Component {
 
     while (date < lastDayOfMonth) {
       const day = {
+        date: date.toDate(),
         dayOfMonth: date.date(),
         dayOfWeek: date.day(),
         weekOfMonth: Math.ceil((date.date() + offset) / 7),
