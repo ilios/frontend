987cda6936d87b905bd77ae331fc253c534bd706
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-02-21 22:12:38 -0800

    Octaneify <IliosCalendarDay> component

--- addon/components/ilios-calendar-day.js
+++ addon/components/ilios-calendar-day.js
@@ -1,52 +1,33 @@
-import Component from '@ember/component';
-import { run } from '@ember/runloop';
-import { computed } from '@ember/object';
-import { isEmpty } from '@ember/utils';
+import Component from '@glimmer/component';
 import moment from 'moment';
 
-export default Component.extend({
-  classNames: ['ilios-calendar-day'],
-  date: null,
-  calendarEvents: null,
-  areEventsSelectable: true,
-  ilmPreWorkEvents: computed('calendarEvents.[]', function () {
-    const calendarEvents = this.calendarEvents || [];
-    const preWork =  calendarEvents.reduce((arr, eventObject) => {
+export default class IliosCalendarDayComponent extends Component {
+  get ilmPreWorkEvents() {
+    const preWork =  this.args.calendarEvents.reduce((arr, eventObject) => {
       return arr.pushObjects(eventObject.prerequisites);
     }, []);
 
     return preWork.filter(ev => ev.ilmSession);
-  }),
+  }
 
-  nonIlmPreWorkEvents: computed('calendarEvents.[]', function () {
-    const calendarEvents = this.calendarEvents || [];
-    return calendarEvents.filter(ev => {
+  get nonIlmPreWorkEvents() {
+    return this.args.calendarEvents.filter(ev => {
       return ev.postrequisites.length === 0 || !ev.ilmSession;
     });
-  }),
-  singleDayEvents: computed('nonIlmPreWorkEvents.[]', function(){
-    const events = this.get('nonIlmPreWorkEvents');
-    if(isEmpty(events)){
-      return [];
-    }
-    return events.filter(
+  }
+
+  get singleDayEvents(){
+    return this.nonIlmPreWorkEvents.filter(
       event => moment(event.startDate).isSame(moment(event.endDate), 'day')
     );
-  }),
-  multiDayEvents: computed('nonIlmPreWorkEvents.[]', function(){
-    const events = this.get('nonIlmPreWorkEvents');
-    if(isEmpty(events)){
-      return [];
-    }
-    return events.filter(
+  }
+  multiDayEvents(){
+    return this.nonIlmPreWorkEvents.filter(
       event => !moment(event.startDate).isSame(moment(event.endDate), 'day')
     );
-  }),
-  didInsertElement(){
-    run.next(() => {
-      if (!this.isDestroyed && !this.isDestroying && this.element) {
-        this.element.querySelector(".el-calendar .week").scrollTop = 500;
-      }
-    });
-  },
-});
+  }
+
+  scroll(element){
+    element.querySelector(".el-calendar .week").scrollTop = 500;
+  }
+}
