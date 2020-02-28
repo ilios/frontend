aa0eb91ce1639ad6fc8d8aa38c14ac31d5864605
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-02-28 12:33:20 -0800

    miragify integration test.

--- tests/integration/components/learnergroup-instructor-manager-test.js
+++ tests/integration/components/learnergroup-instructor-manager-test.js
@@ -1,72 +1,63 @@
-import EmberObject from '@ember/object';
-import RSVP from 'rsvp';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import {
-  render,
-  settled,
-  click,
-  findAll
-} from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-
-const { resolve } = RSVP;
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Integration | Component | learnergroup instructor manager', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(async function() {
+    const school = this.server.create('school');
+    const program = this.server.create('program', { school });
+    const programYear = this.server.create('programYear', { program });
+    this.cohort = this.server.create('cohort', { programYear });
+    this.school = school;
+  });
 
   test('it renders', async function(assert) {
-    const learnerGroup = EmberObject.create({
+    assert.expect(3);
+    const instructor =  this.server.create('user', { firstName: 'test', lastName: 'person', middleName: '' });
+    const instructorGroup = this.server.create('instructorGroup', { title: 'test group'});
+    const learnerGroup = this.server.create('learnerGroup', {
       title: 'this group',
-      instructors: resolve([
-        EmberObject.create({
-          fullName: 'test person'
-        })
-      ]),
-      instructorGroups: resolve([EmberObject.create({
-        title: 'test group'
-      })]),
+      cohort: this.cohort,
+      instructors: [ instructor ],
+      instructorGroups: [ instructorGroup ]
     });
 
+    const learnerGroupModel = await this.owner.lookup('service:store').find('learnerGroup', learnerGroup.id);
+
     this.set('nothing', parseInt);
-    this.set('learnerGroup', learnerGroup);
+    this.set('learnerGroup', learnerGroupModel);
 
     await render(hbs`<LearnergroupInstructorManager
-      @learnerGroup={{learnerGroup}}
-      @save={{action nothing}}
-      @close={{action nothing}}
+      @learnerGroup={{this.learnerGroup}}
+      @save={{action this.nothing}}
+      @close={{action this.nothing}}
     />`);
 
-    assert.dom('li').hasText('test person |');
-    assert.dom(findAll('li')[1]).hasText('test group |');
+    assert.dom('.removable-instructors li').exists({ count: 2 });
+    assert.dom('.removable-instructors li:nth-of-type(1)').hasText('test person');
+    assert.dom('.removable-instructors li:nth-of-type(2)').hasText('test group');
   });
 
   test('can remove groups', async function(assert) {
     assert.expect(5);
-    const group1 = 'li:nth-of-type(3)';
-    const group2 = 'li:nth-of-type(4)';
-    const saveButton = 'button.bigadd';
-    const learnerGroup = EmberObject.create({
+    const instructor =  this.server.create('user', { firstName: 'test', lastName: 'person', middleName: '' });
+    const instructor2 =  this.server.create('user', { firstName: 'test', lastName: 'person 2', middleName: '' });
+    const instructorGroup = this.server.create('instructorGroup', { title: 'test group'});
+    const instructorGroup2 = this.server.create('instructorGroup', { title: 'test group 2'});
+
+    const learnerGroup = this.server.create('learnerGroup', {
       title: 'this group',
-      instructors: resolve([
-        EmberObject.create({
-          fullName: 'test person'
-        }),
-        EmberObject.create({
-          fullName: 'test person 2'
-        })
-      ]),
-      instructorGroups: resolve([
-        EmberObject.create({
-          id: 42,
-          title: 'test group'
-        }),
-        EmberObject.create({
-          id: 'beans',
-          title: 'test group 2'
-        }),
-      ]),
+      cohort: this.cohort,
+      instructors: [ instructor, instructor2 ],
+      instructorGroups: [ instructorGroup, instructorGroup2 ]
     });
+
+    const learnerGroupModel = await this.owner.lookup('service:store').find('learnerGroup', learnerGroup.id);
 
     this.set('nothing', parseInt);
     this.set('save', (users, groups) => {
@@ -74,7 +65,7 @@ module('Integration | Component | learnergroup instructor manager', function(hoo
       assert.equal(groups.length, 1);
       assert.equal(groups[0].get('title'), 'test group 2');
     });
-    this.set('learnerGroup', learnerGroup);
+    this.set('learnerGroup', learnerGroupModel);
 
     await render(hbs`<LearnergroupInstructorManager
       @learnerGroup={{learnerGroup}}
@@ -82,48 +73,36 @@ module('Integration | Component | learnergroup instructor manager', function(hoo
       @close={{action nothing}}
     />`);
 
-    assert.dom(group1).hasText('test group |');
-    assert.dom(group2).hasText('test group 2 |');
+    assert.dom('.removable-instructors li:nth-of-type(3)').hasText('test group');
+    assert.dom('.removable-instructors li:nth-of-type(4)').hasText('test group 2');
 
-    await click(group1);
-
-    return settled(await click(saveButton));
+    await click('.removable-instructors li:nth-of-type(3)');
+    await click('button.bigadd');
   });
 
   test('can remove users', async function(assert) {
     assert.expect(5);
-    const user1 = 'li:nth-of-type(1)';
-    const user2 = 'li:nth-of-type(2)';
-    const saveButton = 'button.bigadd';
-    const learnerGroup = EmberObject.create({
+    const instructor =  this.server.create('user', { firstName: 'test', lastName: 'person', middleName: '' });
+    const instructor2 =  this.server.create('user', { firstName: 'test', lastName: 'person 2', middleName: '' });
+    const instructorGroup = this.server.create('instructorGroup', { title: 'test group'});
+    const instructorGroup2 = this.server.create('instructorGroup', { title: 'test group 2'});
+
+    const learnerGroup = this.server.create('learnerGroup', {
       title: 'this group',
-      instructors: resolve([
-        EmberObject.create({
-          fullName: 'test person'
-        }),
-        EmberObject.create({
-          fullName: 'test person 2'
-        })
-      ]),
-      instructorGroups: resolve([
-        EmberObject.create({
-          id: 42,
-          title: 'test group'
-        }),
-        EmberObject.create({
-          id: 'beans',
-          title: 'test group 2'
-        }),
-      ]),
+      cohort: this.cohort,
+      instructors: [ instructor, instructor2 ],
+      instructorGroups: [ instructorGroup, instructorGroup2 ]
     });
+
+    const learnerGroupModel = await this.owner.lookup('service:store').find('learnerGroup', learnerGroup.id);
 
     this.set('nothing', parseInt);
     this.set('save', (users, groups) => {
       assert.equal(users.length, 1);
-      assert.equal(users[0].get('fullName'), 'test person 2');
       assert.equal(groups.length, 2);
+      assert.equal(users[0].get('fullName'), 'test person 2');
     });
-    this.set('learnerGroup', learnerGroup);
+    this.set('learnerGroup', learnerGroupModel);
 
     await render(hbs`<LearnergroupInstructorManager
       @learnerGroup={{learnerGroup}}
@@ -131,12 +110,11 @@ module('Integration | Component | learnergroup instructor manager', function(hoo
       @close={{action nothing}}
     />`);
 
-    assert.dom(user1).hasText('test person |');
-    assert.dom(user2).hasText('test person 2 |');
+    assert.dom('.removable-instructors li:nth-of-type(1)').hasText('test person');
+    assert.dom('.removable-instructors li:nth-of-type(2)').hasText('test person 2');
 
-    await click(user1);
-
-    return settled(await click(saveButton));
+    await click('.removable-instructors li:nth-of-type(1)');
+    await click('button.bigadd');
   });
 
   test('it closes', async function(assert) {
