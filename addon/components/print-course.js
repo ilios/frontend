b03c614d1c68fd4b773f24e0064f5075416a8b69
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-03-15 22:51:52 -0700

    Octaniefy <PrintCourse> Component

--- addon/components/print-course.js
+++ addon/components/print-course.js
@@ -1,78 +1,29 @@
-import { sort } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { Promise as RSVPPromise } from 'rsvp';
-import ObjectProxy from '@ember/object/proxy';
-import SortableByPosition from 'ilios-common/mixins/sortable-by-position';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { dropTask } from 'ember-concurrency-decorators';
+import sortableByPosition from 'ilios-common/utils/sortable-by-position';
 
-export default Component.extend(SortableByPosition, {
-  store: service(),
-  course: null,
-  includeUnpublishedSessions: false,
-  tagName: 'section',
-  classNames: ['print-course'],
-  sortTitle: null,
-  sortDirectorsBy: null,
-  sortedDirectors: sort('course.directors', 'sortDirectorsBy'),
-  sortedMeshDescriptors: sort('course.meshDescriptors', 'sortTitle'),
+export default class PrintCourseComponent extends Component {
+  @service store;
 
-  /**
-   * A list of proxied course sessions, sorted by title.
-   * @property sortedSessionProxies
-   * @type {Ember.computed}
-   * @public
-   */
-  sortedSessionProxies: computed('course.sessions.[]', 'includeUnpublishedSessions', function(){
-    return new RSVPPromise(resolve => {
+  @tracked sortTitle;
+  @tracked sortDirectorsBy;
+  @tracked sessions = [];
+  @tracked courseLearningMaterials = [];
 
-      const course = this.get('course');
-      if(!course){
-        resolve([]);
-        return;
-      }
+  @dropTask
+  *load(event, [course]) {
+    if (!course) {
+      return;
+    }
+    this.courseLearningMaterials = (yield course.learningMaterials).toArray().sort(sortableByPosition);
 
-      const SessionProxy = ObjectProxy.extend({
-        sortTitle: null,
-        sortedMeshDescriptors: sort('content.meshDescriptors', 'sortTitle'),
-        sessionLearningMaterials: computed('content', function(){
-          return new RSVPPromise(resolve => {
-            this.content.get('learningMaterials').then(learningMaterials => {
-              resolve(learningMaterials.toArray().sort(this.get('positionSortingCallback')));
-            });
-          });
-        }),
-        init() {
-          this._super(...arguments);
-          this.set('sortTitle', ['title']);
-        },
-      });
-      course.get('sessions').then(sessions => {
-        if (!this.get('includeUnpublishedSessions')) {
-          sessions = sessions.filterBy('isPublishedOrScheduled');
-        }
-        const proxiedSessions = sessions.map(function(session){
-          return SessionProxy.create({
-            content: session
-          });
-        });
-        resolve(proxiedSessions.sortBy('title'));
-      });
+    let sessions = yield course.sessions;
+    if (!this.args.includeUnpublishedSessions) {
+      sessions = sessions.filterBy('isPublishedOrScheduled');
+    }
 
-    });
-  }),
-
-  courseLearningMaterials: computed('course', function(){
-    return new RSVPPromise(resolve => {
-      this.course.get('learningMaterials').then(learningMaterials => {
-        resolve(learningMaterials.toArray().sort(this.get('positionSortingCallback')));
-      });
-    });
-  }),
-
-  init(){
-    this._super(...arguments);
-    this.set('sortTitle', ['title']);
-    this.set('sortDirectorsBy', ['lastName', 'firstName']);
-  },
-});
+    this.sessions = sessions;
+  }
+}
