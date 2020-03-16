41b9b4667d5439a9bff6db4b2d3e9d22f190475d
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-03-15 22:57:20 -0700

    Octaneify <ProgressBar> Component

--- addon/components/progress-bar.js
+++ addon/components/progress-bar.js
@@ -1,14 +1,10 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
 import { htmlSafe } from '@ember/string';
 
-export default Component.extend({
-  classNames: ['progress-bar'],
-  percentage: 0,
-  widthStyle: computed('percentage', function(){
-    const percentage = this.get('percentage');
-    const str = `width: ${percentage}%`;
+export default class ProgressBarComponent extends Component {
+  get widthStyle(){
+    const str = `width: ${this.args.percentage}%`;
 
     return htmlSafe(str);
-  }),
-});
+  }
+}
