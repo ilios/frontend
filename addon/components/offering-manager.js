54ee73cedfcfb2ea93d46c39ff36c2e47ae036d0
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-03-15 22:26:28 -0700

    Octaneify <OfferingManager> component

--- addon/components/offering-manager.js
+++ addon/components/offering-manager.js
@@ -1,41 +1,15 @@
-import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { action, computed } from '@ember/object';
-import { isEmpty } from '@ember/utils';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  currentUser: service(),
-  classNameBindings: [':offering-manager', 'showRemoveConfirmation'],
-  offering: null,
-  editable: true,
-
-  isEditing: false,
-  showRemoveConfirmation: false,
-  'data-test-offering-manager': true,
-
-  userCanDelete: computed('editable', 'offering.session.course', 'offering.allInstructors.[]', 'currentUser.model.directedCourses.[]', async function(){
-    const offering = this.get('offering');
-    if (isEmpty(offering)) {
-      return false;
-    }
-    return this.get('editable');
-  }),
+export default class OfferingManagerComponent extends Component {
+  @tracked isEditing =  false;
+  @tracked showRemoveConfirmation = false;
 
   @action
   save(startDate, endDate, room, learnerGroups, instructorGroups, instructors){
-    const offering = this.get('offering');
-    offering.setProperties({startDate, endDate, room, learnerGroups, instructorGroups, instructors});
+    this.args.offering.setProperties({startDate, endDate, room, learnerGroups, instructorGroups, instructors});
 
-    return offering.save();
-  },
-
-  @action
-  cancelRemove() {
-    this.set('showRemoveConfirmation', false);
-  },
-
-  @action
-  confirmRemove() {
-    this.set('showRemoveConfirmation', true);
-  },
-});
+    return this.args.offering.save();
+  }
+}
