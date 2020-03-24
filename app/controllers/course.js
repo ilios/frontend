75971c7abff4f26b087883463755f6d89144d781
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-03-23 23:31:06 -0700

    Remove controllers we get directly from common

--- app/controllers/course.js
+++
@@ -1,4 +0,0 @@
-import Controller from '@ember/controller';
-import CourseController from 'ilios-common/mixins/course-controller';
-
-export default Controller.extend(CourseController);
--- app/controllers/session.js
+++
@@ -1,4 +0,0 @@
-import Controller from '@ember/controller';
-import SessionController from 'ilios-common/mixins/session-controller';
-
-export default Controller.extend(SessionController);
