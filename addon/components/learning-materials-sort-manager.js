4957b3a5686976293fd0b4511cac4d44deb1212d
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-03-30 22:30:11 -0700

    Octaneify LearningMaterialSortManager component

--- addon/components/learning-materials-sort-manager.js
+++ addon/components/learning-materials-sort-manager.js
@@ -1,23 +1,18 @@
-import Component from '@ember/component';
-import SortableByPosition from 'ilios-common/mixins/sortable-by-position';
-import { task } from 'ember-concurrency';
+import Component from '@glimmer/component';
+import { dropTask, restartableTask } from 'ember-concurrency-decorators';
+import { tracked } from '@glimmer/tracking';
+import sortableByPosition from 'ilios-common/utils/sortable-by-position';
 
-export default Component.extend(SortableByPosition, {
-  classNames: ['learning-materials-sort-manager'],
-  sortableObjectList: null,
-  subject: null,
-  'data-test-detail-learning-materials-sort-manager': true,
+export default class LearningMaterialSortManagerComponent extends Component {
+  @tracked sortableObjectList;
 
-  didReceiveAttrs() {
-    this._super(...arguments);
-    const subject = this.get('subject');
-    this.get('loadAttr').perform(subject);
-  },
-  loadAttr: task(function * (subject) {
-    const learningMaterials = yield subject.get('learningMaterials');
-    this.set('sortableObjectList', learningMaterials.toArray().sort(this.get('positionSortingCallback')));
-  }),
-  callSave: task(function* () {
-    yield this.save(this.get('sortableObjectList'));
-  }),
-});
+  @restartableTask
+  *load(element, [subject]) {
+    const learningMaterials = (yield subject.learningMaterials).toArray();
+    this.sortableObjectList = learningMaterials.sort(sortableByPosition);
+  }
+  @dropTask
+  *callSave() {
+    yield this.args.save(this.sortableObjectList);
+  }
+}
