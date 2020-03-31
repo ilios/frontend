740a324bd4cbe3997fee9cfe741efc3ce73643f3
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-03-30 22:48:15 -0700

    Octaneify <ObjectiveSortManager> component

--- addon/components/objective-sort-manager.js
+++ addon/components/objective-sort-manager.js
@@ -1,22 +1,14 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { restartableTask } from 'ember-concurrency-decorators';
 import sortableByPosition from 'ilios-common/utils/sortable-by-position';
-import { task } from 'ember-concurrency';
 
-export default Component.extend({
-  classNames: ['objective-sort-manager'],
-  sortableObjectList: null,
-  subject: null,
+export default class ObjectiveSortManagerComponent extends Component {
+  @tracked sortableObjectList;
 
-  didReceiveAttrs() {
-    this._super(...arguments);
-    const subject = this.get('subject');
-    this.get('loadAttr').perform(subject);
-
-  },
-
-  loadAttr: task(function * (subject) {
-    const objectives = yield subject.get('objectives');
-    this.set('sortableObjectList', objectives.toArray().sort(sortableByPosition));
-  }),
-
-});
+  @restartableTask
+  *load(element, [subject]) {
+    const objectives = (yield subject.objectives).toArray();
+    this.sortableObjectList = objectives.sort(sortableByPosition);
+  }
+}
