53de71a5ebd02d0b150f060db1c1ff953ef48960
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-04-05 23:46:51 -0700

    Rely on routes from common
    Common is providing routes now instead of mixins, this works great for
    everything but the dashboard. Since we override the dashboard.hbs
    template we have to provide the route here.

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,5 +1,38 @@
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import DashboardRoute from 'ilios-common/mixins/dashboard-route';
+import { hash } from 'rsvp';
 
-export default Route.extend(DashboardRoute, AuthenticatedRouteMixin);
+export default class DashboardRoute extends Route.extend(AuthenticatedRouteMixin) {
+  @service store;
+  @service currentUser;
+
+  async model() {
+    return hash({
+      schools: this.store.findAll('school'),
+      academicYears: this.store.findAll('academic-year'),
+    });
+  }
+
+  async afterModel() {
+    const user = await this.currentUser.getModel();
+    const school = await user.school;
+    const p1 = this.store.query('session-type', {
+      filters: {
+        school: school.id,
+      },
+    });
+    const p2 = this.store.query('vocabulary', {
+      filters: {
+        school: school.id,
+      },
+    });
+    const p3 = this.store.query('term', {
+      filters: {
+        schools: [school.id],
+      },
+    });
+
+    return Promise.all([p1, p2, p3]);
+  }
+}
