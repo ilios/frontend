575f5a7d7930eec143b953a84a4dcfc6f9afb9f8
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-04-08 11:45:53 -0700

    Remove route mixins

--- addon/mixins/session-route.js
+++
@@ -1,9 +0,0 @@
-import Mixin from '@ember/object/mixin';
-
-export default Mixin.create({
-  async afterModel(model) {
-    const store = this.get('store');
-    const course = await model.get('course');
-    await store.query('session', { filters: { course: course.get('id') } });
-  }
-});
