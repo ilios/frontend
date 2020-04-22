845a325c6540e3acab4269558d8812b6ce4a8521
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-04-21 23:33:19 -0700

    Copy object directly
    Ember.Copy is deprecated and we don't need it here to do a shallow copy of an event object which is a POJO.

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -4,7 +4,6 @@ window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
     { handler: "silence", matchId: "computed-property.override"}, //ember-modal
-    { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable"},
     { handler: "silence", matchId: "computed-property.volatile"},
     { handler: "silence", matchId: "common.async-computed"},
     { handler: "silence", matchId: "ember-cli-page-object.old-finders"},
