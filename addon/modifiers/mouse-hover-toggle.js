7fe4751f034f49655968edd893000a6890c48710
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-04-30 14:27:55 -0700

    Add modifier for mouse hover toggle
    Only need one modifier this way for on and off which makes adding tooltip easier.

---
+++ addon/modifiers/mouse-hover-toggle.js
@@ -0,0 +1,13 @@
+import { modifier } from 'ember-modifier';
+
+export default modifier(function mouseHoverToggle(element, [setter]) {
+  const handleMouseOver = () => setter(true);
+  const handleMouseOut = () => setter(false);
+  element.addEventListener('mouseover', handleMouseOver);
+  element.addEventListener('mouseout', handleMouseOut);
+
+  return () => {
+    element.removeEventListener('mouseover', handleMouseOver);
+    element.removeEventListener('mouseout', handleMouseOut);
+  };
+});
