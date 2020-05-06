21e6a6c402aee21dac387ab56fc45a2c7a1a5ee9
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-05-06 14:00:19 -0700

    corrects location of test.

--- tests/integration/components/objective-list-item-terms-test.js
+++ tests/integration/components/objective-list-item-terms-test.js
@@ -5,13 +5,13 @@ import { hbs } from 'ember-cli-htmlbars';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
-module('Integration | Component | session/objective-list-item-terms', function(hooks) {
+module('Integration | Component | objective-list-item-terms', function(hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
   skip('it renders and is A11Y compliant ', async function(assert) {
     assert.expect(0);
-    await render(hbs`<Session::ObjectiveListItemTerms`);
+    await render(hbs`<ObjectiveListItemTerms>`);
     await a11yAudit(this.element);
     assert.ok(true, 'no a11y errors found!');
   });
