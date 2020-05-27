3c4ccdad502b8ca31cadaa95926f088496c6a967
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-05-27 09:25:00 -0700

    realigns linter rules with frontend.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,55 +2,17 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
-  },
-  plugins: [
-    'ember'
-  ],
   extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
+    '@ilios/eslint-config-ember'
   ],
-  env: {
-    browser: true
-  },
   rules: {
-    'ember/no-jquery': 'error'
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-        "node/no-extraneous-require": ["error", {
-            "allowModules": ["ember-auto-import", "postcss-scss"]
-        }],
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      })
-    }
-  ]
+    'ember/use-ember-get-and-set': 0,
+    'ember/no-jquery': 0,
+    'ember/use-brace-expansion': 0,
+    'ember/no-new-mixins': 0,
+    'ember/no-capital-letters-in-routes': 0,
+    'ember/no-unnecessary-route-path-option': 0,
+    'ember/routes-segments-snake-case': 0,
+    'sort-imports': 0
+  }
 };
