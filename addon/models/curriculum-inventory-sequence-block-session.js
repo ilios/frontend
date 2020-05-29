43471a8f1f72ff8bf4432baf15f345ef9dd0addb
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-05-29 08:54:36 -0700

    Remove curriculum-inventory-sequence-block-session model
    This appears to be cruft and isn't in use anywhere nor is it backed by
    an API endpoint.

--- addon/models/curriculum-inventory-sequence-block-session.js
+++
@@ -1,7 +0,0 @@
-import Model, { belongsTo, attr } from '@ember-data/model';
-
-export default Model.extend({
-  countOfferingsOnce: attr('boolean'),
-  sequenceBlock: belongsTo('curriculum-inventory-sequence-block', {async: true}),
-  session: belongsTo('session', {async: true}),
-});
