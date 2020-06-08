82329d59e3e08b4c5b931e4dd0414d8f6154b2b3
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-06-07 22:42:49 -0700

    Use JSON:API in mirage
    This causes data coming from mirage to look exactly like it does in our
    API.

--- addon-mirage-support/serializers/application.js
+++ addon-mirage-support/serializers/application.js
@@ -1,6 +1,14 @@
-import { RestSerializer } from 'ember-cli-mirage';
+import { JSONAPISerializer } from 'ember-cli-mirage';
+import { camelize } from '@ember/string';
 
-export default RestSerializer.extend({
+export default JSONAPISerializer.extend({
   serializeIds: 'always',
   normalizeIds: 'always',
+  alwaysIncludeLinkageData: true,
+  keyForAttribute(key) {
+    return camelize(key);
+  },
+  keyForRelationship(key) {
+    return camelize(key);
+  },
 });
