89fe7f504577e9738fcaa6f9dd9b3950a9281683
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-06-23 12:10:46 -0700

    Cleanup LearnergroupSummary Test
    Use mirage models instead of mocks to ensure that sort-by works on the
    component.

--- tests/integration/components/learnergroup-summary-test.js
+++ tests/integration/components/learnergroup-summary-test.js
@@ -1,75 +1,59 @@
-import EmberObject from '@ember/object';
-import RSVP from 'rsvp';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import {
   render,
-  settled,
   click,
   fillIn
 } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-
-const { resolve } = RSVP;
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Integration | Component | learnergroup summary', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-  test('renders with data', async function(assert) {
-    const user1 = EmberObject.create({
-      fullName: 'user1'
-    });
-    const user2 = EmberObject.create({
-      fullName: 'user2'
-    });
-    const user3 = EmberObject.create({
-      fullName: 'user3'
-    });
-    const user4 = EmberObject.create({
-      fullName: 'user4'
-    });
-    const user5 = EmberObject.create({
-      fullName: 'user5'
-    });
-    const user6 = EmberObject.create({
-      fullName: 'user6'
-    });
+  test('renders with data', async function (assert) {
+    const user1 = this.server.create('user');
+    const user2 = this.server.create('user');
+    const user3 = this.server.create('user');
+    const user4 = this.server.create('user');
+    const user5 = this.server.create('user');
+    const user6 = this.server.create('user');
 
-    const cohort = EmberObject.create({
+    const cohort = this.server.create('cohort', {
       title: 'this cohort',
-      users: resolve([user1, user2, user3, user4]),
+      users: [user1, user2, user3, user4],
     });
-    const subGroup = EmberObject.create({
+    const subGroup = this.server.create('learner-group', {
       title: 'test sub-group',
-      users: resolve([]),
-      children: resolve([]),
     });
-    const learnerGroup = EmberObject.create({
+
+    const course = this.server.create('course');
+    const session = this.server.create('session', { course });
+    const offering = this.server.create('offering', { session });
+
+    const course2 = this.server.create('course');
+    const session2 = this.server.create('session', { course: course2 });
+    const ilm = this.server.create('ilm-session', { session: session2 });
+
+    const learnerGroup = this.server.create('learner-group', {
       title: 'test group',
       location: 'test location',
-      children: resolve([subGroup]),
-      allInstructors: resolve([user5, user6]),
-      users: resolve([user1, user2]),
-      allDescendantUsers: resolve([user1, user2]),
-      usersOnlyAtThisLevel: resolve([user1, user2]),
-      courses: resolve([
-        {title: 'test course 1'},
-        {title: 'test course 2'},
-      ]),
-      cohort: resolve(cohort),
-      allDescendants: resolve([subGroup]),
+      children: [subGroup],
+      instructors: [user5, user6],
+      users: [user1, user2],
+      offerings: [offering],
+      ilmSessions: [ilm],
+      cohort,
     });
-    learnerGroup.set('topLevelGroup', resolve(learnerGroup));
-    user1.set('getLowestMemberGroupInALearnerGroupTree', () => resolve(learnerGroup));
-    user2.set('getLowestMemberGroupInALearnerGroupTree', () => resolve(learnerGroup));
+    const learnerGroupModel = await this.owner.lookup('service:store').find('learner-group', learnerGroup.id);
 
-    this.set('nothing', parseInt);
-    this.set('learnerGroup', learnerGroup);
+    this.set('learnerGroup', learnerGroupModel);
 
     await render(hbs`<LearnergroupSummary
-      @setIsEditing={{action nothing}}
-      @setSortUsersBy={{action nothing}}
-      @setIsBulkAssigning={{action nothing}}
+      @setIsEditing={{noop}}
+      @setSortUsersBy={{noop}}
+      @setIsBulkAssigning={{noop}}
       @sortUsersBy="firstName"
       @learnerGroup={{learnerGroup}}
       @isEditing={{false}}
@@ -78,56 +62,29 @@ module('Integration | Component | learnergroup summary', function(hooks) {
 
     const defaultLocation = '.learnergroup-overview .defaultlocation span:nth-of-type(1)';
     const instructors = '.learnergroup-overview .defaultinstructors span';
-    const courses = '.learnergroup-overview .associatedcourses div';
+    const coursesList = '.learnergroup-overview .associatedcourses div';
 
-    return settled().then(()=>{
-      assert.dom(defaultLocation).hasText('test location');
-      assert.dom(instructors).hasText('user5; user6');
-      assert.dom(courses).hasText('test course 1; test course 2');
-    });
+    assert.dom(defaultLocation).hasText('test location');
+    assert.dom(instructors).hasText('4 guy M. Mc4son; 5 guy M. Mc5son');
+    assert.dom(coursesList).hasText('course 0; course 1');
   });
 
   test('Update location', async function(assert) {
-    assert.expect(3);
+    assert.expect(2);
 
-    const cohort = EmberObject.create({
-      title: 'this cohort',
-      users: resolve([]),
-    });
-    const subGroup = EmberObject.create({
-      title: 'test sub-group',
-      users: resolve([]),
-      children: resolve([]),
-    });
-    const learnerGroup = EmberObject.create({
-      title: 'test group',
+    const cohort = this.server.create('cohort');
+    const learnerGroup = this.server.create('learner-group', {
       location: 'test location',
-      children: resolve([subGroup]),
-      allInstructors: resolve([]),
-      users: resolve([]),
-      allDescendantUsers: resolve([]),
-      usersOnlyAtThisLevel: resolve([]),
-      courses: resolve([
-        {title: 'test course 1'},
-        {title: 'test course 2'},
-      ]),
-      cohort: resolve(cohort),
-      allDescendants: resolve([subGroup]),
-      save(){
-        assert.equal(this.get('location'), 'new location name');
-        return resolve(this);
-      },
+      cohort,
     });
-    learnerGroup.set('topLevelGroup', resolve(learnerGroup));
-
-    this.set('nothing', parseInt);
-    this.set('learnerGroup', learnerGroup);
+    const learnerGroupModel = await this.owner.lookup('service:store').find('learner-group', learnerGroup.id);
+    this.set('learnerGroup', learnerGroupModel);
 
     await render(hbs`<LearnergroupSummary
       @canUpdate={{true}}
-      @setIsEditing={{action nothing}}
-      @setSortUsersBy={{action nothing}}
-      @setIsBulkAssigning={{action nothing}}
+      @setIsEditing={{noop}}
+      @setSortUsersBy={{noop}}
+      @setIsBulkAssigning={{noop}}
       @learnerGroup={{learnerGroup}}
       @isEditing={{false}}
       @isBulkAssigning={{false}}
