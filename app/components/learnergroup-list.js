0d1b7d3fc0741e291ecb2cf00980670b5350b47a
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-07-02 11:26:54 -0700

    Octaneify LearnergroupList component

--- app/components/learnergroup-list.js
+++ app/components/learnergroup-list.js
@@ -1,84 +1,57 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency-decorators';
 
-export default Component.extend({
-  tagName: "",
-  bubbleSort: true,
-  canCreate: false,
-  canDelete: false,
-  learnerGroups: null,
-  query: null,
-  sortBy: 'title',
-  copy() {},
-  remove() {},
-  setSortBy() {},
+export default class LearnerGroupListComponent extends Component {
+  @tracked learnerGroupsForCopy = [];
+  @tracked learnerGroupsForRemovalConfirmation = [];
+  @tracked localSortBy = 'title';
 
-  sortedAscending: computed('sortBy', function() {
+  get sortBy() {
+    return this.args.sortBy ?? this.localSortBy;
+  }
+
+  get sortedAscending(){
     return this.sortBy.search(/desc/) === -1;
-  }),
+  }
 
-  sortedLearnerGroups: computed('learnerGroups.[]', 'sortBy', 'sortedAscending', function () {
-    if (!this.learnerGroups) {
-      return [];
-    }
-    let sortBy = this.sortBy;
-    if (sortBy.indexOf(':') !== -1) {
-      sortBy = sortBy.split(':', 1)[0];
-    }
+  @action
+  cancelRemove(learnerGroup) {
+    this.learnerGroupsForRemovalConfirmation = this.learnerGroupsForRemovalConfirmation.filter(lg => lg !== learnerGroup);
+  }
 
-    let sortedLearnerGroups = this.learnerGroups.sortBy(sortBy);
-
-    if (!this.sortedAscending) {
-      sortedLearnerGroups = sortedLearnerGroups.slice().reverse();
-    }
-
-    return sortedLearnerGroups;
-  }),
-
-  init() {
-    this._super(...arguments);
-    this.set('learnerGroupsForCopy', []);
-    this.set('learnerGroupsForRemovalConfirmation', []);
-  },
-
-  actions: {
-    cancelRemove(learnerGroup) {
-      this.learnerGroupsForRemovalConfirmation.removeObject(learnerGroup);
-    },
-
-    confirmRemove(learnerGroup) {
-      const canDelete = this.canDelete;
-      if (canDelete) {
-        this.learnerGroupsForRemovalConfirmation.pushObject(learnerGroup);
-      }
-    },
-
-    cancelCopy(learnerGroup) {
-      this.learnerGroupsForCopy.removeObject(learnerGroup);
-    },
-
-    startCopy(learnerGroup) {
-      this.learnerGroupsForCopy.pushObject(learnerGroup);
-    },
-
-    async copy(withLearners, learnerGroup) {
-      const copy = this.copy;
-      await copy(withLearners, learnerGroup);
-      this.learnerGroupsForCopy.removeObject(learnerGroup);
-    },
-
-    sortBy(what) {
-      const sortBy = this.sortBy;
-
-      if (sortBy === what){
-        what += ':desc';
-      }
-
-      if (this.bubbleSort) {
-        this.setSortBy(what);
-      } else {
-        this.set('sortBy', what);
-      }
+  @action
+  confirmRemove(learnerGroup) {
+    if (this.args.canDelete) {
+      this.learnerGroupsForRemovalConfirmation = [...this.learnerGroupsForRemovalConfirmation, learnerGroup];
     }
   }
-});
+
+  @action
+  cancelCopy(learnerGroup) {
+    this.learnerGroupsForCopy = this.learnerGroupsForCopy.filter(lg => lg !== learnerGroup);
+  }
+
+  @action
+  startCopy(learnerGroup) {
+    this.learnerGroupsForCopy = [...this.learnerGroupsForCopy, learnerGroup];
+  }
+
+  @task
+  *copy(withLearners, learnerGroup) {
+    yield this.args.copy(withLearners, learnerGroup);
+    this.cancelCopy(learnerGroup);
+  }
+
+  @action
+  setSortBy(what) {
+    if(this.sortBy === what){
+      what += ':desc';
+    }
+    if (this.args.setSortBy) {
+      this.args.setSortBy(what);
+    }
+    this.localSortBy = what;
+  }
+}
