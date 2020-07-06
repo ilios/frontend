329d2014e87bdc3566b0f87229711ad037fb9e3c
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-07-05 20:52:07 -0700

    Convert LearnergroupSubgroupList component test to page object pattern
    Then re-use that PO in the learnergroup acceptance test.

--- tests/pages/learner-group.js
+++ tests/pages/learner-group.js
@@ -4,12 +4,12 @@ import {
   collection,
   fillable,
   hasClass,
-  isPresent,
   isVisible,
   text,
   visitable
 } from 'ember-cli-page-object';
 import learnerGroupUserManager from './components/learnergroup-user-manager';
+import learnergroupSubgroupList from './components/learnergroup-subgroup-list';
 
 export default create({
   visit: visitable('/learnergroups/:learnerGroupId'),
@@ -87,57 +87,7 @@ export default create({
     canSubmitFinalData: isVisible('[data-test-finalize-users-submit]'),
     submitFinalData: clickable('[data-test-finalize-users-submit]'),
   },
-  subgroups: {
-    scope: '[data-test-learnergroup-subgroup-list]',
-    title: text('[data-test-title]'),
-    headings: collection('thead th', {
-      title: text(),
-    }),
-    groups: collection('tbody tr', {
-      title: text('td', { at: 0 }),
-      visit: clickable('td:nth-of-type(1) a'),
-      members: text('td', { at: 1 }),
-      subgroups: text('td', { at: 2 }),
-      courses: text('td', { at: 3 }),
-      hasRemoveStyle: hasClass('confirm-removal'),
-      actions: {
-        scope: '[data-test-actions]',
-        canRemove: isPresent('[data-test-remove]'),
-        remove: clickable('[data-test-remove]'),
-        canCopy: isPresent('[data-test-copy]'),
-        copy: clickable('[data-test-copy]'),
-      },
-    }),
-    confirmRemoval: {
-      scope: '[data-test-confirm-removal]',
-      confirm: clickable('[data-test-confirm]'),
-      cancel: clickable('[data-test-cancel]'),
-      confirmation: text('[data-test-confirmation]'),
-    },
-    confirmCopy: {
-      scope: '[data-test-confirm-copy]',
-      confirmWithLearners: clickable('[data-test-confirm-with-learners]'),
-      confirmWithoutLearners: clickable('[data-test-confirm-without-learners]'),
-      canCopyWithLearners: isPresent('[data-test-confirm-with-learners]'),
-      canCopyWithoutLearners: isPresent('[data-test-confirm-without-learners]'),
-    },
-    newForm: {
-      scope: '[data-test-new-learner-group]',
-      title: fillable('[data-test-title]'),
-      save: clickable('.done'),
-      cancel: clickable('.cancel'),
-      isVisible: isVisible(),
-      singleGroupSelected: isPresent('[data-test-first-button][data-test-active]'),
-      multipleGroupSelected: isPresent('[data-test-second-button][data-test-active]'),
-      chooseSingleGroups: clickable('[data-test-first-button]'),
-      chooseMultipleGroups: clickable('[data-test-second-button]'),
-      setNumberOfGroups: fillable('[data-test-number-of-groups]'),
-    },
-    emptyListRowIsVisible: isVisible('[data-test-empty-list]'),
-    savedResult: text('.saved-result'),
-    toggleNewForm: clickable('[data-test-expand-collapse-button] button'),
-    hasNewGroupToggle: isPresent('[data-test-expand-collapse-button]'),
-  },
+  subgroups: learnergroupSubgroupList,
   usersInCohort: {
     scope: '.cohortmembers',
     list: collection('tbody tr', {
