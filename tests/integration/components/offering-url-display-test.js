edad417ecb3e4361479844fbb590d0d42f437ada
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-07-06 12:56:55 -0700

    Add OfferingUrlDisplay Component
    Contains common style and interactions for showing and copying the URL
    for an offering.

---
+++ tests/integration/components/offering-url-display-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | offering-url-display', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders URL', async function(assert) {
+    await render(hbs`<OfferingUrlDisplay @url="https://example.edu" />`);
+
+    assert.equal(this.element.textContent.trim(), 'Virtual Session Link');
+    assert.dom('a').hasAttribute('href', 'https://example.edu');
+  });
+
+  test('it renders nothing when no URL sent', async function(assert) {
+    await render(hbs`<OfferingUrlDisplay />`);
+
+    assert.equal(this.element.textContent, '');
+  });
+});
