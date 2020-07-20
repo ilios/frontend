580facf72ba707ad1f3a24cf6b6ff52ae727230e
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-07-20 08:30:26 -0700

    Update Transitive Dependencies
    Also ignored some new eslint rules temporarily.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,13 @@ module.exports = {
   ],
   rules: {
     'ember/no-side-effects': 0,
+    'ember/no-get': 0,
     'ember/use-ember-get-and-set': 0,
     'ember/use-brace-expansion': 0,
     'ember/no-new-mixins': 0,
+    'ember/no-mixins': 0,
     'sort-imports': 0,
+    'ember/require-computed-property-dependencies': 0,
+    'ember/require-computed-macros': 0,
   }
 };
