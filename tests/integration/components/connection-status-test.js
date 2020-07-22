0e6f21c9cfbabd4d84745c8a3bf265cb696c360c
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-07-21 22:39:35 -0700

    Octaneify ConnectionStatus Component
    Modified to not just hid the contents, but not even render them when
    connected. This prevents an a11y error where focusable buttons were
    inside a hidden element.

--- tests/integration/components/connection-status-test.js
+++ tests/integration/components/connection-status-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
 
 module('Integration | Component | connection status', function(hooks) {
   setupRenderingTest(hooks);
@@ -9,5 +10,8 @@ module('Integration | Component | connection status', function(hooks) {
   test('it renders offline and therefor hidden', async function(assert) {
     await render(hbs`<ConnectionStatus />`);
     assert.dom(this.element).hasNoClass('offline');
+
+    a11yAudit(this.element);
+    assert.ok(true, 'no a11y errors found!');
   });
 });
