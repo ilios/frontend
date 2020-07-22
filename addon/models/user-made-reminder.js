47c95c4cf39ae26a42ed23f8d07cce7fedf0309c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-07-22 16:09:13 -0700

    removes user-made-reminders from data model.

--- addon/models/user-made-reminder.js
+++
@@ -1,9 +0,0 @@
-import Model, { belongsTo, attr } from '@ember-data/model';
-
-export default Model.extend({
-  note: attr('string'),
-  createdAt: attr('date'),
-  dueDate: attr('date'),
-  closed: attr('boolean'),
-  user: belongsTo('user', {async: false}),
-});
