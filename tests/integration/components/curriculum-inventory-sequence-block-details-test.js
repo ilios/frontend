2254dcc92377ce527fdec744e8eed18c0691d3a0
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-08-10 13:21:42 -0700

    octanifies <CurriculumInventorySequenceBlockDetails> component.

--- tests/integration/components/curriculum-inventory-sequence-block-details-test.js
+++ tests/integration/components/curriculum-inventory-sequence-block-details-test.js
@@ -1,100 +1,77 @@
-import RSVP from 'rsvp';
-import EmberObject from '@ember/object';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, settled, findAll } from '@ember/test-helpers';
+import { render, findAll } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import hbs from 'htmlbars-inline-precompile';
 import moment from 'moment';
 import { setupIntl } from 'ember-intl/test-support';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
-const { resolve } = RSVP;
-
+// @todo use a page object here [ST 2020/08/10]
 module('Integration | Component | curriculum inventory sequence block details', function(hooks) {
   setupRenderingTest(hooks);
   setupIntl(hooks);
+  setupMirage(hooks);
 
   test('it renders', async function(assert) {
     assert.expect(7);
 
-    const school = EmberObject.create({ id() { return 1; }});
-
+    const school = this.server.create('school');
     const academicLevels = [];
     for (let i = 0; i < 10; i++) {
-      academicLevels.pushObject(EmberObject.create({ id: i, name: `Year ${i + 1}` }));
+      academicLevels.pushObject(this.server.create('curriculumInventoryAcademicLevel', { name: `Year ${i + 1}` }));
     }
-
-    const program = EmberObject.create({
-      belongsTo() {
-        return school;
-      }
-    });
-
-    const report = EmberObject.create({
-      academicLevels,
-      year: '2016',
-      program: resolve(program),
-      linkedCourses: resolve([]),
-      isFinalized: resolve(false)
-    });
-
-    const grandParentBlock = EmberObject.create({
-      id: 1,
-      title: 'Okely Dokely',
-    });
-
-    const parentBlock = EmberObject.create({
-      id: 2,
+    const program = this.server.create('program', { school });
+    const report = this.server.create('curriculumInventoryReport', { academicLevels, year: '2016', program });
+    const grandParentBlock = this.server.create('curriculumInventorySequenceBlock', { title: 'Okely Dokely', report });
+    const parentBlock = this.server.create('curriculumInventorySequenceBlock', {
       title: 'Foo',
-      parent: resolve(grandParentBlock)
+      parent: grandParentBlock
     });
-
-    grandParentBlock.set('children', resolve([ parentBlock ]));
-
-    const academicLevel = academicLevels[0];
-
-    const block = EmberObject.create({
-      id: 3,
+    const block = this.server.create('curriculumInventorySequenceBlock', {
+      title: 'bar',
       description: 'lorem ipsum',
-      report: resolve(report),
-      parent: resolve(parentBlock),
-      sessions: resolve([]),
+      report,
+      parent: parentBlock,
       duration: 12,
       startDate: moment('2015-01-02'),
       endDate: moment('2015-04-30'),
       childSequenceOrder: 1,
       orderInSequence: 1,
-      course: resolve(null),
       required: 2,
       track: true,
       minimum: 2,
       maximum: 15,
-      academicLevel: resolve(academicLevel),
-      title: 'bar',
-      allParents: resolve([ parentBlock, grandParentBlock ])
+      academicLevel: academicLevels[0],
     });
 
-    parentBlock.set('children', resolve([ block ]));
+    const reportModel = await this.owner.lookup('service:store').find('curriculum-inventory-report', report.id);
+    const blockModel = await this.owner.lookup('service:store').find('curriculum-inventory-sequence-block', block.id);
+    const parentBlockModel = await this.owner.lookup('service:store').find('curriculum-inventory-sequence-block', parentBlock.id);
+    const grandParentBlockModel = await this.owner.lookup('service:store').find('curriculum-inventory-sequence-block', grandParentBlock.id);
 
-    this.set('report', report);
-    this.set('sequenceBlock', block);
+    this.set('report', reportModel);
+    this.set('sequenceBlock', blockModel);
     this.set('sortBy', 'title');
-    this.set('setSortSessionsBy', null);
+    this.set('canUpdate', true);
 
-    await render(hbs`<CurriculumInventorySequenceBlockDetails 
-      @sequenceBlock={{sequenceBlock}} 
-      @canUpdate={{true}} 
-      @sortSessionsBy={{sortBy}} 
-      @setSortSessionBy={{action (mut sortSessionsBy)}}
+    await render(hbs`<CurriculumInventorySequenceBlockDetails
+      @report={{this.report}}
+      @sequenceBlock={{this.sequenceBlock}}
+      @canUpdate={{this.canUpdate}}
+      @sortSessionsBy={{this.sortBy}}
+      @setSortSessionBy={{noop}}
     />`);
-    return settled().then(() => {
-      assert.dom('.curriculum-inventory-sequence-block-header .title').hasText(block.get('title'), 'Block header is visible.');
-      assert.dom('.curriculum-inventory-sequence-block-overview .description .editinplace').hasText(block.get('description'), 'Block overview is visible.');
-      assert.dom('.breadcrumbs span').exists({ count: 4 }, 'Breadcrumb has the right number of elements');
-      assert.dom('.breadcrumbs span').hasText('Curriculum Inventory Report');
-      assert.dom(findAll('.breadcrumbs span')[1]).hasText(grandParentBlock.get('title'));
-      assert.dom(findAll('.breadcrumbs span')[2]).hasText(parentBlock.get('title'));
-      assert.dom(findAll('.breadcrumbs span')[3]).hasText(block.get('title'));
-    });
+
+    assert.dom('.curriculum-inventory-sequence-block-header .title').hasText(
+      blockModel.get('title'), 'Block header is visible.'
+    );
+    assert.dom('.curriculum-inventory-sequence-block-overview .description .editinplace').hasText(
+      blockModel.get('description'), 'Block overview is visible.'
+    );
+    assert.dom('.breadcrumbs span').exists({ count: 4 }, 'Breadcrumb has the right number of elements');
+    assert.dom('.breadcrumbs span').hasText('Curriculum Inventory Report');
+    assert.dom(findAll('.breadcrumbs span')[1]).hasText(grandParentBlockModel.get('title'));
+    assert.dom(findAll('.breadcrumbs span')[2]).hasText(parentBlockModel.get('title'));
+    assert.dom(findAll('.breadcrumbs span')[3]).hasText(blockModel.get('title'));
   });
 });
-
