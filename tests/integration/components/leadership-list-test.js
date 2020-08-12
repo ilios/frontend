dcd775c6636e3f9fc0700a966c9573a7f5e4f74c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-08-12 15:23:44 -0700

    use mirage instead of ember object.

--- tests/integration/components/leadership-list-test.js
+++ tests/integration/components/leadership-list-test.js
@@ -1,29 +1,35 @@
-import EmberObject from '@ember/object';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Integration | Component | leadership list', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
+  hooks.beforeEach(async function() {
+    const user1 = this.server.create('user', {
+      firstName: 'a',
+      middleName: 'b',
+      lastName: 'person',
+    });
+    const user2 = this.server.create('user', {
+      firstName: 'b',
+      middleName: 'a',
+      lastName: 'person',
+    });
+
+    this.user1 = await this.owner.lookup('service:store').find('user', user1.id);
+    this.user2 = await this.owner.lookup('service:store').find('user', user2.id);
+  });
 
   test('it renders with data', async function(assert) {
     assert.expect(7);
-    const user1 = EmberObject.create({
-      firstName: 'a',
-      lastName: 'person',
-      fullName: 'a b person',
-    });
-    const user2 = EmberObject.create({
-      firstName: 'b',
-      lastName: 'person',
-      fullName: 'b a person',
-    });
-    this.set('directors', [user1]);
-    this.set('administrators', [user2, user1]);
-    this.set('studentAdvisors', [user2]);
+
+    this.set('directors', [this.user1]);
+    this.set('administrators', [this.user2, this.user1]);
+    this.set('studentAdvisors', [this.user2]);
 
     await render(hbs`<LeadershipList
       @directors={{this.directors}}
@@ -38,23 +44,19 @@ module('Integration | Component | leadership list', function(hooks) {
     const studentAdvisors = 'table tbody tr:nth-of-type(1) td:nth-of-type(3) li [data-test-name]';
 
     assert.dom(directors).exists({ count: 1 });
-    assert.dom(findAll(directors)[0]).hasText('a b person');
+    assert.dom(findAll(directors)[0]).hasText('a b. person');
     assert.dom(administrators).exists({ count: 2 });
-    assert.dom(findAll(administrators)[0]).hasText('a b person');
-    assert.dom(findAll(administrators)[1]).hasText('b a person');
+    assert.dom(findAll(administrators)[0]).hasText('a b. person');
+    assert.dom(findAll(administrators)[1]).hasText('b a. person');
     assert.dom(studentAdvisors).exists({ count: 1 });
-    assert.dom(findAll(studentAdvisors)[0]).hasText('b a person');
+    assert.dom(findAll(studentAdvisors)[0]).hasText('b a. person');
   });
 
   test('it renders without directors', async function(assert) {
     assert.expect(4);
-    const user1 = EmberObject.create({
-      firstName: 'a',
-      lastName: 'person',
-      fullName: 'a b person',
-    });
-    this.set('administrators', [user1]);
-    this.set('studentAdvisors', [user1]);
+
+    this.set('administrators', [this.user1]);
+    this.set('studentAdvisors', [this.user1]);
 
     await render(hbs`<LeadershipList
       @showDirectors={{false}}
@@ -67,20 +69,16 @@ module('Integration | Component | leadership list', function(hooks) {
     const studentAdvisors = 'table tbody tr:nth-of-type(1) td:nth-of-type(2) li [data-test-name]';
 
     assert.dom(administrators).exists({ count: 1 });
-    assert.dom(findAll(administrators)[0]).hasText('a b person');
+    assert.dom(findAll(administrators)[0]).hasText('a b. person');
     assert.dom(studentAdvisors).exists({ count: 1 });
-    assert.dom(findAll(studentAdvisors)[0]).hasText('a b person');
+    assert.dom(findAll(studentAdvisors)[0]).hasText('a b. person');
   });
 
   test('it renders without administrators', async function(assert) {
     assert.expect(4);
-    const user1 = EmberObject.create({
-      firstName: 'a',
-      lastName: 'person',
-      fullName: 'a b person',
-    });
-    this.set('directors', [user1]);
-    this.set('studentAdvisors', [user1]);
+
+    this.set('directors', [this.user1]);
+    this.set('studentAdvisors', [this.user1]);
 
     await render(hbs`<LeadershipList
       @showAdministrators={{false}}
@@ -93,20 +91,16 @@ module('Integration | Component | leadership list', function(hooks) {
     const studentAdvisors = 'table tbody tr:nth-of-type(1) td:nth-of-type(2) li [data-test-name]';
 
     assert.dom(directors).exists({ count: 1 });
-    assert.dom(findAll(directors)[0]).hasText('a b person');
+    assert.dom(findAll(directors)[0]).hasText('a b. person');
     assert.dom(studentAdvisors).exists({ count: 1 });
-    assert.dom(findAll(studentAdvisors)[0]).hasText('a b person');
+    assert.dom(findAll(studentAdvisors)[0]).hasText('a b. person');
   });
 
   test('it renders without student advisors', async function(assert) {
     assert.expect(4);
-    const user1 = EmberObject.create({
-      firstName: 'a',
-      lastName: 'person',
-      fullName: 'a b person',
-    });
-    this.set('directors', [user1]);
-    this.set('administrators', [user1]);
+
+    this.set('directors', [this.user1]);
+    this.set('administrators', [this.user1]);
 
     await render(hbs`<LeadershipList
       @showDirectors={{true}}
@@ -119,13 +113,14 @@ module('Integration | Component | leadership list', function(hooks) {
     const administrators = 'table tbody tr:nth-of-type(1) td:nth-of-type(2) li [data-test-name]';
 
     assert.dom(directors).exists({ count: 1 });
-    assert.dom(findAll(directors)[0]).hasText('a b person');
+    assert.dom(findAll(directors)[0]).hasText('a b. person');
     assert.dom(administrators).exists({ count: 1 });
-    assert.dom(findAll(administrators)[0]).hasText('a b person');
+    assert.dom(findAll(administrators)[0]).hasText('a b. person');
   });
 
   test('it renders without data', async function(assert) {
     assert.expect(6);
+
     this.set('directors', []);
     this.set('administrators', []);
     this.set('studentAdvisors', []);
@@ -152,21 +147,12 @@ module('Integration | Component | leadership list', function(hooks) {
 
   test('disabled users are indicated with an icon', async function(assert) {
     assert.expect(10);
-    const user1 = EmberObject.create({
-      enabled: true,
-      firstName: 'a',
-      lastName: 'person',
-      fullName: 'a b person',
-    });
-    const user2 = EmberObject.create({
-      enabled: false,
-      firstName: 'b',
-      lastName: 'person',
-      fullName: 'b a person',
-    });
-    this.set('directors', [user1]);
-    this.set('administrators', [user2, user1]);
-    this.set('studentAdvisors', [user2]);
+
+    this.user2.set('enabled', false);
+
+    this.set('directors', [this.user1]);
+    this.set('administrators', [this.user2, this.user1]);
+    this.set('studentAdvisors', [this.user2]);
 
     await render(hbs`<LeadershipList
       @directors={{this.directors}}
@@ -188,13 +174,13 @@ module('Integration | Component | leadership list', function(hooks) {
 
     assert.dom(directors).exists({ count: 1 });
     assert.dom(disabledDirectors).doesNotExist();
-    assert.dom(findAll(directorNames)[0]).hasText('a b person');
+    assert.dom(findAll(directorNames)[0]).hasText('a b. person');
     assert.dom(administrators).exists({ count: 2 });
     assert.dom(disabledAdministrators).exists({ count: 1 });
-    assert.dom(findAll(administratorNames)[0]).hasText('a b person');
-    assert.dom(findAll(administratorNames)[1]).hasText('b a person');
+    assert.dom(findAll(administratorNames)[0]).hasText('a b. person');
+    assert.dom(findAll(administratorNames)[1]).hasText('b a. person');
     assert.dom(studentAdvisors).exists({ count: 1 });
     assert.dom(disabledStudentAdvisors).exists({ count: 1 });
-    assert.dom(findAll(studentAdvisorNames)[0]).hasText('b a person');
+    assert.dom(findAll(studentAdvisorNames)[0]).hasText('b a. person');
   });
 });
