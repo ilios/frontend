f818a2e5824a3f4bbd2e37d72ed0e8875b851507
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-10-08 23:07:44 -0700

    Replace page-title helper with a noop
    In the latest version of ember-page-title the title attribute is sought
    and modified at the document level, the LTI has no business attempting
    to modify the document title so the helper is replaced with one that
    does nothing. This ensures that invocations from common still work.

---
+++ tests/integration/helpers/page-title-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | page-title', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('page title does nothing', async function(assert) {
+    await render(hbs`{{page-title "Jayden Rules!"}}`);
+    assert.dom().hasText("");
+  });
+});
