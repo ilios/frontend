967df63522c50ab6d6447a812e040607eaacef75
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-11-03 15:43:07 -0800

    improves URL validation.

--- addon/components/new-learningmaterial.js
+++ addon/components/new-learningmaterial.js
@@ -3,16 +3,17 @@ import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { dropTask } from 'ember-concurrency-decorators';
-import { validatable, Length, NotBlank, IsTrue } from 'ilios-common/decorators/validation';
+import { validatable, Length, NotBlank, IsTrue, IsURL } from 'ilios-common/decorators/validation';
 import { ValidateIf } from "class-validator";
 import { guidFor } from '@ember/object/internals';
+
+const DEFAULT_URL_VALUE = 'https://';
 
 @validatable
 export default class NewLearningmaterialComponent extends Component {
   @service store;
   @service currentUser;
   @service iliosConfig;
-
   @ValidateIf(o => o.isFile) @NotBlank() @tracked filename;
   @ValidateIf(o => o.isFile) @NotBlank() @tracked fileHash;
   @tracked statusId;
@@ -20,7 +21,7 @@ export default class NewLearningmaterialComponent extends Component {
   @tracked description;
   @NotBlank() @Length(2, 80) @tracked originalAuthor;
   @NotBlank() @Length(4, 120) @tracked title;
-  @ValidateIf(o => o.isLink) @NotBlank() @tracked link = 'http://';
+  @ValidateIf(o => o.isLink)  @NotBlank() @IsURL() @tracked link = DEFAULT_URL_VALUE;
   @ValidateIf(o => o.isFile && !o.copyrightRationale) @IsTrue() @tracked copyrightPermission = false;
   @ValidateIf(o => o.isFile) @Length(2, 65000) @tracked copyrightRationale;
   @tracked owner;
@@ -67,6 +68,10 @@ export default class NewLearningmaterialComponent extends Component {
     return this.args.learningMaterialUserRoles.get('firstObject');
   }
 
+  get bestLink() {
+    return this.link ?? DEFAULT_URL_VALUE;
+  }
+
   @dropTask
   * prepareSave() {
     this.addErrorDisplayForAllFields();
@@ -95,6 +100,7 @@ export default class NewLearningmaterialComponent extends Component {
       break;
     }
     case 'link': {
+      console.log(this.link);
       learningMaterial.set('link', this.link);
       break;
     }
@@ -116,5 +122,22 @@ export default class NewLearningmaterialComponent extends Component {
   @action
   changeUserRoleId(event) {
     this.userRoleId = event.target.value;
+  }
+
+  @action
+  changeLink(value) {
+    value = value.trim();
+    const regex = RegExp('https://http[s]?:');
+    if (regex.test(value)) {
+      value = value.substring(8);
+    }
+    this.link = value;
+  }
+
+  @action
+  selectAllText({ target }) {
+    if (target.value === DEFAULT_URL_VALUE) {
+      target.select();
+    }
   }
 }
