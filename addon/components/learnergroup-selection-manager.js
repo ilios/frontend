a3d712e02f8e702e368f33a0f4fc9d326e874261
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-11-13 09:45:52 -0800

    sort learnergroups numerically in selection manager.

--- addon/components/learnergroup-selection-manager.js
+++ addon/components/learnergroup-selection-manager.js
@@ -3,4 +3,9 @@ import { tracked } from '@glimmer/tracking';
 
 export default class LearnergroupSelectionManager extends Component {
   @tracked filter = '';
+
+  sortByTitle(learnerGroupA, learnerGroupB) {
+    const locale = this.intl.get('locale');
+    return learnerGroupA.title.localeCompare(learnerGroupB.title, locale, {numeric: true});
+  }
 }
