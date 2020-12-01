4ed4c6e352246216e5e976ec0d923b1cbbeff985
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-12-01 15:11:20 -0800

    octanifies users controller.

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,24 +1,24 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
-export default Controller.extend({
-  queryParams: {
+export default class UsersController extends Controller {
+  queryParams = [{
     offset: 'offset',
     limit: 'limit',
     query: 'filter',
     showNewUserForm: 'addUser',
     showBulkNewUserForm: 'addUsers',
     searchTerms: 'search',
-  },
-  offset: 0,
-  limit: 25,
-  query: null,
-  showNewUserForm: false,
-  showBulkNewUserForm: false,
-  searchTerms: null,
+  }];
+  offset = 0;
+  limit = 25;
+  query = null;
+  showNewUserForm = false;
+  showBulkNewUserForm = false;
+  searchTerms = null;
 
-  actions: {
-    transitionToUser(userId){
-      this.transitionToRoute('user', userId);
-    },
+  @action
+  transitionToUser(userId){
+    this.transitionToRoute('user', userId);
   }
-});
+}
