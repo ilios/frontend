54b116203b3db81a014e8d4bdd3f6fe633c62e9c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-11-30 16:53:34 -0800

    wip: octanifies NewUser component.

--- app/components/new-user.js
+++ app/components/new-user.js
@@ -1,78 +1,167 @@
-import Component from '@ember/component';
-import NewUser from 'ilios/mixins/newuser';
-import { validator, buildValidations } from 'ember-cp-validations';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import {all, filter} from 'rsvp';
+import {dropTask, restartableTask} from 'ember-concurrency-decorators';
+import moment from "moment";
+import { validatable, Length, NotBlank } from 'ilios-common/decorators/validation';
 
-const Validations = buildValidations({
-  firstName: [
-    validator('presence', true),
-    validator('length', {
-      max: 50
-    })
-  ],
-  middleName: [
-    validator('length', {
-      max: 20
-    })
-  ],
-  lastName: [
-    validator('presence', true),
-    validator('length', {
-      max: 50
-    })
-  ],
-  username: [
-    validator('presence', true),
-    validator('length', {
-      max: 100
-    })
-  ],
-  password: [
-    validator('presence', true)
-  ],
-  campusId: [
-    validator('length', {
-      max: 16
-    })
-  ],
-  otherId: [
-    validator('length', {
-      max: 16
-    })
-  ],
-  email: [
-    validator('presence', true),
-    validator('length', {
-      max: 100
-    }),
-    validator('format', {
-      type: 'email'
-    })
-  ],
-  phone: [
-    validator('length', {
-      max: 20
-    })
-  ]
-});
+@validatable
+export default class NewUserComponent extends Component {
+  @service store;
+  @service currentUser;
+  @service flashMessages;
+  @service permissionChecker;
 
-export default Component.extend(NewUser, Validations, {
-  classNames: ['new-user'],
+  @tracked @Length(1, 50) @NotBlank() firstName = null;
+  @tracked @Length(1, 20) middleName = null;
+  @tracked @Length(1, 50) @NotBlank() lastName = null;
+  @tracked @Length(1, 16) campusId = null;
+  @tracked @Length(1, 16) otherId = null;
+  // @todo check if given input is a valid email address. needs a new modifier [ST 2020/11/30]
+  @tracked @Length(1, 100) @NotBlank() email = null;
+  @tracked @Length(1, 100) @NotBlank() username = null;
+  @tracked @NotBlank() password = null;
+  @tracked @Length(1, 20) phone = null;
+  @tracked schoolId = null;
+  @tracked primaryCohortId = null;
+  @tracked primarySchool = null;
+  @tracked cohorts = [];
+  @tracked schools = [];
+  @tracked isSaving = false;
+  @tracked nonStudentMode = true;
 
-  keyUp(event) {
+  get bestSelectedSchool() {
+    if (this.schoolId) {
+      const currentSchool = this.schools.findBy('id', this.schoolId);
+
+      if (currentSchool) {
+        return currentSchool;
+      }
+    }
+    return this.primarySchool;
+  }
+
+  get bestSelectedCohort() {
+    if (! this.currentSchoolCohorts) {
+      return null;
+    }
+
+    if (this.primaryCohortId) {
+      const currentCohort = this.currentSchoolCohorts.findBy('id', this.primaryCohortId);
+
+      if (currentCohort) {
+        return currentCohort;
+      }
+    }
+
+    return this.currentSchoolCohorts.lastObject;
+  }
+
+  @restartableTask
+  *load(){
+    const user = yield this.currentUser.model;
+    this.primarySchool = yield user.school;
+    this.schools = yield this.loadSchools.perform();
+    this.currentSchoolCohorts = yield this.bestSelectedSchool?.cohorts;
+    this.cohorts = yield this.loadCohorts.perform(this.bestSelectedSchool);
+  }
+
+  @restartableTask
+  *reload() {
+    this.currentSchoolCohorts = yield this.bestSelectedSchool?.cohorts;
+    this.cohorts = yield this.loadCohorts.perform(this.bestSelectedSchool);
+  }
+
+  @restartableTask
+  *loadSchools() {
+    const store = this.store;
+    const schools = yield store.findAll('school', { reload: true });
+    return filter(schools.toArray(), async school => {
+      return this.permissionChecker.canCreateUser(school);
+    });
+  }
+
+  @restartableTask
+  *loadCohorts(school) {
+    if (! school) {
+      return;
+    }
+    const cohorts = yield this.store.query('cohort', {
+      filters: {
+        schools: [ school.id ],
+      }
+    });
+
+    //prefetch programYears and programs so that ember data will coalesce these requests.
+    const programYears = yield all(cohorts.getEach('programYear'));
+    yield all(programYears.getEach('program'));
+
+    const objects = yield all(cohorts.toArray().map(async cohort => {
+      const obj = {
+        id: cohort.get('id')
+      };
+      const programYear = await cohort.programYear;
+      const program = await programYear.program;
+      obj.title = program.title + ' ' + cohort.title;
+      obj.startYear = programYear.startYear;
+      obj.duration = program.duration;
+
+      return obj;
+    }));
+
+    const lastYear = parseInt(moment().subtract(1, 'year').format('YYYY'), 10);
+    return objects.filter(obj => {
+      const finalYear = parseInt(obj.startYear, 10) + parseInt(obj.duration, 10);
+      return finalYear > lastYear;
+    });
+  }
+
+  @dropTask
+  *save() {
+    this.addErrorDisplaysFor(['firstName', 'middleName', 'lastName', 'campusId', 'otherId', 'email', 'phone', 'username', 'password']);
+    const isValid = yield this.isValid();
+    if (! isValid) {
+      return false;
+    }
+    const roles = yield this.store.findAll('user-role');
+    const primaryCohort = this.bestSelectedCohort;
+    let user = this.store.createRecord('user', {
+      firstName: this.firstName,
+      middleName: this.middleName,
+      lastName: this.lastName,
+      campusId: this.campusId,
+      otherId: this.otherId,
+      email: this.email,
+      phone: this.phone,
+      school: this.bestSelectedSchool,
+      enabled: true,
+      root: false,
+    });
+    if (! this.nonStudentMode) {
+      user.set('primaryCohort', primaryCohort);
+      const studentRole = roles.findBy('title', 'Student');
+      user.set('roles', [studentRole]);
+    }
+    user = yield user.save();
+    const authentication = this.store.createRecord('authentication', {
+      user,
+      username: this.userName,
+      password: this.password,
+    });
+    yield authentication.save();
+    this.clearErrorDisplay();
+    this.flashMessages.success('general.saved');
+    this.args.transitionToUser(user.get('id'));
+  }
+
+  @dropTask
+  *saveOrCancel(event) {
     const keyCode = event.keyCode;
-    const target = event.target;
-
-    if ('text' !== target.type) {
-      return;
-    }
-
     if (13 === keyCode) {
-      this.save.perform();
-      return;
-    }
-
-    if (27 === keyCode) {
-      this.close();
+      yield this.save.perform();
+    } else if (27 === keyCode) {
+      this.args.close();
     }
   }
-});
+}
