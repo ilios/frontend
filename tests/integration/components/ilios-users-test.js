db52ee27cca12ac6f3047f485a05c4726dceab12
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-12-02 15:53:27 -0800

    modernizes and fleshes out tests coverage for IliosUsers component.

--- tests/integration/components/ilios-users-test.js
+++ tests/integration/components/ilios-users-test.js
@@ -2,16 +2,16 @@ import { resolve } from 'rsvp';
 import Service from '@ember/service';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find, findAll, fillIn } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { component } from 'ilios/tests/pages/components/ilios-users';
 
 module('Integration | Component | ilios users', function(hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
   test('it renders', async function(assert) {
-    const title = '.users .title';
     await render(hbs`<IliosUsers
       @limit=25
       @offset=25
@@ -25,13 +25,12 @@ module('Integration | Component | ilios users', function(hooks) {
       @setSearchTerms={{noop}}
       @transitionToUser={{noop}}
     />`);
-    assert.dom(title).hasText('Users');
+    assert.equal(component.title.text, 'Users');
   });
 
   test('param passing', async function(assert) {
     assert.expect(2);
 
-    const query = '.user-search input';
     const value = 'nothing';
     const newValue = 'test';
     this.set('value', value);
@@ -52,20 +51,21 @@ module('Integration | Component | ilios users', function(hooks) {
       @transitionToUser={{noop}}
     />`);
 
-    assert.equal(find(query).value.trim(), value);
-    await fillIn(query, newValue);
+    assert.equal(component.search.value, value);
+    await component.search.set(newValue);
   });
 
   test('add user form renders when configured to', async function(assert) {
-    assert.expect(4);
     const { apiVersion } = this.owner.resolveRegistration('config:environment');
     assert.ok(apiVersion);
     this.server.get('application/config', function() {
-      return { config: {
-        type: 'form',
-        userSearchType: 'form',
-        apiVersion
-      }};
+      return {
+        config: {
+          type: 'form',
+          userSearchType: 'form',
+          apiVersion
+        }
+      };
     });
     const school = this.server.create('school');
     const user = this.server.create('user', {
@@ -91,24 +91,21 @@ module('Integration | Component | ilios users', function(hooks) {
       @setSearchTerms={{noop}}
       @transitionToUser={{noop}}
     />`);
-    const form = '.new-user-form';
-    const blocks = `${form} .item`;
-    const directorySearchBox = '.new-directory-user-search-tools';
-    assert.dom(form).exists({ count: 1 }, 'the user search form is present');
-    assert.ok(findAll(blocks).length > 4, 'there are many form fields for adding a new user');
-    assert.dom(directorySearchBox).doesNotExist('the directory form search form is not present');
+    assert.ok(component.newUserForm.isPresent, 'the new user form is present');
+    assert.notOk(component.newDirectoryUserForm.isPresent, 'the new directory user form is not present');
   });
 
   test('directory search renders when configured to', async function(assert) {
-    assert.expect(4);
     const { apiVersion } = this.owner.resolveRegistration('config:environment');
     assert.ok(apiVersion);
     this.server.get('application/config', function() {
-      return { config: {
-        type: 'form',
-        userSearchType: 'ldap',
-        apiVersion
-      }};
+      return {
+        config: {
+          type: 'form',
+          userSearchType: 'ldap',
+          apiVersion
+        }
+      };
     });
 
     const school = this.server.create('school');
@@ -125,8 +122,6 @@ module('Integration | Component | ilios users', function(hooks) {
     });
     this.owner.register('service:currentUser', currentUserMock);
 
-    this.set('nothing', parseInt);
-
     await render(hbs`<IliosUsers
       @showNewUserForm={{true}}
       @searchTerms={{array}}
@@ -138,11 +133,136 @@ module('Integration | Component | ilios users', function(hooks) {
       @setSearchTerms={{noop}}
       @transitionToUser={{noop}}
     />`);
-    const form = '.new-user-form';
-    const blocks = `${form} .item`;
-    const directorySearchBox = '.new-directory-user-search-tools';
-    assert.dom(form).doesNotExist('the user search form is not present');
-    assert.dom(blocks).doesNotExist('there are no form fields for adding a new user');
-    assert.dom(directorySearchBox).exists({ count: 1 }, 'the directory form search form is present');
+    assert.notOk(component.newUserForm.isPresent, 'the new user form is not present');
+    assert.ok(component.newDirectoryUserForm.isPresent, 'the new directory form is present');
+  });
+
+  test('close bulk new users form callback fires', async function(assert) {
+    assert.expect(1);
+    const { apiVersion } = this.owner.resolveRegistration('config:environment');
+    this.server.get('application/config', function() {
+      return {
+        config: {
+          type: 'form',
+          userSearchType: 'form',
+          apiVersion
+        }
+      };
+    });
+    const school = this.server.create('school');
+    const user = this.server.create('user', {
+      school
+    });
+    const userModel = await this.owner.lookup('service:store').find('user', user.id);
+
+    const currentUserMock = Service.extend({
+      model: resolve(userModel),
+      getRolesInSchool() {
+        return [];
+      }
+    });
+    this.owner.register('service:currentUser', currentUserMock);
+
+    this.set('setShowBulkNewUserForm', (value) => {
+      assert.notOk(value);
+    });
+    await render(hbs`<IliosUsers
+      @showBulkNewUserForm={{true}}
+      @searchTerms={{array}}
+      @setQuery={{noop}}
+      @setLimit={{noop}}
+      @setOffset={{noop}}
+      @setShowNewUserForm={{noop}}
+      @setShowBulkNewUserForm={{this.setShowBulkNewUserForm}}
+      @setSearchTerms={{noop}}
+      @transitionToUser={{noop}}
+    />`);
+    await component.newBulkUserForm.cancel();
+  });
+
+  test('close new user form callback fires', async function(assert) {
+    assert.expect(1);
+    const { apiVersion } = this.owner.resolveRegistration('config:environment');
+    this.server.get('application/config', function() {
+      return {
+        config: {
+          type: 'form',
+          userSearchType: 'form',
+          apiVersion
+        }
+      };
+    });
+    const school = this.server.create('school');
+    const user = this.server.create('user', {
+      school
+    });
+    const userModel = await this.owner.lookup('service:store').find('user', user.id);
+
+    const currentUserMock = Service.extend({
+      model: resolve(userModel),
+      getRolesInSchool() {
+        return [];
+      }
+    });
+    this.owner.register('service:currentUser', currentUserMock);
+    this.set('setShowNewUserForm', (value) => {
+      assert.notOk(value);
+    });
+    await render(hbs`<IliosUsers
+      @showNewUserForm={{true}}
+      @searchTerms={{array}}
+      @setQuery={{noop}}
+      @setLimit={{noop}}
+      @setOffset={{noop}}
+      @setShowNewUserForm={{this.setShowNewUserForm}}
+      @setShowBulkNewUserForm={{noop}}
+      @setSearchTerms={{noop}}
+      @transitionToUser={{noop}}
+    />`);
+    await component.newUserForm.cancel();
+  });
+
+  test('collapse button fires', async function(assert) {
+    assert.expect(1);
+    assert.expect(1);
+    const { apiVersion } = this.owner.resolveRegistration('config:environment');
+    this.server.get('application/config', function() {
+      return {
+        config: {
+          type: 'form',
+          userSearchType: 'form',
+          apiVersion
+        }
+      };
+    });
+    const school = this.server.create('school');
+    const user = this.server.create('user', {
+      school
+    });
+    const userModel = await this.owner.lookup('service:store').find('user', user.id);
+
+    const currentUserMock = Service.extend({
+      model: resolve(userModel),
+      getRolesInSchool() {
+        return [];
+      }
+    });
+    this.owner.register('service:currentUser', currentUserMock);
+
+    this.set('setShowBulkNewUserForm', (value) => {
+      assert.notOk(value);
+    });
+    await render(hbs`<IliosUsers
+      @showBulkNewUserForm={{true}}
+      @searchTerms={{array}}
+      @setQuery={{noop}}
+      @setLimit={{noop}}
+      @setOffset={{noop}}
+      @setShowNewUserForm={{noop}}
+      @setShowBulkNewUserForm={{this.setShowBulkNewUserForm}}
+      @setSearchTerms={{noop}}
+      @transitionToUser={{noop}}
+    />`);
+    await component.collapseForm();
   });
 });
---
+++ tests/pages/components/ilios-users.js
@@ -0,0 +1,49 @@
+import { clickable, create, fillable, value } from 'ember-cli-page-object';
+import newUserForm from 'ilios/tests/pages/components/new-user';
+import userList from 'ilios/tests/pages/components/user-list';
+import pagedlistControls from 'ilios/tests/pages/components/pagedlist-controls';
+
+const definition = {
+  scope: '[data-test-ilios-users]',
+  title: {
+    scope: '[data-test-title]',
+  },
+  search: {
+    scope: '[data-test-filters]',
+    set: fillable('input'),
+    value: value('input'),
+  },
+  collapseForm: clickable('[data-test-collapse]'),
+  showNewUserFormButton: {
+    scope: '[data-test-show-new-user-form]',
+  },
+  showBulkUsersFormButton: {
+    scope: '[data-test-show-bulk-new-user-form]',
+  },
+  newUserForm,
+  // @todo replace with page object. [ST 2020/12/02]
+  newDirectoryUserForm: {
+    scope: '.new-directory-user',
+    cancel: clickable('.buttons button.cancel')
+  },
+  // @todo replace with page object. [ST 2020/12/02]
+  newBulkUserForm: {
+    scope: '.bulk-new-users',
+    cancel: clickable('.buttons button.cancel'),
+  },
+  topPaginator: {
+    scope: '[data-test-top-paged-list-controls',
+    pagedlistControls,
+  },
+  userList,
+  noUsers: {
+    scope: '[data-test-no-users]',
+  },
+  bottomPaginator: {
+    scope: '[data-test-bottom-paged-list-controls',
+    pagedlistControls,
+  },
+};
+
+export default definition;
+export const component = create(definition);
