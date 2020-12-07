11290e256bf5f6cd46b9932c20423ccb6aa5dc15
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-12-07 13:20:47 -0800

    adds Hex Color validator.

---
+++ addon/decorators/validation/is-hex-color.js
@@ -0,0 +1,27 @@
+import { registerDecorator } from "class-validator";
+import { getOwner } from '@ember/application';
+
+const HEX_COLOR_PATTERN = /^#[a-fA-F0-9]{6}$/;
+
+export function IsHexColor(validationOptions) {
+  return function (object, propertyName) {
+    registerDecorator({
+      name: 'IsHexColor',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value) {
+          return HEX_COLOR_PATTERN.test(value);
+        },
+        defaultMessage({ object: target }) {
+          const owner = getOwner(target);
+          const intl = owner.lookup('service:intl');
+          const description = intl.t('errors.description');
+
+          return intl.t('errors.invalid', { description });
+        }
+      },
+    });
+  };
+}
