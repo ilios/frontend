0d2f65bddd30fb22ccd940187b74486f95aafbae
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-12-16 13:55:12 -0800

    removes click-event handler
    it was making the date picker too trigger happy.
    turns out it is not necessary in order to close the picker.

--- addon/components/date-picker.js
+++ addon/components/date-picker.js
@@ -4,7 +4,6 @@ import { dropTask, restartableTask } from 'ember-concurrency-decorators';
 import { waitForProperty } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
 import flatpickr from "flatpickr";
-import { action } from '@ember/object';
 import { later } from '@ember/runloop';
 
 export default class DatePickerComponent extends Component {
@@ -55,11 +54,6 @@ export default class DatePickerComponent extends Component {
       maxDate: this.args.maxDate ?? null,
       minDate: this.args.minDate ?? null,
     });
-  }
-
-  @action
-  close() {
-    this?._flatPickerInstance.close();
   }
 
   willDestroy() {
