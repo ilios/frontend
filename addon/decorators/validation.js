7717a63810d5e40079f4d7f7d8532b5415a8cf82
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-01-04 14:31:52 -0800

    adds Custom validation decorator.

--- addon/decorators/validation.js
+++ addon/decorators/validation.js
@@ -14,6 +14,7 @@ import { IsURL } from './validation/is-url';
 import { validatable } from './validation/validatable';
 import { ArrayNotEmpty } from './validation/array-not-empty';
 import { GteProp } from './validation/gte-prop';
+import { Custom } from './validation/custom';
 
 
 export {
@@ -32,5 +33,6 @@ export {
   Length,
   NotBlank,
   HtmlNotBlank,
-  validatable
+  validatable,
+  Custom
 };
