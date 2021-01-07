00d7268a1115c2055d012233d44f9d7c1869841a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-01-07 13:02:56 -0800

    modernizes test coverage.

--- tests/integration/components/dashboard-mycourses-test.js
+++ tests/integration/components/dashboard-mycourses-test.js
@@ -4,13 +4,13 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { authenticateSession } from 'ember-simple-auth/test-support';
+import { component } from 'ilios/tests/pages/components/dashboard-mycourses';
 
 module('Integration | Component | dashboard mycourses', function(hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
   test('list courses for privileged users', async function(assert) {
-    assert.expect(10);
     const user = this.server.create('user');
     const jwtObject = {
       'user_id': user.id,
@@ -32,37 +32,20 @@ module('Integration | Component | dashboard mycourses', function(hooks) {
       assert.ok('my' in queryParams);
       return schema.courses.all();
     });
+
     await render(hbs`<DashboardMycourses />`);
 
-    const header = '.dashboard-block-header';
-    const allLinks = `table a`;
-    const courses = `table tr`;
-    const firstCourse = `${courses}:nth-of-type(1)`;
-    const firstCourseYear = `${firstCourse} td:nth-of-type(1)`;
-    const firstCourseTitle = `${firstCourse} td:nth-of-type(2)`;
-    const secondCourse = `${courses}:nth-of-type(2)`;
-    const secondCourseYear = `${secondCourse} td:nth-of-type(1)`;
-    const secondCourseTitle = `${secondCourse} td:nth-of-type(2)`;
-    const thirdCourse = `${courses}:nth-of-type(3)`;
-    const thirdCourseYear = `${thirdCourse} td:nth-of-type(1)`;
-    const thirdCourseTitle = `${thirdCourse} td:nth-of-type(2)`;
-
-    assert.dom(header).hasText('My Courses');
-    assert.dom(allLinks).exists({ count: 6 });
-    assert.dom(courses).exists({ count: 3 });
-
-    assert.dom(firstCourseYear).hasText('2013 - 2014');
-    assert.dom(firstCourseTitle).hasText('course 0 (ABC123)');
-
-    assert.dom(secondCourseYear).hasText('2013 - 2014');
-    assert.dom(secondCourseTitle).hasText('course 1');
-
-    assert.dom(thirdCourseYear).hasText('2013 - 2014');
-    assert.dom(thirdCourseTitle).hasText('course 2');
+    assert.equal(component.title, 'My Courses');
+    assert.equal(component.courses.length, 3);
+    assert.equal(component.courses[0].text, '2013 - 2014 course 0 (ABC123)');
+    assert.equal(component.courses[1].text, '2013 - 2014 course 1');
+    assert.equal(component.courses[2].text, '2013 - 2014 course 2');
+    assert.ok(component.courses[0].isLinked);
+    assert.ok(component.courses[1].isLinked);
+    assert.ok(component.courses[2].isLinked);
   });
 
   test('list courses for un-privileged users', async function(assert) {
-    assert.expect(10);
     const user = this.server.create('user');
     const jwtObject = {
       'user_id': user.id,
@@ -87,35 +70,16 @@ module('Integration | Component | dashboard mycourses', function(hooks) {
 
     await render(hbs`<DashboardMycourses />`);
 
-    const header = '.dashboard-block-header';
-    const allLinks = `table a`;
-    const courses = `table tr`;
-    const firstCourse = `${courses}:nth-of-type(1)`;
-    const firstCourseYear = `${firstCourse} td:nth-of-type(1)`;
-    const firstCourseTitle = `${firstCourse} td:nth-of-type(2)`;
-    const secondCourse = `${courses}:nth-of-type(2)`;
-    const secondCourseYear = `${secondCourse} td:nth-of-type(1)`;
-    const secondCourseTitle = `${secondCourse} td:nth-of-type(2)`;
-    const thirdCourse = `${courses}:nth-of-type(3)`;
-    const thirdCourseYear = `${thirdCourse} td:nth-of-type(1)`;
-    const thirdCourseTitle = `${thirdCourse} td:nth-of-type(2)`;
-
-    assert.dom(header).hasText('My Courses');
-    assert.dom(allLinks).doesNotExist();
-    assert.dom(courses).exists({ count: 3 });
-
-    assert.dom(firstCourseYear).hasText('2013 - 2014');
-    assert.dom(firstCourseTitle).hasText('course 0');
-
-    assert.dom(secondCourseYear).hasText('2013 - 2014');
-    assert.dom(secondCourseTitle).hasText('course 1');
-
-    assert.dom(thirdCourseYear).hasText('2013 - 2014');
-    assert.dom(thirdCourseTitle).hasText('course 2');
+    assert.equal(component.courses.length, 3);
+    assert.equal(component.courses[0].text, '2013 - 2014 course 0');
+    assert.equal(component.courses[1].text, '2013 - 2014 course 1');
+    assert.equal(component.courses[2].text, '2013 - 2014 course 2');
+    assert.notOk(component.courses[0].isLinked);
+    assert.notOk(component.courses[1].isLinked);
+    assert.notOk(component.courses[2].isLinked);
   });
 
   test('display none when no courses', async function(assert) {
-    assert.expect(2);
     await authenticateSession();
     this.server.get('/api/courses', (schema, { queryParams }) => {
       assert.ok('my' in queryParams);
@@ -123,9 +87,8 @@ module('Integration | Component | dashboard mycourses', function(hooks) {
     });
 
     await render(hbs`<DashboardMycourses />`);
-    assert.dom('.dashboard-block-header').hasText('My Courses');
 
-    assert.dom('.dashboard-block-body').hasText('None');
-
+    assert.equal(component.courses.length, 1);
+    assert.equal(component.courses[0].text, 'None');
   });
 });
