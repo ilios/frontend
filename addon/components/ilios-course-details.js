0e01f70bb37fa889a7bd4e21c8f2ca7e84407e3e
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-01-14 13:41:52 -0800

    Prevent editing course when rolling over
    This is confusing as users may believe they're modifying the rollover
    process and not the source course.

--- addon/components/ilios-course-details.js
+++ addon/components/ilios-course-details.js
@@ -1,12 +1,19 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import scrollIntoView from 'scroll-into-view';
+import { inject as service } from '@ember/service';
 
 export default class IliosCourseDetailsComponent extends Component {
+  @service router;
+
   @action
   collapse() {
     //when the button is clicked to collapse, animate the focus to the top of the page
     scrollIntoView(this.topSection);
     this.args.setShowDetails(false);
+  }
+
+  get notRolloverRoute(){
+    return this.router.currentRouteName !== 'course.rollover';
   }
 }
