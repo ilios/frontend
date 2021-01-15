f9952bc276c3b57e53197e264b1cd1583666a503
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-01-14 16:48:27 -0800

    octanifies loading-bar component.

---
+++ tests/pages/components/loading-bar.js
@@ -0,0 +1,9 @@
+import { attribute, create } from 'ember-cli-page-object';
+
+export default create({
+  scope: '[data-test-loading-bar]',
+  bar: {
+    scope: '[data-test-bar]',
+    value: attribute('data-value'),
+  }
+});
