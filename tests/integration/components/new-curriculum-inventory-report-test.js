178d88cca483c0a74b94a78ca643a2dc18285b34
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-02-17 13:54:59 -0800

    guts ci reports controller and pushes functionality into component.
    octanifies new ci report component while at it, and labels
    academic years based on application config.

--- tests/integration/components/new-curriculum-inventory-report-test.js
+++ tests/integration/components/new-curriculum-inventory-report-test.js
@@ -25,14 +25,14 @@ module('Integration | Component | new curriculum inventory report', function(hoo
 
     this.set('program', programModel);
 
-    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{program}} />`);
+    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{this.program}} @save={{noop}} @cancel={{noop}} />`);
 
     const currentYear = parseInt(moment().format('YYYY'), 10);
-    const currentYearLabel = `${currentYear} - ${currentYear + 1}`;
+    const currentYearLabel = `${currentYear}`;
     const firstYear = currentYear - 5;
-    const firstYearLabel = `${firstYear} - ${firstYear + 1}`;
+    const firstYearLabel = `${firstYear}`;
     const lastYear = currentYear + 5;
-    const lastYearLabel = `${lastYear} - ${lastYear + 1}`;
+    const lastYearLabel = `${lastYear}`;
 
     assert.dom('[data-test-program-title] label').hasText('Program:', 'program title is labeled correctly.');
     assert.dom('[data-test-program-title] span').hasText(program.title, 'Program title is displayed.');
@@ -61,10 +61,31 @@ module('Integration | Component | new curriculum inventory report', function(hoo
     assert.dom('button.cancel').hasText('Cancel', 'Cancel button is labeled correctly.');
   });
 
+  test('academic year options labeled as range when app configuration is set to cross calendar-year boundaries', async function(assert) {
+    this.server.get('application/config', function() {
+      return { config: {
+        academicYearCrossesCalendarYearBoundaries: true,
+      }};
+    });
+    const program = this.server.create('program', { id: 1, title: 'Doctor of Medicine' });
+    const programModel = await this.owner.lookup('service:store').find('program', program.id);
+
+    this.set('program', programModel);
+
+    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{this.program}} @save={{noop}} @cancel={{noop}} />`);
+
+    const currentYear = parseInt(moment().format('YYYY'), 10);
+    const currentYearLabel = `${currentYear} - ${currentYear + 1}`;
+    const firstYearLabel = `${currentYear - 5} - ${currentYear - 4}`;
+    const lastYearLabel = `${currentYear + 5} - ${currentYear + 6}`;
+    const select = find('[data-test-academic-year] select');
+    assert.dom(select.options[select.selectedIndex]).hasText(currentYearLabel, 'Current year label is correct.');
+    assert.dom('[data-test-academic-year] option').hasText(firstYearLabel, 'First year label is correct.');
+    assert.dom(findAll('[data-test-academic-year] option')[10]).hasText(lastYearLabel, 'Last year label is correct.');
+  });
 
   test('save', async function(assert) {
     assert.expect(6);
-
     const program = this.server.create('program', { id: 1, title: 'Doctor of Medicine' });
     const programModel = await this.owner.lookup('service:store').find('program', program.id);
 
@@ -77,6 +98,41 @@ module('Integration | Component | new curriculum inventory report', function(hoo
       assert.equal(report.get('year'), expectedSelectedYear, 'Selected academic year gets passed.');
       assert.equal(
         moment(report.get('startDate')).format('YYYY-MM-DD'),
+        `${expectedSelectedYear}-01-01`,
+        'Start date gets calculated and passed.'
+      );
+      assert.equal(
+        moment(report.get('endDate')).format('YYYY-MM-DD'),
+        `${expectedSelectedYear}-12-31`,
+        'End date gets calculated and passed.'
+      );
+      assert.ok(report.get('program'), 'Program gets passed.');
+      return true;
+    });
+
+    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{this.program}} @save={{this.saveReport}} @cancel={{noop}} />`);
+    await fillIn('[data-test-name] input', 'new report');
+    await fillIn('[data-test-description] textarea', 'lorem ipsum');
+    await fillIn('[data-test-academic-year] select', expectedSelectedYear);
+    await click('button.done');
+  });
+
+  test('save with academic year crossing calendar-year boundaries', async function(assert) {
+    assert.expect(2);
+    this.server.get('application/config', function() {
+      return { config: {
+        academicYearCrossesCalendarYearBoundaries: true,
+      }};
+    });
+    const program = this.server.create('program', { id: 1, title: 'Doctor of Medicine' });
+    const programModel = await this.owner.lookup('service:store').find('program', program.id);
+
+    this.set('program', programModel);
+    const currentYear = parseInt(moment().format('YYYY'), 10);
+    const expectedSelectedYear = currentYear - 5;
+    this.set('saveReport', (report) => {
+      assert.equal(
+        moment(report.get('startDate')).format('YYYY-MM-DD'),
         `${expectedSelectedYear}-07-01`,
         'Start date gets calculated and passed.'
       );
@@ -85,11 +141,10 @@ module('Integration | Component | new curriculum inventory report', function(hoo
         `${expectedSelectedYear + 1}-06-30`,
         'End date gets calculated and passed.'
       );
-      assert.ok(report.get('program'), 'Program gets passed.');
-      return resolve(true);
+      return true;
     });
 
-    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{program}} @save={{action saveReport}} />`);
+    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{this.program}} @save={{this.saveReport}} @cancel={{noop}} />`);
     await fillIn('[data-test-name] input', 'new report');
     await fillIn('[data-test-description] textarea', 'lorem ipsum');
     await fillIn('[data-test-academic-year] select', expectedSelectedYear);
@@ -106,7 +161,7 @@ module('Integration | Component | new curriculum inventory report', function(hoo
     this.set('cancelReport', () => {
       assert.ok(true, 'Cancel action got invoked.');
     });
-    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{program}} @cancel={{action cancelReport}} />`);
+    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{this.program}} @cancel={{this.cancelReport}} @save={{noop}} />`);
     await click('button.cancel');
   });
 
@@ -122,9 +177,9 @@ module('Integration | Component | new curriculum inventory report', function(hoo
       return resolve(true);
     });
 
-    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{program}} @save={{action saveReport}} />`);
+    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{this.program}} @save={{this.saveReport}} @cancel={{noop}} />`);
     await fillIn('[data-test-name] input', 'new report');
-    await triggerKeyEvent('[data-test-name] input', 'keyup', 13);
+    await triggerKeyEvent('[data-test-name] input', 'keydown', 13);
   });
 
   test('validation errors do not show up initially', async function(assert) {
@@ -133,7 +188,7 @@ module('Integration | Component | new curriculum inventory report', function(hoo
     const programModel = await this.owner.lookup('service:store').find('program', program.id);
 
     this.set('program', programModel);
-    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{program}} />`);
+    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{this.program}} @save={{noop}} @cancel={{noop}}/>`);
     assert.dom('.validation-error-message').doesNotExist();
   });
 
@@ -143,7 +198,7 @@ module('Integration | Component | new curriculum inventory report', function(hoo
     const programModel = await this.owner.lookup('service:store').find('program', program.id);
 
     this.set('program', programModel);
-    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{program}} />`);
+    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{this.program}} @save={{noop}} @cancel={{noop}}/>`);
     await click('button.done');
     assert.dom('.validation-error-message').exists({ count: 1 });
   });
@@ -154,7 +209,7 @@ module('Integration | Component | new curriculum inventory report', function(hoo
     const programModel = await this.owner.lookup('service:store').find('program', program.id);
 
     this.set('program', programModel);
-    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{program}} />`);
+    await render(hbs`<NewCurriculumInventoryReport @currentProgram={{this.program}} @save={{noop}} @cancel={{noop}}/>`);
     await fillIn('[data-test-name] input', '0123456789'.repeat(7));
     await click('button.done');
     assert.dom('.validation-error-message').exists({ count: 1 });
