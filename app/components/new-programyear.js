3183b290be571f713f02cf1577b3cca207e11441
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-02-25 15:39:55 -0800

    reload component if given list of program years changes.
    stream line code a bit while at it

--- app/components/new-programyear.js
+++ app/components/new-programyear.js
@@ -4,6 +4,7 @@ import { action } from '@ember/object';
 import { dropTask } from 'ember-concurrency';
 
 export default class NewProgramyearComponent extends Component {
+  @tracked allYears;
   @tracked year;
   @tracked availableAcademicYears = [];
 
@@ -18,29 +19,34 @@ export default class NewProgramyearComponent extends Component {
     if (! this.year) {
       return this.availableAcademicYears.firstObject;
     }
-    return this.availableAcademicYears.findBy('value', parseInt(this.year, 10));
+    return this.availableAcademicYears.findBy('value', this.year);
   }
 
-  @action
-  load() {
+  constructor() {
+    super(...arguments);
     const firstYear = new Date().getFullYear() - 5;
     const years = [];
     for (let i = 0; i < 10; i++) {
       years.push(firstYear + i);
     }
-    this.availableAcademicYears = years.filter(year => {
+    this.allYears = years;
+  }
+
+  @action
+  load() {
+    this.availableAcademicYears = this.allYears.filter(year => {
       return ! this.existingStartYears.includes(year.toString());
     }).map((startYear) => {
       return {
         label: this.args.academicYearCrossesCalendarYearBoundaries ? `${startYear} - ${startYear + 1}` : startYear.toString(),
-        value: startYear,
+        value: startYear.toString(),
       };
     });
   }
 
   @dropTask
   *saveNewYear() {
-    const startYear = parseInt(this.selectedYear.value, 10);
+    const startYear = this.selectedYear.value;
     yield this.args.save(startYear);
   }
 }
