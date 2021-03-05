8d51dc5f0be85c0e48ddb2ebf49632c4652d872a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-03-05 12:12:05 -0800

    corrects import.

--- app/components/program-year/list-item.js
+++ app/components/program-year/list-item.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
-import AsyncProcess from 'ilios-common/classes/resolve-async-value';
+import AsyncProcess from 'ilios-common/classes/async-process';
 import { use } from 'ember-could-get-used-to-this';
 import { inject as service } from '@ember/service';
 import { dropTask } from 'ember-concurrency';
