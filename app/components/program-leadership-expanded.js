396f238c7ea254c0612c3ea3efe1fd19fad486a7
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-03-08 15:12:52 -0800

    Octaneify Program Leadership
    The computed directors property wasn't working in the latest ember, this
    was the easiest way to fix it.

--- app/components/program-leadership-expanded.js
+++ app/components/program-leadership-expanded.js
@@ -1,60 +1,45 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { reads } from '@ember/object/computed';
-import { task, timeout } from 'ember-concurrency';
+import Component from '@glimmer/component';
+import { dropTask } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
+import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
+import { use } from 'ember-could-get-used-to-this';
+import { action } from '@ember/object';
 
-export default Component.extend({
-  tagName: '',
-  canUpdate: null,
-  program: null,
-  isManaging: false,
-  collapse() {},
-  expand() {},
-  setIsManaging() {},
-  isSaving: reads('save.isRunning'),
+export default class ProgramLeadershipComponent extends Component {
+  @tracked directorsToAdd = [];
+  @tracked directorsToRemove = [];
+  @use programDirectors = new ResolveAsyncValue(() => [this.args.program.directors]);
 
-  directors: computed(
-    'directorsToAdd.[]',
-    'directorsToRemove.[]',
-    'program.directors',
-    async function () {
-      const directors = await this.program.directors;
-      return directors
-        .toArray()
-        .concat(this.directorsToAdd)
-        .filter((user) => !this.directorsToRemove.includes(user));
-    }
-  ),
+  get directors() {
+    const directors = this.programDirectors?.toArray() || [];
+    return [...directors, ...this.directorsToAdd].filter(
+      (user) => !this.directorsToRemove.includes(user)
+    );
+  }
 
-  init() {
-    this._super(...arguments);
-    this.setProperties({ directorsToAdd: [], directorsToRemove: [] });
-  },
+  @action
+  addDirector(user) {
+    this.directorsToAdd = [...this.directorsToAdd, user];
+    this.directorsToRemove = this.directorsToRemove.filter((d) => d !== user);
+  }
 
-  actions: {
-    addDirector(user) {
-      this.directorsToRemove.removeObject(user);
-      this.directorsToAdd.pushObject(user);
-    },
+  @action
+  removeDirector(user) {
+    this.directorsToRemove = [...this.directorsToRemove, user];
+    this.directorsToAdd = this.directorsToAdd.filter((d) => d !== user);
+  }
 
-    removeDirector(user) {
-      this.directorsToAdd.removeObject(user);
-      this.directorsToRemove.pushObject(user);
-    },
-
-    cancel() {
-      this.setIsManaging(false);
-      this.setProperties({ directorsToAdd: [], directorsToRemove: [] });
-    },
-  },
-
-  save: task(function* () {
-    yield timeout(10);
-    const program = this.program;
-    const directors = yield this.directors;
-    program.set('directors', directors);
-    this.expand();
-    yield program.save();
-    this.setIsManaging(false);
-  }),
-});
+  @action
+  close() {
+    this.directorsToAdd = [];
+    this.directorsToRemove = [];
+    this.args.setIsManaging(false);
+  }
+  @dropTask
+  *save() {
+    this.args.program.set('directors', this.directors);
+    this.args.expand();
+    yield this.args.program.save();
+    this.close();
+  }
+}
