4613d19167f61ffb576ed49bd9a5b7f578868be8
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-03-15 16:37:43 -0700

    converts controller to use class syntax.

--- app/controllers/curriculum-inventory-sequence-block.js
+++ app/controllers/curriculum-inventory-sequence-block.js
@@ -1,19 +1,19 @@
+import { tracked } from '@glimmer/tracking';
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { all } from 'rsvp';
 
-export default Controller.extend({
-  queryParams: ['sortSessionsBy'],
-  sortSessionsBy: 'title',
-  canUpdate: false,
-  actions: {
-    removeChildSequenceBlock(block) {
-      const parent = this.model;
-      block.destroyRecord().then(() => {
-        parent.reload().then((parent) => {
-          parent.get('children').then((children) => {
-            children.invoke('reload');
-          });
-        });
-      });
-    },
-  },
-});
+export default class CurriculumInventorySequenceBlockController extends Controller {
+  queryParams = ['sortSessionsBy'];
+  @tracked sortSessionsBy = 'title';
+  @tracked canUpdate = false;
+
+  @action
+  async removeChildSequenceBlock(block) {
+    await block.destroyRecord();
+    // @todo is all of this below still necessary? [ST 2021/03/15]
+    await this.model.reload();
+    const children = await this.model.children;
+    await all(children.invoke('reload'));
+  }
+}
