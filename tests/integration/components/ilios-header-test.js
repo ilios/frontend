9f9641dc4c178bd763abe798271ea20d9e062b10
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-03-16 14:18:51 -0700

    Activate mirage in header test
    This is needed because the component makes calls to the configuration
    API. If mirage isn't setup then these calls don't get intercepted and
    throw and error in CI.

--- tests/integration/components/ilios-header-test.js
+++ tests/integration/components/ilios-header-test.js
@@ -4,9 +4,11 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
 import { component } from 'ilios/tests/pages/components/ilios-header';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Integration | Component | ilios-header', function (hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks); //even though we're not using mirage directly we need to ensure that /config API is owned
 
   test('it renders and is accessible', async function (assert) {
     this.set('title', 'Some Title');
