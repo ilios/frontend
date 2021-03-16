4739ce20a9349094c9037158da01782e5620b539
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-03-16 16:30:51 -0700

    throw out async sorting for good.
    i give up on this crap, for now. tracking changes to attributes of
    elements within an array, like it was possible with computed properties,
    is not doable with this resource task approach at the moment.

--- app/components/curriculum-inventory/sequence-block-list.js
+++ app/components/curriculum-inventory/sequence-block-list.js
@@ -2,58 +2,14 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { dropTask } from 'ember-concurrency';
-import { use } from 'ember-could-get-used-to-this';
-import { Task } from 'ember-resource-tasks';
-import { all, map } from 'rsvp';
+import { all } from 'rsvp';
 
 export default class SequenceBlockListComponent extends Component {
   @tracked editorOn = false;
   @tracked savedBlock;
 
-  @use sortedBlocks = new Task(() => {
-    return {
-      named: {
-        args: [this.isInOrderedSequence, this.args.sequenceBlocks, this.args.sequenceBlocks.length],
-        fn: this.sort,
-      },
-    };
-  });
-
   get isInOrderedSequence() {
     return this.args.parent && this.args.parent.isOrdered;
-  }
-
-  get sequenceBlocks() {
-    if (!this.sortedBlocks.value) {
-      return [];
-    }
-    return this.sortedBlocks.value;
-  }
-
-  @action
-  async sort(isOrderedInSequence, sequenceBlocks /* ,sequenceBlocksLength */) {
-    if (!sequenceBlocks) {
-      return [];
-    }
-    const blocks = sequenceBlocks.toArray();
-
-    if (isOrderedInSequence) {
-      blocks.sort(this.sortOrdered);
-      return blocks;
-    }
-
-    const proxies = await map(blocks, async (block) => {
-      const academicLevel = await block.academicLevel;
-      const level = academicLevel.level;
-      return {
-        level,
-        block,
-      };
-    });
-    proxies.sort(this.sortUnordered);
-    return proxies.map((proxy) => {
-      return proxy.block;
-    });
   }
 
   @action
@@ -81,41 +37,33 @@ export default class SequenceBlockListComponent extends Component {
     }
   }
 
+  @action
   sortUnordered(a, b) {
     // Sort in the following order:
-    // 1. sequenceBlock.academicLevel.level, ascending
-    // 2. sequenceBlock.title, ascending
+    // 1. sequenceBlock.title, ascending
     // 3. sequenceBlock.startDate, ascending
     // 4. sequenceBlock.id, ascending
-    // We're working with proxy objects here,
-    // so sequenceBlock.academicLevel.level is mapped to proxy.level,
-    // and sequenceBlock is mapped to proxy.block.
-    if (a.level > b.level) {
-      return 1;
-    } else if (a.level < b.level) {
-      return -1;
-    }
-
-    const titleComparison = a.block.title.localeCompare(b.block.title);
+    const titleComparison = a.title.localeCompare(b.title);
     if (0 !== titleComparison) {
       return titleComparison;
     }
 
-    if (a.block.startDate > b.block.startDate) {
+    if (a.startDate > b.startDate) {
       return 1;
-    } else if (a.block.startDate < b.block.startDate) {
+    } else if (a.startDate < b.startDate) {
       return -1;
     }
 
-    if (a.block.id > b.block.id) {
+    if (a.id > b.id) {
       return 1;
-    } else if (a.block.id < b.block.id) {
+    } else if (a.id < b.id) {
       return -1;
     }
 
     return 0;
   }
 
+  @action
   sortOrdered(a, b) {
     // sort in the following order:
     // 1. sequenceBlock.orderInSequence, ascending
