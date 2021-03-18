1f08c46cccb10bb36b7392d6b2757a465b56c359
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-03-18 14:48:38 -0700

    simplifies component by waiting for promise resolution in template.

--- app/components/curriculum-inventory/sequence-block-session-list.js
+++ app/components/curriculum-inventory/sequence-block-session-list.js
@@ -1,44 +1,18 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { task } from 'ember-concurrency';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default Component.extend({
-  tagName: '',
-  excludedSessions: null,
-  linkedSessions: null,
-  sessions: null,
-  sessionsBuffer: null,
-  sortBy: 'title',
-
-  sortedAscending: computed('sortBy', function () {
-    const sortBy = this.sortBy;
+export default class SequenceBlockSessionListComponent extends Component {
+  get sortedAscending() {
+    const sortBy = this.args.sortBy;
     return sortBy.search(/desc/) === -1;
-  }),
+  }
 
-  didReceiveAttrs() {
-    this._super(...arguments);
-    const sequenceBlock = this.sequenceBlock;
-    const sessions = this.sessions;
-    this.loadAttr.perform(sequenceBlock, sessions);
-  },
-
-  actions: {
-    sortBy(what) {
-      const sortBy = this.sortBy;
-      if (sortBy === what) {
-        what += ':desc';
-      }
-      this.setSortBy(what);
-    },
-  },
-
-  loadAttr: task(function* (sequenceBlock, sessions) {
-    const linkedSessions = yield sequenceBlock.get('sessions');
-    const excludedSessions = yield sequenceBlock.get('excludedSessions');
-    this.setProperties({
-      linkedSessions,
-      excludedSessions,
-      sessionsBuffer: sessions,
-    });
-  }),
-});
+  @action
+  changeSortOrder(what) {
+    const sortBy = this.args.sortBy;
+    if (sortBy === what) {
+      what += ':desc';
+    }
+    this.args.setSortBy(what);
+  }
+}
