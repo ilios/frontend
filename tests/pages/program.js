15a4c999a7a1abe28b8b15a785fe1a9f9f6f0113
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-03-30 14:48:52 -0700

    Octaneify Program Header component

--- tests/pages/program.js
+++ tests/pages/program.js
@@ -1,19 +1,15 @@
-import { clickable, create, fillable, text, visitable } from 'ember-cli-page-object';
+import { clickable, create, text, visitable } from 'ember-cli-page-object';
 
 import leadershipCollapsed from 'ilios-common/page-objects/components/leadership-collapsed';
 import leadershipList from 'ilios-common/page-objects/components/leadership-list';
 import leadershipManager from 'ilios-common/page-objects/components/leadership-manager';
 import overview from './components/program/overview';
+import header from './components/program/header';
 
 export default create({
   scope: '[data-test-program-details]',
   visit: visitable('/programs/:programId'),
-  title: {
-    scope: '[data-test-program-header] [data-test-title]',
-    edit: clickable('[data-test-edit]'),
-    set: fillable('input'),
-    save: clickable('.done'),
-  },
+  header,
   overview,
   leadershipCollapsed,
   leadershipExpanded: {
