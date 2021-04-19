7356a067b1062c29adf89de96135d7bdedf7070e
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-04-19 16:43:55 -0700

    octanify login form.
    adds the ability to submit login form by pressing enter in
    username/password fields.

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,17 +1,19 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { task } from 'ember-concurrency';
 
-export default Component.extend({
-  session: service(),
-  tagName: '',
-  noAccountExistsError: false,
-  noAccountExistsAccount: null,
-  username: null,
-  password: null,
-  authenticate: task(function* () {
+export default class LoginFormComponent extends Component {
+  @service session;
+  @tracked error;
+  @tracked username;
+  @tracked password;
+
+  @task
+  *authenticate() {
     try {
-      this.set('error', null);
+      this.error = null;
       const session = this.session;
       const authenticator = 'authenticator:ilios-jwt';
       yield session.authenticate(authenticator, {
@@ -22,7 +24,15 @@ export default Component.extend({
       const keys = response.json.errors.map((key) => {
         return 'general.' + key;
       });
-      this.set('error', { keys });
+      this.error = { keys };
     }
-  }),
-});
+  }
+
+  @action
+  async submitOnEnter(event) {
+    const keyCode = event.keyCode;
+    if (13 === keyCode) {
+      await this.authenticate.perform();
+    }
+  }
+}
