980655c11ac5619f8c274e475933ef7ffb5cc187
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-04-29 14:49:02 -0700

    modernize ci report controller.

--- app/controllers/curriculum-inventory-report/index.js
+++ app/controllers/curriculum-inventory-report/index.js
@@ -1,12 +1,10 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
-export default Controller.extend({
-  actions: {
-    removeSequenceBlock(block) {
-      const report = this.model;
-      block.destroyRecord().then(() => {
-        report.reload();
-      });
-    },
-  },
-});
+export default class CurriculumInventoryReportIndexController extends Controller {
+  @action
+  async removeSequenceBlock(block) {
+    await block.destroyRecord();
+    this.model.reload();
+  }
+}
