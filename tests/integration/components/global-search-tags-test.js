68670e751803369116e27f30337a6325fc02e66c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-05-03 12:58:41 -0700

    modernizes component tests.

--- tests/integration/components/global-search-tags-test.js
+++ tests/integration/components/global-search-tags-test.js
@@ -1,20 +1,22 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { findAll, render } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { component } from 'ilios/tests/pages/components/global-search-tags';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
 
 module('Integration | Component | global-search-tags', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it displays tags properly', async function (assert) {
-    assert.expect(4);
-
+  test('it renders and is accessible', async function (assert) {
     this.set('tags', ['terms', 'meshdescriptors', 'id', 'learningmaterials']);
-    await render(hbs`<GlobalSearchTags @tags={{tags}} />`);
-    const [tag1, tag2, tag3, tag4] = findAll('.global-search-tag');
-    assert.equal(tag1.textContent.trim(), 'Terms');
-    assert.equal(tag2.textContent.trim(), 'MeSH');
-    assert.equal(tag3.textContent.trim(), 'ID');
-    assert.equal(tag4.textContent.trim(), 'Learning Materials');
+    await render(hbs`<GlobalSearchTags @tags={{this.tags}} />`);
+    assert.equal(component.tags.length, 4);
+    assert.equal(component.tags[0].text, 'Terms');
+    assert.equal(component.tags[1].text, 'MeSH');
+    assert.equal(component.tags[2].text, 'ID');
+    assert.equal(component.tags[3].text, 'Learning Materials');
+    await a11yAudit(this.element);
+    assert.ok(true, 'no a11y errors found!');
   });
 });
