087569d74ef323669960a08c6948de64c33501a6
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-05-04 23:05:50 -0700

    Octaneify LearnerGroupUpload component

--- app/components/learnergroup-upload-data.js
+++ app/components/learnergroup-upload-data.js
@@ -1,148 +1,144 @@
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { Promise as RSVPPromise, map } from 'rsvp';
-import { isEmpty, isPresent } from '@ember/utils';
-import EmberObject, { computed } from '@ember/object';
-import { task, timeout } from 'ember-concurrency';
+import { map } from 'rsvp';
+import { restartableTask, task, timeout } from 'ember-concurrency';
 import PapaParse from 'papaparse';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { use } from 'ember-could-get-used-to-this';
+import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
 
-export default Component.extend({
-  store: service(),
-  iliosConfig: service(),
-  intl: service(),
-  tagName: '',
-  file: null,
-  data: null,
-  learnerGroup: null,
+export default class LearnerGroupUploadDataComponent extends Component {
+  @service store;
+  @service iliosConfig;
+  @service intl;
 
-  sampleData: computed(function () {
+  @tracked file;
+  @use data = new ResolveAsyncValue(() => [this.parseFile.perform(this.parsedFileData)]);
+  @tracked parsedFileData = [];
+
+  get sampleData() {
     const sampleUploadFields = ['First', 'Last', 'CampusID', 'Sub Group Name'];
 
     const str = sampleUploadFields.join('\t');
     const encoded = window.btoa(str);
 
     return encoded;
-  }),
+  }
 
-  validUsers: computed('data.[]', function () {
-    const data = this.data;
-    if (!data) {
+  get validUsers() {
+    if (!this.data) {
       return [];
     }
-    return data.filterBy('isValid');
-  }),
+    return this.data.filterBy('isValid');
+  }
 
-  invalidUsers: computed('data.[]', function () {
-    const data = this.data;
-    if (!data) {
+  get invalidUsers() {
+    if (!this.data) {
       return [];
     }
-    return data.filter((obj) => !obj.isValid);
-  }),
+    return this.data.filter((obj) => !obj.isValid);
+  }
 
-  matchedGroups: computed('data.[]', 'learnerGroup', async function () {
-    const data = this.data;
-    const learnerGroup = this.learnerGroup;
-    if (!data) {
+  @action
+  reset() {
+    this.parsedFileData = [];
+  }
+
+  async getMatchedGroups() {
+    if (!this.data) {
       return [];
     }
-    const uploadedSubGroups = data
-      .mapBy('subGroupName')
-      .uniq()
-      .filter((str) => isPresent(str));
-    const groups = await learnerGroup.get('allDescendants');
+    const uploadedSubGroups = this.data.mapBy('subGroupName').uniq().filter(Boolean);
+    const groups = await this.args.learnerGroup.allDescendants;
     const matchObjects = uploadedSubGroups.map((groupName) => {
       const group = groups.findBy('title', groupName);
-      return EmberObject.create({
+      return {
         name: groupName,
         group,
-      });
+      };
     });
-    return matchObjects.filter((obj) => isPresent(obj.get('group')));
-  }),
+    return matchObjects.filter((obj) => Boolean(obj.group));
+  }
 
-  init() {
-    this._super(...arguments);
-    this.set('data', []);
-  },
-
-  actions: {
-    async updateSelectedFile(files) {
-      // Check for the various File API support.
-      if (window.File && window.FileReader && window.FileList && window.Blob) {
-        if (files.length > 0) {
-          await this.parseFile.perform(files[0]);
-        }
-      } else {
-        throw new Error('This browser is not supported');
+  @action
+  updateSelectedFile(files) {
+    // Check for the various File API support.
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      if (files.length > 0) {
+        this.readFileContents(files[0]);
       }
-    },
-  },
+    } else {
+      throw new Error('This browser is not supported');
+    }
+  }
 
-  parseFile: task(function* (file) {
-    const store = this.store;
-    const intl = this.intl;
-    const learnerGroup = this.learnerGroup;
-    const cohort = yield learnerGroup.get('cohort');
-    const proposedUsers = yield this.getFileContents(file);
+  @restartableTask
+  *parseFile(proposedUsers) {
+    const cohort = yield this.args.learnerGroup.cohort;
     const data = yield map(
       proposedUsers,
       async ({ firstName, lastName, campusId, subGroupName }) => {
         const errors = [];
         const warnings = [];
-        if (isEmpty(firstName)) {
-          errors.push(intl.t('errors.required', { description: intl.t('general.firstName') }));
+        if (!firstName) {
+          errors.push(
+            this.intl.t('errors.required', { description: this.intl.t('general.firstName') })
+          );
         }
-        if (isEmpty(lastName)) {
-          errors.push(intl.t('errors.required', { description: intl.t('general.lastName') }));
+        if (!lastName) {
+          errors.push(
+            this.intl.t('errors.required', { description: this.intl.t('general.lastName') })
+          );
         }
-        if (isEmpty(campusId)) {
-          errors.push(intl.t('errors.required', { description: intl.t('general.campusId') }));
+        if (!campusId) {
+          errors.push(
+            this.intl.t('errors.required', { description: this.intl.t('general.campusId') })
+          );
         }
         let userRecord = null;
         if (errors.length === 0) {
-          const users = await store.query('user', {
+          const users = await this.store.query('user', {
             filters: {
               campusId,
               enabled: true,
             },
           });
-          if (users.get('length') === 0) {
-            errors.push(intl.t('general.couldNotFindUserCampusId', { campusId }));
-          } else if (users.get('length') > 1) {
-            errors.push(intl.t('general.multipleUsersFoundWithCampusId', { campusId }));
+          if (users.length === 0) {
+            errors.push(this.intl.t('general.couldNotFindUserCampusId', { campusId }));
+          } else if (users.length > 1) {
+            errors.push(this.intl.t('general.multipleUsersFoundWithCampusId', { campusId }));
           } else {
             const user = users.get('firstObject');
-            const cohorts = await user.get('cohorts');
-            const cohortIds = cohorts.mapBy('id');
-            if (!cohortIds.includes(cohort.get('id'))) {
+            const cohortIds = (await user.cohorts).mapBy('id');
+            if (!cohortIds.includes(cohort.id)) {
               errors.push(
-                intl.t('general.userNotInGroupCohort', { cohortTitle: cohort.get('title') })
+                this.intl.t('general.userNotInGroupCohort', { cohortTitle: cohort.get('title') })
               );
             }
-            if (user.get('firstName') != firstName) {
+            if (user.firstName != firstName) {
               warnings.push(
-                intl.t('general.doesNotMatchUserRecord', {
-                  description: intl.t('general.firstName'),
-                  record: user.get('firstName'),
+                this.intl.t('general.doesNotMatchUserRecord', {
+                  description: this.intl.t('general.firstName'),
+                  record: user.firstName,
                 })
               );
             }
-            if (user.get('lastName') != lastName) {
+            if (user.lastName != lastName) {
               warnings.push(
-                intl.t('general.doesNotMatchUserRecord', {
-                  description: intl.t('general.lastName'),
-                  record: user.get('lastName'),
+                this.intl.t('general.doesNotMatchUserRecord', {
+                  description: this.intl.t('general.lastName'),
+                  record: user.lastName,
                 })
               );
             }
 
-            const topLevelGroup = await learnerGroup.get('topLevelGroup');
+            const topLevelGroup = await this.args.learnerGroup.get('topLevelGroup');
             const allUsersInGroupHierarchy = await topLevelGroup.get('allDescendantUsers');
             if (allUsersInGroupHierarchy.includes(user)) {
               errors.push(
-                intl.t('general.userExistsInGroupHierarchy', {
-                  groupTitle: topLevelGroup.get('title'),
+                this.intl.t('general.userExistsInGroupHierarchy', {
+                  groupTitle: topLevelGroup.title,
                 })
               );
             }
@@ -169,72 +165,57 @@ export default Component.extend({
     const campusIds = [];
     data.forEach((user) => {
       if (campusIds.includes(user.campusId)) {
-        user.errors.push(intl.t('general.userExistsMultipleTimesInUpload'));
+        user.errors.push(this.intl.t('general.userExistsMultipleTimesInUpload'));
         user.isValid = false;
       } else {
         campusIds.push(user.campusId);
       }
     });
 
-    this.set('data', data);
-  }).restartable(),
+    return data;
+  }
 
   /**
    * Extract the contents of a file into an array of user like objects
-   * @param Object file
-   *
-   * @return array
    **/
-  getFileContents(file) {
-    return new RSVPPromise((resolve) => {
-      this.set('fileUploadError', false);
-      const allowedFileTypes = ['text/plain', 'text/csv', 'text/tab-separated-values'];
-      if (!allowedFileTypes.includes(file.type)) {
-        this.set('fileUploadError', true);
-        throw new Error(`Unable to accept files of type ${file.type}`);
-      }
+  readFileContents(file) {
+    this.fileUploadError = false;
+    const allowedFileTypes = ['text/plain', 'text/csv', 'text/tab-separated-values'];
+    if (!allowedFileTypes.includes(file.type)) {
+      this.fileUploadError = true;
+      throw new Error(`Unable to accept files of type ${file.type}`);
+    }
 
-      const ProposedUser = EmberObject.extend({});
-      const complete = ({ data }) => {
-        const proposedUsers = data.map((arr) => {
-          return ProposedUser.create({
-            firstName: isPresent(arr[0]) ? arr[0] : null,
-            lastName: isPresent(arr[1]) ? arr[1] : null,
-            campusId: isPresent(arr[2]) ? arr[2] : null,
-            subGroupName: isPresent(arr[3]) ? arr[3] : null,
-          });
-        });
-        const notHeaderRow = proposedUsers.filter(
-          (obj) =>
-            String(obj.firstName).toLowerCase() !== 'first' ||
-            String(obj.lastName).toLowerCase() !== 'last'
-        );
-        const skipEmpty = notHeaderRow.filter((obj) => {
-          return !(
-            isEmpty(obj.firstName) &&
-            isEmpty(obj.lastName) &&
-            isEmpty(obj.campusId) &&
-            isEmpty(obj.subGroupName)
-          );
-        });
-
-        resolve(skipEmpty);
-      };
-
-      PapaParse.parse(file, {
-        complete,
-        skipEmptyLines: true,
+    const complete = ({ data }) => {
+      const proposedUsers = data.map((arr) => {
+        return {
+          firstName: arr[0] ?? null,
+          lastName: arr[1] ?? null,
+          campusId: arr[2] ?? null,
+          subGroupName: arr[3] ?? null,
+        };
       });
-    });
-  },
+      const notHeaderRow = proposedUsers.filter(
+        (obj) =>
+          String(obj.firstName).toLowerCase() !== 'first' ||
+          String(obj.lastName).toLowerCase() !== 'last'
+      );
+      this.parsedFileData = notHeaderRow.filter((obj) => {
+        return obj.firstName || obj.lastName || obj.campusId || obj.subGroupName;
+      });
+    };
 
-  continue: task(function* () {
+    PapaParse.parse(file, {
+      complete,
+      skipEmptyLines: true,
+    });
+  }
+
+  @task
+  *continue() {
     yield timeout(10);
-    const validUsers = this.validUsers;
-    const matchedGroups = yield this.matchedGroups;
-    const sendValidUsers = this.sendValidUsers;
-    const sendMatchedGroups = this.sendMatchedGroups;
-    sendValidUsers(validUsers);
-    sendMatchedGroups(matchedGroups);
-  }),
-});
+    const matchedGroups = yield this.getMatchedGroups();
+    this.args.sendValidUsers(this.validUsers);
+    this.args.sendMatchedGroups(matchedGroups);
+  }
+}
