5b40cface2131a4ded1cd7a1a2f84d576344e80a
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-05-10 21:54:14 -0700

    Silence upstream deprecations
    we can't do anything about these yet.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -17,5 +17,7 @@ window.deprecationWorkflow.config = {
     { handler: 'silence', matchId: 'ember-metal.get-with-default' },
     { handler: 'silence', matchId: 'ember-test-helpers.trigger-event.options-blob-array' },
     { handler: 'silence', matchId: 'ember-string.htmlsafe-ishtmlsafe' },
+    { handler: 'silence', matchId: 'implicit-injections' }, //https://github.com/simplabs/ember-simple-auth/issues/2302
+    { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' }, //https://github.com/emberjs/ember-render-modifiers/issues/32
   ],
 };
