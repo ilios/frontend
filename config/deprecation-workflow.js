ce5be4bfcac17b4d90828430cff274b5e6522009
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-05-10 22:03:40 -0700

    Silence lifeline deprecation
    This will require a small re-write of our connections status component
    to bind the event listeners we need instead of relying on this very
    convenient and now deprecated functionality.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -20,5 +20,6 @@ window.deprecationWorkflow.config = {
     { handler: 'silence', matchId: 'implicit-injections' }, //https://github.com/simplabs/ember-simple-auth/issues/2302
     { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' }, //https://github.com/emberjs/ember-render-modifiers/issues/32
     { handler: 'silence', matchId: 'this-property-fallback' },
+    { handler: 'silence', matchId: 'ember-lifeline-deprecated-addeventlistener' },
   ],
 };
