2b8941aa5aca2db87acde8302ecb19b3c7da18fc
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-05-24 13:27:11 -0700

    Remove jQuery integration
    The only place we seem to still need jQuery is in some tests and it is
    provided there by ember-page-object. So This change removes the flag to
    provide the Ember.$ option and move us forward into the future a bit.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -11,7 +11,6 @@ window.deprecationWorkflow.config = {
     { handler: 'silence', matchId: 'deprecate-router-events' }, // requires https://github.com/ember-a11y/ember-a11y/issues/73
     { handler: 'silence', matchId: 'remove-handler-infos' },
     { handler: 'silence', matchId: 'ember-name-key-usage' }, //waiting for https://github.com/offirgolan/ember-cp-validations/issues/620
-    { handler: 'silence', matchId: 'ember-views.curly-components.jquery-element' },
     { handler: 'silence', matchId: 'computed-property.volatile' },
     { handler: 'silence', matchId: 'common.async-computed' },
     { handler: 'silence', matchId: 'ember-metal.get-with-default' },
