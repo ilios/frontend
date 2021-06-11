3a29decb627a0a6a67351bd8b1b5e191fe2e58af
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-06-11 13:30:22 -0700

    fixes dependency linter issue with ember-inflector.

--- tests/dummy/config/dependency-lint.js
+++ tests/dummy/config/dependency-lint.js
@@ -3,7 +3,6 @@
 
 module.exports = {
   allowedVersions: {
-    'ember-inflector': '3.0.1 || 4.0.1',
     'ember-in-element-polyfill': '*',
     'ember-get-config': '0.2.4 || 0.3.0',
     '@embroider/util': '*',
