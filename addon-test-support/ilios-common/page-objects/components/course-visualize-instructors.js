ecd8e9b02b90a031daba79a899c84b90d12b6f1f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-06-23 16:44:44 -0700

    repairs filter on instructors chart. improves placeholder text on filter input.

--- addon-test-support/ilios-common/page-objects/components/course-visualize-instructors.js
+++ addon-test-support/ilios-common/page-objects/components/course-visualize-instructors.js
@@ -1,8 +1,18 @@
-import { create, text } from 'ember-cli-page-object';
+import { collection, create, fillable, text, value } from 'ember-cli-page-object';
 
 const definition = create({
   scope: '[data-test-course-visualize-instructors]',
   title: text('[data-test-title]'),
+  filter: {
+    scope: '[data-test-filter]',
+    value: value('input'),
+    set: fillable('input'),
+  },
+  chart: {
+    scope: '.simple-chart-horz-bar',
+    bars: collection('.bars rect'),
+    labels: collection('.bars text'),
+  },
 });
 
 export default definition;
--- tests/integration/components/course-visualize-instructors-test.js
+++ tests/integration/components/course-visualize-instructors-test.js
@@ -19,6 +19,30 @@ module('Integration | Component | course-visualize-instructors', function (hooks
     assert.equal(component.title, 'course 0 2021');
   });
 
+  test('filter works', async function (assert) {
+    const school = this.server.create('school');
+    const course = this.server.create('course', { year: 2021, school });
+    const session = this.server.create('session', { course });
+    const instructor1 = this.server.create('user', { firstName: 'foo' });
+    const instructor2 = this.server.create('user', { firstName: 'bar' });
+    this.server.create('offering', {
+      session,
+      startDate: new Date('2021/03/01'),
+      endDate: new Date('2021/04/01'),
+      instructors: [instructor1, instructor2],
+    });
+    const courseModel = await this.owner.lookup('service:store').find('course', course.id);
+    this.set('course', courseModel);
+
+    await render(hbs`<CourseVisualizeInstructors @model={{this.course}} />`);
+    assert.equal(component.title, 'course 0 2021');
+    assert.equal(component.chart.bars.length, 2);
+    assert.equal(component.chart.labels.length, 2);
+    await component.filter.set('foo');
+    assert.equal(component.chart.bars.length, 1);
+    assert.equal(component.chart.labels.length, 1);
+  });
+
   test('course year is shown as range if applicable by configuration', async function (assert) {
     this.server.get('application/config', function () {
       return {
