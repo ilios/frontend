2b19b64c5711cadcbfdab11e00caf38ea8c6c403
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-07-27 21:00:45 -0700

    Cleanup some deprecations
    These are no longer relevant for us and can be removed.

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -3,9 +3,6 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'computed-property.volatile' },
-    { handler: 'silence', matchId: 'ember-metal.get-with-default' },
-    { handler: 'silence', matchId: 'ember-string.htmlsafe-ishtmlsafe' },
     { handler: 'silence', matchId: 'implicit-injections' }, //https://github.com/simplabs/ember-simple-auth/issues/2302
     { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' }, //https://github.com/emberjs/ember-render-modifiers/issues/32
     { handler: 'silence', matchId: 'this-property-fallback' }, //Lots of these in ember-file-upload we have linting to ensure we don't add any ourselves
