3f4cefeccdc38b6cb244da593ec358ba2568e675
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-08-08 21:18:07 -0700

    Update Deprecations
    Remove untriggered deprecations from our ignore list and add new ones
    that we'll need to handle.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,20 +3,14 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-metal.run.sync' },
-    { handler: 'silence', matchId: 'ember-routing.route-router' },
-    { handler: 'silence', matchId: 'ember-runtime.deprecate-copy-copyable' },
-    { handler: 'silence', matchId: 'ember-polyfills.deprecate-merge' },
-    { handler: 'silence', matchId: 'deprecate-router-events' }, // requires https://github.com/ember-a11y/ember-a11y/issues/73
-    { handler: 'silence', matchId: 'remove-handler-infos' },
-    { handler: 'silence', matchId: 'ember-name-key-usage' }, //waiting for https://github.com/offirgolan/ember-cp-validations/issues/620
-    { handler: 'silence', matchId: 'computed-property.volatile' },
     { handler: 'silence', matchId: 'common.async-computed' },
     { handler: 'silence', matchId: 'ember-metal.get-with-default' },
-    { handler: 'silence', matchId: 'ember-test-helpers.trigger-event.options-blob-array' },
-    { handler: 'silence', matchId: 'ember-string.htmlsafe-ishtmlsafe' },
     { handler: 'silence', matchId: 'implicit-injections' }, //https://github.com/simplabs/ember-simple-auth/issues/2302
     { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' }, //https://github.com/emberjs/ember-render-modifiers/issues/32
     { handler: 'silence', matchId: 'this-property-fallback' },
+    { handler: 'silence', matchId: 'ember-lifeline-deprecated-addeventlistener' },
+    { handler: 'silence', matchId: 'ember-test-helpers.setup-rendering-context.render' },
+    { handler: 'silence', matchId: 'routing.transition-methods' },
+    { handler: 'silence', matchId: 'ember-cli-page-object.is-property' },
   ],
 };
