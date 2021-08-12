245fda5a47a2889424a2630ca7da9a14385ee60d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-08-12 14:38:47 -0700

    streamlines initializing of internal attr from input arg.

--- addon/components/single-event-objective-list.js
+++ addon/components/single-event-objective-list.js
@@ -3,7 +3,7 @@ import { tracked } from '@glimmer/tracking';
 
 export default class SingleEventObjectiveList extends Component {
   @tracked groupByCompetencies = true;
-  @tracked isExpanded = false;
+  @tracked isExpanded = !!this.args.isExpandedByDefault;
 
   get showDisplayModeToggle() {
     if (!this.args.objectives) {
