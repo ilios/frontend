4ecbef1176618cd9ed8f4c39fc5f7fb2b1b10ef5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-09-01 16:26:06 -0700

    updates test coverage for school competencies expanded component.

--- tests/integration/components/school-competencies-expanded-test.js
+++ tests/integration/components/school-competencies-expanded-test.js
@@ -1,15 +1,16 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { setupMirage } from 'ember-cli-mirage/test-support';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
+import { component } from 'ilios/tests/pages/components/school-competencies-expanded';
 
 module('Integration | Component | school competencies expanded', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
   test('it renders', async function (assert) {
-    assert.expect(5);
     const school = this.server.create('school');
     const domain = this.server.create('competency', {
       school,
@@ -26,22 +27,23 @@ module('Integration | Component | school competencies expanded', function (hooks
       parent: domain,
     });
     const schoolModel = await this.owner.lookup('service:store').find('school', school.id);
-
     this.set('school', schoolModel);
     await render(hbs`<SchoolCompetenciesExpanded
-      @school={{school}}
-      @expand={{noop}}
+      @school={{this.school}}
+      @canUpdate={{true}}
+      @canDelete={{true}}
+      @canCreate={{true}}
       @collapse={{noop}}
+      @expand={{noop}}
+      @isManaging={{false}}
+      @setSchoolManageCompetencies={{noop}}
     />`);
-
-    const title = '.title';
-    const domains = '[data-test-domains] > li';
-    const domainTitle = `${domains}:nth-of-type(1)`;
-
-    assert.dom(title).hasText('Competencies (1/2)');
-    assert.dom(domains).exists({ count: 1 });
-    assert.ok(find(domainTitle).textContent.includes('domain 0'));
-    assert.ok(find(domainTitle).textContent.includes('competency 0'));
-    assert.ok(find(domainTitle).textContent.includes('competency 1'));
+    assert.equal(component.collapser.text, 'Competencies (1/2)');
+    assert.equal(component.competenciesList.items.length, 3);
+    assert.equal(component.competenciesList.items[0].title.text, 'domain 0');
+    assert.equal(component.competenciesList.items[1].title.text, 'competency 0');
+    assert.equal(component.competenciesList.items[2].title.text, 'competency 1');
+    await a11yAudit(this.element);
+    assert.ok(true, 'no a11y errors found!');
   });
 });
