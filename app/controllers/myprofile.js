117a014d12d0bd94ae4d85c9708d6c82ed244c38
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-09-14 14:19:32 +0200

    makes school and year selections for permissions in my-profile sticky.

--- app/controllers/myprofile.js
+++ app/controllers/myprofile.js
@@ -4,7 +4,11 @@ export default Controller.extend({
   queryParams: {
     showCreateNewToken: 'newtoken',
     showInvalidateTokens: 'invalidatetokens',
+    permissionsSchool: null,
+    permissionsYear: null,
   },
   showCreateNewToken: false,
   showInvalidateTokens: false,
+  permissionsSchool: null,
+  permissionsYear: null,
 });
