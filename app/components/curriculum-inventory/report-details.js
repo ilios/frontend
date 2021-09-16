a946baa77c889e4243f63834a88beaf34a82eb35
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-09-16 12:20:58 +0200

    removes Wait/Saving modal from ci report finalisation.
    instead, lock component and sub-components while report
    is being finalized.
    slaps a spinner on the finalize button for some visual indication
    that things are moving.

--- app/components/curriculum-inventory/report-details.js
+++ app/components/curriculum-inventory/report-details.js
@@ -7,13 +7,21 @@ export default class CurriculumInventoryReportDetailsComponent extends Component
   @service store;
   @tracked showFinalizeConfirmation = false;
 
+  get isFinalizing() {
+    return this.finalize.isRunning;
+  }
+
+  get canUpdate() {
+    return this.args.canUpdate && !this.isFinalizing;
+  }
+
   @dropTask
   *finalize() {
     const newExport = this.store.createRecord('curriculumInventoryExport', {
       report: this.args.report,
     });
+    this.showFinalizeConfirmation = false;
     const savedExport = yield newExport.save();
     this.args.report.set('export', savedExport);
-    this.showFinalizeConfirmation = false;
   }
 }
