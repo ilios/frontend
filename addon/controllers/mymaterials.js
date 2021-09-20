662a5f63389cb9990f398279b005c3c6e2efce7c
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-09-20 15:57:56 -0700

    Improve Query Params on MyMaterials Controller
    Instead of empty strings this uses null for the defaults making it
    easier to work with the public router service and to migrate later when
    ember makes changes.

--- addon/controllers/mymaterials.js
+++ addon/controllers/mymaterials.js
@@ -1,9 +1,38 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  queryParams: ['course', 'filter', 'sortBy'],
+export default class MyMaterialsController extends Controller {
+  queryParams = [{ courseParam: 'course' }, { filterParam: 'filter' }, { sortByParam: 'sortBy' }];
 
-  course: '',
-  filter: '',
-  sortBy: 'firstOfferingDate:desc',
-});
+  @tracked courseParam = null;
+  @tracked filterParam = null;
+  @tracked sortByParam = null;
+
+  get course() {
+    return this.courseParam ?? '';
+  }
+
+  get filter() {
+    return this.filterParam ?? '';
+  }
+
+  get sortBy() {
+    return this.sortByParam ?? 'firstOfferingDate:desc';
+  }
+
+  @action
+  setCourse(value) {
+    this.courseParam = value === '' ? null : value;
+  }
+
+  @action
+  setFilter(value) {
+    this.filterParam = value === '' ? null : value;
+  }
+
+  @action
+  setSortBy(value) {
+    this.sortByParam = value === 'firstOfferingDate:desc' ? null : value;
+  }
+}
