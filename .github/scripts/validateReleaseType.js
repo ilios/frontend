74b016cdc9edf50b9dddf8d0f77eb8e3287f9ea4
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-10-20 00:40:39 -0700

    Use a command to validate the input
    This is more readable than the script version.

--- .github/scripts/validateReleaseType.js
+++
@@ -1,5 +0,0 @@
-const types = ['major', 'minor', 'patch'];
-if (!types.includes(process.env.releaseType)) {
-  console.log(`${process.env.releaseType} is not a valid release type. Must be ones of ` + types.join(', '));
-  process.exit(1);
-}
