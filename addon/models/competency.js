580eae6f7703d56838aff3de7a846f14cf709230
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-10-20 12:11:34 -0700

    octanifies competency model.

--- addon/models/competency.js
+++ addon/models/competency.js
@@ -1,39 +1,71 @@
 import Model, { hasMany, belongsTo, attr } from '@ember-data/model';
-import { computed } from '@ember/object';
+import { use } from 'ember-could-get-used-to-this';
+import DeprecatedAsyncCP from 'ilios-common/classes/deprecated-async-cp';
 import { isEmpty } from '@ember/utils';
-import RSVP from 'rsvp';
 
-const { not } = computed;
-const { all } = RSVP;
+export default class CompetencyModel extends Model {
+  @attr('boolean')
+  active;
 
-export default Model.extend({
-  active: attr('boolean'),
-  title: attr('string'),
-  school: belongsTo('school', { async: true }),
-  parent: belongsTo('competency', { async: true, inverse: 'children' }),
-  children: hasMany('competency', { async: true, inverse: 'parent' }),
-  aamcPcrses: hasMany('aamc-pcrs', { async: true }),
-  programYears: hasMany('program-year', { async: true }),
-  programYearObjectives: hasMany('program-year-objectives', { async: true }),
-  isNotDomain: not('isDomain'),
-  isDomain: computed('parent', function () {
+  @attr('string')
+  title;
+
+  @belongsTo('school', { async: true })
+  school;
+  @belongsTo('competency', { async: true, inverse: 'children' })
+  parent;
+
+  @hasMany('competency', { async: true, inverse: 'parent' })
+  children;
+
+  @hasMany('aamc-pcrs', { async: true })
+  aamcPcrses;
+
+  @hasMany('program-year', { async: true })
+  programYears;
+
+  @hasMany('program-year-objectives', { async: true })
+  programYearObjectives;
+
+  get childCount() {
+    const childrenIds = this.hasMany('children').ids();
+    return childrenIds.length;
+  }
+
+  get isNotDomain() {
+    return !this.isDomain;
+  }
+
+  get isDomain() {
     return !this.belongsTo('parent').id();
-  }),
+  }
 
-  domain: computed('parent', 'parent.domain', async function () {
+  @use domain = new DeprecatedAsyncCP(() => [
+    this._getDomain.bind(this),
+    'competency.domain',
+    this.parent,
+  ]);
+
+  @use treeChildren = new DeprecatedAsyncCP(() => [
+    this._treeChildren.bind(this),
+    'competency.treeChildren',
+    this.children,
+  ]);
+
+  async _getDomain() {
     const parent = await this.parent;
     if (!parent) {
       return this;
     }
     return await parent.get('domain');
-  }),
+  }
 
-  treeChildren: computed('children.[]', async function () {
+  async _treeChildren() {
     const rhett = [];
     const children = await this.children;
     rhett.pushObjects(children.toArray());
 
-    const trees = await all(children.mapBy('treeChildren'));
+    const trees = await Promise.all(children.mapBy('treeChildren'));
     const competencies = trees.reduce((array, set) => {
       return array.pushObjects(set.toArray());
     }, []);
@@ -41,10 +73,5 @@ export default Model.extend({
     return rhett.uniq().filter((item) => {
       return !isEmpty(item);
     });
-  }),
-
-  childCount: computed('children.[]', function () {
-    const childrenIds = this.hasMany('children').ids();
-    return childrenIds.length;
-  }),
-});
+  }
+}
