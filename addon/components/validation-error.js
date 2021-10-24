913c23674e1b501cafd9b9edcc5c52d0454df7bc
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-10-24 15:33:06 -0700

    Use Resource to Load Errors
    Handle the async in the helper and make the component easier to use and
    template only.

--- addon/components/validation-error.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-import { use } from 'ember-could-get-used-to-this';
-import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
-
-export default class ValidationErrorComponent extends Component {
-  @use errors = new ResolveAsyncValue(() => [this.args.errors, []]);
-}
