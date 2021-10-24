5138cdc3e65b24a872d97b97c7868462397848fc
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-10-24 14:29:28 -0700

    Add has-error-for helper
    Using a resource for this simplifies the API a bit since we don't need
    to wait for the promise in the template.

---
+++ tests/integration/helpers/has-error-for-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | has-error-for', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it renders', async function (assert) {
+    this.set('inputValue', '1234');
+
+    await render(hbs`{{has-error-for this.inputValue}}`);
+
+    assert.dom(this.element).hasText('1234');
+  });
+});
