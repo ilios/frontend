9395b5fe2282b65fcdc9bfdd032ba42a6962c10c
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-10-26 09:03:18 -0700

    Don't render empty materials
    We have to check for course materials here as well. Fixed and added
    tests.

--- addon/components/week-glance-event.js
+++ addon/components/week-glance-event.js
@@ -4,4 +4,10 @@ export default class WeekGlanceEvent extends Component {
   sortString(a, b) {
     return a.localeCompare(b);
   }
+  get hasSessionLearningMaterials() {
+    return this.sessionLearningMaterials.length > 0;
+  }
+  get sessionLearningMaterials() {
+    return this.args.event.learningMaterials?.filterBy('sessionLearningMaterial') ?? [];
+  }
 }
