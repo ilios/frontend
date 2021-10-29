737bda3df7b5fbfa3b6409055000ddc89be5f5e2
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-10-28 20:34:17 -0700

    Widen range for ember-get-config
    Still API compatible, just used across many addons.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -4,7 +4,7 @@
 module.exports = {
   allowedVersions: {
     '@ember/render-modifiers': '1.0.2 || 2.0.0',
-    'ember-get-config': '0.3.0 || 0.4.0',
+    'ember-get-config': '0.3.0 || 0.4.0 || 0.5.0',
     'ember-concurrency': '1.3.0 || ^2.0.0',
   },
 };
