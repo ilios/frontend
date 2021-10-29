45da270b707c9b77adc02f2e1aa6601a869ad371
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-10-29 10:40:01 -0700

    Octaneify Program Year model

--- tests/unit/models/program-year-test.js
+++ tests/unit/models/program-year-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { waitForResource } from 'ilios-common';
 
 module('Unit | Model | ProgramYear', function (hooks) {
   setupTest(hooks);
@@ -16,11 +17,11 @@ module('Unit | Model | ProgramYear', function (hooks) {
     const program = store.createRecord('program', { id: 99, duration: 1 });
     model.set('program', program);
     model.set('startYear', 2000);
-    assert.strictEqual(await model.get('classOfYear'), 2001);
+    assert.strictEqual(await waitForResource(model, 'classOfYear'), '2001');
     program.set('duration', 5);
-    assert.strictEqual(await model.get('classOfYear'), 2005);
+    assert.strictEqual(await waitForResource(model, 'classOfYear'), '2005');
     model.set('startYear', 2001);
-    assert.strictEqual(await model.get('classOfYear'), 2006);
+    assert.strictEqual(await waitForResource(model, 'classOfYear'), '2006');
   });
 
   test('sortedProgramYearObjectives', async function (assert) {
@@ -45,10 +46,51 @@ module('Unit | Model | ProgramYear', function (hooks) {
       title: 'Foo',
       position: 2,
     });
-    const objectives = await programYear.get('sortedProgramYearObjectives');
+    const objectives = await waitForResource(programYear, 'sortedProgramYearObjectives');
     assert.strictEqual(objectives.length, 3);
     assert.strictEqual(objectives[0], programYearObjective3);
     assert.strictEqual(objectives[1], programYearObjective2);
     assert.strictEqual(objectives[2], programYearObjective1);
+  });
+
+  test('assignableVocabularies', async function (assert) {
+    assert.expect(3);
+    const store = this.owner.lookup('service:store');
+    const programYear = store.createRecord('program-year');
+    const school = store.createRecord('school');
+    const vocabulary1 = store.createRecord('vocabulary', {
+      title: 'Sowjetunion',
+      school,
+    });
+    const vocabulary2 = store.createRecord('vocabulary', {
+      title: 'DDR',
+      school,
+    });
+    store.createRecord('program', { school, programYears: [programYear] });
+
+    const vocabularies = await waitForResource(programYear, 'assignableVocabularies');
+    assert.strictEqual(vocabularies.length, 2);
+    assert.strictEqual(vocabularies[0], vocabulary2);
+    assert.strictEqual(vocabularies[1], vocabulary1);
+  });
+
+  test('associatedVocabularies', async function (assert) {
+    assert.expect(3);
+    const store = this.owner.lookup('service:store');
+    const programYear = store.createRecord('program-year');
+    const vocabulary1 = store.createRecord('vocabulary');
+    const vocabulary2 = store.createRecord('vocabulary');
+    store.createRecord('vocabulary');
+
+    const term1 = store.createRecord('term', { vocabulary: vocabulary1 });
+    const term2 = store.createRecord('term', { vocabulary: vocabulary1 });
+    const term3 = store.createRecord('term', { vocabulary: vocabulary1 });
+    const term4 = store.createRecord('term', { vocabulary: vocabulary2 });
+    programYear.terms.pushObjects([term1, term2, term3, term4]);
+
+    const vocabularies = await waitForResource(programYear, 'associatedVocabularies');
+    assert.strictEqual(vocabularies.length, 2);
+    assert.strictEqual(vocabularies[0], vocabulary1);
+    assert.strictEqual(vocabularies[1], vocabulary2);
   });
 });
