920af4d15911412d52994f1d763a09f9dafbc013
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-11-02 20:53:16 -0700

    Add timeout to wait-for-resource helper
    This ensures the test doesn't just hang forever.

--- addon-test-support/ilios-common/helpers/wait-for-resource.js
+++ addon-test-support/ilios-common/helpers/wait-for-resource.js
@@ -2,9 +2,10 @@ import { settled } from '@ember/test-helpers';
 import { next } from '@ember/runloop';
 
 export default async function waitForResource(obj, keyname) {
+  const start = performance.now();
   do {
     next(() => {});
     await settled();
-  } while (!obj[keyname]);
+  } while (!obj[keyname] && performance.now() - start < 1000);
   return obj[keyname];
 }
