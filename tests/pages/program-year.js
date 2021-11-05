a00677e43e522f0df45b24fa0806f6a36afe93c5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-11-05 15:50:44 -0700

    use page object in program year overview acceptance test.

--- tests/pages/program-year.js
+++ tests/pages/program-year.js
@@ -1,7 +1,11 @@
 import { create, visitable } from 'ember-cli-page-object';
 import details from './components/program-year/details';
+import overview from './components/program-year/overview';
+import header from './components/program-year/header';
 
 export default create({
   visit: visitable('/programs/:programId/programyears/:programYearId'),
   details,
+  overview,
+  header,
 });
