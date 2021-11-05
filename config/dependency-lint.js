91fd3ee77c33caee8653a795f90ef77c684faf7c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-11-05 09:25:25 -0700

    updates dependency-linter exceptions.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -6,5 +6,6 @@ module.exports = {
     '@ember/render-modifiers': '1.0.2 || 2.0.0',
     'ember-get-config': '0.3.0 || 0.4.0 || 0.5.0',
     'ember-concurrency': '1.3.0 || ^2.0.0',
+    'ember-modifier': '2.1.2 || 3.0.0',
   },
 };
