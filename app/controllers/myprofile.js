ae3508c7bdf243bc62e93f170145ec265023a4f6
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-11-07 12:48:42 -0800

    Octaneify MyProfile controller

--- app/controllers/myprofile.js
+++ app/controllers/myprofile.js
@@ -1,14 +1,16 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  queryParams: {
-    showCreateNewToken: 'newtoken',
-    showInvalidateTokens: 'invalidatetokens',
-    permissionsSchool: null,
-    permissionsYear: null,
-  },
-  showCreateNewToken: false,
-  showInvalidateTokens: false,
-  permissionsSchool: null,
-  permissionsYear: null,
-});
+export default class MyProfileController extends Controller {
+  queryParams = [
+    { showCreateNewToken: 'newtoken' },
+    { showInvalidateTokens: 'invalidatetokens' },
+    'permissionsYear',
+    'permissionsSchool',
+  ];
+
+  @tracked showCreateNewToken = false;
+  @tracked showInvalidateTokens = false;
+  @tracked permissionsSchool = null;
+  @tracked permissionsYear = null;
+}
