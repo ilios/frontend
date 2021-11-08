9a6f4e3503e036cf7adf8704e38c188ea479d32e
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-11-07 23:18:39 -0800

    Load permissions when school isn't defined yet
    Can't use the resource here because we can't pass an undefined school to
    the checker so we need to check if it exists first.

--- app/controllers/learner-group.js
+++ app/controllers/learner-group.js
@@ -3,13 +3,18 @@ import { tracked } from '@glimmer/tracking';
 import { use } from 'ember-could-get-used-to-this';
 import PermissionChecker from 'ilios/classes/permission-checker';
 import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
+import { inject as service } from '@ember/service';
 
 export default class LearnerGroupController extends Controller {
+  @service permissionChecker;
+
   queryParams = [{ isEditing: 'edit' }, { isBulkAssigning: 'bulkupload', sortUsersBy: 'usersBy' }];
   @use school = new ResolveAsyncValue(() => [this.model.school]);
   @use canUpdate = new PermissionChecker(() => ['canUpdateLearnerGroup', this.model]);
   @use canDelete = new PermissionChecker(() => ['canDeleteLearnerGroup', this.model]);
-  @use canCreate = new PermissionChecker(() => ['canCreateLearnerGroup', this.school]);
+  @use canCreate = new ResolveAsyncValue(() => [
+    this.school ? this.permissionChecker.canCreateLearnerGroup(this.school) : false,
+  ]);
 
   @tracked isEditing = false;
   @tracked isBulkAssigning = false;
