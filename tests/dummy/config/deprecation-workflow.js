e26860ea3a95ea651f89f1742c3c2f557548454f
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-11-07 16:21:37 -0800

    Remove silenced deprecation
    With the latest update to ESA we're no longer seeing this deprecation
    message.

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -3,7 +3,6 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'implicit-injections' }, //https://github.com/simplabs/ember-simple-auth/issues/2302
     { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' }, //https://github.com/emberjs/ember-render-modifiers/issues/32
     { handler: 'silence', matchId: 'this-property-fallback' }, //Lots of these in ember-file-upload we have linting to ensure we don't add any ourselves
   ],
