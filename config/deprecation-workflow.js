41e38cf5affb703233b4aa0097af8e7d10e4e817
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-11-07 23:09:24 -0800

    Remove get with default deprecation
    This is cleared in our app, we don't need to silence it anymore.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,7 +3,6 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-metal.get-with-default' },
     { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' }, //https://github.com/emberjs/ember-render-modifiers/issues/32
     { handler: 'silence', matchId: 'this-property-fallback' },
     { handler: 'silence', matchId: 'ember-lifeline-deprecated-addeventlistener' },
