96abc6e2055c0cc84de76c21d4ad2923bb32a7d2
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-11-08 22:10:23 -0800

    Improve empty array handling for wait for resource helper
    Sometimes a getter returns an empty array, but a few iterations later it
    would be filled. In order to watch for this I've added a 1 second delay
    when an empty array is seen to wait and see if it gets filled with data.

--- addon-test-support/ilios-common/helpers/wait-for-resource.js
+++ addon-test-support/ilios-common/helpers/wait-for-resource.js
@@ -1,11 +1,41 @@
-import { settled } from '@ember/test-helpers';
 import { next } from '@ember/runloop';
+import { settled } from '@ember/test-helpers';
 
 export default async function waitForResource(obj, keyname) {
-  const start = performance.now();
-  do {
-    next(() => {});
-    await settled();
-  } while (!obj[keyname] && performance.now() - start < 1000);
-  return obj[keyname];
+  const da = new DelayedAccess(obj, keyname);
+  return da.getValue();
+}
+
+class DelayedAccess {
+  value = undefined;
+  start = performance.now();
+
+  constructor(obj, keyname) {
+    this.obj = obj;
+    this.keyname = keyname;
+  }
+
+  get done() {
+    if (Array.isArray(this.value) && this.value.length === 0) {
+      if (!this.emptyArrayTimer) {
+        this.emptyArrayTimer = performance.now();
+      }
+      return performance.now() - this.emptyArrayTimer > 1000;
+    }
+    if (performance.now() - this.start > 1000) {
+      return true;
+    }
+
+    return Boolean(this.value);
+  }
+
+  async getValue() {
+    do {
+      this.value = this.obj[this.keyname];
+      next(() => {});
+      await settled();
+    } while (!this.done);
+
+    return this.value;
+  }
 }
