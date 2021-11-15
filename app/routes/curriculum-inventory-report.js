6730d37007e97043ab74aba1734ea5114309af90
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-11-15 14:48:38 -0800

    explicitly load model.

--- app/routes/curriculum-inventory-report.js
+++ app/routes/curriculum-inventory-report.js
@@ -3,8 +3,16 @@ import { inject as service } from '@ember/service';
 
 export default class CurriculumInventoryReportReport extends Route {
   @service session;
+  @service store;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+  }
+
+  model(params) {
+    return this.store.findRecord(
+      'curriculum-inventory-report',
+      params.curriculum_inventory_report_id
+    );
   }
 }
