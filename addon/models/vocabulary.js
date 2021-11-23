444dfe4004d43320e9b6a0ec182733fae464e0bc
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-11-22 16:22:48 -0800

    rm unused resource and corresponding method.

--- addon/models/vocabulary.js
+++ addon/models/vocabulary.js
@@ -1,6 +1,6 @@
 import Model, { belongsTo, hasMany, attr } from '@ember-data/model';
 import Inflector from 'ember-inflector';
-import { all, filter } from 'rsvp';
+import { filter } from 'rsvp';
 import { use } from 'ember-could-get-used-to-this';
 import DeprecatedAsyncCP from 'ilios-common/classes/deprecated-async-cp';
 
@@ -24,22 +24,11 @@ export default class Vocabulary extends Model {
     'vocabulary.topLevelTerms',
   ]);
 
-  @use associatedVocabularies = new DeprecatedAsyncCP(() => [
-    this.getAssociatedVocabularies.bind(this),
-    'term.associatedVocabularies',
-  ]);
-
   async getTopLevelTerms() {
     const terms = await this.terms;
     return filter(terms.toArray(), async (term) => {
       return !(await term.parent);
     });
-  }
-
-  async getAssociatedVocabularies() {
-    const terms = await this.terms;
-    const vocabularies = await all(terms.toArray().mapBy('vocabulary'));
-    return vocabularies.uniq().sortBy('title');
   }
 
   get termCount() {
