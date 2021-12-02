02ea65cb8d0fbb7b7751ea9e0451767080a67f7b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-12-02 10:01:54 -0800

    absorbs publication-status into report-list-item component.

--- app/components/curriculum-inventory/publication-status.js
+++
@@ -1,5 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class CurriculumInventoryReportPublicationStatus extends Component {
-  isFinalized = this.args.item.belongsTo('export').id();
-}
--- tests/pages/components/curriculum-inventory/publication-status.js
+++
@@ -1,9 +0,0 @@
-import { create, hasClass } from 'ember-cli-page-object';
-
-const definition = {
-  scope: '[data-test-curriculum-inventory-report-publication-status]',
-  isFinalized: hasClass('published'),
-};
-
-export default definition;
-export const component = create(definition);
