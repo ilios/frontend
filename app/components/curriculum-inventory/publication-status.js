c5f9296ec95a61d08817fb4788fa943a7ae26791
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-12-02 09:04:31 -0800

    no need to track isFinalized attr, remove the decorator.

--- app/components/curriculum-inventory/publication-status.js
+++ app/components/curriculum-inventory/publication-status.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 
 export default class CurriculumInventoryReportPublicationStatus extends Component {
-  @tracked isFinalized = this.args.item.belongsTo('export').id();
+  isFinalized = this.args.item.belongsTo('export').id();
 }
