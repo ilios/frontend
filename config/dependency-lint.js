7e1a7c3ca8b7d2f26bf520714380348188bf87b9
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-12-19 22:19:53 -0800

    Slightly widen dependency range
    We can accept any of the v2 line here and should.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   allowedVersions: {
-    '@ember/render-modifiers': '1.0.2 || 2.0.0',
+    '@ember/render-modifiers': '1.0.2 || ^2.0.0',
     'ember-get-config': '0.3.0 || 0.4.0 || 0.5.0',
     'ember-concurrency': '1.3.0 || ^2.0.0',
     'ember-modifier': '2.1.2 || ^3.0.0',
