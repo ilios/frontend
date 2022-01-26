893f92b2652069e7657e70e8c757c2e2e0e0a9ee
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-01-26 15:14:03 -0800

    Update to new mirage patterns
    We need to install mirage directly now because it isn't provided by
    common. Also updating to the new mirage config style along with the
    routes import changes from common.

--- mirage/config.js
+++ mirage/config.js
@@ -1,29 +1,78 @@
-import moment from 'moment';
-import Mirage from 'ember-cli-mirage';
+import commonRoutes from './routes';
 import ENV from 'ilios/config/environment';
+import { discoverEmberDataModels } from 'ember-cli-mirage';
+import { createServer, Response } from 'miragejs';
+import moment from 'moment';
 
 const { apiVersion } = ENV;
 
-import setupMirage from './setup';
+export default function (config) {
+  let finalConfig = {
+    ...config,
+    models: { ...discoverEmberDataModels(), ...config.models },
+    routes() {
+      this.timing = 100;
+      this.namespace = '/';
+      this.passthrough('/write-coverage');
+      commonRoutes(this);
+      this.post('auth/login', function (schema, request) {
+        const errors = [];
+        var attrs = JSON.parse(request.requestBody);
+        if (!('username' in attrs) || !attrs.username) {
+          errors.push('missingUsername');
+        }
+        if (!('password' in attrs) || !attrs.password) {
+          errors.push('missingPassword');
+        }
+        const username = attrs.username.toLowerCase();
+        if (errors.length === 0) {
+          if (username === 'demo' && attrs.password === 'demo') {
+            const now = moment();
+            const nextWeek = now.clone().add(1, 'week');
+            const header = '{"alg":"none"}';
+            const body = `{"iss": "ilios","aud": "ilios","iat": "${now.format(
+              'X'
+            )}","exp": "${nextWeek.format('X')}","user_id": 4136}`;
 
-export default function () {
-  this.timing = 100;
-  this.namespace = '/';
-  this.passthrough('/write-coverage');
-  setupMirage(this);
+            const encodedData = window.btoa(header) + '.' + window.btoa(body) + '.';
+            return {
+              jwt: encodedData,
+            };
+          } else {
+            errors.push('badCredentials');
+          }
+        }
+        return new Response(400, {}, { errors: errors });
+      });
 
-  this.post('auth/login', function (schema, request) {
-    const errors = [];
-    var attrs = JSON.parse(request.requestBody);
-    if (!('username' in attrs) || !attrs.username) {
-      errors.push('missingUsername');
-    }
-    if (!('password' in attrs) || !attrs.password) {
-      errors.push('missingPassword');
-    }
-    const username = attrs.username.toLowerCase();
-    if (errors.length === 0) {
-      if (username === 'demo' && attrs.password === 'demo') {
+      this.get('auth/logout', function () {
+        return new Response(200);
+      });
+
+      this.get('auth/whoami', function () {
+        return {
+          userId: 4136,
+        };
+      });
+
+      this.get('application/config', function () {
+        return {
+          config: {
+            type: 'form',
+            apiVersion,
+          },
+        };
+        // return { config: {
+        //   type: 'shibboleth',
+        //   shibbolethLoginUrl: '/fakeshiblogin'
+        // }};
+      });
+
+      this.get('auth/token', function () {
+        //un comment to send unauthenticated user data
+        // return {
+        //   jwt: null
+        // };
         const now = moment();
         const nextWeek = now.clone().add(1, 'week');
         const header = '{"alg":"none"}';
@@ -35,55 +84,13 @@ export default function () {
         return {
           jwt: encodedData,
         };
-      } else {
-        errors.push('badCredentials');
-      }
-    }
-    return new Mirage.Response(400, {}, { errors: errors });
-  });
+      });
 
-  this.get('auth/logout', function () {
-    return new Mirage.Response(200);
-  });
+      this.post('errors', function () {
+        //doesn't do anything, just swallows errors
+      });
+    },
+  };
 
-  this.get('auth/whoami', function () {
-    return {
-      userId: 4136,
-    };
-  });
-
-  this.get('application/config', function () {
-    return {
-      config: {
-        type: 'form',
-        apiVersion,
-      },
-    };
-    // return { config: {
-    //   type: 'shibboleth',
-    //   shibbolethLoginUrl: '/fakeshiblogin'
-    // }};
-  });
-
-  this.get('auth/token', function () {
-    //un comment to send unauthenticated user data
-    // return {
-    //   jwt: null
-    // };
-    const now = moment();
-    const nextWeek = now.clone().add(1, 'week');
-    const header = '{"alg":"none"}';
-    const body = `{"iss": "ilios","aud": "ilios","iat": "${now.format(
-      'X'
-    )}","exp": "${nextWeek.format('X')}","user_id": 4136}`;
-
-    const encodedData = window.btoa(header) + '.' + window.btoa(body) + '.';
-    return {
-      jwt: encodedData,
-    };
-  });
-
-  this.post('errors', function () {
-    //doesn't do anything, just swallows errors
-  });
+  return createServer(finalConfig);
 }
