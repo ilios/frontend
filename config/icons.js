5c1b3fdcd3e36aac1c4702bb066723f5f97c1449
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-02-11 10:51:41 -0800

    replaces home icon into house icon.

--- config/icons.js
+++ config/icons.js
@@ -29,7 +29,7 @@ module.exports = function () {
       'gears',
       'globe',
       'graduation-cap',
-      'home',
+      'house',
       'info-circle',
       'italic',
       'level-down-alt',
