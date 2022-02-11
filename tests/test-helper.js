969993aa98697f8843a1f9f11657c04015c22afc
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-02-11 09:58:17 -0800

    Install page object for tests
    We're not providing this from common anymore so we need to add it
    ourselves. This beta is embroider compatible and safe to use since we're
    only using it in tests.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,9 +4,12 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import DefaultAdapter from 'ember-cli-page-object/adapters/rfc268';
+import { setAdapter } from 'ember-cli-page-object/adapters';
 
 import './helpers/flash-message';
 
+setAdapter(new DefaultAdapter());
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
