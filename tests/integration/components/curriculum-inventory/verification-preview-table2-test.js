6bb87952e4c84cae8c91d3c4369b6d95905eb032
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-02-14 11:10:00 -0800

    consolidate start/end academic level into one column.
    this realigns our preview tables with how the actual verification report
    lays out data.

--- tests/integration/components/curriculum-inventory/verification-preview-table2-test.js
+++ tests/integration/components/curriculum-inventory/verification-preview-table2-test.js
@@ -42,12 +42,11 @@ module(
         component.title,
         'Table 2: Primary Instructional Method by Non-Clerkship Sequence Block'
       );
-      assert.strictEqual(component.table.firstHeadings.length, 4);
+      assert.strictEqual(component.table.firstHeadings.length, 3);
       assert.strictEqual(component.table.firstHeadings[0].text, 'Non-Clerkship Sequence Blocks');
-      assert.strictEqual(component.table.firstHeadings[1].text, 'Start Level');
-      assert.strictEqual(component.table.firstHeadings[2].text, 'End Level');
+      assert.strictEqual(component.table.firstHeadings[1].text, 'Phases (Start - End)');
       assert.strictEqual(
-        component.table.firstHeadings[3].text,
+        component.table.firstHeadings[2].text,
         'Number of Formal Instructional Hours Per Course'
       );
       assert.strictEqual(component.table.secondHeadings.length, 3);
@@ -55,26 +54,23 @@ module(
       assert.strictEqual(component.table.secondHeadings[1].text, 'bar');
       assert.strictEqual(component.table.secondHeadings[2].text, 'Total');
       assert.strictEqual(component.table.rows.length, 2);
-      assert.strictEqual(component.table.rows[0].cells.length, 6);
+      assert.strictEqual(component.table.rows[0].cells.length, 5);
       assert.strictEqual(component.table.rows[0].cells[0].text, 'Alpha');
-      assert.strictEqual(component.table.rows[0].cells[1].text, '2');
-      assert.strictEqual(component.table.rows[0].cells[2].text, '3');
-      assert.strictEqual(component.table.rows[0].cells[3].text, '2.50');
-      assert.strictEqual(component.table.rows[0].cells[4].text, '6.67');
-      assert.strictEqual(component.table.rows[0].cells[5].text, '9.17');
+      assert.strictEqual(component.table.rows[0].cells[1].text, '2 - 3');
+      assert.strictEqual(component.table.rows[0].cells[2].text, '2.50');
+      assert.strictEqual(component.table.rows[0].cells[3].text, '6.67');
+      assert.strictEqual(component.table.rows[0].cells[4].text, '9.17');
       assert.strictEqual(component.table.rows[1].cells[0].text, 'Beta');
-      assert.strictEqual(component.table.rows[1].cells[1].text, '4');
-      assert.strictEqual(component.table.rows[1].cells[2].text, '5');
-      assert.strictEqual(component.table.rows[1].cells[3].text, '0.83');
-      assert.strictEqual(component.table.rows[1].cells[4].text, '');
-      assert.strictEqual(component.table.rows[1].cells[5].text, '0.83');
-      assert.strictEqual(component.table.footer.length, 6);
+      assert.strictEqual(component.table.rows[1].cells[1].text, '4 - 5');
+      assert.strictEqual(component.table.rows[1].cells[2].text, '0.83');
+      assert.strictEqual(component.table.rows[1].cells[3].text, '');
+      assert.strictEqual(component.table.rows[1].cells[4].text, '0.83');
+      assert.strictEqual(component.table.footer.length, 5);
       assert.strictEqual(component.table.footer[0].text, 'Total');
       assert.strictEqual(component.table.footer[1].text, '');
-      assert.strictEqual(component.table.footer[2].text, '');
-      assert.strictEqual(component.table.footer[3].text, '3.33');
-      assert.strictEqual(component.table.footer[4].text, '6.67');
-      assert.strictEqual(component.table.footer[5].text, '10.00');
+      assert.strictEqual(component.table.footer[2].text, '3.33');
+      assert.strictEqual(component.table.footer[3].text, '6.67');
+      assert.strictEqual(component.table.footer[4].text, '10.00');
     });
   }
 );
