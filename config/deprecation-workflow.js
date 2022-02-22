f279a9473936680613494bc4af6e7689012555fc
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-02-22 11:53:46 -0800

    Remove old deprecation silencer
    We're not seeing this anymore, I think we removed this library.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -5,7 +5,6 @@ window.deprecationWorkflow.config = {
   workflow: [
     { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' }, //https://github.com/emberjs/ember-render-modifiers/issues/32
     { handler: 'silence', matchId: 'this-property-fallback' }, //https://github.com/sethwebster/ember-cli-new-version/pull/91
-    { handler: 'silence', matchId: 'ember-lifeline-deprecated-addeventlistener' },
     { handler: 'silence', matchId: 'common.user-performs-non-learner-function' },
     { handler: 'silence', matchId: 'common.curriculum-inventory-report-is-finalized' },
     { handler: 'silence', matchId: 'common.async-computed' },
