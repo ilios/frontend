7a05185ca60df80b511ee401477f0f50ce3efe5e
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-03-08 14:08:35 -0800

    replaces deprecated routes methods calls with router methods.

--- addon/routes/dashboard.js
+++ addon/routes/dashboard.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class DashboardRoute extends Route {
+  @service router;
+
   beforeModel() {
-    this.transitionTo('dashboard.week');
+    this.router.transitionTo('dashboard.week');
   }
 }
--- addon/routes/dashboard/index.js
+++ addon/routes/dashboard/index.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class DashboardIndexRoute extends Route {
+  @service router;
+
   beforeModel() {
-    this.replaceWith('dashboard.week');
+    this.router.replaceWith('dashboard.week');
   }
 }
