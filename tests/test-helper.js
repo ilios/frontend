6924c8735fa94a6b7803aba18410c9e97dd6a732
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-03-11 14:41:27 -0800

    realigns test helper with frontend.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,15 @@
 import Application from 'lti-app/app';
 import config from 'lti-app/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import DefaultAdapter from 'ember-cli-page-object/adapters/rfc268';
+import { setAdapter } from 'ember-cli-page-object/adapters';
 
+setAdapter(new DefaultAdapter());
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
