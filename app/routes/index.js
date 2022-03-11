9eb70305818266768d07047aa137d6c2db41e2f6
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-03-11 15:33:42 -0800

    rm unnecessary route params.

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,20 +7,6 @@ export default class IndexRoute extends Route {
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login-error');
-    this.router.replaceWith('dashboard', {
-      queryParams: {
-        cohorts: null,
-        courseFilters: null,
-        courseLevels: null,
-        courses: null,
-        mySchedule: null,
-        reportYear: null,
-        sessionTypes: null,
-        show: null,
-        showFilters: null,
-        terms: null,
-        view: null,
-      },
-    });
+    this.router.replaceWith('dashboard');
   }
 }
