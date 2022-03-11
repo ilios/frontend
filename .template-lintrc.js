9f8aa267a202d3f1497e42dc5a63c58bcf10281b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-03-11 13:32:57 -0800

    updates template linter config.

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane'
+  plugins: ['@ilios/ember-template-lint-plugin'],
+  extends: 'ilios:recommended',
+  rules: {
+    'no-implicit-this': {
+      //our helpers which do not take arguments have to be listed here
+      allow: ['noop'],
+    },
+  },
 };
