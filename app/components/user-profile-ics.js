2e83476753054fc844099d87d3152d204433fb8f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-03-30 14:50:16 -0700

    use page objects for user-profile-ics component tests.

--- app/components/user-profile-ics.js
+++ app/components/user-profile-ics.js
@@ -36,7 +36,7 @@ export default class UserProfileIcsComponent extends Component {
    * Generate a random token from a combination of
    * the user id, a random string and the current time
    * Implementation lifted from https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
-   * @param String userId
+   * @param userId
    * @return String
    */
   async randomToken(userId) {
@@ -45,8 +45,7 @@ export default class UserProfileIcsComponent extends Component {
     const msgUint8 = new TextEncoder().encode(userId + randomValue + now); // encode as (utf-8) Uint8Array
     const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8); // hash the message
     const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
-    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
-    return hashHex;
+    return hashArray.map((b) => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
   }
 
   @dropTask
