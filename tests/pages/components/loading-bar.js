218e60ec82ff7814582532334312e2da00ebde51
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-05-25 21:37:19 -0700

    Remove the loading bar
    We have better transitions and significantly faster loading from the API
    making this component unnecessary.

--- tests/pages/components/loading-bar.js
+++
@@ -1,9 +0,0 @@
-import { attribute, create } from 'ember-cli-page-object';
-
-export default create({
-  scope: '[data-test-loading-bar]',
-  bar: {
-    scope: '[data-test-bar]',
-    value: attribute('data-value'),
-  },
-});
