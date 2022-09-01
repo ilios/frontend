1b4ff9d01d074e904aba1f201299eff45ea277e1
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-08-31 21:50:54 -0700

    Extract current date from flatpickr
    Instead of relying on the value of the input element we can get the
    current date from the flatpickr instance which seems to be more reliable
    on different browsers.

--- addon-test-support/ilios-common/index.js
+++ addon-test-support/ilios-common/index.js
@@ -1,7 +1,7 @@
 export { default as setupAuthentication } from './helpers/setup-authentication';
 export { default as waitForResource } from './helpers/wait-for-resource';
 export { freezeDateAt, unfreezeDate } from './helpers/mockdate';
-export { flatpickrDatePicker } from './helpers/flatpickr-date-picker';
+export { flatpickrDatePicker, flatpickrDateValue } from './helpers/flatpickr-date-picker';
 export {
   fillInFroalaEditor,
   froalaEditorValue,
--- addon-test-support/ilios-common/page-objects/components/date-picker.js
+++ addon-test-support/ilios-common/page-objects/components/date-picker.js
@@ -1,10 +1,10 @@
-import { create, value } from 'ember-cli-page-object';
-import { flatpickrDatePicker } from 'ilios-common';
+import { create } from 'ember-cli-page-object';
+import { flatpickrDatePicker, flatpickrDateValue } from 'ilios-common';
 
 const definition = {
   scope: '[data-test-date-picker]',
   set: flatpickrDatePicker(),
-  value: value(),
+  value: flatpickrDateValue(),
 };
 
 export default definition;
