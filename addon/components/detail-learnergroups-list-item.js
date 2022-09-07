090caa93819928a7500d5e5ad4ebbc8cbd570b83
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-09-07 16:48:31 -0700

    rm kruft.

--- addon/components/detail-learnergroups-list-item.js
+++ addon/components/detail-learnergroups-list-item.js
@@ -1,19 +1,9 @@
 import Component from '@glimmer/component';
 import { use } from 'ember-could-get-used-to-this';
 import AsyncProcess from 'ilios-common/classes/async-process';
-import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
 
 export default class DetailLearnergroupsListItemComponent extends Component {
   @use allParentTitles = new AsyncProcess(() => [
     this.args.group.getAllParentTitles.bind(this.args.group),
   ]);
-
-  @use parent = new ResolveAsyncValue(() => [this.args.group.parent]);
-
-  get isTopLevel() {
-    if (undefined === this.parent) {
-      return false;
-    }
-    return !this.parent;
-  }
 }
