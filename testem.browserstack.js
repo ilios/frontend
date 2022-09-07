74775dc24b428a9bc9222b71c70e50bdf4ae47f4
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-09-07 13:00:00 -0700

    Improve test page for testem
    Adding some more params to clean up our output, stolen from
    ember-cli-browserstack's own config.

--- testem.browserstack.js
+++ testem.browserstack.js
@@ -66,7 +66,7 @@ const BrowserStackLaunchers = {
 };
 
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
+  test_page: 'tests/index.html?hidepassed&hideskipped&timeout=60000',
   timeout: 1800,
   reporter: FailureOnlyPerBrowserReporter,
   browser_start_timeout: 2000,
