e8be13fdd1b3a6ad7f20d20ea4cc9e976b77d139
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-09-08 09:33:35 -0700

    Add uuid polyfill
    We need this for Firefox ESR 91 (and possibly other browsers as well).

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ module.exports = {
         ],
       },
     },
+    '@embroider/macros': {
+      setConfig: {
+        '@ember-data/store': {
+          polyfillUUID: true,
+        },
+      },
+    },
   },
 
   included: function () {
