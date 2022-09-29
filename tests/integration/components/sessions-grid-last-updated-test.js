b9f69a2791165110af32d7d7e303e9b3fec99fc9
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-09-28 20:05:49 -0700

    Replace moment with luxon in test

--- tests/integration/components/sessions-grid-last-updated-test.js
+++ tests/integration/components/sessions-grid-last-updated-test.js
@@ -4,21 +4,24 @@ import { setupIntl } from 'ember-intl/test-support';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import moment from 'moment';
+import { DateTime } from 'luxon';
 
 module('Integration | Component | sessions-grid-last-updated', function (hooks) {
   setupRenderingTest(hooks);
   setupIntl(hooks, 'en-us');
   setupMirage(hooks);
 
-  hooks.beforeEach(function () {
-    this.owner.lookup('service:moment').setLocale('en');
-  });
-
   test('it renders', async function (assert) {
     assert.expect(1);
     const session = this.server.create('session', {
-      updatedAt: moment('2019-07-09 17:00:00').toDate(),
+      updatedAt: DateTime.fromObject({
+        year: 2019,
+        month: 7,
+        day: 9,
+        hour: 17,
+        minute: 0,
+        second: 0,
+      }).toJSDate(),
     });
 
     const sessionModel = await this.owner.lookup('service:store').findRecord('session', session.id);
