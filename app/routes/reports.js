d7995debb7fe6f1aeef80619b10090e34ab2e412
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-09-30 15:15:18 -0700

    ensure user is authn on reports route.

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -1,3 +1,12 @@
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
-export default class ReportsRoute extends Route {}
+export default class ReportsRoute extends Route {
+  @service store;
+  @service currentUser;
+  @service session;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+  }
+}
