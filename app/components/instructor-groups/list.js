45ad828e3dd9728cb11f560b2fab475041cd7fb5
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-09-30 22:17:01 -0700

    Make sort code more readable
    Too much complexity in all the return statements before, splitting this
    up seems more readable.

--- app/components/instructor-groups/list.js
+++ app/components/instructor-groups/list.js
@@ -1,11 +1,9 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class LearnerGroupListComponent extends Component {
   @service intl;
-  @tracked sortBy = 'title';
 
   get sortedAscending() {
     return !this.args.sortBy.includes(':desc');
@@ -19,33 +17,25 @@ export default class LearnerGroupListComponent extends Component {
     this.args.setSortBy(what);
   }
 
-  get sortGroups() {
-    const locale = this.intl.get('primaryLocale');
-    if (this.args.sortBy === 'title') {
-      return (instructorGroupA, instructorGroupB) => {
-        return instructorGroupA.title.localeCompare(instructorGroupB.title, locale, {
+  get sortedGroups() {
+    const sortBy = this.args.sortBy;
+    const instructorGroups = this.args.instructorGroups.slice();
+    if (sortBy.includes('title')) {
+      const locale = this.intl.get('primaryLocale');
+      return instructorGroups.sort((a, b) =>
+        a.title.localeCompare(b.title, locale, {
           numeric: true,
-        });
-      };
+        })
+      );
     }
-    if (this.args.sortBy === 'title:desc') {
-      return (instructorGroupA, instructorGroupB) => {
-        return instructorGroupB.title.localeCompare(instructorGroupA.title, locale, {
-          numeric: true,
-        });
-      };
-    }
-    if (this.args.sortBy === 'courses') {
-      return (a, b) => {
-        return a.courses.length - b.courses.length;
-      };
-    }
-    if (this.args.sortBy === 'courses:desc') {
-      return (a, b) => {
-        return b.courses.length - a.courses.length;
-      };
+    if (sortBy.includes('courses')) {
+      return instructorGroups.sort((a, b) => a.courses.length - b.courses.length);
     }
 
-    return this.args.sortBy;
+    return instructorGroups.sort((a, b) => a.usersCount - b.usersCount);
+  }
+
+  get orderedGroups() {
+    return this.sortedAscending ? this.sortedGroups : this.sortedGroups.reverse();
   }
 }
