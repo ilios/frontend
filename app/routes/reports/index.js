906485c03a016c538ae2a06c4c72d87daa14d910
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-10-03 11:24:22 -0700

    adds missing reports/index route. have it forward to reports/subject.

---
+++ app/routes/reports/index.js
@@ -0,0 +1,17 @@
+import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
+
+export default class ReportsIndexRoute extends Route {
+  @service session;
+  @service router;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+    this.router.replaceWith('reports.subject', {
+      queryParams: {
+        report: null,
+        reportYear: null,
+      },
+    });
+  }
+}
