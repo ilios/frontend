efb7bf3d573bed22302466022347b05234a7043d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-10-03 11:26:25 -0700

    rm broken route replacement in reports route.
    route replacement happens correctly in reports/index, but it's not working in
    here.

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -3,15 +3,8 @@ import { inject as service } from '@ember/service';
 
 export default class ReportsRoute extends Route {
   @service session;
-  @service router;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
-    this.router.replaceWith('reports.subject', {
-      queryParams: {
-        report: null,
-        reportYear: null,
-      },
-    });
   }
 }
