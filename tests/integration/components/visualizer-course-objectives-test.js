3c790a5d94a951eb69ca0a5c03d7bdd0ff1b5e00
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-10-07 11:58:24 -0700

    updates test coverage to include data table.

--- tests/integration/components/visualizer-course-objectives-test.js
+++ tests/integration/components/visualizer-course-objectives-test.js
@@ -11,10 +11,29 @@ module('Integration | Component | visualizer-course-objectives', function (hooks
   setupIntl(hooks, 'en-us');
   setupMirage(hooks);
 
-  test('it renders', async function (assert) {
+  hooks.beforeEach(async function () {
     const course = this.server.create('course');
-    const courseObjectives = this.server.createList('courseObjective', 3, {
+    const competencies = this.server.createList('competency', 3);
+    const programYearObjective1 = this.server.create('programYearObjective', {
+      competency: competencies[0],
+    });
+    const programYearObjective2 = this.server.create('programYearObjective', {
+      competency: competencies[1],
+    });
+    const programYearObjective3 = this.server.create('programYearObjective', {
+      competency: competencies[2],
+    });
+    const courseObjective1 = this.server.create('courseObjective', {
       course,
+      programYearObjectives: [programYearObjective1],
+    });
+    const courseObjective2 = this.server.create('courseObjective', {
+      course,
+      programYearObjectives: [programYearObjective2],
+    });
+    const courseObjective3 = this.server.create('courseObjective', {
+      course,
+      programYearObjectives: [programYearObjective3],
     });
     const session1 = this.server.create('session', {
       title: 'Berkeley Investigations',
@@ -30,15 +49,15 @@ module('Integration | Component | visualizer-course-objectives', function (hooks
     });
     this.server.create('sessionObjective', {
       session: session1,
-      courseObjectives: [courseObjectives[0]],
+      courseObjectives: [courseObjective1],
     });
     this.server.create('sessionObjective', {
       session: session2,
-      courseObjectives: [courseObjectives[1]],
+      courseObjectives: [courseObjective2],
     });
     this.server.create('sessionObjective', {
       session: session3,
-      courseObjectives: [courseObjectives[2]],
+      courseObjectives: [courseObjective3],
     });
 
     this.server.create('offering', {
@@ -57,11 +76,14 @@ module('Integration | Component | visualizer-course-objectives', function (hooks
       endDate: new Date('2019-12-05T21:00:00'),
     });
 
-    const courseModel = await this.owner.lookup('service:store').findRecord('course', course.id);
+    this.course = await this.owner.lookup('service:store').findRecord('course', course.id);
+  });
 
-    this.set('course', courseModel);
-
-    await render(hbs`<VisualizerCourseObjectives @course={{this.course}} @isIcon={{false}} />`);
+  test('it renders', async function (assert) {
+    this.set('course', this.course);
+    await render(
+      hbs`<VisualizerCourseObjectives @course={{this.course}} @isIcon={{false}} @showDataTable={{true}} />`
+    );
     //let the chart animations finish
     await waitFor('.loaded');
     await waitFor('svg .slice');
@@ -72,6 +94,129 @@ module('Integration | Component | visualizer-course-objectives', function (hooks
     assert.notOk(component.unlinkedObjectives.isPresent);
     assert.strictEqual(component.untaughtObjectives.items.length, 1);
     assert.strictEqual(component.untaughtObjectives.items[0].text, 'course objective 2');
+    assert.strictEqual(component.dataTable.header.percentage.text, 'Percentage');
+    assert.strictEqual(component.dataTable.header.objective.text, 'Course Objective');
+    assert.strictEqual(component.dataTable.header.competency.text, 'Competency');
+    assert.strictEqual(component.dataTable.header.sessions.text, 'Sessions');
+    assert.strictEqual(component.dataTable.header.minutes.text, 'Minutes');
+    assert.strictEqual(component.dataTable.rows.length, 3);
+    assert.strictEqual(component.dataTable.rows[0].percentage, '77.8%');
+    assert.strictEqual(component.dataTable.rows[0].objective, 'course objective 0');
+    assert.strictEqual(component.dataTable.rows[0].competency, 'competency 0');
+    assert.strictEqual(component.dataTable.rows[0].sessions.links.length, 1);
+    assert.strictEqual(
+      component.dataTable.rows[0].sessions.links[0].text,
+      'Berkeley Investigations'
+    );
+    assert.strictEqual(component.dataTable.rows[0].sessions.links[0].url, '/courses/1/sessions/1');
+    assert.strictEqual(component.dataTable.rows[0].minutes, '630');
+    assert.strictEqual(component.dataTable.rows[1].percentage, '22.2%');
+    assert.strictEqual(component.dataTable.rows[1].objective, 'course objective 1');
+    assert.strictEqual(component.dataTable.rows[1].competency, 'competency 1');
+    assert.strictEqual(component.dataTable.rows[1].sessions.links.length, 1);
+    assert.strictEqual(
+      component.dataTable.rows[1].sessions.links[0].text,
+      'The San Leandro Horror'
+    );
+    assert.strictEqual(component.dataTable.rows[1].sessions.links[0].url, '/courses/1/sessions/2');
+    assert.strictEqual(component.dataTable.rows[1].minutes, '180');
+    assert.strictEqual(component.dataTable.rows[2].percentage, '0.0%');
+    assert.strictEqual(component.dataTable.rows[2].objective, 'course objective 2');
+    assert.strictEqual(component.dataTable.rows[2].competency, 'competency 2');
+    assert.strictEqual(component.dataTable.rows[2].sessions.links.length, 1);
+    assert.strictEqual(component.dataTable.rows[2].sessions.links[0].text, 'Empty Session');
+    assert.strictEqual(component.dataTable.rows[2].sessions.links[0].url, '/courses/1/sessions/3');
+    assert.strictEqual(component.dataTable.rows[2].minutes, '0');
+  });
+
+  test('sort data table by percentages', async function (assert) {
+    this.set('course', this.course);
+    await render(
+      hbs`<VisualizerCourseObjectives @course={{this.course}} @isIcon={{false}} @showDataTable={{true}} />`
+    );
+    assert.strictEqual(component.dataTable.rows[0].percentage, '77.8%');
+    assert.strictEqual(component.dataTable.rows[1].percentage, '22.2%');
+    assert.strictEqual(component.dataTable.rows[2].percentage, '0.0%');
+    await component.dataTable.header.percentage.toggle();
+    assert.strictEqual(component.dataTable.rows[0].percentage, '0.0%');
+    assert.strictEqual(component.dataTable.rows[1].percentage, '22.2%');
+    assert.strictEqual(component.dataTable.rows[2].percentage, '77.8%');
+    await component.dataTable.header.percentage.toggle();
+    assert.strictEqual(component.dataTable.rows[0].percentage, '77.8%');
+    assert.strictEqual(component.dataTable.rows[1].percentage, '22.2%');
+    assert.strictEqual(component.dataTable.rows[2].percentage, '0.0%');
+  });
+
+  test('sort data-table by objectives', async function (assert) {
+    this.set('course', this.course);
+    await render(
+      hbs`<VisualizerCourseObjectives @course={{this.course}} @isIcon={{false}} @showDataTable={{true}} />`
+    );
+    assert.strictEqual(component.dataTable.rows[0].objective, 'course objective 0');
+    assert.strictEqual(component.dataTable.rows[1].objective, 'course objective 1');
+    assert.strictEqual(component.dataTable.rows[2].objective, 'course objective 2');
+    await component.dataTable.header.objective.toggle();
+    assert.strictEqual(component.dataTable.rows[0].objective, 'course objective 0');
+    assert.strictEqual(component.dataTable.rows[1].objective, 'course objective 1');
+    assert.strictEqual(component.dataTable.rows[2].objective, 'course objective 2');
+    await component.dataTable.header.objective.toggle();
+    assert.strictEqual(component.dataTable.rows[0].objective, 'course objective 2');
+    assert.strictEqual(component.dataTable.rows[1].objective, 'course objective 1');
+    assert.strictEqual(component.dataTable.rows[2].objective, 'course objective 0');
+  });
+
+  test('sort data-table by competencies', async function (assert) {
+    this.set('course', this.course);
+    await render(
+      hbs`<VisualizerCourseObjectives @course={{this.course}} @isIcon={{false}} @showDataTable={{true}} />`
+    );
+    assert.strictEqual(component.dataTable.rows[0].competency, 'competency 0');
+    assert.strictEqual(component.dataTable.rows[1].competency, 'competency 1');
+    assert.strictEqual(component.dataTable.rows[2].competency, 'competency 2');
+    await component.dataTable.header.competency.toggle();
+    assert.strictEqual(component.dataTable.rows[0].competency, 'competency 0');
+    assert.strictEqual(component.dataTable.rows[1].competency, 'competency 1');
+    assert.strictEqual(component.dataTable.rows[2].competency, 'competency 2');
+    await component.dataTable.header.competency.toggle();
+    assert.strictEqual(component.dataTable.rows[0].competency, 'competency 2');
+    assert.strictEqual(component.dataTable.rows[1].competency, 'competency 1');
+    assert.strictEqual(component.dataTable.rows[2].competency, 'competency 0');
+  });
+
+  test('sort data-table by sessions', async function (assert) {
+    this.set('course', this.course);
+    await render(
+      hbs`<VisualizerCourseObjectives @course={{this.course}} @isIcon={{false}} @showDataTable={{true}} />`
+    );
+    assert.strictEqual(component.dataTable.rows[0].sessions.text, 'Berkeley Investigations');
+    assert.strictEqual(component.dataTable.rows[1].sessions.text, 'The San Leandro Horror');
+    assert.strictEqual(component.dataTable.rows[2].sessions.text, 'Empty Session');
+    await component.dataTable.header.sessions.toggle();
+    assert.strictEqual(component.dataTable.rows[0].sessions.text, 'Berkeley Investigations');
+    assert.strictEqual(component.dataTable.rows[1].sessions.text, 'Empty Session');
+    assert.strictEqual(component.dataTable.rows[2].sessions.text, 'The San Leandro Horror');
+    await component.dataTable.header.sessions.toggle();
+    assert.strictEqual(component.dataTable.rows[0].sessions.text, 'The San Leandro Horror');
+    assert.strictEqual(component.dataTable.rows[1].sessions.text, 'Empty Session');
+    assert.strictEqual(component.dataTable.rows[2].sessions.text, 'Berkeley Investigations');
+  });
+
+  test('sort data-table by minutes', async function (assert) {
+    this.set('course', this.course);
+    await render(
+      hbs`<VisualizerCourseObjectives @course={{this.course}} @isIcon={{false}} @showDataTable={{true}} />`
+    );
+    assert.strictEqual(component.dataTable.rows[0].minutes, '630');
+    assert.strictEqual(component.dataTable.rows[1].minutes, '180');
+    assert.strictEqual(component.dataTable.rows[2].minutes, '0');
+    await component.dataTable.header.minutes.toggle();
+    assert.strictEqual(component.dataTable.rows[0].minutes, '0');
+    assert.strictEqual(component.dataTable.rows[1].minutes, '180');
+    assert.strictEqual(component.dataTable.rows[2].minutes, '630');
+    await component.dataTable.header.minutes.toggle();
+    assert.strictEqual(component.dataTable.rows[0].minutes, '630');
+    assert.strictEqual(component.dataTable.rows[1].minutes, '180');
+    assert.strictEqual(component.dataTable.rows[2].minutes, '0');
   });
 
   test('no linked objectives', async function (assert) {
