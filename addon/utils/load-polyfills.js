e192d99a5c7d5263bd2efcafbe73500731138b22
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-10-24 21:08:36 -0700

    Remove unneeded polyfills
    We needed these for safari 13 and FF 91 both of which are no longer
    supported.

--- addon/utils/load-polyfills.js
+++ addon/utils/load-polyfills.js
@@ -1,39 +1,4 @@
 export async function loadPolyfills() {
-  await intlLocale();
-  await intlPluralRules();
-  await intlRelativeTimeFormat();
-}
-
-async function intlLocale() {
-  if ('Intl' in window && 'locale' in Intl) {
-    return;
-  }
-
-  await import('@formatjs/intl-locale/polyfill');
-}
-
-async function intlPluralRules() {
-  if ('Intl' in window && 'PluralRules' in Intl) {
-    return;
-  }
-
-  await import('@formatjs/intl-pluralrules/polyfill');
-  await Promise.all([
-    import('@formatjs/intl-pluralrules/locale-data/en'),
-    import('@formatjs/intl-pluralrules/locale-data/es'),
-    import('@formatjs/intl-pluralrules/locale-data/fr'),
-  ]);
-}
-
-async function intlRelativeTimeFormat() {
-  if ('Intl' in window && 'RelativeTimeFormat' in Intl) {
-    return;
-  }
-
-  await import('@formatjs/intl-relativetimeformat/polyfill');
-  await Promise.all([
-    import('@formatjs/intl-relativetimeformat/locale-data/en'),
-    import('@formatjs/intl-relativetimeformat/locale-data/es'),
-    import('@formatjs/intl-relativetimeformat/locale-data/fr'),
-  ]);
+  //Currently empty as we don't need any polyfills right now
+  //Keeping it here case we need to add new polyfills later [JJ 10/2022]
 }
