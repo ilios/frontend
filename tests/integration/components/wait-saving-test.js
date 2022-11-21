bd993728252570b11e907c4ad2e707a849b9220d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-11-21 15:42:42 -0800

    fixes template linter warnings in wait-saving component test.

--- tests/integration/components/wait-saving-test.js
+++ tests/integration/components/wait-saving-test.js
@@ -18,23 +18,27 @@ module('Integration | Component | wait saving', function (hooks) {
 
   test('it in block form', async function (assert) {
     assert.expect(1);
+    this.set('content', 'template block text');
     await render(hbs`
       <WaitSaving>
-        template block text
+        {{this.content}}
       </WaitSaving>
-    
+
 `);
 
     assert.dom(this.element).hasText('template block text');
   });
 
   test('it traps focus and returns it', async function (assert) {
+    this.set('label', 'lorem ipsum');
     await render(hbs`
       {{#if this.show}}
         <WaitSaving />
       {{/if}}
-      <input type="text" />
-    
+      <label>
+        <input type="text" />
+        {{this.label}}
+      </label>
 `);
     await fillIn('input', 'text');
     assert.dom('input').isFocused();
