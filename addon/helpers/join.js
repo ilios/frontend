6e454f5633b0cab40b8c4b9b165d6ac2f2292fbd
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-11-23 10:01:51 -0800

    drop EmberArray support in join helper.
    insisting that a separator string must be passed as first argument to
    our join template helper allows us to drop EmberArray check. since we're
    explicit about the separator in all of our uses of this helper, we can
    drop the alternative, separator-less form without further code changes.

--- addon/helpers/join.js
+++ addon/helpers/join.js
@@ -1,15 +1,8 @@
 // taken from Ember Composable Helpers (https://github.com/DockYard/ember-composable-helpers), then modified.
 import { helper } from '@ember/component/helper';
-import { isArray as isEmberArray } from '@ember/array';
 import asArray from '../utils/as-array';
 
 export default helper(function join([separator, rawArray]) {
   let array = asArray(rawArray);
-
-  if (isEmberArray(separator)) {
-    array = separator;
-    separator = ',';
-  }
-
   return array.join(separator);
 });
