405412e2827edf4a09f70b2bdbb1b253bc1e5fe8
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-11-28 13:32:21 -0800

    drop support for EmberArray from intersect helper.

--- addon/helpers/intersect.js
+++ addon/helpers/intersect.js
@@ -1,11 +1,10 @@
 // taken from Ember Composable Helpers (https://github.com/DockYard/ember-composable-helpers), then modified.
 import { helper } from '@ember/component/helper';
-import { isArray as isEmberArray } from '@ember/array';
 import asArray from '../utils/as-array';
 
 export default helper(function intersect([...arrays]) {
   let confirmedArrays = asArray(arrays).map((array) => {
-    return isEmberArray(array) ? array : [];
+    return Array.isArray(array) ? array : [];
   });
   // copied from https://github.com/emberjs/ember.js/blob/315ec6472ff542ac714432036cc96fe4bd62bd1f/packages/%40ember/object/lib/computed/reduce_computed_macros.js#L1063-L1100
   return confirmedArrays.pop().filter((candidate) => {
