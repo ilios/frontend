53502ae6de6463016bfed4820dba30667c58783d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-01-30 14:09:31 -0800

    adds sortable headers to school session type table.

---
+++ app/components/school-session-types-list.js
@@ -0,0 +1,52 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { use } from 'ember-could-get-used-to-this';
+import AsyncProcess from 'ilios-common/classes/async-process';
+import { sortBy as sortArray } from 'ilios-common/utils/array-helpers';
+import { map } from 'rsvp';
+
+export default class SchoolSessionTypesListComponent extends Component {
+  @tracked sort = 'title';
+  @tracked sortedAscending = true;
+  @use sortedSessionTypes = new AsyncProcess(() => [
+    this.sortSessionTypes,
+    this.args.sessionTypes,
+    this.sortedAscending,
+    this.sortBy,
+  ]);
+
+  get sortBy() {
+    return this.sort || 'title';
+  }
+
+  @action
+  setSortBy(what) {
+    this.sort = what;
+    this.sortedAscending = !this.sortedAscending;
+  }
+
+  async sortSessionTypes(sessionTypes, isSortedAscending, sortBy) {
+    let items = sessionTypes.toArray();
+    if (['title', 'calendarColor', 'assessment', 'sessionCount'].includes(sortBy)) {
+      items = sortArray(sessionTypes, sortBy);
+    }
+
+    if ('assessmentOption' === sortBy) {
+      const sortProxies = await map(items, async (sessionType) => {
+        const assessmentOption = await sessionType.assessmentOption;
+        return {
+          sessionType,
+          assessmentOptionName: assessmentOption?.name,
+        };
+      });
+      items = sortArray(sortProxies, 'assessmentOptionName');
+      items = items.map((item) => item.sessionType);
+    }
+
+    if (!isSortedAscending) {
+      items.reverse();
+    }
+    return items;
+  }
+}
