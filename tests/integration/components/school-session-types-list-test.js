1420e6690d72b6fbfdc766ff92d6edfe7f2d668b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-01-30 15:59:10 -0800

    DRY: rewrites component test to reuse fixtures.

--- tests/integration/components/school-session-types-list-test.js
+++ tests/integration/components/school-session-types-list-test.js
@@ -11,7 +11,7 @@ module('Integration | Component | school session types list', function (hooks) {
   setupIntl(hooks, 'en-us');
   setupMirage(hooks);
 
-  test('it renders', async function (assert) {
+  hooks.beforeEach(async function () {
     const school = this.server.create('school');
     const assessmentOption = this.server.create('assessment-option', {
       id: 1,
@@ -56,8 +56,11 @@ module('Integration | Component | school session types list', function (hooks) {
       active: true,
     });
 
-    const sessionTypeModels = await this.owner.lookup('service:store').findAll('session-type');
-    this.set('sessionTypes', sessionTypeModels);
+    this.sessionTypes = await this.owner.lookup('service:store').findAll('session-type');
+  });
+
+  test('it renders', async function (assert) {
+    this.set('sessionTypes', this.sessionTypes);
     await render(
       hbs`<SchoolSessionTypesList @sessionTypes={{this.sessionTypes}} @manageSessionType={{(noop)}} />`
     );
@@ -67,39 +70,30 @@ module('Integration | Component | school session types list', function (hooks) {
     assert.ok(component.sessionTypes[0].isActive);
     assert.strictEqual(component.sessionTypes[0].sessionCount, '2');
     assert.notOk(component.sessionTypes[0].isAssessment);
-    assert.strictEqual(component.sessionTypes[0].aamcMethod, aamcMethod1.description);
+    assert.strictEqual(component.sessionTypes[0].aamcMethod, 'Lorem Ipsum');
     assert.strictEqual(component.sessionTypes[0].assessmentOption, '');
     assert.strictEqual(component.sessionTypes[0].calendarColor, 'background-color: #cccccc');
     assert.strictEqual(component.sessionTypes[1].title.text, 'second');
     assert.ok(component.sessionTypes[1].isActive);
     assert.strictEqual(component.sessionTypes[1].sessionCount, '0');
     assert.ok(component.sessionTypes[1].isAssessment);
-    assert.strictEqual(component.sessionTypes[1].aamcMethod, aamcMethod2.description);
+    assert.strictEqual(component.sessionTypes[1].aamcMethod, 'Dolor Et');
     assert.strictEqual(component.sessionTypes[1].assessmentOption, 'formative');
     assert.strictEqual(component.sessionTypes[1].calendarColor, 'background-color: #123456');
     assert.strictEqual(component.sessionTypes[2].title.text, 'not needed anymore');
     assert.ok(component.sessionTypes[2].isInactive);
     assert.strictEqual(component.sessionTypes[2].sessionCount, '2');
     assert.notOk(component.sessionTypes[2].isAssessment);
-    assert.strictEqual(component.sessionTypes[2].aamcMethod, aamcMethod1.description);
+    assert.strictEqual(component.sessionTypes[2].aamcMethod, 'Lorem Ipsum');
     assert.strictEqual(component.sessionTypes[2].assessmentOption, '');
     assert.strictEqual(component.sessionTypes[2].calendarColor, 'background-color: #ffffff');
   });
 
   test('clicking edit fires action', async function (assert) {
     assert.expect(1);
-    const school = this.server.create('school');
-    this.server.create('session-type', {
-      school,
-      title: 'first',
-      assessment: false,
-      calendarColor: '#fff',
-    });
-    const sessionTypeModels = await this.owner.lookup('service:store').findAll('session-type');
-
-    this.set('sessionTypes', sessionTypeModels);
+    this.set('sessionTypes', this.sessionTypes);
     this.set('manageSessionType', (sessionTypeId) => {
-      assert.strictEqual(parseInt(sessionTypeId, 10), 1);
+      assert.strictEqual(parseInt(sessionTypeId, 10), 3);
     });
     await render(hbs`<SchoolSessionTypesList
       @sessionTypes={{this.sessionTypes}}
@@ -111,18 +105,9 @@ module('Integration | Component | school session types list', function (hooks) {
 
   test('clicking title fires action', async function (assert) {
     assert.expect(1);
-    const school = this.server.create('school');
-    this.server.create('session-type', {
-      school,
-      title: 'first',
-      assessment: false,
-      calendarColor: '#fff',
-    });
-    const sessionTypeModels = await this.owner.lookup('service:store').findAll('session-type');
-
-    this.set('sessionTypes', sessionTypeModels);
+    this.set('sessionTypes', this.sessionTypes);
     this.set('manageSessionType', (sessionTypeId) => {
-      assert.strictEqual(parseInt(sessionTypeId, 10), 1);
+      assert.strictEqual(parseInt(sessionTypeId, 10), 3);
     });
     await render(hbs`<SchoolSessionTypesList
       @sessionTypes={{this.sessionTypes}}
@@ -133,25 +118,7 @@ module('Integration | Component | school session types list', function (hooks) {
   });
 
   test('session types without sessions can be deleted', async function (assert) {
-    const school = this.server.create('school');
-    this.server.create('session-type', {
-      school,
-      title: 'unlinked',
-      assessment: false,
-      active: true,
-      calendarColor: '#fff',
-    });
-    this.server.create('session-type', {
-      school,
-      title: 'linked',
-      active: true,
-      assessment: false,
-      calendarColor: '#fff',
-      sessions: this.server.createList('session', 5),
-    });
-
-    const sessionTypeModels = await this.owner.lookup('service:store').findAll('session-type');
-    this.set('sessionTypes', sessionTypeModels);
+    this.set('sessionTypes', this.sessionTypes);
     await render(hbs`<SchoolSessionTypesList
       @sessionTypes={{this.sessionTypes}}
       @manageSessionType={{(noop)}}
@@ -160,31 +127,24 @@ module('Integration | Component | school session types list', function (hooks) {
 
     assert.notOk(component.sessionTypes[0].isDeletable);
     assert.ok(component.sessionTypes[1].isDeletable);
+    assert.notOk(component.sessionTypes[2].isDeletable);
   });
 
   test('delete session type', async function (assert) {
-    const school = this.server.create('school');
-    this.server.create('session-type', {
-      school,
-    });
-
-    const sessionTypeModels = await this.owner.lookup('service:store').findAll('session-type');
-
-    this.set('sessionTypes', sessionTypeModels);
+    this.set('sessionTypes', this.sessionTypes);
     await render(hbs`<SchoolSessionTypesList
       @sessionTypes={{this.sessionTypes}}
       @manageSessionType={{(noop)}}
       @canDelete={{true}}
     />`);
-
-    assert.strictEqual(this.server.db.sessionTypes.length, 1);
-    assert.notOk(component.sessionTypes[0].confirmRemoval.isVisible);
-    await component.sessionTypes[0].delete();
+    assert.strictEqual(this.server.db.sessionTypes.length, 3);
+    assert.notOk(component.sessionTypes[1].confirmRemoval.isVisible);
+    await component.sessionTypes[1].delete();
     assert.strictEqual(
-      component.sessionTypes[0].confirmRemoval.message,
+      component.sessionTypes[1].confirmRemoval.message,
       'Are you sure you want to delete this session type? This action cannot be undone. Yes Cancel'
     );
-    await component.sessionTypes[0].confirmRemoval.confirm();
-    assert.strictEqual(this.server.db.sessionTypes.length, 0);
+    await component.sessionTypes[1].confirmRemoval.confirm();
+    assert.strictEqual(this.server.db.sessionTypes.length, 2);
   });
 });
--- tests/pages/components/school-session-types-list.js
+++ tests/pages/components/school-session-types-list.js
@@ -1,10 +1,43 @@
-import { collection, create } from 'ember-cli-page-object';
+import { clickable, collection, create, hasClass } from 'ember-cli-page-object';
 
 import listItem from './school-session-types-list-item';
 
 const definition = {
   scope: '[data-test-school-session-types-list]',
   sessionTypes: collection('[data-test-school-session-types-list-item]', listItem),
+  sortByTitle: clickable('button', { scope: '[data-test-headings] th:nth-of-type(1)' }),
+  sortByActiveStatus: clickable('button', { scope: '[data-test-headings] th:nth-of-type(2)' }),
+  sortBySessions: clickable('button', { scope: '[data-test-headings] th:nth-of-type(3)' }),
+  sortByAssessment: clickable('button', { scope: '[data-test-headings] th:nth-of-type(4)' }),
+  sortByAssessmentOption: clickable('button', { scope: '[data-test-headings] th:nth-of-type(5)' }),
+  sortByColor: clickable('button', { scope: '[data-test-headings] th:nth-of-type(7)' }),
+  isSortedByTitleAscending: hasClass('fa-arrow-down-a-z', '[data-test-headings] th:eq(0) svg'),
+  isSortedByTitleDescending: hasClass('fa-arrow-down-z-a', '[data-test-headings] th:eq(0) svg'),
+  isSortedByActiveStatusAscending: hasClass(
+    'fa-arrow-down-1-9',
+    '[data-test-headings] th:eq(1) svg'
+  ),
+  isSortedByActiveStatusDescending: hasClass(
+    'fa-arrow-down-9-1',
+    '[data-test-headings] th:eq(1) svg'
+  ),
+  isSortedBySessionsAscending: hasClass('fa-arrow-down-1-9', '[data-test-headings] th:eq(2) svg'),
+  isSortedBySessionsDescending: hasClass('fa-arrow-down-9-1', '[data-test-headings] th:eq(2) svg'),
+  isSortedByAssessmentAscending: hasClass('fa-arrow-down-1-9', '[data-test-headings] th:eq(3) svg'),
+  isSortedByAssessmentDescending: hasClass(
+    'fa-arrow-down-9-1',
+    '[data-test-headings] th:eq(3) svg'
+  ),
+  isSortedByAssessmentOptionAscending: hasClass(
+    'fa-arrow-down-a-z',
+    '[data-test-headings] th:eq(4) svg'
+  ),
+  isSortedByAssessmentOptionDescending: hasClass(
+    'fa-arrow-down-z-a',
+    '[data-test-headings] th:eq(4) svg'
+  ),
+  isSortedByColorAscending: hasClass('fa-arrow-down-1-9', '[data-test-headings] th:eq(6) svg'),
+  isSortedByColorDescending: hasClass('fa-arrow-down-9-1', '[data-test-headings] th:eq(6) svg'),
 };
 
 export default definition;
