ba12767e0d780c79a16b6304e77c7ae8be319532
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-02-16 14:34:06 -0800

    adds missing auth check to subject reports route.

--- app/routes/reports/subjects.js
+++ app/routes/reports/subjects.js
@@ -1,3 +1,10 @@
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
-export default class ReportsSubjectsRoute extends Route {}
+export default class ReportsSubjectsRoute extends Route {
+  @service session;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+  }
+}
