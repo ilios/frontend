f24dd02d0905ae8ec70ca6be3c89bd736e06360f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-03-10 15:32:08 -0800

    replace waiting on mesh terms in template with Resource and getters.

--- addon/components/detail-learning-materials-item.js
+++ addon/components/detail-learning-materials-item.js
@@ -2,12 +2,22 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { dropTask } from 'ember-concurrency';
 import AsyncProcess from 'ilios-common/classes/async-process';
+import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
 import { use } from 'ember-could-get-used-to-this';
 
 export default class DetailLearningMaterialsItemComponent extends Component {
   @tracked showRemoveConfirmation = false;
 
   @use owningUser = new AsyncProcess(() => [this.getOwningUser.bind(this), this.args.lm]);
+  @use meshDescriptorsResource = new ResolveAsyncValue(() => [this.args.lm.meshDescriptors]);
+
+  get meshDescriptorsLoaded() {
+    return !!this.meshDescriptorsResource;
+  }
+
+  get meshDescriptors() {
+    return this.meshDescriptorsResource ? this.meshDescriptorsResource.slice() : [];
+  }
 
   async getOwningUser(lm) {
     const learningMaterial = await lm.learningMaterial;
