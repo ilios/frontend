4b2fd7f049b8d7078c9b2f7942d66bfa564ffc68
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-03-10 15:14:42 -0800

    replace awaiting the lm owner in the template with a Resource-based data loading process.

--- addon/components/detail-learning-materials-item.js
+++ addon/components/detail-learning-materials-item.js
@@ -1,9 +1,18 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { dropTask } from 'ember-concurrency';
+import AsyncProcess from 'ilios-common/classes/async-process';
+import { use } from 'ember-could-get-used-to-this';
 
 export default class DetailLearningMaterialsItemComponent extends Component {
   @tracked showRemoveConfirmation = false;
+
+  @use owningUser = new AsyncProcess(() => [this.getOwningUser.bind(this), this.args.lm]);
+
+  async getOwningUser(lm) {
+    const learningMaterial = await lm.learningMaterial;
+    return await learningMaterial.owningUser;
+  }
 
   remove = dropTask(async (lm) => {
     await this.args.remove.perform(lm);
