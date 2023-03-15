67d2e18e9c0a8f8a1d7491644578112d41b8513b
Jonathan Johnson <jon.johnson@ucsf.edu>
2023-03-15 06:42:57 -0700

    Extract Calendar Filters into Component
    Putting all of these in a component while removing the loader pattern.
    Should make it easier to make more changes in the future.

---
+++ addon/components/dashboard/calendar-filters.js
@@ -0,0 +1,27 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { mapBy, sortBy } from '../../utils/array-helpers';
+import { use } from 'ember-could-get-used-to-this';
+import AsyncProcess from 'ilios-common/classes/async-process';
+
+export default class DashboardCalendarFiltersComponent extends Component {
+  @service dataLoader;
+
+  @use sessionTypes = new AsyncProcess(() => [this.loadSessionTypes.bind(this), this.args.school]);
+  @use vocabularies = new AsyncProcess(() => [this.loadVocabularies.bind(this), this.args.school]);
+
+  courseLevels = ['1', '2', '3', '4', '5'];
+
+  async loadSessionTypes(school) {
+    await this.dataLoader.loadSchoolForCalendar(school.id);
+    const types = await school.sessionTypes;
+    return sortBy(types.slice(), 'title');
+  }
+
+  async loadVocabularies(school) {
+    await this.dataLoader.loadSchoolForCalendar(school.id);
+    const vocabularies = await school.vocabularies;
+    await Promise.all(mapBy(vocabularies, 'terms'));
+    return sortBy(vocabularies.slice(), 'title');
+  }
+}
---
+++ tests/integration/components/dashboard/calendar-filters-test.js
@@ -0,0 +1,14 @@
+import { module, skip } from 'qunit';
+import { setupRenderingTest } from 'dummy/tests/helpers';
+
+module('Integration | Component | dashboard/calendar-filters', function (hooks) {
+  setupRenderingTest(hooks);
+
+  skip('it renders', async function () {
+    /*
+    Filters are well tested in acceptance tests
+    Leaving this file here in case we have future need
+    for some specific integration tests on this component
+    */
+  });
+});
