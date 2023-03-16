cf9ef37e76c4c950a9553a8eaa84efa0016c74c5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-03-16 14:24:58 -0700

    replaces await in templates with Resource backed data loading process.

--- addon/components/detail-taxonomies.js
+++ addon/components/detail-taxonomies.js
@@ -3,14 +3,16 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { dropTask } from 'ember-concurrency';
+import { use } from 'ember-could-get-used-to-this';
+import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
 
 export default class DetailTaxonomiesComponent extends Component {
   @service store;
   @service intl;
   @service flashMessages;
-
   @tracked bufferedTerms = [];
   @tracked isManaging = false;
+  @use terms = new ResolveAsyncValue(() => [this.args.subject.terms]);
 
   get showCollapsible() {
     const terms = this.args.subject.hasMany('terms').ids();
