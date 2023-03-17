3ed4977b6546962b6fda3d91ee3f841f5b61204a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-03-17 14:42:57 -0700

    replace any await on cohorts in templates with Resources.

--- addon/components/offering-manager.js
+++ addon/components/offering-manager.js
@@ -13,6 +13,9 @@ export default class OfferingManagerComponent extends Component {
   @tracked hoveredGroups = [];
 
   @use learnerGroups = new ResolveAsyncValue(() => [this.args.offering.learnerGroups]);
+  @use session = new ResolveAsyncValue(() => [this.args.offering?.session]);
+  @use course = new ResolveAsyncValue(() => [this.args.session?.course]);
+  @use cohorts = new ResolveAsyncValue(() => [this.args.course?.cohorts]);
 
   @action
   save(startDate, endDate, room, url, learnerGroups, learners, instructorGroups, instructors) {
