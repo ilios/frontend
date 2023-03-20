7fe7b1578a1d68171249bbca3fe01665f2c3baa7
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2023-03-20 08:49:55 -0700

    Update addon/components/sessions-grid-offering.js
    Co-authored-by: Jon Johnson <jon.johnson@ucsf.edu>

--- addon/components/sessions-grid-offering.js
+++ addon/components/sessions-grid-offering.js
@@ -14,7 +14,7 @@ export default class SessionsGridOffering extends Component {
   @tracked wasUpdated = false;
   @use session = new ResolveAsyncValue(() => [this.args.offering.session]);
   @use course = new ResolveAsyncValue(() => [this.session?.course]);
-  @use cohorts = new ResolveAsyncValue(() => [this.args.course?.cohorts]);
+  @use cohorts = new ResolveAsyncValue(() => [this.course?.cohorts]);
 
   @action
   revertRoomChanges() {
