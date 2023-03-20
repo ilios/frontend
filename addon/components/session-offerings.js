94cb52ada76e6490f8ba702499ecb9b7a7aebf54
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-03-20 11:31:51 -0700

    replaces await with Resources.

--- addon/components/session-offerings.js
+++ addon/components/session-offerings.js
@@ -4,5 +4,7 @@ import { use } from 'ember-could-get-used-to-this';
 
 export default class SessionOfferingsComponent extends Component {
   @use course = new ResolveAsyncValue(() => [this.args.session.course]);
+  @use courseStartDate = new ResolveAsyncValue(() => [this.course?.startDate]);
+  @use courseEndDate = new ResolveAsyncValue(() => [this.course?.endDate]);
   @use cohorts = new ResolveAsyncValue(() => [this.course?.cohorts]);
 }
