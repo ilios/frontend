7ff8d904c6ac6315729e13fd914acaa2414e0f92
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-03-22 15:32:52 -0700

    rm await from print course template.
    replace it with Resources and use a sub-component for sessions.

---
+++ addon/components/print-course-session.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import ResolveAsyncValue from '../classes/resolve-async-value';
+import { use } from 'ember-could-get-used-to-this';
+
+export default class PrintCourseSessionComponent extends Component {
+  @use sessionObjectives = new ResolveAsyncValue(() => [this.args.session.sessionObjectives, []]);
+  @use learningMaterials = new ResolveAsyncValue(() => [this.args.session.learningMaterials, []]);
+  @use meshDescriptors = new ResolveAsyncValue(() => [this.args.session.meshDescriptors, []]);
+  @use offerings = new ResolveAsyncValue(() => [this.args.session.offerings, []]);
+  @use terms = new ResolveAsyncValue(() => [this.args.session.terms, []]);
+}
