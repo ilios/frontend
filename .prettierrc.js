ed56fa1ad70f595646f61df71bc62c1a75f5bf56
Jonathan Johnson <jon.johnson@ucsf.edu>
2023-03-26 23:00:47 -0700

    Update Ember CLI v4.8.0...v4.11.0
    Along with blueprints

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = {
-  singleQuote: true,
-  printWidth: 100,
+  overrides: [
+    {
+      files: '*.{js,ts}',
+      options: {
+        singleQuote: true,
+        printWidth: 100,
+      },
+    },
+  ],
 };
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -45,6 +45,7 @@ module.exports = function (environment) {
       injectionFactories: [],
     },
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
