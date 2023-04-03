5d8c9baf2d44d8e03a4175647f7d198ebd87bc4d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-04-03 14:25:16 -0700

    replaces await in template with Resource + getter

--- app/components/curriculum-inventory/sequence-block-list-item.js
+++ app/components/curriculum-inventory/sequence-block-list-item.js
@@ -1,6 +1,19 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { use } from 'ember-could-get-used-to-this';
+import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
 
 export default class CurriculumInventorySequenceBlockListItemComponent extends Component {
+  @service intl;
   @tracked showConfirmRemoval;
+
+  @use course = new ResolveAsyncValue(() => [this.args.sequenceBlock.course]);
+
+  get courseTitle() {
+    if (this.course) {
+      return this.course.title;
+    }
+    return this.intl.t('general.notApplicableAbbr');
+  }
 }
