6d77ce16c8cce5d9046614eea1ceb43422ec4453
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-04-03 14:14:19 -0700

    replace await in component template with Resources.
    remove all unnecessary uses of await without replacement.

--- app/components/curriculum-inventory/sequence-block-session-list.js
+++ app/components/curriculum-inventory/sequence-block-session-list.js
@@ -1,7 +1,15 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { use } from 'ember-could-get-used-to-this';
+import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
 
 export default class SequenceBlockSessionListComponent extends Component {
+  @use sessions = new ResolveAsyncValue(() => [this.args.sequenceBlock.sessions, []]);
+  @use excludedSessions = new ResolveAsyncValue(() => [
+    this.args.sequenceBlock.excludedSessions,
+    [],
+  ]);
+
   get sortedAscending() {
     const sortBy = this.args.sortBy;
     return sortBy.search(/desc/) === -1;
