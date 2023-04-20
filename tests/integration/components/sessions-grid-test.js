f88f4736296840cc0ed8c784658cb0be4eee8fea
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-04-20 16:21:52 -0700

    fix up component tests.

--- tests/integration/components/sessions-grid-test.js
+++ tests/integration/components/sessions-grid-test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'dummy/tests/helpers';
 import { setupIntl } from 'ember-intl/test-support';
+import Service from '@ember/service';
 import { click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupMirage } from 'ember-cli-mirage/test-support';
@@ -10,96 +11,80 @@ module('Integration | Component | sessions-grid', function (hooks) {
   setupIntl(hooks, 'en-us');
   setupMirage(hooks);
 
+  hooks.beforeEach(function () {
+    class PermissionCheckerStub extends Service {
+      canDeleteSession() {
+        return true;
+      }
+      canUpdateSession() {
+        return true;
+      }
+    }
+    this.owner.register('service:permissionChecker', PermissionCheckerStub);
+  });
+
   test('it renders with no results', async function (assert) {
     this.set('sessions', []);
     this.set('sortBy', 'title');
-    this.set('setSortBy', () => {});
     await render(hbs`<SessionsGrid
       @sessions={{this.sessions}}
       @sortBy={{this.sortBy}}
-      @setSortBy={{this.setSortBy}}
+      @setSortBy={{(noop)}}
     />
 `);
-
     assert.dom(this.element).hasText('No results found. Please try again.');
   });
 
   test('clicking expand fires action', async function (assert) {
     assert.expect(1);
-    const session = {
-      id: 1,
-    };
-    this.set('sessions', [
-      {
-        session,
-        offeringCount: 1,
-      },
-    ]);
+    const session = this.server.create('session');
+    this.server.create('offering', { session });
+    const sessionModel = await this.owner.lookup('service:store').findRecord('session', session.id);
+    this.set('sessions', [sessionModel]);
     this.set('sortBy', 'title');
-    this.set('setSortBy', () => {});
     this.set('expandSession', (s) => {
-      assert.strictEqual(s, session);
+      assert.strictEqual(s, sessionModel);
     });
     await render(hbs`<SessionsGrid
       @sessions={{this.sessions}}
       @sortBy={{this.sortBy}}
-      @setSortBy={{this.setSortBy}}
+      @setSortBy={{(noop)}}
       @expandSession={{this.expandSession}}
     />
 `);
-
     await click('[data-test-expand-collapse-control] svg');
   });
 
   test('clicking expand does not fire action when there are no offerings', async function (assert) {
     assert.expect(0);
-    const session = {
-      id: 1,
-    };
-    this.set('sessions', [
-      {
-        session,
-        offeringCount: 0,
-      },
-    ]);
+    const session = this.server.create('session');
+    const sessionModel = await this.owner.lookup('service:store').findRecord('session', session.id);
+    this.set('sessions', [sessionModel]);
     this.set('sortBy', 'title');
-    this.set('setSortBy', () => {});
     this.set('expandSession', () => {
       assert.ok(false);
     });
     await render(hbs`<SessionsGrid
       @sessions={{this.sessions}}
       @sortBy={{this.sortBy}}
-      @setSortBy={{this.setSortBy}}
+      @setSortBy={{(noop)}}
       @expandSession={{this.expandSession}}
     />
 `);
-
     await click('[data-test-expand-collapse-control] svg');
   });
 
   // @see issue ilios/common#1820 [ST 2020/12/10]
   test('deletion of session is disabled if it has prerequisites', async function (assert) {
-    const sessions = this.server.createList('session', 2);
+    const session1 = this.server.create('session');
+    const session2 = this.server.create('session', { postrequisite: session1 });
     const sessionModel1 = await this.owner
       .lookup('service:store')
-      .findRecord('session', sessions[0].id);
+      .findRecord('session', session1.id);
     const sessionModel2 = await this.owner
       .lookup('service:store')
-      .findRecord('session', sessions[0].id);
-
-    this.set('sessions', [
-      {
-        sessionModel1,
-        prerequisiteCount: 1,
-        canUpdate: true,
-      },
-      {
-        sessionModel2,
-        prerequisiteCount: 0,
-        canUpdate: true,
-      },
-    ]);
+      .findRecord('session', session2.id);
+    this.set('sessions', [sessionModel1, sessionModel2]);
     await render(hbs`<SessionsGrid
       @sessions={{this.sessions}}
       @sortBy="title"
@@ -107,7 +92,6 @@ module('Integration | Component | sessions-grid', function (hooks) {
       @expandSession={{(noop)}}
     />
 `);
-
     assert.dom('[data-test-session]:nth-of-type(1) [data-test-delete-disabled]').isVisible();
     assert.dom('[data-test-session]:nth-of-type(1) [data-test-delete]').isNotVisible();
     assert.dom('[data-test-session]:nth-of-type(2) [data-test-delete-disabled]').isNotVisible();
