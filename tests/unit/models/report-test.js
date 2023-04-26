e5fa4f8edcddebc91e5108628698e2d2640a9e05
Jonathan Johnson <jon.johnson@ucsf.edu>
2023-04-26 14:36:05 -0700

    Fix some issues with inverse relationships
    Some typos and bad data when adding these, I've fixed them and added
    some tests that will probably never be needed again.

--- tests/unit/models/report-test.js
+++ tests/unit/models/report-test.js
@@ -4,8 +4,9 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Model | Report', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function (assert) {
+  test('it exists', async function (assert) {
     const model = this.owner.lookup('service:store').createRecord('report');
     assert.ok(!!model);
+    assert.deepEqual(await model.school, null);
   });
 });
