51535a93af8cfdb9259ba58b1252fa0d90b688a5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-05-10 11:49:41 -0700

    replaces CP with Resource.
    realigned with the course-publicationcheck component.

--- addon/components/session-publicationcheck.js
+++ addon/components/session-publicationcheck.js
@@ -1,36 +1,24 @@
-/* eslint-disable ember/no-computed-properties-in-native-classes */
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
-import { action, computed } from '@ember/object';
-import { restartableTask } from 'ember-concurrency';
+import { action } from '@ember/object';
 import ResolveAsyncValue from 'ilios-common/classes/resolve-async-value';
 import { use } from 'ember-could-get-used-to-this';
 
 export default class SessionPublicationCheckComponent extends Component {
   @service router;
-  @tracked objectivesRelationship;
 
   @use course = new ResolveAsyncValue(() => [this.args.session.course]);
   @use school = new ResolveAsyncValue(() => [this.course?.school]);
   @use sessionTypes = new ResolveAsyncValue(() => [this.school?.sessionTypes]);
+  @use sessionObjectives = new ResolveAsyncValue(() => [this.args.session.sessionObjectives, []]);
 
-  @computed('objectivesRelationship.@each.courseObjectives')
   get showUnlinkIcon() {
-    if (!this.objectivesRelationship) {
-      return false;
-    }
-    return Boolean(
-      this.objectivesRelationship.find((objective) => {
-        const parentIds = objective.hasMany('courseObjectives').ids();
-        return parentIds.length === 0;
-      })
-    );
-  }
+    const objectivesWithoutParents = this.sessionObjectives.filter((objective) => {
+      return objective.courseObjectives.length === 0;
+    });
 
-  load = restartableTask(async () => {
-    this.objectivesRelationship = await this.args.session.sessionObjectives;
-  });
+    return objectivesWithoutParents.length > 0;
+  }
 
   @action
   transitionToSession() {
