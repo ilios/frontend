d7a8d575ab80ab4d0b63e29b75e1cc351e118949
Jonathan Johnson <jon.johnson@ucsf.edu>
2023-05-14 22:21:23 -0700

    Temporarily allow multiple ESA versions
    During the release candidate cycle we need to allow other versions of
    both of these libraries.

--- tests/dummy/config/dependency-lint.js
+++ tests/dummy/config/dependency-lint.js
@@ -6,5 +6,7 @@ module.exports = {
     'ember-in-element-polyfill': '*',
     'ember-get-config': '^1.0.0 || ^2.0.0',
     'ember-modifier': '3.2.7 || ^ 4.0.0',
+    'ember-simple-auth': '^5.0.0 || 6.0.0-rc.1',
+    'ember-cookies': '^0.5.2 || ^1.0.0',
   },
 };
