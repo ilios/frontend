82a37bac11ccc97781699a58f9153133fac7c45b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-06-12 15:38:15 -0700

    rm await template helper with getter/Resource-backed equivalent.

--- app/components/school-competencies-list-item-pcrs.js
+++ app/components/school-competencies-list-item-pcrs.js
@@ -1,9 +1,20 @@
 import Component from '@glimmer/component';
+import { cached } from '@glimmer/tracking';
 import { dropTask } from 'ember-concurrency';
+import { TrackedAsyncData } from 'ember-async-data';
 
 export default class SchoolCompetenciesListItemPcrsComponent extends Component {
   @dropTask
   *save() {
     yield this.args.save();
+  }
+
+  @cached
+  get aamcPcrsesData() {
+    return new TrackedAsyncData(this.args.competency.aamcPcrses);
+  }
+
+  get aamcPcrses() {
+    return this.aamcPcrsesData.isResolved ? this.aamcPcrsesData.value.slice() : [];
   }
 }
