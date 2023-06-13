fabae167bf3ede59b8779f445df2245cf8e2d1a8
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-06-13 16:12:47 -0700

    replace await template helper with Resource/getter-backed equivalent.

--- app/components/program-year/competency-list-item.js
+++ app/components/program-year/competency-list-item.js
@@ -1,3 +1,14 @@
 import Component from '@glimmer/component';
+import { cached } from '@glimmer/tracking';
+import { TrackedAsyncData } from 'ember-async-data';
 
-export default class ProgramYearCompetencyListItemComponent extends Component {}
+export default class ProgramYearCompetencyListItemComponent extends Component {
+  @cached
+  get childrenData() {
+    return new TrackedAsyncData(this.args.domain.children);
+  }
+
+  get children() {
+    return this.childrenData.isResolved ? this.childrenData.value : [];
+  }
+}
--- app/components/program-year/managed-competency-list-item.js
+++ app/components/program-year/managed-competency-list-item.js
@@ -1,3 +1,14 @@
 import Component from '@glimmer/component';
+import { cached } from '@glimmer/tracking';
+import { TrackedAsyncData } from 'ember-async-data';
 
-export default class ProgramYearManagedCompetencyListItemComponent extends Component {}
+export default class ProgramYearManagedCompetencyListItemComponent extends Component {
+  @cached
+  get childrenData() {
+    return new TrackedAsyncData(this.args.domain.children);
+  }
+
+  get children() {
+    return this.childrenData.isResolved ? this.childrenData.value : [];
+  }
+}
