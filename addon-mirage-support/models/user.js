395592d4b207d6c269a4b26db802579e26181c72
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-06-28 11:02:09 -0700

    adds mirage models for user and user-related entities.

---
+++ addon-mirage-support/models/user.js
@@ -0,0 +1,30 @@
+import { Model, belongsTo, hasMany } from 'miragejs';
+
+export default Model.extend({
+  reports: belongsTo('report', { inverse: 'user' }),
+  school: belongsTo('school', { inverse: null }),
+  authentication: belongsTo('authentication', { inverse: 'user' }),
+  directedCourses: hasMany('course', { inverse: 'directors' }),
+  administeredCourses: hasMany('course', { inverse: 'administrators' }),
+  studentAdvisedCourses: hasMany('course', { inverse: 'studentAdvisors' }),
+  studentAdvisedSessions: hasMany('session', { inverse: 'studentAdvisors' }),
+  learnerGroups: hasMany('learner-group', { inverse: 'users' }),
+  instructorGroups: hasMany('instructor-group', { inverse: 'users' }),
+  instructorIlmSessions: hasMany('ilm-session', { inverse: 'instructors' }),
+  learnerIlmSessions: hasMany('ilm-session', { inverse: 'learners' }),
+  offerings: hasMany('offering', { inverse: 'learners' }),
+  instructedOfferings: hasMany('offering', { inverse: 'instructors' }),
+  programYears: hasMany('program-year', { inverse: 'directors' }),
+  roles: hasMany('user-year', { inverse: null }),
+  directedSchools: hasMany('school', { inverse: 'directors' }),
+  administeredSchools: hasMany('school', { inverse: 'administrators' }),
+  administeredSessions: hasMany('session', { inverse: 'administrators' }),
+  directedPrograms: hasMany('program', { inverse: 'directors' }),
+  cohorts: hasMany('cohort', { inverse: 'users' }),
+  primaryCohort: belongsTo('cohort', { inverse: null }),
+  pendingUserUpdates: hasMany('pending-user-update', { inverse: 'user' }),
+  administeredCurriculumInventoryReports: hasMany('curriculum-inventory-report', {
+    inverse: 'administrators',
+  }),
+  sessionMaterialStatuses: hasMany('user-session-material-status', { inverse: 'user' }),
+});
