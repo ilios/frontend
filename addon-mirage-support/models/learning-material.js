c7995bd873a782825d9786e7fc37bca5b0775c63
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-06-28 14:22:22 -0700

    adds mirage models for learning materials entities.

---
+++ addon-mirage-support/models/learning-material.js
@@ -0,0 +1,9 @@
+import { Model, belongsTo, hasMany } from 'miragejs';
+
+export default Model.extend({
+  userRole: belongsTo('learning-material-user-role', { inverse: null }),
+  status: belongsTo('learning-material-status', { inverse: null }),
+  owningUser: belongsTo('user', { inverse: null }),
+  sessionLearningMaterials: hasMany('session-learning-material', { inverse: 'learningMaterial' }),
+  courseLearningMaterial: hasMany('course-learning-material', { inverse: 'learningMaterial' }),
+});
