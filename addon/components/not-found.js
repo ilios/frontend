61e9ca53c72ff542edec2d5ffa8eba67b96f47c3
Jonathan Johnson <jon.johnson@ucsf.edu>
2023-07-07 12:25:00 -0700

    Replace has-route in NotFound component
    This was works with embroider and doesn't require us to do any hackery.

---
+++ addon/components/not-found.js
@@ -0,0 +1,14 @@
+import Component from '@glimmer/component';
+import { service } from '@ember/service';
+
+export default class NotFoundComponent extends Component {
+  @service router;
+
+  get showLink() {
+    try {
+      return Boolean(this.router.urlFor('dashboard'));
+    } catch (error) {
+      return false;
+    }
+  }
+}
