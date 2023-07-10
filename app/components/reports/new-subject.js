53ce22670fd9fabc98997a2f9bec70f211f63aba
Jonathan Johnson <jon.johnson@ucsf.edu>
2023-07-09 20:42:41 -0700

    Break out new report objects into their own components
    This allows us to control the UI and data loading individually.

--- app/components/reports/new-subject.js
+++ app/components/reports/new-subject.js
@@ -1,16 +1,14 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { map } from 'rsvp';
-import { dropTask, restartableTask } from 'ember-concurrency';
-import { dasherize } from '@ember/string';
+import { dropTask } from 'ember-concurrency';
 import { validatable, Length, Custom } from 'ilios-common/decorators/validation';
 import { TrackedAsyncData } from 'ember-async-data';
 import { cached } from '@glimmer/tracking';
-import AsyncProcess from 'ilios-common/classes/async-process';
 import { findById } from 'ilios-common/utils/array-helpers';
-import { use } from 'ember-could-get-used-to-this';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { ensureSafeComponent } from '@embroider/util';
+import NewCompetencyComponent from './subject/new/competency';
 
 @validatable
 export default class ReportsNewSubjectComponent extends Component {
@@ -37,10 +35,125 @@ export default class ReportsNewSubjectComponent extends Component {
   @tracked selectedYear = null;
   @tracked schoolChanged = false;
   @tracked @Length(1, 240) title;
-  subjectList = [];
-  fullPrepositionalObjectList = [];
-
-  loadedPrepositionalObjects = new Map();
+  subjectList = [
+    { value: 'course', label: this.intl.t('general.courses') },
+    { value: 'session', label: this.intl.t('general.sessions') },
+    { value: 'program', label: this.intl.t('general.programs') },
+    { value: 'program year', label: this.intl.t('general.programYears') },
+    { value: 'instructor', label: this.intl.t('general.instructors') },
+    {
+      value: 'instructor group',
+      label: this.intl.t('general.instructorGroups'),
+    },
+    {
+      value: 'learning material',
+      label: this.intl.t('general.learningMaterials'),
+    },
+    { value: 'competency', label: this.intl.t('general.competencies') },
+    { value: 'mesh term', label: this.intl.t('general.meshTerms') },
+    { value: 'term', label: this.intl.t('general.terms') },
+    { value: 'session type', label: this.intl.t('general.sessionTypes') },
+  ];
+  fullPrepositionalObjectList = [
+    {
+      value: 'course',
+      label: this.intl.t('general.course'),
+      subjects: [
+        'session',
+        'program',
+        'program year',
+        'instructor',
+        'instructor group',
+        'learning material',
+        'competency',
+        'mesh term',
+        'session type',
+        'term',
+      ],
+    },
+    {
+      value: 'session',
+      label: this.intl.t('general.session'),
+      subjects: [
+        'program',
+        'program year',
+        'instructor',
+        'instructor group',
+        'learning material',
+        'competency',
+        'mesh term',
+        'term',
+      ],
+    },
+    {
+      value: 'program year',
+      label: this.intl.t('general.programYear'),
+      subjects: ['term'],
+    },
+    {
+      value: 'program',
+      label: this.intl.t('general.program'),
+      subjects: ['course', 'session', 'session type', 'term'],
+    },
+    {
+      value: 'instructor',
+      label: this.intl.t('general.instructor'),
+      subjects: [
+        'course',
+        'session',
+        'instructor group',
+        'learning material',
+        'session type',
+        'term',
+      ],
+    },
+    {
+      value: 'instructor group',
+      label: this.intl.t('general.instructorGroup'),
+      subjects: ['course', 'session', 'instructor', 'learning material', 'session type'],
+    },
+    {
+      value: 'learning material',
+      label: this.intl.t('general.learningMaterial'),
+      subjects: [
+        'course',
+        'session',
+        'instructor',
+        'instructor group',
+        'mesh term',
+        'session type',
+        'term',
+      ],
+    },
+    {
+      value: 'competency',
+      label: this.intl.t('general.competency'),
+      subjects: ['course', 'session', 'session type', 'term'],
+    },
+    {
+      value: 'mesh term',
+      label: this.intl.t('general.meshTerm'),
+      subjects: ['course', 'session', 'learning material', 'session type', 'term'],
+    },
+    {
+      value: 'session type',
+      label: this.intl.t('general.sessionType'),
+      subjects: [
+        'session',
+        'instructor',
+        'instructor group',
+        'learning material',
+        'competency',
+        'mesh term',
+        'term',
+      ],
+    },
+    {
+      value: 'term',
+      label: this.intl.t('general.term'),
+      subjects: ['course', 'session', 'program', 'program year', 'session type'],
+    },
+  ];
 
   userModelData = new TrackedAsyncData(this.currentUser.getModel());
 
@@ -67,6 +180,15 @@ export default class ReportsNewSubjectComponent extends Component {
     return this.allSchoolsData.isResolved ? this.allSchoolsData.value : [];
   }
 
+  get newPrepositionalObjectComponent() {
+    switch (this.currentPrepositionalObject) {
+      case 'competency':
+        return ensureSafeComponent(NewCompetencyComponent, this);
+    }
+
+    return null;
+  }
+
   @cached
   get allAcademicYearsData() {
     return new TrackedAsyncData(this.store.findAll('academic-year'));
@@ -76,248 +198,10 @@ export default class ReportsNewSubjectComponent extends Component {
     return this.allAcademicYearsData.isResolved ? this.allAcademicYearsData.value : [];
   }
 
-  @use prepositionalObjectIdList = new AsyncProcess(() => [
-    this.getPrepositionalObjectIdList.bind(this),
-    this.currentPrepositionalObject,
-    this.currentSchool,
-    this.isCourse,
-    this.isSession,
-  ]);
-
-  get isCourse() {
-    return this.currentPrepositionalObject === 'course';
-  }
-  get isSession() {
-    return this.currentPrepositionalObject === 'session';
-  }
-
-  get isPrepositionalObjectIdListLoaded() {
-    return Boolean(this.prepositionalObjectIdList);
-  }
-
-  constructor() {
-    super(...arguments);
-    this.subjectList = [
-      { value: 'course', label: this.intl.t('general.courses') },
-      { value: 'session', label: this.intl.t('general.sessions') },
-      { value: 'program', label: this.intl.t('general.programs') },
-      { value: 'program year', label: this.intl.t('general.programYears') },
-      { value: 'instructor', label: this.intl.t('general.instructors') },
-      {
-        value: 'instructor group',
-        label: this.intl.t('general.instructorGroups'),
-      },
-      {
-        value: 'learning material',
-        label: this.intl.t('general.learningMaterials'),
-      },
-      { value: 'competency', label: this.intl.t('general.competencies') },
-      { value: 'mesh term', label: this.intl.t('general.meshTerms') },
-      { value: 'term', label: this.intl.t('general.terms') },
-      { value: 'session type', label: this.intl.t('general.sessionTypes') },
-    ];
-
-    this.fullPrepositionalObjectList = [
-      {
-        value: 'course',
-        label: this.intl.t('general.course'),
-        subjects: [
-          'session',
-          'program',
-          'program year',
-          'instructor',
-          'instructor group',
-          'learning material',
-          'competency',
-          'mesh term',
-          'session type',
-          'term',
-        ],
-      },
-      {
-        value: 'session',
-        label: this.intl.t('general.session'),
-        subjects: [
-          'program',
-          'program year',
-          'instructor',
-          'instructor group',
-          'learning material',
-          'competency',
-          'mesh term',
-          'term',
-        ],
-      },
-      {
-        value: 'program year',
-        label: this.intl.t('general.programYear'),
-        subjects: ['term'],
-      },
-      {
-        value: 'program',
-        label: this.intl.t('general.program'),
-        subjects: ['course', 'session', 'session type', 'term'],
-      },
-      {
-        value: 'instructor',
-        label: this.intl.t('general.instructor'),
-        subjects: [
-          'course',
-          'session',
-          'instructor group',
-          'learning material',
-          'session type',
-          'term',
-        ],
-      },
-      {
-        value: 'instructor group',
-        label: this.intl.t('general.instructorGroup'),
-        subjects: ['course', 'session', 'instructor', 'learning material', 'session type'],
-      },
-      {
-        value: 'learning material',
-        label: this.intl.t('general.learningMaterial'),
-        subjects: [
-          'course',
-          'session',
-          'instructor',
-          'instructor group',
-          'mesh term',
-          'session type',
-          'term',
-        ],
-      },
-      {
-        value: 'competency',
-        label: this.intl.t('general.competency'),
-        subjects: ['course', 'session', 'session type', 'term'],
-      },
-      {
-        value: 'mesh term',
-        label: this.intl.t('general.meshTerm'),
-        subjects: ['course', 'session', 'learning material', 'session type', 'term'],
-      },
-      {
-        value: 'session type',
-        label: this.intl.t('general.sessionType'),
-        subjects: [
-          'session',
-          'instructor',
-          'instructor group',
-          'learning material',
-          'competency',
-          'mesh term',
-          'term',
-        ],
-      },
-      {
-        value: 'term',
-        label: this.intl.t('general.term'),
-        subjects: ['course', 'session', 'program', 'program year', 'session type'],
-      },
-    ];
-  }
-
   get prepositionalObjectList() {
     return this.fullPrepositionalObjectList.filter((item) =>
       item.subjects.includes(this.currentSubject)
     );
-  }
-
-  async getPrepositionalObjectIdList() {
-    if (
-      !this.currentPrepositionalObject ||
-      this.currentPrepositionalObject === 'instructor' ||
-      this.currentPrepositionalObject === 'mesh term'
-    ) {
-      return [];
-    }
-
-    const key = [
-      this.currentPrepositionalObject,
-      this.school?.id ?? 'noschool',
-      this.isCourse ? 'course' : 'nocourse',
-      this.isSession ? 'session' : 'nosession',
-    ].join();
-
-    if (!this.loadedPrepositionalObjects.has(key)) {
-      this.loadedPrepositionalObjects.set(key, this.getPrepositionalObjectIdListPromise());
-    }
-
-    return await this.loadedPrepositionalObjects.get(key);
-  }
-
-  async getPrepositionalObjectIdListPromise() {
-    const model = dasherize(this.currentPrepositionalObject);
-    const query = {
-      filters: {},
-    };
-    if (this.currentSchool) {
-      const schoolScopedModels = [
-        'session',
-        'course',
-        'program',
-        'session-type',
-        'instructor-group',
-        'competency',
-        'term',
-      ];
-      if (schoolScopedModels.includes(model)) {
-        if ('session' === model || 'term' === model) {
-          query.filters.schools = [this.currentSchool.id];
-        } else {
-          query.filters.school = this.currentSchool.id;
-        }
-      }
-    }
-    if ('session' === model) {
-      query.include = 'course';
-    }
-    const objects = await this.store.query(model, query);
-
-    return await map(objects.slice(), async (obj) => {
-      const po = new PrepositionObject(this.currentPrepositionalObject, obj);
-      const academicYear = await po.getAcademicYear();
-      const label = await po.getLabel();
-      const { active, value } = po;
-      const payload = { academicYear, active, label, value };
-
-      if (this.isCourse) {
-        payload.externalId = po.model.externalId;
-      }
-
-      if (this.isSession) {
-        const course = await po.model.course;
-        payload.courseTitle = course.title;
-      }
-
-      return payload;
-    });
-  }
-
-  get currentYear() {
-    return this.selectedYear ? Number(this.selectedYear) : null;
-  }
-
-  get filteredPrepositionalObjectIdList() {
-    const list = this.prepositionalObjectIdList ?? [];
-
-    return this.filterPrepositionalObjectsByAcademicYear(
-      list,
-      this.currentYear,
-      this.currentPrepositionalObject
-    );
-  }
-
-  filterPrepositionalObjectsByAcademicYear(list, year, currentPrepositionalObject) {
-    return list.filter((obj) => {
-      if (!year || !['course', 'session'].includes(currentPrepositionalObject)) {
-        return true;
-      }
-
-      return obj.academicYear === year;
-    });
   }
 
   get currentSubjectLabel() {
@@ -369,7 +253,6 @@ export default class ReportsNewSubjectComponent extends Component {
   changePrepositionalObject(object) {
     this.currentPrepositionalObject = object;
     this.currentPrepositionalObjectId = null;
-    this.resetCurrentPrepositionalObjectId.perform();
     this.clearErrorDisplay();
   }
 
@@ -379,21 +262,6 @@ export default class ReportsNewSubjectComponent extends Component {
     this.currentPrepositionalObjectId = null;
     this.resetCurrentPrepositionalObjectId.perform();
     this.clearErrorDisplay();
-  }
-
-  @action
-  changePrepositionalObjectId(id) {
-    this.currentPrepositionalObjectId = id;
-  }
-
-  @action
-  chooseInstructor(user) {
-    this.currentPrepositionalObjectId = user.id;
-  }
-
-  @action
-  chooseMeshTerm(term) {
-    this.currentPrepositionalObjectId = term.id;
   }
 
   keyUp(event) {
@@ -440,19 +308,6 @@ export default class ReportsNewSubjectComponent extends Component {
     this.schoolChanged = true;
   }
 
-  @restartableTask
-  *resetCurrentPrepositionalObjectId() {
-    const list = this.filterPrepositionalObjectsByAcademicYear(
-      yield this.getPrepositionalObjectIdList(),
-      this.currentYear,
-      this.currentPrepositionalObject
-    );
-
-    if (list.length) {
-      this.currentPrepositionalObjectId = list[0].value;
-    }
-  }
-
   @action
   validateCurrentPrepositionalObjectIdCallback() {
     return !(this.currentPrepositionalObject && !this.currentPrepositionalObjectId);
@@ -488,49 +343,5 @@ export default class ReportsNewSubjectComponent extends Component {
         return this.intl.t('errors.reportMissingObjectForMeshTerm');
       }
     }
-  }
-}
-
-class PrepositionObject {
-  model = null;
-  type = null;
-
-  constructor(type, model) {
-    this.type = type;
-    this.model = model;
-  }
-
-  get value() {
-    return this.model.id;
-  }
-
-  async getLabel() {
-    if (this.type === 'mesh term') {
-      return this.model.name;
-    } else if (this.type === 'term') {
-      const vocabulary = await this.model.vocabulary;
-      const titleWithParentTitles = await this.model.titleWithParentTitles;
-      return `${vocabulary.title} > ${titleWithParentTitles}`;
-    }
-
-    return this.model.title;
-  }
-
-  async getAcademicYear() {
-    if (this.type === 'course') {
-      return Number(this.model.year);
-    } else if (this.type === 'session') {
-      const course = await this.model.course;
-      return Number(course.year);
-    }
-
-    return null;
-  }
-
-  get active() {
-    if (['session type', 'term'].includes(this.type)) {
-      return this.model.active;
-    }
-    return true;
   }
 }
---
+++ app/components/reports/subject/new/competency.js
@@ -0,0 +1,47 @@
+import Component from '@glimmer/component';
+import { TrackedAsyncData } from 'ember-async-data';
+import { cached } from '@glimmer/tracking';
+import { service } from '@ember/service';
+import { task, timeout } from 'ember-concurrency';
+import { sortBy } from 'ilios-common/utils/array-helpers';
+
+export default class ReportsSubjectNewCompetencyComponent extends Component {
+  @service store;
+
+  @cached
+  get allCompetencies() {
+    return new TrackedAsyncData(this.store.findAll('competency'));
+  }
+
+  get isLoaded() {
+    return this.allCompetencies.isResolved;
+  }
+
+  get filteredCompetencies() {
+    if (this.args.school) {
+      return this.allCompetencies.value.filter(
+        (c) => c.belongsTo('school').id() === this.args.school.id
+      );
+    }
+
+    return this.allCompetencies.value;
+  }
+
+  get sortedCompetencies() {
+    return sortBy(this.filteredCompetencies, 'title');
+  }
+
+  @task
+  *setInitialValue() {
+    yield timeout(1); //wait a moment so we can render before setting
+    const ids = this.sortedCompetencies.map(({ id }) => id);
+    if (ids.includes(this.args.currentId)) {
+      return;
+    }
+    if (!this.sortedCompetencies.length) {
+      this.args.changeId(null);
+    } else {
+      this.args.changeId(this.sortedCompetencies[0].id);
+    }
+  }
+}
