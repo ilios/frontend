82f866b56c9333cb111b8fd0226ea552d2791e9c
Jonathan Johnson <jon.johnson@ucsf.edu>
2023-10-10 20:29:46 -0700

    Update Ember CLI to v5.3.0
    With all blueprints

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -6,7 +6,7 @@ import {
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { setupIntl } from 'ember-intl/test-support';
 
-// This file exists to provide wrappers around ember-qunit's / ember-mocha's
+// This file exists to provide wrappers around ember-qunit's
 // test setup functions. This way, you can easily extend the setup that is
 // needed per test type.
 
