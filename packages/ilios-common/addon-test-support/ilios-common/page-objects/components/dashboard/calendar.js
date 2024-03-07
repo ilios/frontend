31d9f21e7f8de8ffaae7b6d00b06dfab48737fa8
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-03-06 16:52:09 -0800

    provide acceptance test coverage for transitions from weekly- and monthly-calendar into daily-calendar

--- packages/ilios-common/addon-test-support/ilios-common/page-objects/components/dashboard/calendar.js
+++ packages/ilios-common/addon-test-support/ilios-common/page-objects/components/dashboard/calendar.js
@@ -2,12 +2,14 @@ import { create } from 'ember-cli-page-object';
 import weeklyCalendar from './../weekly-calendar';
 import dailyCalendar from './../daily-calendar';
 import weekGlance from './../week-glance';
+import monthlyCalendar from './../monthly-calendar';
 
 const definition = {
   scope: '[data-test-dashboard-calendar]',
   dailyCalendar,
   weekGlance,
   weeklyCalendar,
+  monthlyCalendar,
 };
 
 export default definition;
