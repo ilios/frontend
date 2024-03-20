e135037aeaa4d4a98f230a7690f2e41f91bd949c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-03-20 15:01:02 -0700

    switch to resouce-based loading process.
    this realigns this component with Program::LeadershipExpanded.

--- packages/frontend/app/components/program-year/leadership-expanded.js
+++ packages/frontend/app/components/program-year/leadership-expanded.js
@@ -1,48 +1,62 @@
 import Component from '@glimmer/component';
-import { dropTask, timeout } from 'ember-concurrency';
+import { dropTask } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
+import { TrackedAsyncData } from 'ember-async-data';
+import { cached } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ProgramYearLeadershipExpandedComponent extends Component {
-  @tracked directors = [];
+  @tracked directorsToAdd = [];
+  @tracked directorsToRemove = [];
 
+  @cached
   get count() {
     return this.directors.length;
   }
 
+  @cached
+  get programYearDirectors() {
+    return new TrackedAsyncData(this.args.programYear.directors);
+  }
+
+  @cached
+  get directors() {
+    const directors = this.programYearDirectors.isResolved
+      ? this.programYearDirectors.value.slice()
+      : [];
+    return [...directors, ...this.directorsToAdd].filter(
+      (user) => !this.directorsToRemove.includes(user),
+    );
+  }
+
+  resetBuffers() {
+    this.directorsToAdd = [];
+    this.directorsToRemove = [];
+  }
+
   @action
   addDirector(user) {
-    this.directors = [...this.directors, user];
+    this.directorsToAdd = [...this.directorsToAdd, user];
+    this.directorsToRemove = this.directorsToRemove.filter((d) => d !== user);
   }
 
   @action
   removeDirector(user) {
-    this.directors = this.directors.filter((obj) => obj !== user);
+    this.directorsToRemove = [...this.directorsToRemove, user];
+    this.directorsToAdd = this.directorsToAdd.filter((d) => d !== user);
   }
 
   @action
-  manage() {
-    this.args.setIsManaging(true);
+  close() {
+    this.resetBuffers();
+    this.args.setIsManaging(false);
   }
-
-  async setBuffers() {
-    this.directors = (await this.args.programYear.directors).slice();
-  }
-
-  load = dropTask(async () => {
-    await this.setBuffers();
-  });
 
   save = dropTask(async () => {
-    await timeout(10);
     this.args.programYear.set('directors', this.directors);
     this.args.expand();
+    this.resetBuffers();
     await this.args.programYear.save();
-    this.args.setIsManaging(false);
-  });
-
-  cancel = dropTask(async () => {
-    await this.setBuffers();
     this.args.setIsManaging(false);
   });
 }
