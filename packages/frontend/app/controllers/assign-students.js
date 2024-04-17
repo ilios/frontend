15edc7b7d1639b2ad1fd9be4e7661ef919968634
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-04-17 08:39:18 -0700

    replace actions with template helper equivalent.

--- packages/frontend/app/controllers/assign-students.js
+++ packages/frontend/app/controllers/assign-students.js
@@ -1,5 +1,4 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class AssignStudentsController extends Controller {
@@ -7,14 +6,4 @@ export default class AssignStudentsController extends Controller {
 
   @tracked query = '';
   @tracked schoolId = null;
-
-  @action
-  setSchoolId(schoolId) {
-    this.schoolId = schoolId;
-  }
-
-  @action
-  setQuery(query) {
-    this.query = query;
-  }
 }
