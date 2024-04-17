6546708103b4b3ef6074e0958c67b4b12809737c
Jonathan Johnson <jon.johnson@ucsf.edu>
2024-04-17 08:41:23 -0700

    Refactor search in IliosUsers
    Intercepting the set of values before passing them up and running the
    search once in the constructor let's us remove the element modifiers
    from this component.

--- packages/frontend/app/components/ilios-users.js
+++ packages/frontend/app/components/ilios-users.js
@@ -5,6 +5,7 @@ import { restartableTask, timeout } from 'ember-concurrency';
 import { TrackedAsyncData } from 'ember-async-data';
 import { cached } from '@glimmer/tracking';
 import { ensureSafeComponent } from '@embroider/util';
+import { action } from '@ember/object';
 import NewDirectoryUser from './new-directory-user';
 import NewUser from './new-user';
 
@@ -13,7 +14,13 @@ const DEBOUNCE_TIMEOUT = 250;
 export default class IliosUsersComponent extends Component {
   @service iliosConfig;
   @service store;
+  @service dataLoader;
   searchTypeConfig = new TrackedAsyncData(this.iliosConfig.itemFromConfig('userSearchType'));
+
+  constructor() {
+    super(...arguments);
+    this.searchForUsers.perform();
+  }
 
   @cached
   get userSearchType() {
@@ -22,19 +29,13 @@ export default class IliosUsersComponent extends Component {
 
   @cached
   get loadAllSchoolCohortsPromise() {
-    return this.store.findAll('cohort', {
-      include: 'programYear.program.school',
-    });
+    return this.dataLoader.loadSchoolsForLearnerGroups();
   }
 
   get newUserComponent() {
     const component = this.userSearchType === 'ldap' ? NewDirectoryUser : NewUser;
     return ensureSafeComponent(component, this);
   }
-
-  load = restartableTask(async () => {
-    await this.searchForUsers.perform();
-  });
 
   searchForUsers = restartableTask(async () => {
     const q = cleanQuery(this.args.query);
@@ -47,4 +48,22 @@ export default class IliosUsersComponent extends Component {
       'order_by[firstName]': 'ASC',
     });
   });
+
+  @action
+  setQuery(query) {
+    this.args.setQuery(query);
+    this.searchForUsers.perform();
+  }
+
+  @action
+  setLimit(limit) {
+    this.args.setLimit(limit);
+    this.searchForUsers.perform();
+  }
+
+  @action
+  setOffset(offset) {
+    this.args.setOffset(offset);
+    this.searchForUsers.perform();
+  }
 }
