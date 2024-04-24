3febf182da0b9b59d00d6e223c62c88025bf5691
Jonathan Johnson <jon.johnson@ucsf.edu>
2024-04-23 21:36:15 -0700

    Respect showCampusNameOfRecord setting
    When configured to hide this data by configuration we don't show the
    icon or tooltip. The data is still in the API response, but it is hidden
    from the UI.

--- packages/ilios-common/addon/components/user-name-info.js
+++ packages/ilios-common/addon/components/user-name-info.js
@@ -1,10 +1,22 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { TrackedAsyncData } from 'ember-async-data';
+import { service } from '@ember/service';
 
 export default class UserNameInfoComponent extends Component {
+  @service iliosConfig;
   @tracked isHoveringOverUsernameInfo = false;
   @tracked element;
+
+  showCampusNameOfRecordConfig = new TrackedAsyncData(
+    this.iliosConfig.itemFromConfig('showCampusNameOfRecord'),
+  );
+  get showCampusNameOfRecord() {
+    return this.showCampusNameOfRecordConfig.isResolved
+      ? this.showCampusNameOfRecordConfig.value
+      : false;
+  }
 
   @action
   toggleUsernameInfoHover(isHovering) {
