fa9075389921622e09bf89ec65f2b101178848c1
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-01 13:33:49 -0700

    fixed substring logic

--- packages/frontend/app/helpers/year-next.js
+++ packages/frontend/app/helpers/year-next.js
@@ -3,5 +3,5 @@ import ENV from 'frontend/config/environment';
 export default function yearNext(year) {
   const yearNext = (parseInt(year) + 1).toString();
 
-  return ENV.environment != 'production' ? yearNext.substring(-2) : yearNext;
+  return ENV.environment != 'production' ? yearNext.substring(2) : yearNext;
 }
