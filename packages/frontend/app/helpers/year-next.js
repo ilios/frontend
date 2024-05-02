4f4234bad7db088ba868d5489123b28ce673a312
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-02 15:40:25 -0700

    removed unneeded global yearNext helper

--- packages/frontend/app/helpers/year-next.js
+++
@@ -1,7 +0,0 @@
-import ENV from 'frontend/config/environment';
-
-export default function yearNext(year) {
-  const yearNext = (parseInt(year) + 1).toString();
-
-  return ENV.environment != 'production' ? yearNext.substring(2) : yearNext;
-}
