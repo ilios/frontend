62386cb58a07f47a948a6b7ab8ba2c750bc0cc91
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-09 15:30:51 -0700

    ics feed now uses tooltip on mouse hover and flash message for click success

--- packages/ilios-common/addon/components/ics-feed.js
+++ packages/ilios-common/addon/components/ics-feed.js
@@ -1,8 +1,11 @@
 import Component from '@glimmer/component';
-import { restartableTask, timeout } from 'ember-concurrency';
+import { restartableTask } from 'ember-concurrency';
+import { service } from '@ember/service';
 
 export default class IcsFeedComponent extends Component {
+  @service flashMessages;
+
   textCopied = restartableTask(async () => {
-    await timeout(3000);
+    this.flashMessages.success('general.copiedIcsFeedUrl');
   });
 }
