d73c74b58c1c8384b4d2638aab96ad1ba6138346
Michael Chadwick <michael.chadwick@gmail.com>
2024-06-11 08:33:12 -0700

    simplified config's dev log flag logic

--- packages/frontend/config/environment.js
+++ packages/frontend/config/environment.js
@@ -79,11 +79,11 @@ module.exports = function (environment) {
   };
 
   if (environment === 'development') {
-    ENV.APP.LOG_RESOLVER = process.env.LOG_RESOLVER || false;
-    ENV.APP.LOG_ACTIVE_GENERATION = process.env.LOG_ACTIVE_GENERATION || false;
-    ENV.APP.LOG_TRANSITIONS = process.env.LOG_TRANSITIONS || false;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = process.env.LOG_TRANSITIONS_INTERNAL || false;
-    ENV.APP.LOG_VIEW_LOOKUPS = process.env.LOG_VIEW_LOOKUPS || false;
+    ENV.APP.LOG_RESOLVER = !!process.env.LOG_RESOLVER;
+    ENV.APP.LOG_ACTIVE_GENERATION = !!process.env.LOG_ACTIVE_GENERATION;
+    ENV.APP.LOG_TRANSITIONS = !!process.env.LOG_TRANSITIONS;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = !!process.env.LOG_TRANSITIONS_INTERNAL;
+    ENV.APP.LOG_VIEW_LOOKUPS = !!process.env.LOG_VIEW_LOOKUPS;
     ENV.redirectAfterShibLogin = false;
 
     //Remove mirage in developemnt, we only use it in testing
