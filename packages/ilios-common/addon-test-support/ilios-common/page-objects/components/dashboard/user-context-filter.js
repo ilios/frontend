3de5019b40fae6e247b8ca3c1600b7eb52fd6187
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-06-28 14:18:57 -0700

    render all buttons as active if no user context is selected.

--- packages/ilios-common/addon-test-support/ilios-common/page-objects/components/dashboard/user-context-filter.js
+++ packages/ilios-common/addon-test-support/ilios-common/page-objects/components/dashboard/user-context-filter.js
@@ -1,4 +1,4 @@
-import { clickable, create, property, text } from 'ember-cli-page-object';
+import { clickable, create, hasClass, property, text } from 'ember-cli-page-object';
 
 const definition = {
   scope: '[data-test-dashboard-user-context-filter]',
@@ -6,16 +6,19 @@ const definition = {
     isChecked: property('checked', '[data-test-instructing-input]'),
     label: text('[data-test-instructing-label]'),
     toggle: clickable('[data-test-instructing-label]'),
+    isActive: hasClass('active', '[data-test-instructing-label]'),
   },
   learning: {
     isChecked: property('checked', '[data-test-learning-input]'),
     label: text('[data-test-learning-label]'),
     toggle: clickable('[data-test-learning-label]'),
+    isActive: hasClass('active', '[data-test-learning-label]'),
   },
   admin: {
     isChecked: property('checked', '[data-test-admin-input]'),
     label: text('[data-test-admin-label]'),
     toggle: clickable('[data-test-admin-label]'),
+    isActive: hasClass('active', '[data-test-admin-label]'),
   },
 };
 
