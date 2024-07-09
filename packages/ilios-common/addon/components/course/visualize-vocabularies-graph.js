be9849a7f19adc1aebafbf0e62620608026d1225
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-07-09 16:12:42 -0700

    completely redefines course/vocab viz as bar chart and adds data table to output.
    forcing this data into a donut chart gives an inaccurate representation
    - since vocabs can overlap on assigned offering/ilm time, there is no
    100% and therefore no pie to slice.
    using a horizontal bar chart seems more appropriate.

--- packages/ilios-common/addon/components/course/visualize-vocabularies-graph.js
+++ packages/ilios-common/addon/components/course/visualize-vocabularies-graph.js
@@ -5,16 +5,15 @@ import { restartableTask, timeout } from 'ember-concurrency';
 import { service } from '@ember/service';
 import { cached, tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { use } from 'ember-could-get-used-to-this';
 import { TrackedAsyncData } from 'ember-async-data';
-import AsyncProcess from 'ilios-common/classes/async-process';
-import { findBy, mapBy } from 'ilios-common/utils/array-helpers';
+import { findById, mapBy, uniqueById } from 'ilios-common/utils/array-helpers';
 
 export default class CourseVisualizeVocabulariesGraph extends Component {
   @service router;
   @service intl;
   @tracked tooltipContent = null;
   @tracked tooltipTitle = null;
+  @tracked sortBy = 'minutes';
 
   @cached
   get sessionsData() {
@@ -25,16 +24,47 @@ export default class CourseVisualizeVocabulariesGraph extends Component {
     return this.sessionsData.isResolved ? this.sessionsData.value : [];
   }
 
-  @use dataObjects = new AsyncProcess(() => [this.getDataObjects.bind(this), this.sessions]);
+  @cached
+  get outputData() {
+    return new TrackedAsyncData(this.getDataObjects(this.sessions));
+  }
+
+  get data() {
+    return this.outputData.isResolved ? this.outputData.value : [];
+  }
+
+  get tableData() {
+    return this.data.map((obj) => {
+      const rhett = {};
+      rhett.minutes = obj.data;
+      rhett.sessions = obj.meta.sessions;
+      rhett.vocabulary = obj.meta.vocabulary.title;
+      rhett.sessionTitles = mapBy(rhett.sessions, 'title').join(', ');
+      return rhett;
+    });
+  }
 
   get isLoaded() {
-    return !!this.dataObjects;
+    return this.outputData.isResolved;
+  }
+
+  get sortedAscending() {
+    return this.sortBy.search(/desc/) === -1;
+  }
+
+  @action
+  setSortBy(prop) {
+    if (this.sortBy === prop) {
+      prop += ':desc';
+    }
+    this.sortBy = prop;
   }
 
   async getDataObjects(sessions) {
-    if (!sessions) {
+    if (!sessions.length) {
       return [];
     }
+
     const sessionsWithMinutes = await map(sessions.slice(), async (session) => {
       const hours = await session.getTotalSumDuration();
       return {
@@ -42,56 +72,73 @@ export default class CourseVisualizeVocabulariesGraph extends Component {
         minutes: Math.round(hours * 60),
       };
     });
-    return map(sessionsWithMinutes, async ({ session, minutes }) => {
-      const terms = (await session.terms).slice();
-      const vocabularies = await all(mapBy(terms, 'vocabulary'));
-      return {
-        sessionTitle: session.title,
-        vocabularies,
-        minutes,
-      };
-    });
-  }
 
-  get data() {
-    return this.dataObjects.reduce((set, obj) => {
-      obj.vocabularies.forEach((vocabulary) => {
-        const vocabularyTitle = vocabulary.get('title');
-        let existing = findBy(set, 'label', vocabularyTitle);
-        if (!existing) {
-          existing = {
-            data: 0,
-            label: vocabularyTitle,
-            meta: {
-              vocabulary,
-              sessions: [],
-            },
-          };
-          set.push(existing);
-        }
-        existing.data += obj.minutes;
-        existing.meta.sessions.push(obj.sessionTitle);
+    const sessionWithMinutesAndVocabs = await map(
+      sessionsWithMinutes,
+      async ({ session, minutes }) => {
+        const terms = (await session.terms).slice();
+        const vocabularies = await all(mapBy(terms, 'vocabulary'));
+        return {
+          session,
+          vocabularies: uniqueById(vocabularies),
+          minutes,
+        };
+      },
+    );
+
+    return sessionWithMinutesAndVocabs
+      .reduce((set, obj) => {
+        obj.vocabularies.forEach((vocabulary) => {
+          const id = vocabulary.id;
+          let existing = findById(set, id);
+          if (!existing) {
+            existing = {
+              id,
+              data: 0,
+              label: vocabulary.title,
+              meta: {
+                vocabulary,
+                sessions: [],
+              },
+            };
+            set.push(existing);
+          }
+          existing.data += obj.minutes;
+          existing.meta.sessions.push(obj.session);
+        });
+
+        return set;
+      }, [])
+      .map((obj) => {
+        delete obj.id;
+        return obj;
+      })
+      .sort((first, second) => {
+        return first.data - second.data;
       });
-
-      return set;
-    }, []);
   }
 
-  donutHover = restartableTask(async (obj) => {
+  barHover = restartableTask(async (obj) => {
     await timeout(100);
     if (this.args.isIcon || !obj || obj.empty) {
       this.tooltipTitle = null;
       this.tooltipContent = null;
       return;
     }
-    const { meta } = obj;
+    const { data, meta } = obj;
 
-    this.tooltipTitle = htmlSafe(meta.vocabulary.get('title'));
-    this.tooltipContent = this.intl.t('general.clickForMore');
+    const title = htmlSafe(
+      `${meta.vocabulary.title} &bull; ${data} ${this.intl.t('general.minutes')}`,
+    );
+    const sessionTitles = mapBy(meta.sessions, 'title');
+    const content = sessionTitles.sort().join(', ');
+
+    this.tooltipTitle = title;
+    this.tooltipContent = content;
   });
 
   @action
-  donutClick(obj) {
+  barClick(obj) {
     if (this.args.isIcon || !obj || obj.empty || !obj.meta) {
       return;
     }
