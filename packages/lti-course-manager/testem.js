cb703473929a8c84140f2074dbc99543514b85f1
Jonathan Johnson <jon.johnson@ucsf.edu>
2024-07-12 16:49:30 -0700

    Enable Testing with Ember Exam
    Splits the tests into chunks and launches a bunch of browsers to run
    them. I've defaulted this to 8 for running tests on a local machine, but
    only 3 for github where there are less cores available.

--- packages/lti-course-manager/testem.js
+++ packages/lti-course-manager/testem.js
@@ -7,6 +7,7 @@ module.exports = {
   launch_in_ci: ['Chrome'],
   launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
+  parallel: process.env.EMBER_EXAM_SPLIT_COUNT || -1,
   browser_args: {
     Chrome: {
       ci: [
--- packages/lti-dashboard/tests/test-helper.js
+++ packages/lti-dashboard/tests/test-helper.js
@@ -3,7 +3,7 @@ import config from 'lti-dashboard/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import start from 'ember-exam/test-support/start';
 import DefaultAdapter from 'ember-cli-page-object/adapters/rfc268';
 import { setAdapter } from 'ember-cli-page-object/adapters';
 import 'qunit-theme-ember/qunit.css';
