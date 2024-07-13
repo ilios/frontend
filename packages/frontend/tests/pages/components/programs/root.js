1acf4c1ac2043eff6e472bc18ecfb4d6d09c9541
Jonathan Johnson <jon.johnson@ucsf.edu>
2024-07-13 09:25:36 -0700

    Convert Test to Page Object

--- packages/frontend/tests/pages/components/programs/root.js
+++ packages/frontend/tests/pages/components/programs/root.js
@@ -8,6 +8,7 @@ import {
   value,
 } from 'ember-cli-page-object';
 import list from './list';
+import newProgramForm from '../program/new';
 
 const definition = {
   scope: '[data-test-programs]',
@@ -21,6 +22,7 @@ const definition = {
   toggleNewProgramForm: clickable('[data-test-expand-collapse-button] button'),
   toggleNewProgramFormExists: isVisible('[data-test-expand-collapse-button]'),
   list,
+  newProgramForm,
 };
 
 export default definition;
