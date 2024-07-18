619bc114f3113faa7f0a8b65b3487594c6ce7106
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-07-18 11:05:13 -0700

    fixes bad import.

--- packages/test-app/tests/integration/components/dashboard/user-context-filter-test.js
+++ packages/test-app/tests/integration/components/dashboard/user-context-filter-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupRenderingTest } from 'test-app/tests/helpers';
-import { setupMirage } from 'ember-cli-mirage/test-support';
+import { setupMirage } from 'test-app/tests/test-support/mirage';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 import { component } from 'ilios-common/page-objects/components/dashboard/user-context-filter';
 
