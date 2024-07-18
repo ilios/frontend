aa818c75494f987d77f1e1f0058897acf2f24493
Michael Chadwick <michael.chadwick@gmail.com>
2024-07-18 09:33:21 -0700

    added tests for all the resources I could think of

--- packages/frontend/tests/acceptance/four-oh-four-test.js
+++ packages/frontend/tests/acceptance/four-oh-four-test.js
@@ -39,7 +39,7 @@ module('Acceptance | FourOhFour', function (hooks) {
       );
   });
 
-  test('visiting existing course does not trigger', async function (assert) {
+  test('visiting existing course does NOT trigger 404', async function (assert) {
     this.school = this.server.create('school');
     this.course = this.server.create('course', { school: this.school });
 
@@ -48,8 +48,79 @@ module('Acceptance | FourOhFour', function (hooks) {
     assert.dom('.full-screen-error').doesNotExist();
   });
 
+  test('visiting missing session', async function (assert) {
+    await visit('/courses/1337/sessions/99999');
+    assert.strictEqual(currentURL(), '/courses/1337/sessions/99999');
+    assert
+      .dom('.full-screen-error')
+      .hasText(
+        "Rats! I couldn't find that. Please check your page address, and try again. Back to Dashboard",
+      );
+  });
+
+  test('visiting missing learner group', async function (assert) {
+    await visit('/learnergroups/1776');
+    assert.strictEqual(currentURL(), '/learnergroups/1776');
+    assert
+      .dom('.full-screen-error')
+      .hasText(
+        "Rats! I couldn't find that. Please check your page address, and try again. Back to Dashboard",
+      );
+  });
+
+  test('visiting missing instructor group', async function (assert) {
+    await visit('/instructorgroups/9001');
+    assert.strictEqual(currentURL(), '/instructorgroups/9001');
+    assert
+      .dom('.full-screen-error')
+      .hasText(
+        "Rats! I couldn't find that. Please check your page address, and try again. Back to Dashboard",
+      );
+  });
+
+  test('visiting missing school', async function (assert) {
+    await visit('/schools/99');
+    assert.strictEqual(currentURL(), '/schools/99');
+    assert
+      .dom('.full-screen-error')
+      .hasText(
+        "Rats! I couldn't find that. Please check your page address, and try again. Back to Dashboard",
+      );
+  });
+
+  test('visiting missing program', async function (assert) {
+    await visit('/programs/135');
+    assert.strictEqual(currentURL(), '/programs/135');
+    assert
+      .dom('.full-screen-error')
+      .hasText(
+        "Rats! I couldn't find that. Please check your page address, and try again. Back to Dashboard",
+      );
+  });
+
   test('visiting missing report #5127', async function (assert) {
     await visit('/reports/subjects/1989');
+    assert.strictEqual(currentURL(), '/reports/subjects/1989');
+    assert
+      .dom('.full-screen-error')
+      .hasText(
+        "Rats! I couldn't find that. Please check your page address, and try again. Back to Dashboard",
+      );
+  });
+
+  test('visiting missing user #1111', async function (assert) {
+    await visit('/users/1111');
+    assert.strictEqual(currentURL(), '/users/1111');
+    assert
+      .dom('.full-screen-error')
+      .hasText(
+        "Rats! I couldn't find that. Please check your page address, and try again. Back to Dashboard",
+      );
+  });
+
+  test('visiting missing curriculum inventory report #6324', async function (assert) {
+    await visit('/reports/subjects/2525');
+    assert.strictEqual(currentURL(), '/reports/subjects/2525');
     assert
       .dom('.full-screen-error')
       .hasText(
