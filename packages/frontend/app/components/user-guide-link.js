2cde4b0dcffb2c2442fb6816bfdd0fb5814c3a67
Michael Chadwick <michael.chadwick@gmail.com>
2024-07-25 13:41:19 -0700

    changed back from <button> to <a>; icon little smaller and more removed from other icons

--- packages/frontend/app/components/user-guide-link.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class UserGuideLinkComponent extends Component {
-  @action
-  openUserGuide() {
-    window.open('https://iliosproject.gitbook.io/ilios-user-guide', '_blank');
-  }
-}
