b6941a51cc3224d5e241d526d08ea6bc8a3bfb25
Michael Chadwick <michael.chadwick@gmail.com>
2024-08-16 09:58:31 -0700

    moved generateTests property to correct place in module.exports

--- packages/frontend/config/dependency-lint.js
+++ packages/frontend/config/dependency-lint.js
@@ -3,8 +3,8 @@
 
 module.exports = {
   allowedVersions: {
-    generateTests: false,
     'ember-get-config': '^1.0.0 || ^2.0.0',
     'ember-modifier': '^3.0.0 || ^4.0.0',
   },
+  generateTests: false,
 };
