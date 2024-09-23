73cbe9e71f35b9e9cef12ef688974ec870c937da
Jonathan Johnson <jon.johnson@ucsf.edu>
2024-09-23 11:00:17 -0700

    Replace Render Modifier with TrackedAsyncData

--- packages/ilios-common/addon/components/detail-terms-list.js
+++ packages/ilios-common/addon/components/detail-terms-list.js
@@ -1,33 +1,40 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { dropTask } from 'ember-concurrency';
-import { all } from 'rsvp';
-import { mapBy } from 'ilios-common/utils/array-helpers';
+import { cached } from '@glimmer/tracking';
+import { TrackedAsyncData } from 'ember-async-data';
 
 export default class DetailTermsListComponent extends Component {
-  @tracked sortedTerms;
-
-  load = dropTask(async (event, [terms]) => {
-    if (!terms) {
-      this.sortedTerms = [];
-      return;
-    }
-    const filteredTerms = terms.filter((term) => {
-      const vocabId = term.belongsTo('vocabulary').id();
-      return vocabId === this.args.vocabulary.id;
-    });
-    const proxies = await all(
-      filteredTerms.map(async (term) => {
-        const title = await term.getTitleWithParentTitles();
-        return { term, title };
-      }),
+  @cached
+  get termsData() {
+    const terms = this.args.terms ?? [];
+    return new TrackedAsyncData(
+      Promise.all(
+        terms.map(async (term) => {
+          const title = await term.getTitleWithParentTitles();
+          const vocabulary = await term.vocabulary;
+          return { term, title, vocabulary };
+        }),
+      ),
     );
-    const sortedProxies = proxies.sort((a, b) => {
+  }
+
+  get filteredTerms() {
+    if (!this.termsData.isResolved) {
+      return [];
+    }
+    return this.termsData.value.filter(({ vocabulary }) => {
+      return vocabulary.id === this.args.vocabulary.id;
+    });
+  }
+
+  get sortedTerms() {
+    return this.filteredTerms.sort((a, b) => {
       const titleA = a.title.toLowerCase();
       const titleB = b.title.toLowerCase();
       return titleA > titleB ? 1 : titleA < titleB ? -1 : 0;
     });
+  }
 
-    this.sortedTerms = mapBy(sortedProxies, 'term');
-  });
+  get terms() {
+    return this.sortedTerms.map(({ term }) => term);
+  }
 }
