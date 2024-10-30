00028459a4432ebf3d7b514ba8694698de80ae4a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-10-30 13:30:54 -0700

    rm deprecation warning and support for legacy input arg.

--- packages/ilios-common/addon/components/validation-error.js
+++ packages/ilios-common/addon/components/validation-error.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import { cached } from '@glimmer/tracking';
-import { deprecate } from '@ember/debug';
 import { TrackedAsyncData } from 'ember-async-data';
 
 export default class ValidationError extends Component {
@@ -10,22 +9,6 @@ export default class ValidationError extends Component {
   }
 
   get errors() {
-    if (this.args.errors) {
-      deprecate(
-        `Passing @errors to ValidationError is deprecated, use @errorsFor instead.`,
-        false,
-        {
-          id: 'common.validation-error-error-arg',
-          for: 'ilios-common',
-          since: {
-            available: '87.1.0',
-            enabled: '87.1.0',
-          },
-          until: '88.0.0',
-        },
-      );
-      return this.args.errors;
-    }
     return this.errorsForData.isResolved ? this.errorsForData.value : [];
   }
 }
