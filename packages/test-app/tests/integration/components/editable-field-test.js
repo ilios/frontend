874ac4228c3e9bc306c7abf6fc56dc5dc67acdaa
Michael Chadwick <michael.chadwick@gmail.com>
2024-11-06 16:37:25 -0800

    fixed tests

--- packages/test-app/tests/integration/components/editable-field-test.js
+++ packages/test-app/tests/integration/components/editable-field-test.js
@@ -104,12 +104,10 @@ module('Integration | Component | editable field', function (hooks) {
   test('focus when editor opens on textarea', async function (assert) {
     this.set('value', 'lorem');
     this.set('label', 'Foo Bar');
-    await render(
-      hbs`<EditableField @value={{this.value}}>
+    await render(hbs`<EditableField @value={{this.value}}>
   <label for='textarea'>{{this.label}}</label>
   <textarea id='textarea'></textarea>
-</EditableField>`,
-    );
+</EditableField>`);
     await click('[data-test-edit]');
 
     assert.dom('textarea', this.element).isFocused();
@@ -122,13 +120,25 @@ module('Integration | Component | editable field', function (hooks) {
     const fadedClass = 'is-faded';
     const fadedSelector = '.is-faded';
     this.set('value', text);
-    await render(hbs`<EditableField @value={{this.value}} />`);
+    this.set('fadeTextIsExpanded', false);
+    this.set('expandAllFadeText', (isExpanded) => {
+      this.set('fadeTextIsExpanded', isExpanded);
+    });
+    await render(
+      hbs`<EditableField
+  @value={{this.value}}
+  @fadeTextExpanded={{this.fadeTextIsExpanded}}
+  @onExpandAllFadeText={{this.expandAllFadeText}}
+/>`,
+    );
 
     await waitFor(fadedSelector);
 
     assert.dom('.display-text-wrapper', this.element).hasClass(fadedClass);
     await click('[data-test-expand]');
+
     assert.dom('.display-text-wrapper', this.element).doesNotHaveClass(fadedClass);
+
     await click('[data-test-collapse]');
 
     await waitFor(fadedSelector);
--- packages/test-app/tests/integration/components/fade-text-test.js
+++ packages/test-app/tests/integration/components/fade-text-test.js
@@ -30,7 +30,7 @@ module('Integration | Component | fade-text', function (hooks) {
 
   test('it renders empty', async function (assert) {
     await render(hbs`<FadeText />`);
-    assert.strictEqual(component.text, '');
+    assert.strictEqual(component.text, '', 'has no text');
 
     await render(hbs`<FadeText />`);
     assert.dom(this.element).hasText('');
@@ -54,7 +54,13 @@ module('Integration | Component | fade-text', function (hooks) {
   test('it fades tall text given as component argument', async function (assert) {
     this.set('longHtml', this.longHtml);
 
-    await render(hbs`<FadeText @text={{this.longHtml}} />`);
+    this.set('expanded', false);
+    this.set('onExpandAll', (isExpanded) => {
+      this.set('expanded', isExpanded);
+    });
+    await render(
+      hbs`<FadeText @text={{this.longHtml}} @expanded={{this.expanded}} @onExpandAll={{this.onExpandAll}} />`,
+    );
 
     // slight delay to allow for proper loading of component
     await waitFor(this.fadedSelector);
@@ -75,8 +81,17 @@ module('Integration | Component | fade-text', function (hooks) {
 
   test('it fades tall text given as block', async function (assert) {
     this.set('longHtml', this.longHtml);
+    this.set('expanded', false);
+    this.set('onExpandAll', (isExpanded) => {
+      this.set('expanded', isExpanded);
+    });
 
-    await render(hbs`<FadeText @text={{this.longHtml}} as |displayText expand collapse updateTextDims isFaded expanded|>
+    await render(hbs`<FadeText
+  @text={{this.longHtml}}
+  @expanded={{this.expanded}}
+  @onExpandAll={{this.onExpandAll}}
+  as |displayText expand collapse updateTextDims isFaded expanded|
+>
   <div class='display-text-wrapper{{if isFaded " is-faded"}}'>
     <div class='display-text' {{on-resize updateTextDims}}>
       {{displayText}}
@@ -114,21 +129,27 @@ module('Integration | Component | fade-text', function (hooks) {
     const longText = `An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades. An objective description so long that it fades.`;
     this.set('longHtml', this.longHtml);
     this.set('longText', longText);
-    await render(hbs`<FadeText @text={{this.longHtml}} />`);
+    this.set('expanded', false);
+    this.set('onExpandAll', (isExpanded) => {
+      this.set('expanded', isExpanded);
+    });
+    await render(
+      hbs`<FadeText @text={{this.longHtml}} @expanded={{this.expanded}} @onExpandAll={{this.onExpandAll}} />`,
+    );
 
     // slight delay to allow for proper loading of component
     await waitFor(this.fadedSelector);
 
     assert.dom('.display-text-wrapper', this.element).hasClass(this.fadedClass);
-    assert.strictEqual(component.text, longText);
-    assert.ok(component.expand.isVisible);
-    assert.notOk(component.collapse.isVisible);
+    assert.strictEqual(component.text, longText, 'component text matches');
+    assert.ok(component.expand.isVisible, 'expand button is visible');
+    assert.notOk(component.collapse.isVisible, 'collapse button is NOT visible');
 
     await component.expand.click();
 
     assert.dom('.display-text-wrapper', this.element).doesNotHaveClass(this.fadedClass);
-    assert.notOk(component.expand.isVisible);
-    assert.ok(component.collapse.isVisible);
+    assert.notOk(component.expand.isVisible, 'expand button is NOT visible');
+    assert.ok(component.collapse.isVisible, 'collpase button is visible');
 
     await component.collapse.click();
 
