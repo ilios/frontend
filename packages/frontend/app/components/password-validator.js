9461c122c232b40df852eb6fd29cd6e73c2b7163
Michael Chadwick <michael.chadwick@gmail.com>
2024-11-08 10:13:33 -0800

    updated class to use new Validation interface and keyboard action

--- packages/frontend/app/components/password-validator.js
+++ packages/frontend/app/components/password-validator.js
@@ -1,10 +1,11 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
+import { cached, tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { isEmpty } from '@ember/utils';
 import { dropTask, timeout } from 'ember-concurrency';
 import { validatable, Length, NotBlank } from 'ilios-common/decorators/validation';
+import { TrackedAsyncData } from 'ember-async-data';
 
 @validatable
 export default class PasswordValidatorComponent extends Component {
@@ -13,6 +14,25 @@ export default class PasswordValidatorComponent extends Component {
   @tracked @Length(5) @NotBlank() password = null;
   @tracked isSaving = false;
   @tracked passwordStrengthScore = 0;
+
+  @cached
+  get hasErrorForPasswordData() {
+    return new TrackedAsyncData(this.hasErrorFor('password'));
+  }
+
+  get hasErrorForPassword() {
+    // console.log('hasErrorForPassword', this.hasErrorForPasswordData.value);
+    return this.hasErrorForPasswordData.isResolved ? this.hasErrorForPasswordData.value : false;
+  }
+
+  @action
+  async keyboard(event) {
+    const keyCode = event.keyCode;
+
+    if (13 === keyCode) {
+      await this.save.perform();
+    }
+  }
 
   @action
   async setPassword(password) {
@@ -35,15 +55,5 @@ export default class PasswordValidatorComponent extends Component {
     }
     await timeout(250); // artificial "validation processing"
     this.clearErrorDisplay();
-    console.log('saved password');
-  });
-
-  saveOrCancel = dropTask(async (event) => {
-    const keyCode = event.keyCode;
-    const enterKey = 13;
-
-    if (enterKey === keyCode) {
-      await this.save.perform();
-    }
   });
 }
