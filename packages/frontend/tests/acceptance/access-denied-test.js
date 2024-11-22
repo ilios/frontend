02051a899046e017da76cb57fda3e42cd85dac23
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-11-21 16:32:31 -0800

    adds acceptance test covering access denial to pages on unauthorized access.

--- packages/frontend/tests/acceptance/access-denied-test.js
+++ packages/frontend/tests/acceptance/access-denied-test.js
@@ -13,10 +13,25 @@ module('Acceptance | Access Denied', function (hooks) {
   test.each(
     'user not performing non-learner functions is routed to 404 page when accessing administrative pages',
     [
+      '/course/1/print',
       '/courses',
       '/courses/1',
+      '/courses/1/materials',
+      '/courses/1/publicationcheck',
+      '/courses/1/publishall',
+      '/courses/1/rollover',
       '/courses/1/sessions/1',
-      // Todo: put more routes under test here. [ST 2024/11/21]
+      '/courses/1/sessions/1/copy',
+      '/courses/1/sessions/1/publicationcheck',
+      '/data/courses/1',
+      '/data/courses/1/instructors',
+      '/data/courses/1/instructors/1',
+      '/data/courses/1/objectives',
+      '/data/courses/1/session-types',
+      '/data/courses/1/session-types/1',
+      '/data/courses/1/terms/1',
+      '/data/courses/1/vocabularies',
+      '/data/courses/1/vocabularies/1',
     ],
     async function (assert, url) {
       await visit(url);
