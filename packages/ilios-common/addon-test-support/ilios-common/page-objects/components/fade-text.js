46577dff4b297f2956f0666babb3fdb57a006e58
Michael Chadwick <michael.chadwick@gmail.com>
2024-12-05 11:07:45 -0800

    augmented the FadeText page object with new properties for future acceptance test; imported it into ObjItem and ObjItemParents page objects

--- packages/ilios-common/addon-test-support/ilios-common/page-objects/components/fade-text.js
+++ packages/ilios-common/addon-test-support/ilios-common/page-objects/components/fade-text.js
@@ -1,7 +1,17 @@
-import { create } from 'ember-cli-page-object';
+import { attribute, clickable, create, hasClass, isPresent } from 'ember-cli-page-object';
 
 const definition = {
   scope: '[data-test-fade-text]',
+  enabled: isPresent('[data-test-fade-text-control]'),
+  displayText: {
+    scope: '.display-text-wrapper',
+    isFaded: hasClass('faded'),
+  },
+  control: {
+    scope: '[data-test-fade-text-control]',
+    toggle: clickable('[data-test-expand]'),
+    toggleMode: attribute('aria-label', '[data-test-expand]'),
+  },
   expand: {
     scope: '[data-test-expand]',
   },
