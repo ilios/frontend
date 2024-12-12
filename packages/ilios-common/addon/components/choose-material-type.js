ed112d9c0d2de1851843bcdc9641e142064c9973
Michael Chadwick <michael.chadwick@gmail.com>
2024-12-11 16:24:06 -0800

    removed render modifiers

--- packages/ilios-common/addon/components/choose-material-type.js
+++ packages/ilios-common/addon/components/choose-material-type.js
@@ -1,58 +1,69 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { task, timeout } from 'ember-concurrency';
 
 export default class ChooseMaterialTypeComponent extends Component {
   @tracked isOpen = false;
 
-  focusOnFirstItem(menuElement) {
-    menuElement.querySelector('button:first-of-type').focus();
+  focusFirstLink = task(async () => {
+    await timeout(1);
+    document.querySelector('.choose-material-type .menu button:first-of-type').focus();
+  });
+
+  handleArrowUp(item) {
+    if (item?.previousElementSibling) {
+      item.previousElementSibling.focus();
+    } else {
+      item?.parentElement.lastElementChild.focus();
+    }
+  }
+
+  async handleArrowDown(item) {
+    if (item.classList.value == 'toggle') {
+      this.isOpen = true;
+      await this.focusFirstLink.perform();
+    } else {
+      if (item.nextElementSibling) {
+        item.nextElementSibling.focus();
+      } else {
+        await this.focusFirstLink.perform();
+      }
+    }
   }
 
   @action
-  moveFocus({ key, target }) {
+  async toggleMenu() {
+    this.isOpen = !this.isOpen;
+
+    if (this.isOpen) {
+      await this.focusFirstLink.perform();
+    }
+  }
+  @action
+  keyUp({ key, target }) {
     switch (key) {
       case 'ArrowDown':
-        if (target.nextElementSibling) {
-          target.nextElementSibling.focus();
-        } else {
-          this.menuElement.querySelector('button:nth-of-type(1)').focus();
-        }
+        this.handleArrowDown(target);
         break;
       case 'ArrowUp':
-        if (target.previousElementSibling) {
-          target.previousElementSibling.focus();
-        } else {
-          this.menuElement.querySelector('button:last-of-type').focus();
-        }
+        this.handleArrowUp(target);
         break;
       case 'Escape':
       case 'Tab':
       case 'ArrowRight':
       case 'ArrowLeft':
-        this.isOpen = false;
+        this.close();
         break;
     }
   }
   @action
   clearFocus() {
-    if (this.menuElement) {
-      const buttons = this.menuElement.querySelectorAll('button');
-      buttons.forEach((el) => el.blur());
-    }
+    const buttons = document.querySelectorAll('.choose-material-type .menu button');
+    buttons.forEach((el) => el.blur());
   }
   @action
-  toggleMenu({ key }) {
-    switch (key) {
-      case 'ArrowDown':
-        this.isOpen = true;
-        break;
-      case 'Escape':
-      case 'Tab':
-      case 'ArrowRight':
-      case 'ArrowLeft':
-        this.isOpen = false;
-        break;
-    }
+  close() {
+    this.isOpen = false;
   }
 }
