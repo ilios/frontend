a04f2f97033d706273b6db91b87d1708739cc886
Jonathan Johnson <jon.johnson@ucsf.edu>
2024-12-13 13:02:45 -0800

    Run a courses report
    Pull all the data from the GraphQL API for all of these selected courses
    and put a summary on the screen.
    
    There are some guesses here. I've selected the first offering of ILM to
    determine start date, for example. Will have to test the output and see
    if it's correct.

--- packages/frontend/app/components/reports/new-course.js
+++ packages/frontend/app/components/reports/new-course.js
@@ -5,11 +5,15 @@ import { cached, tracked } from '@glimmer/tracking';
 import { guidFor } from '@ember/object/internals';
 import { TrackedAsyncData } from 'ember-async-data';
 import { uniqueById } from 'ilios-common/utils/array-helpers';
+import { DateTime } from 'luxon';
 
 export default class ReportsNewCourseComponent extends Component {
   @service store;
+  @service graphql;
+  @service router;
 
   @tracked searchResults = null;
+  @tracked reportResults = null;
 
   get passedCourseIds() {
     return this.args.selectedCourseIds?.map(Number) ?? [];
@@ -60,6 +64,80 @@ export default class ReportsNewCourseComponent extends Component {
     this.searchResults = yield this.store.query('course', {
       q,
     });
+  }
+
+  run = restartableTask(async () => {
+    if (!this.passedCourseIds.length) {
+      this.reportResults = null;
+      return;
+    }
+    const filters = [`ids: [${this.passedCourseIds.join(', ')}]`];
+    const sessionQuery =
+      'sessions { id, title, sessionType { title }, offerings { startDate, endDate }, ilmSession { hours, dueDate } }';
+    const result = await this.graphql.find(
+      'courses',
+      filters,
+      `id, title, year, externalId, ${sessionQuery}`,
+    );
+    const { courses } = result.data;
+
+    const activities = this.buildActivities(courses);
+
+    activities.sort((a, b) => DateTime.fromISO(a.startDate) - DateTime.fromISO(b.startDate));
+
+    this.reportResults = activities;
+  });
+
+  buildActivities(courses) {
+    return courses
+      .map((course) => {
+        return course.sessions
+          .map((session) => {
+            const offerings = session.offerings.map(({ startDate, endDate }) => {
+              const luxonStartDate = DateTime.fromISO(startDate);
+              const luxonEndDate = DateTime.fromISO(endDate);
+              const { minutes } = luxonEndDate.diff(luxonStartDate, 'minutes').toObject();
+              return {
+                startDate,
+                endDate,
+                luxonStartDate,
+                minutes,
+              };
+            });
+
+            if (session.ilmSession) {
+              const { hours, dueDate } = session.ilmSession;
+              const luxonStartDate = DateTime.fromISO(dueDate);
+              const luxonEndDate = luxonStartDate.plus({ hours });
+              const { minutes } = luxonEndDate.diff(luxonStartDate, 'minutes').toObject();
+              offerings.push({
+                startDate: dueDate,
+                luxonStartDate,
+                minutes,
+              });
+            }
+
+            offerings.sort((a, b) => a.luxonStartDate - b.luxonStartDate);
+
+            const startDate = offerings.length ? offerings[0].startDate : null;
+            const minutes = offerings.length ? offerings[0].minutes : null;
+            const origin = window.location.origin;
+            const path = this.router.urlFor('session', course.id, session.id);
+
+            return {
+              courseId: course.id,
+              courseTitle: course.title,
+              sessionId: session.id,
+              sessionTitle: session.title,
+              sessionType: session.sessionType.title,
+              startDate,
+              minutes,
+              link: `${origin}${path}`,
+            };
+          })
+          .filter(Boolean);
+      })
+      .flat();
   }
 
   close = () => {
