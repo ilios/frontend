cda9a8c642d72c4693d5badf36fb674e2d0a0ca0
Michael Chadwick <michael.chadwick@gmail.com>
2024-12-19 15:19:11 -0800

    added new get-element custom modifier

---
+++ packages/test-app/tests/integration/modifiers/get-element-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'test-app/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Modifier | get-element', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function (assert) {
+    await render(hbs`<div {{get-element this.setRootElement}}></div>`);
+    assert.ok(true);
+  });
+});
