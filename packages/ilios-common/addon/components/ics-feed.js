43fc9c097ccd214362bd5d595977b6b0a9de2f1e
Michael Chadwick <michael.chadwick@gmail.com>
2025-01-09 16:35:45 -0800

    moved more uniqueId logic from template to class

--- packages/ilios-common/addon/components/ics-feed.js
+++ packages/ilios-common/addon/components/ics-feed.js
@@ -1,13 +1,18 @@
 import Component from '@glimmer/component';
 import { restartableTask } from 'ember-concurrency';
 import { service } from '@ember/service';
+import { guidFor } from '@ember/object/internals';
 
 export default class IcsFeedComponent extends Component {
   @service flashMessages;
 
-  copyButton = (id) => {
-    return document.getElementById(id);
-  };
+  get copyButtonId() {
+    return `ics-feed-copy-button-${guidFor(this)}`;
+  }
+
+  get copyButtonElement() {
+    return document.getElementById(this.copyButtonId);
+  }
 
   textCopied = restartableTask(async () => {
     this.flashMessages.success('general.copiedIcsFeedUrl');
