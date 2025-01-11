0305cc0baad1b50d359deb41e3b6c207b6318412
Michael Chadwick <michael.chadwick@gmail.com>
2025-01-10 16:49:10 -0800

    removed render modifier

--- packages/ilios-common/addon/components/user-name-info.js
+++ packages/ilios-common/addon/components/user-name-info.js
@@ -3,6 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { TrackedAsyncData } from 'ember-async-data';
 import { service } from '@ember/service';
+import { guidFor } from '@ember/object/internals';
 
 export default class UserNameInfoComponent extends Component {
   @service iliosConfig;
@@ -12,6 +13,15 @@ export default class UserNameInfoComponent extends Component {
   showCampusNameOfRecordConfig = new TrackedAsyncData(
     this.iliosConfig.itemFromConfig('showCampusNameOfRecord'),
   );
+
+  get usernameInfoId() {
+    return `user-name-info-${guidFor(this)}`;
+  }
+
+  get usernameInfoElement() {
+    return document.getElementById(this.usernameInfoId);
+  }
+
   get showCampusNameOfRecord() {
     return this.showCampusNameOfRecordConfig.isResolved
       ? this.showCampusNameOfRecordConfig.value
