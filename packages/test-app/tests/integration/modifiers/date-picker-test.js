d510c7b6d1611d4b69d8b1a4187c305d3b35144f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-01-15 12:03:38 -0800

    refactor in-component modifier function out into its own Modifier Class.

---
+++ packages/test-app/tests/integration/modifiers/date-picker-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'test-app/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+// todo: flesh this out. [ST 2025/01/19]
+module('Integration | Modifier | date-picker', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it works with minimal input', async function (assert) {
+    this.set('value', Date.now());
+    this.set('minDate', null);
+    this.set('maxDate', null);
+    this.set('locale', 'en');
+    await render(
+      hbs`<div {{date-picker this.value this.minDate this.maxDate this.locale (noop)}}></div>`,
+    );
+    // todo: add more test assertions here.
+    assert.ok(true);
+  });
+});
