a117008d389a4de86887b53af8ffc3a5ad791662
Michael Chadwick <michael.chadwick@gmail.com>
2025-02-06 15:17:14 -0800

    trying new method of ditching a modifier in favor of a reactive getter

--- packages/frontend/app/components/reports/subject/new/academic-year.js
+++ packages/frontend/app/components/reports/subject/new/academic-year.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { TrackedAsyncData } from 'ember-async-data';
 import { cached } from '@glimmer/tracking';
 import { service } from '@ember/service';
-import { modifier } from 'ember-modifier';
 import { task, timeout } from 'ember-concurrency';
 import currentAcademicYear from 'ilios-common/utils/current-academic-year';
 
@@ -10,22 +9,32 @@ export default class ReportsSubjectNewAcademicYearComponent extends Component {
   @service store;
   @service iliosConfig;
 
+  constructor() {
+    super(...arguments);
+    console.log('this.args', this.args);
+    // this.setInitialValue.perform();
+  }
+
   @cached
   get academicYearsData() {
     return new TrackedAsyncData(this.store.findAll('academic-year'));
   }
 
   get academicYears() {
-    return this.academicYearsData.isResolved ? this.academicYearsData.value : null;
+    return this.academicYearsData.isResolved ? this.academicYearsData.value : [];
+  }
+
+  get bestSelectedAcademicYear() {
+    if (this.academicYears.map(({ id }) => id).includes(this.args.currentId)) {
+      return this.args.currentId;
+    }
+
+    return this.academicYears.find(({ id }) => Number(id) === currentAcademicYear())?.id;
   }
 
   crossesBoundaryConfig = new TrackedAsyncData(
     this.iliosConfig.itemFromConfig('academicYearCrossesCalendarYearBoundaries'),
   );
-
-  loadLatest = modifier((element, [taskInstance]) => {
-    taskInstance.perform();
-  });
 
   @cached
   get academicYearCrossesCalendarYearBoundaries() {
