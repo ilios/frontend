2d333fe4aec7775d7c6827566b43bbfb4d9341c9
Michael Chadwick <michael.chadwick@gmail.com>
2025-02-07 15:03:54 -0800

    added a permission check like the one on ManageUsersSummary component

--- packages/frontend/app/controllers/users.js
+++ packages/frontend/app/controllers/users.js
@@ -1,8 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
+import { filter } from 'rsvp';
+import { cached } from '@glimmer/tracking';
+import { TrackedAsyncData } from 'ember-async-data';
 
 export default class UsersController extends Controller {
+  @service store;
   @service router;
 
   queryParams = [
@@ -23,6 +27,34 @@ export default class UsersController extends Controller {
   showNewUserForm = false;
   showBulkNewUserForm = false;
   searchTerms = null;
+
+  @cached
+  get canCreateData() {
+    return new TrackedAsyncData(this.canCreateInSomeSchool(this.allSchools));
+  }
+
+  get canCreate() {
+    return this.canCreateData.isResolved ? this.canCreateData.value : false;
+  }
+
+  @cached
+  get allSchoolsData() {
+    return new TrackedAsyncData(this.store.findAll('school'));
+  }
+
+  get allSchools() {
+    return this.allSchoolsData.isResolved ? this.allSchoolsData.value : [];
+  }
+
+  async canCreateInSomeSchool(schools) {
+    if (!schools) {
+      return false;
+    }
+    const schoolsWithCreateUserPermission = await filter(schools, async (school) => {
+      return this.permissionChecker.canCreateUser(school);
+    });
+    return schoolsWithCreateUserPermission.length > 0;
+  }
 
   @action
   transitionToUser(userId) {
