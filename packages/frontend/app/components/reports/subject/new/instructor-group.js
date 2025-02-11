9e768136628cb16ed6ec06b61e021e7d6ee87d16
Michael Chadwick <michael.chadwick@gmail.com>
2025-02-11 15:54:17 -0800

    removed render modifier

--- packages/frontend/app/components/reports/subject/new/instructor-group.js
+++ packages/frontend/app/components/reports/subject/new/instructor-group.js
@@ -2,45 +2,40 @@ import Component from '@glimmer/component';
 import { TrackedAsyncData } from 'ember-async-data';
 import { cached } from '@glimmer/tracking';
 import { service } from '@ember/service';
-import { task, timeout } from 'ember-concurrency';
 import { sortBy } from 'ilios-common/utils/array-helpers';
 
 export default class ReportsSubjectNewInstructorGroupComponent extends Component {
   @service store;
 
   @cached
-  get allInstructorGroups() {
+  get allInstructorGroupsData() {
     return new TrackedAsyncData(this.store.findAll('instructor-group'));
   }
 
-  get isLoaded() {
-    return this.allInstructorGroups.isResolved;
+  get allInstructorGroups() {
+    return this.allInstructorGroupsData.isResolved ? this.allInstructorGroupsData.value : [];
   }
 
   get filteredInstructorGroups() {
     if (this.args.school) {
-      return this.allInstructorGroups.value.filter(
+      return this.allInstructorGroups.filter(
         (c) => c.belongsTo('school').id() === this.args.school.id,
       );
     }
 
-    return this.allInstructorGroups.value;
+    return this.allInstructorGroups;
   }
 
   get sortedInstructorGroups() {
     return sortBy(this.filteredInstructorGroups, 'title');
   }
 
-  setInitialValue = task(async () => {
-    await timeout(1); //wait a moment so we can render before setting
+  get bestSelectedInstructorGroup() {
     const ids = this.sortedInstructorGroups.map(({ id }) => id);
     if (ids.includes(this.args.currentId)) {
-      return;
+      return this.args.currentId;
     }
-    if (!this.sortedInstructorGroups.length) {
-      this.args.changeId(null);
-    } else {
-      this.args.changeId(this.sortedInstructorGroups[0].id);
-    }
-  });
+
+    return this.sortedInstructorGroups[0].id;
+  }
 }
