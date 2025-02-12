270abddb32df1e62a1f253e2498eea9db0ae9611
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-02-12 10:17:27 -0800

    Add Report Summary
    Indicates what the purpose of a report is in the UI.

--- packages/frontend/app/components/reports/curriculum/header.js
+++ packages/frontend/app/components/reports/curriculum/header.js
@@ -4,10 +4,24 @@ import { service } from '@ember/service';
 export default class ReportsCurriculumHeader extends Component {
   @service intl;
 
-  reportList = [
-    { value: 'sessionObjectives', label: this.intl.t('general.sessionObjectives') },
-    { value: 'learnerGroups', label: this.intl.t('general.learnerGroups') },
-  ];
+  get reportList() {
+    return [
+      {
+        value: 'sessionObjectives',
+        label: this.intl.t('general.sessionObjectives'),
+        summary: this.intl.t('general.sessionObjectivesReportSummary', {
+          courseCount: this.args.countSelectedCourses,
+        }),
+      },
+      {
+        value: 'learnerGroups',
+        label: this.intl.t('general.learnerGroups'),
+        summary: this.intl.t('general.learnerGroupsReportSummary', {
+          courseCount: this.args.countSelectedCourses,
+        }),
+      },
+    ];
+  }
 
   get selectedReport() {
     return this.reportList.find((r) => r.value === this.args.selectedReportValue);
