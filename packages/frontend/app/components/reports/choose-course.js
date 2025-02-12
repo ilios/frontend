9fa8c9658ead99d62777d6a443b689d4d8dff6c2
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-02-12 10:49:16 -0800

    Add Select All for Course Chooser

--- packages/frontend/app/components/reports/choose-course.js
+++ packages/frontend/app/components/reports/choose-course.js
@@ -56,11 +56,39 @@ export default class ReportsChooseCourse extends Component {
     return this.args.schools.filter(({ years }) => years.length);
   }
 
+  get expandedYearCourseIds() {
+    const year = this.selectedSchool?.years.find(({ year }) => year === this.expandedYear);
+    return year?.courses.map(({ id }) => id) ?? [];
+  }
+
+  get hasSomeExpandedYearCourses() {
+    return (
+      this.args.selectedCourseIds &&
+      !this.hasAllExpandedYearCourses &&
+      this.expandedYearCourseIds.some((id) => this.args.selectedCourseIds.includes(id))
+    );
+  }
+
+  get hasAllExpandedYearCourses() {
+    return (
+      this.args.selectedCourseIds &&
+      this.expandedYearCourseIds.every((id) => this.args.selectedCourseIds.includes(id))
+    );
+  }
+
   toggleYear = (year) => {
     if (this.expandedYear === year) {
       this.expandedYear = null;
     } else {
       this.expandedYear = year;
+    }
+  };
+
+  toggleAllExpandedYearCourseSelection = () => {
+    if (this.hasAllExpandedYearCourses) {
+      this.expandedYearCourseIds.forEach((id) => this.args.remove(id));
+    } else {
+      this.expandedYearCourseIds.forEach((id) => this.args.add(id));
     }
   };
 }
