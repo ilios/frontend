1f4b3da2798bb9e1c4fff285c003fe60d589905a
Michael Chadwick <michael.chadwick@gmail.com>
2025-02-14 14:32:27 -0800

    fixed integration test

--- packages/frontend/tests/integration/components/reports/subject/new/competency-test.js
+++ packages/frontend/tests/integration/components/reports/subject/new/competency-test.js
@@ -28,19 +28,19 @@ module('Integration | Component | reports/subject/new/competency', function (hoo
   @school={{null}}
 />`);
 
-    assert.strictEqual(component.options.length, 5);
-    assert.strictEqual(component.options[0].text, 'competency 0');
+    assert.strictEqual(component.options.length, 6);
+    assert.strictEqual(component.options[0].text, '');
     assert.ok(component.options[0].isSelected);
-    assert.strictEqual(component.value, '1');
+    assert.strictEqual(component.value, '');
 
     for (let i = 1; i < 5; i++) {
-      assert.strictEqual(component.options[i].text, `competency ${i}`);
+      assert.strictEqual(component.options[i].text, `competency ${i - 1}`);
       assert.notOk(component.options[i].isSelected);
     }
 
     this.set('currentId', '3');
     assert.notOk(component.options[0].isSelected);
-    assert.ok(component.options[2].isSelected);
+    assert.ok(component.options[3].isSelected);
     assert.strictEqual(component.value, '3');
   });
 
@@ -56,15 +56,16 @@ module('Integration | Component | reports/subject/new/competency', function (hoo
       assert.strictEqual(id, '3');
       this.set('currentId', id);
     });
-    assert.ok(component.options[0].isSelected);
+
+    assert.ok(component.options[1].isSelected);
     await component.set('3');
     assert.notOk(component.options[0].isSelected);
-    assert.ok(component.options[2].isSelected);
+    assert.ok(component.options[3].isSelected);
     assert.strictEqual(component.value, '3');
   });
 
   test('it filters by school', async function (assert) {
-    assert.expect(7);
+    assert.expect(9);
     const schoolModel = await this.owner.lookup('service:store').findRecord('school', 2);
     this.set('currentId', null);
     this.set('school', schoolModel);
@@ -78,29 +79,43 @@ module('Integration | Component | reports/subject/new/competency', function (hoo
   @school={{this.school}}
 />`);
 
-    assert.strictEqual(component.options.length, 3);
-    assert.strictEqual(component.options[0].text, 'competency 2');
+    assert.strictEqual(component.options.length, 4);
+    assert.strictEqual(component.options[0].text, '');
     assert.ok(component.options[0].isSelected);
-    assert.strictEqual(component.options[1].text, 'competency 3');
+    assert.strictEqual(component.options[1].text, 'competency 2');
     assert.notOk(component.options[1].isSelected);
-    assert.strictEqual(component.options[2].text, 'competency 4');
+    assert.strictEqual(component.options[2].text, 'competency 3');
     assert.notOk(component.options[2].isSelected);
+    assert.strictEqual(component.options[3].text, 'competency 4');
+    assert.notOk(component.options[3].isSelected);
   });
 
   test('changing school resets default value', async function (assert) {
-    assert.expect(4);
+    assert.expect(6);
     const schoolModels = await this.owner.lookup('service:store').findAll('school');
+    this.set('currentId', null);
     this.set('school', schoolModels[0]);
+    this.set('changeId', (id) => {
+      assert.strictEqual(id, '1');
+    });
     await render(
-      hbs`<Reports::Subject::New::Competency @currentId={{null}} @changeId={{noop}} @school={{this.school}} />`,
+      hbs`<Reports::Subject::New::Competency
+  @currentId={{null}}
+  @changeId={{this.changeId}}
+  @school={{this.school}}
+/>`,
     );
 
-    assert.strictEqual(component.options[0].text, 'competency 0');
+    assert.strictEqual(component.options[0].text, '');
+
+    await component.set('1');
+    assert.strictEqual(component.value, '1');
+
     this.set('school', schoolModels[1]);
-    assert.strictEqual(component.options[0].text, 'competency 2');
+    assert.strictEqual(component.options[0].text, '');
     this.set('school', null);
-    assert.strictEqual(component.options[0].text, 'competency 0');
+    assert.strictEqual(component.options[0].text, '');
     this.set('school', schoolModels[0]);
-    assert.strictEqual(component.options[0].text, 'competency 0');
+    assert.strictEqual(component.options[0].text, '');
   });
 });
