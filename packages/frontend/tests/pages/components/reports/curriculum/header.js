d289b70ebc7d02e246bdc546c5827a9324150e9f
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-02-14 00:01:09 -0800

    Test Curriculum Report Header
    Mostly tests, but found an oddity in the API where we were exposing the
    selection internals up a layer (probably copy / paste). Cleaned that up.

---
+++ packages/frontend/tests/pages/components/reports/curriculum/header.js
@@ -0,0 +1,29 @@
+import { clickable, create, collection, fillable, property, text } from 'ember-cli-page-object';
+
+const definition = {
+  scope: '[data-test-reports-curriculum-header]',
+  runSummaryText: text('[data-test-run-summary]'),
+  reportSelector: {
+    scope: '[data-test-report-selector]',
+    set: fillable('select'),
+    options: collection('option', {
+      isSelected: property('selected'),
+    }),
+    value: property('value', 'select'),
+  },
+  runReport: {
+    scope: '[data-test-run]',
+    click: clickable(),
+  },
+  close: {
+    scope: '[data-test-close]',
+    click: clickable(),
+  },
+  download: {
+    scope: '[data-test-download]',
+    click: clickable(),
+  },
+};
+
+export default definition;
+export const component = create(definition);
