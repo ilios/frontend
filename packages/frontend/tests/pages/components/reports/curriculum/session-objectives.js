58797ad70f032a49ab978d7b8dbb2218d2706f96
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-02-19 11:50:22 -0800

    Test Session Objectives Report

---
+++ packages/frontend/tests/pages/components/reports/curriculum/session-objectives.js
@@ -0,0 +1,16 @@
+import { create, collection, text } from 'ember-cli-page-object';
+
+import header from './header';
+
+const definition = {
+  header,
+  results: collection('[data-test-report-results] [data-test-result]', {
+    courseTitle: text('td', { at: 0 }),
+    sessionCount: text('td', { at: 1 }),
+    instructorCount: text('td', { at: 2 }),
+    objectiveCount: text('td', { at: 3 }),
+  }),
+};
+
+export default definition;
+export const component = create(definition);
