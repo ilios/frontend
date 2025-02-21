7cfc82d387a38e878d899ed2e554226b370329b1
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-02-20 23:08:35 -0800

    Add Some Acceptance Tests for Curriculum Reports
    These are pretty basic, but add some percy snapshots and a bit of data
    checking.

--- packages/frontend/tests/pages/reports.js
+++ packages/frontend/tests/pages/reports.js
@@ -1,11 +1,14 @@
 import { create, visitable } from 'ember-cli-page-object';
 import subjects from './components/reports/subjects-list';
 import switcher from './components/reports/switcher';
+import curriculum from './components/reports/curriculum';
 
 const page = {
   visit: visitable('/reports'),
+  visitCurriculumReports: visitable('/reports/curriculum'),
   switcher,
   subjects,
+  curriculum,
 };
 
 export default create(page);
