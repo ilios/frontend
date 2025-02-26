d0b366bbb43aacb0eab9397a2d272b49ab4d068b
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-02-25 21:05:36 -0800

    Restore Auth Check
    Not sure when this got removed, but it's needed.

--- packages/frontend/app/routes/reports.js
+++ packages/frontend/app/routes/reports.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class ReportsRoute extends Route {}
+export default class ReportsRoute extends Route {
+  @service session;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+  }
+}
