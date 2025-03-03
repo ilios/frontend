b48fec46beb927aab5354ee948dc88bb25694c06
Michael Chadwick <michael.chadwick@gmail.com>
2025-03-03 13:55:48 -0800

    removed render modifier

--- packages/ilios-common/addon/components/course/visualize-instructors.js
+++ packages/ilios-common/addon/components/course/visualize-instructors.js
@@ -1,16 +1,22 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
+import { cached, tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
-import { restartableTask } from 'ember-concurrency';
+import { TrackedAsyncData } from 'ember-async-data';
 
 export default class CourseVisualizeInstructorsComponent extends Component {
   @service iliosConfig;
-  @tracked academicYearCrossesCalendarYearBoundaries = false;
   @tracked name;
 
-  load = restartableTask(async () => {
-    this.academicYearCrossesCalendarYearBoundaries = await this.iliosConfig.itemFromConfig(
-      'academicYearCrossesCalendarYearBoundaries',
+  @cached
+  get academicYearCrossesCalendarYearBoundariesData() {
+    return new TrackedAsyncData(
+      this.iliosConfig.itemFromConfig('academicYearCrossesCalendarYearBoundaries'),
     );
-  });
+  }
+
+  get academicYearCrossesCalendarYearBoundaries() {
+    return this.academicYearCrossesCalendarYearBoundariesData.isResolved
+      ? this.academicYearCrossesCalendarYearBoundariesData.value
+      : false;
+  }
 }
