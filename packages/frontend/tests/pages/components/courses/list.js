1cf010af5df4deb3452c610d5f5c19682c09a011
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-03-09 22:46:45 -0700

    Use Component to Replace Ember Responsive Colspans
    There isn't a reasonable way to set colspan to cover the entire table as
    rendered so I'm doing this weird thing. I create two td elements with
    the correct dimensions for different table sizes and then I hide the one
    we don't need. Is it weird.? Yes. Does it work? Also yes.

--- packages/frontend/tests/pages/components/courses/list.js
+++ packages/frontend/tests/pages/components/courses/list.js
@@ -5,14 +5,16 @@ const definition = {
   scope: '[data-test-courses-list]',
   courses: collection('[data-test-courses-list-item]', listItem),
   emptyListRow: {
-    scope: '[data-test-empty-list]',
+    scope: '[data-test-empty-list] td:nth-of-type(1)',
   },
   sortByTitle: clickable('button', { scope: '[data-test-course-headings] th:nth-of-type(1)' }),
   sortByLevel: clickable('button', { scope: '[data-test-course-headings] th:nth-of-type(2)' }),
   sortByStartDate: clickable('button', { scope: '[data-test-course-headings] th:nth-of-type(3)' }),
   sortByEndDate: clickable('button', { scope: '[data-test-course-headings] th:nth-of-type(4)' }),
   sortByStatus: clickable('button', { scope: '[data-test-course-headings] th:nth-of-type(5)' }),
-  confirmCourseRemoval: clickable('[data-test-courses] .confirm-removal button.remove'),
+  confirmCourseRemoval: clickable(
+    '[data-test-courses] .confirm-removal td:nth-of-type(1) button.remove',
+  ),
   isSortedByTitleAscending: hasClass(
     'fa-arrow-down-a-z',
     '[data-test-course-headings] th:eq(0) svg',
