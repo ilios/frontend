2f97e4a0a138254331d00524203c75d4cb0f09ac
Michael Chadwick <michael.chadwick@gmail.com>
2025-04-14 11:38:48 -0700

    make curriculum controller track school info to pass on to components, and save in query string

--- packages/frontend/app/controllers/reports/curriculum.js
+++ packages/frontend/app/controllers/reports/curriculum.js
@@ -2,11 +2,34 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 
 export default class ReportsCurriculumController extends Controller {
-  queryParams = [{ courses: 'courses' }, { report: 'report' }, { run: 'run' }];
+  queryParams = [
+    { schools: 'schools' },
+    { courses: 'courses' },
+    { report: 'report' },
+    { run: 'run' },
+  ];
 
+  @tracked schools = null;
   @tracked courses = null;
   @tracked report = null;
   @tracked run = false;
+
+  get selectedSchoolIds() {
+    return this.schools?.split('-') ?? [];
+  }
+
+  setSelectedSchoolIds = (ids) => {
+    if (!ids || !ids.length) {
+      this.schools = null;
+    } else {
+      //use a Set to remove duplicates
+      this.schools = [...new Set(ids)].join('-');
+    }
+  };
+
+  get hasMultipleSchools() {
+    return this.selectedSchoolIds ? this.selectedSchoolIds.length > 1 : false;
+  }
 
   get selectedCourseIds() {
     return this.courses?.split('-') ?? [];
