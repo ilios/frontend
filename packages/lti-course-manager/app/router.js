b18beadcc50bf8c26c0d5a72839c6050a3c28208
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-04-22 13:29:58 -0700

    Drop @embroider/router
    We don't need this anymore as we've disabled splitAtRoutes. Will add it
    again when we re-enable this feature.

--- packages/lti-course-manager/app/router.js
+++ packages/lti-course-manager/app/router.js
@@ -1,4 +1,4 @@
-import EmberRouter from '@embroider/router';
+import EmberRouter from '@ember/routing/router';
 import config from 'lti-course-manager/config/environment';
 
 export default class Router extends EmberRouter {
