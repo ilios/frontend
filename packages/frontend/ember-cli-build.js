549cfee811e273174f627b4f5545e355863ff5e8
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-05-02 13:15:00 -0700

    Update Ember CLI to v6.4.0
    Using ember-cli update across all packages to get all blueprints
    updated.

--- packages/frontend/ember-cli-build.js
+++ packages/frontend/ember-cli-build.js
@@ -59,6 +59,10 @@ module.exports = async function (defaults) {
   setConfig(app, __dirname, {
     compatWith: '5.2',
     deprecations: {
+      // New projects can safely leave this deprecation disabled.
+      // If upgrading, to opt-into the deprecated behavior, set this to true and then follow:
+      // https://deprecations.emberjs.com/id/ember-data-deprecate-store-extends-ember-object
+      // before upgrading to Ember Data 6.0
       DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
     },
   });
