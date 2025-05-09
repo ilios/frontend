64d73beb982691089ef579fe32aab57b174a31ff
Michael Chadwick <michael.chadwick@gmail.com>
2025-05-09 15:58:49 -0700

    does ilios accept and integrate with this new component? indubitably

---
+++ packages/frontend/tests/pages/components/reports/subject-copy-new.js
@@ -0,0 +1,14 @@
+import { attribute, clickable, create, text } from 'ember-cli-page-object';
+
+const definition = {
+  scope: '[data-test-subject-report-copy-new]',
+  button: {
+    scope: '[data-test-button]',
+    label: text(),
+    click: clickable(),
+    link: attribute('href'),
+  },
+};
+
+export default definition;
+export const component = create(definition);
