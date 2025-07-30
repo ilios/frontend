d1ee2b6e8211ce604ed4dc6a85e3e8f4489d9764
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-07-30 08:30:26 -0700

    replace usage of scroll-into-view functionality in scroll modifier with native element.scrollIntoView() equivalent.

--- packages/ilios-common/addon/modifiers/scroll-into-view.js
+++ packages/ilios-common/addon/modifiers/scroll-into-view.js
@@ -1,10 +1,10 @@
 import { modifier } from 'ember-modifier';
 import { scheduleOnce } from '@ember/runloop';
-import { default as scroll } from 'scroll-into-view';
 
 export default modifier(function scrollIntoView(element, positional, { disabled }) {
   function deferredScroll() {
-    scroll(element, { align: { top: 0 } });
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+    element.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' });
   }
   if (!disabled) {
     // eslint-disable-next-line ember/no-runloop
