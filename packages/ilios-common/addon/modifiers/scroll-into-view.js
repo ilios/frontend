a503120e450acd2a820b13556b647f854c80692d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-07-30 08:41:49 -0700

    make scroll behaviour configurable, defaulting to smooth scrolling.

--- packages/ilios-common/addon/modifiers/scroll-into-view.js
+++ packages/ilios-common/addon/modifiers/scroll-into-view.js
@@ -1,10 +1,15 @@
 import { modifier } from 'ember-modifier';
 import { scheduleOnce } from '@ember/runloop';
 
-export default modifier(function scrollIntoView(element, positional, { disabled }) {
+export default modifier(function scrollIntoView(element, positional, { disabled, scrollBehavior }) {
+  const options = {
+    block: 'start',
+    inline: 'nearest',
+    behavior: scrollBehavior ?? 'smooth',
+  };
   function deferredScroll() {
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
-    element?.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' });
+    element?.scrollIntoView(options);
   }
   if (!disabled) {
     // eslint-disable-next-line ember/no-runloop
