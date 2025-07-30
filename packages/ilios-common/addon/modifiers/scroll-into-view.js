0e7f98e0b143176f731eddf838c3650fdc89e880
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-07-30 08:32:08 -0700

    make code more resilient by prevent method invocation on nullish value.
    there isn't a guarantee that the scroll target is still on the screen
    when the scroll function is invoked during the after-render queue
    processing.
    adding this check should prevents us from (potentially) frontend
    breaking errors that could stem from this.

--- packages/ilios-common/addon/modifiers/scroll-into-view.js
+++ packages/ilios-common/addon/modifiers/scroll-into-view.js
@@ -4,7 +4,7 @@ import { scheduleOnce } from '@ember/runloop';
 export default modifier(function scrollIntoView(element, positional, { disabled }) {
   function deferredScroll() {
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
-    element.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' });
+    element?.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' });
   }
   if (!disabled) {
     // eslint-disable-next-line ember/no-runloop
