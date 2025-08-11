5fe998909b33191bc344dc5af3857faafeed1cd4
Michael Chadwick <michael.chadwick@gmail.com>
2025-08-11 15:47:29 -0700

    added better comment explaining where undo/redo svg markup comes from and why

--- packages/ilios-common/addon/utils/load-quill-editor.js
+++ packages/ilios-common/addon/utils/load-quill-editor.js
@@ -1,9 +1,10 @@
 export async function loadQuillEditor() {
   const { default: QuillEditor } = await import('quill');
 
-  // Quill doesn't include redo/undo icons by default, so gotta hack 'em in
+  // Quill doesn't include redo/undo icons by default,
+  // so have to hack in from `quill/assets/icons/[undo|redo].svg`
+  // https://github.com/slab/quill/issues/885#issuecomment-609384079
   const icons = QuillEditor.import('ui/icons');
-
   icons['undo'] =
     `<svg viewbox="0 0 18 18"><polygon class="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10"></polygon><path class="ql-stroke" d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"></path></svg>`;
   icons['redo'] =
