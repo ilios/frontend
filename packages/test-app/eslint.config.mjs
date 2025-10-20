0e68cd85b207d7465e57fcf18f5819056b8caaf5
Michael Chadwick <michael.chadwick@gmail.com>
2025-10-20 11:18:29 -0700

    update test-app package to v6.8

--- packages/test-app/eslint.config.mjs
+++ packages/test-app/eslint.config.mjs
@@ -72,6 +72,7 @@ export default [
     },
   },
   {
+    ...qunit.configs.recommended,
     files: ['tests/**/*-test.{js,gjs}'],
     plugins: {
       qunit,
@@ -81,6 +82,7 @@ export default [
    * CJS node files
    */
   {
+    ...n.configs['flat/recommended-script'],
     files: [
       '**/*.cjs',
       'config/**/*.js',
@@ -109,6 +111,7 @@ export default [
    * ESM node files
    */
   {
+    ...n.configs['flat/recommended-module'],
     files: ['**/*.mjs'],
     plugins: {
       n,
