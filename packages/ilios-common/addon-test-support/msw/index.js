5896de9a8c8454e9e7785b0ff252f4bfe8d72013
Jonathan Johnson <jon.johnson@ucsf.edu>
2026-01-14 12:07:51 -0800

    Only Start Server Once
    We don't want to start and stop the server on every test, it's not designed that way. Instead we use a singleton pattern and a promise to start the server and shut it down using Qunit when all tests are done.

--- packages/ilios-common/addon-test-support/msw/index.js
+++ packages/ilios-common/addon-test-support/msw/index.js
@@ -1,5 +1,5 @@
 export { setupMSW } from './setup.js';
-export { startMSW } from './start-msw.js';
+export { startMSW, stopMSW } from './start-msw.js';
 export { db } from './db.js';
 export { factoryDefaults } from './factories.js';
 export { HttpResponse } from 'msw';
