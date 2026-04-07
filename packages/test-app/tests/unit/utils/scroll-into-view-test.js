b6a0f0a79eaed306d9fcdb03f0aa4123abfec77a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-04-07 16:11:22 -0700

    flesh out test coverage for scroll-into-view util.

--- packages/test-app/tests/unit/utils/scroll-into-view-test.js
+++ packages/test-app/tests/unit/utils/scroll-into-view-test.js
@@ -1,8 +1,17 @@
 import { settled } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import scrollIntoView from 'test-app/utils/scroll-into-view';
+import { setPreferReducedMotion } from 'ilios-common';
 
-module('Unit | Utility | scroll into view', function () {
+module('Unit | Utility | scroll into view', function (hooks) {
+  hooks.beforeEach(function () {
+    this.originalMatchMedia = window.matchMedia;
+  });
+
+  hooks.afterEach(function () {
+    window.matchMedia = this.originalMatchMedia;
+  });
+
   test('defaults', async function (assert) {
     const mockElement = {
       scrollIntoView(options) {
@@ -42,5 +51,31 @@ module('Unit | Utility | scroll into view', function () {
     scrollIntoView(mockElement, { disabled: true });
     await settled();
     assert.verifySteps([]);
+  });
+
+  test('override smooth scrolling if user prefers reduced motion', async function (assert) {
+    setPreferReducedMotion(true);
+    const mockElement = {
+      scrollIntoView(options) {
+        assert.step('scrollIntoView called');
+        assert.strictEqual(options.behavior, 'auto');
+      },
+    };
+    scrollIntoView(mockElement, { opts: { behavior: 'smooth' } });
+    await settled();
+    assert.verifySteps(['scrollIntoView called']);
+  });
+
+  test('smooth scrolling overrides works if user does not prefers reduced motion', async function (assert) {
+    setPreferReducedMotion(false);
+    const mockElement = {
+      scrollIntoView(options) {
+        assert.step('scrollIntoView called');
+        assert.strictEqual(options.behavior, 'smooth');
+      },
+    };
+    scrollIntoView(mockElement, { opts: { behavior: 'smooth' } });
+    await settled();
+    assert.verifySteps(['scrollIntoView called']);
   });
 });
