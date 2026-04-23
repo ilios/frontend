c96e703a444ff209791e9fd98d02d6c993adcfcb
Michael Chadwick <michael.chadwick@gmail.com>
2026-04-23 16:10:25 -0700

    added new acceptance test for sorting feature

--- packages/frontend/tests/pages/components/program-year/list.js
+++ packages/frontend/tests/pages/components/program-year/list.js
@@ -1,10 +1,19 @@
-import { clickable, create, collection } from 'ember-cli-page-object';
+import { clickable, create, collection, hasClass } from 'ember-cli-page-object';
 import listItem from './list-item';
 import newProgramYear from './new';
 
 const definition = {
   scope: '[data-test-program-year-list]',
   items: collection('[data-test-program-year-list-item]', listItem),
+  header: {
+    scope: 'thead',
+    startYear: {
+      scope: 'th:nth-of-type(1)',
+      isSortedAscending: hasClass('fa-arrow-down-1-9', 'svg'),
+      isSortedDescending: hasClass('fa-arrow-down-9-1', 'svg'),
+      click: clickable('button'),
+    },
+  },
   newProgramYear,
   expandCollapse: {
     scope: '[data-test-expand-collapse-button]',
