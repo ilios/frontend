c373921bef83460f2da5f98188756c00e2b22928
Michael Chadwick <michael.chadwick@gmail.com>
2026-05-01 09:48:08 -0700

    updaated login acceptance test for valid and invalid login types

--- packages/frontend/tests/acceptance/login-test.js
+++ packages/frontend/tests/acceptance/login-test.js
@@ -10,8 +10,43 @@ module('Acceptance | login', function (hooks) {
     await visit('/login');
     await takeScreenshot(assert);
 
-    assert.strictEqual(currentURL(), '/login');
+    assert.strictEqual(currentURL(), '/login', 'transitions to user route');
     await a11yAudit();
-    assert.ok(true);
+    assert.ok(true, 'no a11y errors found');
+  });
+
+  test('form login type shows form', async function (assert) {
+    this.server.get('application/config', function () {
+      assert.step('API called');
+      return {
+        config: {
+          type: 'form',
+        },
+      };
+    });
+
+    await visit('/login');
+    assert.dom('.login-form').exists();
+    assert.verifySteps(['API called']);
+  });
+
+  test('invalid login type does not show form and throws error', async function (assert) {
+    this.server.get('application/config', function () {
+      assert.step('API called');
+      return {
+        config: {
+          type: 'boofar',
+        },
+      };
+    });
+
+    await visit('/login');
+    assert.dom('.login-form').doesNotExist();
+    assert
+      .dom('.full-screen-error')
+      .hasText(
+        'Zoinks! Something is very wrong. Please refresh your browser and let us know at support@iliosproject.org if you continue to see this message. Refresh the Browser',
+      );
+    assert.verifySteps(['API called']);
   });
 });
