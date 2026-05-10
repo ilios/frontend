80dec9b98eb40c8845091d5a9485b282e480b741
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-05-10 14:33:08 -0700

    adds model update method to server.

---
+++ packages/ilios-common/addon-test-support/msw/update-model.js
@@ -0,0 +1,29 @@
+import { db } from './db.js';
+
+/**
+ * Updates a given record with given attributes in a given collection.
+ *
+ * @param { string } collectionName The name of the model collection that the given model belongs to.
+ * @param { object } model The model to update
+ * @param { obj } attrs an object holding the model attributes to update as key/value pairs.
+ * @returns { object } The updated model.
+ *
+ */
+export async function updateModel(collectionName, model, attrs) {
+  if (!Object.hasOwn(db, collectionName)) {
+    console.error(`Unknown collection ${collectionName} in mock database.`);
+  }
+  attrs = new Map(Object.entries(attrs));
+  if (!attrs.size) {
+    return;
+  }
+
+  const collection = db[collectionName];
+  return await collection.update(model, {
+    data(model) {
+      attrs.forEach((k, v) => {
+        model[k] = v;
+      });
+    },
+  });
+}
