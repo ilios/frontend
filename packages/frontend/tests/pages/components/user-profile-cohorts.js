3f8173492b6465a78a919e148f94fd4ff3143a47
Michael Chadwick <michael.chadwick@gmail.com>
2026-05-26 13:58:58 -0700

    added test to check for user cohorts save disabled property

--- packages/frontend/tests/pages/components/user-profile-cohorts.js
+++ packages/frontend/tests/pages/components/user-profile-cohorts.js
@@ -1,10 +1,14 @@
-import { clickable, create, text } from 'ember-cli-page-object';
+import { clickable, create, property, text } from 'ember-cli-page-object';
 import details from './user-profile-cohorts-details';
 import manager from './user-profile-cohorts-manager';
 const definition = {
   scope: '[data-test-user-profile-cohorts]',
   title: text('[data-test-title]', { at: 0 }),
-  save: clickable('[data-test-save]'),
+  save: {
+    scope: '[data-test-save]',
+    click: clickable(),
+    isDisabled: property('disabled'),
+  },
   cancel: clickable('[data-test-cancel]'),
   manage: clickable('[data-test-manage]'),
   details,
